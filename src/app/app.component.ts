312edc46833da9150171ecb33fc7df01e660d248
Arup Banerjee <arupdvsa@alanaamy.net>
2019-09-16 16:11:12 +0100

    tech record added Authorization token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,11 @@
-import {Component} from '@angular/core';
-
-import {Platform} from '@ionic/angular';
-import {SplashScreen} from '@ionic-native/splash-screen/ngx';
-import {StatusBar} from '@ionic-native/status-bar/ngx';
+import { Component } from '@angular/core';
+import { MsAdalAngular6Service } from 'microsoft-adal-angular6';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+  title = 'app';
 }
