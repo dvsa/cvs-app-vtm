b66b10047447140dbe0a2a3ba0263ea31550b674
Arup Banerjee <arupdvsa@alanaamy.net>
2019-10-03 12:09:43 +0100

    ntermediate push ... tests not resolved

--- src/app/components/technical-record/technical-record.component.spec.ts
+++ src/app/components/technical-record/technical-record.component.spec.ts
@@ -1,6 +1,9 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TechnicalRecordComponent } from './technical-record.component';
+import { TechnicalRecordService } from './technical-record.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('TechnicalRecordComponent', () => {
   let component: TechnicalRecordComponent;
@@ -8,7 +11,10 @@ describe('TechnicalRecordComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TechnicalRecordComponent ]
+      declarations: [ TechnicalRecordComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [HttpClientModule],
+      providers: [TechnicalRecordService]
     })
     .compileComponents();
   }));
