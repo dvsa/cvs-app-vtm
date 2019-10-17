2d340de67472c98d8ae7a8cd6a70aeb3f7dc46d6
Irina Stanciu <istanciu@deloittece.com>
2019-10-17 18:27:03 +0300

    CVSB-7238

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,8 +42,8 @@ import {VehicleNotFoundDialogComponent} from "@app/vehicle-not-found-dialog/vehi
     MatDialogModule,
     HttpClientModule,
     MsAdalAngular6Module.forRoot({
-      tenant: '6c448d90-4ca1-4caf-ab59-0a2aa67d7801',
-      clientId: '54d151b6-2ca8-4018-8c70-f9ee600d91c7',
+      tenant: '',
+      clientId: '',
       redirectUri: window.location.origin,
       endpoints: {
         "https://localhost/Api/": "xxx-bae6-4760-b434-xxx"
