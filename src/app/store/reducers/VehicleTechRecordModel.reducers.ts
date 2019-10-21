04fc1e5bf6b797ee2b1f25e24ead78702f4368aa
Martin Kemp <martinkemp@deloitte.co.uk>
2019-10-21 17:38:36 +0100

    Angular is not happy with arrow functions
    Fixes failing build
    
    https://github.com/auth0/angular2-jwt/issues/537#issuecomment-428185990

--- src/app/store/reducers/VehicleTechRecordModel.reducers.ts
+++ src/app/store/reducers/VehicleTechRecordModel.reducers.ts
@@ -1,9 +1,10 @@
-import {initialVehicleTechRecordModelState, IVehicleTechRecordModelState} from '@app/store/state/VehicleTechRecordModel.state';
-import {EVehicleTechRecordModelActions, VehicleTechRecordModelActions} from '@app/store/actions/VehicleTechRecordModel.actions';
-import {IAppState} from '@app/store/state/app.state';
+import { initialVehicleTechRecordModelState, IVehicleTechRecordModelState } from '@app/store/state/VehicleTechRecordModel.state';
+import { EVehicleTechRecordModelActions, VehicleTechRecordModelActions } from '@app/store/actions/VehicleTechRecordModel.actions';
+import { IAppState } from '@app/store/state/app.state';
 
-export const VehicleTechRecordModelReducers = (state = initialVehicleTechRecordModelState, action: VehicleTechRecordModelActions):
-  IVehicleTechRecordModelState => {
+export function VehicleTechRecordModelReducers(
+  state: IVehicleTechRecordModelState = initialVehicleTechRecordModelState,
+  action: VehicleTechRecordModelActions): IVehicleTechRecordModelState {
   switch (action.type) {
     case EVehicleTechRecordModelActions.GetVehicleTechRecordModelSuccess: {
       return {
