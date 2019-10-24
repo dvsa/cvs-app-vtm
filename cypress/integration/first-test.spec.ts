29cca414637304afb25c6b320a21a227e518a464
Catalin-Andrei Stancu <castancu@deloittece.com>
2019-10-24 15:43:15 +0300

    finished 7239 ACs

--- cypress/integration/first-test.spec.ts
+++
@@ -1,8 +0,0 @@
-context('First test', () => {
-  it('should visit home page', () => {
-    cy.visit('/home');
-    cy.get('.searchbar-input').type('1234567889').should('have.value', '1234567889');
-    cy.get('.searchbar-input').type('{enter}');
-    cy.get('.record-search-title-text').should('have.text', 'Technical records from searched (vin/vrm) 1234567889');
-  });
-});
