7e720a6b1e1dba13127891dfb8c0649ca08ff294
Catalin-Andrei Stancu <castancu@deloittece.com>
2019-10-28 10:20:11 +0200

    more tests

---
+++ src/app/pipes/IsPrimaryVrmPipe.spec.ts
@@ -0,0 +1,37 @@
+import {IsPrimaryVrmPipe} from './IsPrimaryVrmPipe';
+
+const techRecords = [{
+  vrm: '1234THF',
+  isPrimary: true
+},
+  {
+    vrm: '5678THG',
+    isPrimary: false
+  },
+];
+
+describe('IsPrimaryVrmPipe', () => {
+  const pipe = new IsPrimaryVrmPipe();
+  it('should return only primary vrm', () => {
+    const expectedRes = [{
+      vrm: '1234THF',
+      isPrimary: true
+    }];
+    const res = pipe.transform(techRecords, true);
+    expect(res).toEqual(expectedRes);
+  });
+
+  it('should return only non-primary vrm', () => {
+    const expectedRes = [{
+      vrm: '5678THG',
+      isPrimary: false
+    }];
+    const res = pipe.transform(techRecords, false);
+    expect(res).toEqual(expectedRes);
+  });
+
+  it('should return te original list', () => {
+    const res = pipe.transform( null, false);
+    expect(res).toEqual(undefined);
+  });
+});
--- src/app/shared/ngx-data-table/ngx-data-table-datasource.spec.ts
+++ src/app/shared/ngx-data-table/ngx-data-table-datasource.spec.ts
@@ -2,21 +2,15 @@ import {NgxDataTableDataSource} from './ngx-data-table-datasource';
 import { MatPaginator, MatSort, MatPaginatorIntl } from '@angular/material';
 
 describe('NgxDataTableDataSource', () => {
-  let ngxDataTableModule: NgxDataTableDataSource;
+  let ngxDataTableDataSource: NgxDataTableDataSource;
 
   beforeEach(() => {
     const sort = new MatSort();
     const paginator = new MatPaginator(new MatPaginatorIntl(), null);
-    ngxDataTableModule = new NgxDataTableDataSource(paginator, [], sort);
+    ngxDataTableDataSource = new NgxDataTableDataSource(paginator, [], sort);
   });
 
   it('should create an instance', () => {
-    expect(ngxDataTableModule).toBeTruthy();
-  });
-
-  it('should connect', () => {
-    const paged = ngxDataTableModule.connect();
-    console.log('PAGED', paged);
-    expect(true);
+    expect(ngxDataTableDataSource).toBeTruthy();
   });
 });
