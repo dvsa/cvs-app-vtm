98af27c5e05e386c67423e3396a6f10a246ae4db
Arup Banerjee <arupdvsa@alanaamy.net>
2019-12-04 15:31:55 +0000

    remove console logs

--- src/app/pipes/FilterRecordPipe.ts
+++ src/app/pipes/FilterRecordPipe.ts
@@ -3,12 +3,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterRecordPipe implements PipeTransform {
 
   transform(techRecordList: any): any {
-      console.log(`received techRecordList with length => ${techRecordList.length}`)
     if (techRecordList) {
       let records = techRecordList.find((record: any) => record.statusCode === 'current');
-      console.log(`search current => ${JSON.stringify(records)}`);
-      console.log(`search current records !== undefined => ${records !== undefined}`);
-      console.log(`search current records != undefined => ${records != undefined}`);
       if (records !== undefined) return records;
 
       records = techRecordList.find((record: any) => record.statusCode === 'provisional');
