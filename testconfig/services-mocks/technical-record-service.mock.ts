ad470aec079e08362b8ef2cacc5c8d7db627562e
Arup Banerjee <arupdvsa@alanaamy.net>
2019-12-05 13:49:43 +0000

    reset mocks

---
+++ testconfig/services-mocks/technical-record-service.mock.ts
@@ -0,0 +1,13 @@
+import {Observable, of} from 'rxjs';
+
+export class TechnicalRecordServiceMock {
+
+  getTechnicalRecords(): Observable<any> {
+    return of({vin: 'XXXXX11XX1X111111', techRecord: [], vrms: []});
+  }
+
+  getTechnicalRecordsAllStatuses(): Observable<any> {
+    return of({ techRecord: [], vin: 'XXXXX11XX1X111111', vrms: []});
+  }
+
+}
