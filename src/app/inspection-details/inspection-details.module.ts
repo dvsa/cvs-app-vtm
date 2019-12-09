3ab18e2da8404bd25c1efa29f557fad961486129
Arup Banerjee <arupdvsa@alanaamy.net>
2019-12-09 19:42:07 +0000

    added inspection details child component

---
+++ src/app/inspection-details/inspection-details.module.ts
@@ -0,0 +1,38 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { SharedModule } from '../shared/shared.module';
+import { NgrxFormsModule } from 'ngrx-forms';
+import { StoreModule } from '@ngrx/store';
+import { adrDetailsReducer } from '@app/store/reducers/adrDetailsForm.reducer';
+import { NgxJsonViewerModule } from 'ngx-json-viewer';
+import { MaterialModule } from '@app/material.module';
+import {InspectionDetailsComponent} from '@app/inspection-details/inspection-details.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    StoreModule.forFeature('adrDetails', adrDetailsReducer),
+    FormsModule,
+    SharedModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    NgrxFormsModule,
+    NgxJsonViewerModule
+  ],
+  declarations: [
+    InspectionDetailsComponent
+  ],
+  exports: [
+    InspectionDetailsComponent
+  ],
+  entryComponents: [InspectionDetailsComponent],
+})
+export class InspectionDetailsModule {
+}
