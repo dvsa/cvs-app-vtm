1a63dc2672fd92c3256a836a257c5ad37335c7b7
Arup Banerjee <arupdvsa@alanaamy.net>
2019-12-22 11:51:55 +0000

    restructured and reimplemented

--- src/app/technical-record-search/technical-record.service.ts
+++ src/app/technical-record-search/technical-record.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { AppConfig } from '@app/app.config';
-import { map, switchMap, tap, catchError } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +16,8 @@ export class TechnicalRecordService {
     this.routes = {
       techRecords: (searchIdentifier: string) => `${this.apiServer.APITechnicalRecordServerUri}/vehicles/${searchIdentifier}/tech-records`,
       techRecordsAllStatuses: (searchIdentifier: string) => `${this.apiServer.APITechnicalRecordServerUri}/vehicles/${searchIdentifier}/tech-records?status=all&metadata=true`,
-      getDocumentUrl: (vin: string) => `${this.apiServer.APIDocumentsServerUri}/vehicles/${vin}/download-file`
+      getDocumentUrl: (vin: string) => `${this.apiServer.APIDocumentsServerUri}/vehicles/${vin}/download-file`,
+      downloadBlobUrl: (url: string) => `${url}`
     };
   }
 
@@ -33,27 +34,17 @@ export class TechnicalRecordService {
     console.log(`getDocumentUrl vin => ${JSON.stringify(this.routes.getDocumentUrl(vin))}`);
     const headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
     return this.httpClient.get<string>(this.routes.getDocumentUrl(vin), {
-      params: {
-        filename: fileName
-      },
-      headers: headers,
-      responseType: 'text' as 'json'
-    }).pipe(
+      params: { filename: fileName }, headers: headers, responseType: 'text' as 'json' }).pipe(
       switchMap(response => of({ blobUrl: response, fileName: fileName })),
-      tap(_ => console.log(`getDocumentUrl => ${_}`))
+      tap(_ => console.log(`getDocumentUrl => ${JSON.stringify(_)}`))
     );
   }
 
   downloadBlob(blobUrl: string, fileName: string): Observable<{ blob: Blob, fileName?: string }> {
-    console.log(`downloadBlob route => ${blobUrl}`);
-    return this.httpClient
-      .get<Blob>(blobUrl, {
-        observe: 'response',
-        responseType: 'blob' as 'json'
-      }).pipe(
-        switchMap(res => of({
-          blob: res.body,
-          fileName: fileName
-        })));
+    console.log(`downloadBlobUrl route => ${JSON.stringify(this.routes.downloadBlobUrl(blobUrl))}`);
+    const headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
+    return this.httpClient.get<Blob>(this.routes.downloadBlobUrl(blobUrl), { 
+      headers: headers, observe: 'response', responseType: 'blob' as 'json' }).pipe(
+      switchMap(response => of({ blob: response.body, fileName: fileName })));
   }
 }
