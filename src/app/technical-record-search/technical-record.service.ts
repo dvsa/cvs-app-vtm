47fcdfbab5558efddc6d570c95c39f6ba416a8c1
Arup Banerjee <arupdvsa@alanaamy.net>
2020-01-07 14:41:33 +0000

    test document download

--- src/app/technical-record-search/technical-record.service.ts
+++ src/app/technical-record-search/technical-record.service.ts
@@ -17,7 +17,8 @@ export class TechnicalRecordService {
     this.routes = {
       techRecords: (searchIdentifier: string) => `${this.apiServer.APITechnicalRecordServerUri}/vehicles/${searchIdentifier}/tech-records`,
       techRecordsAllStatuses: (searchIdentifier: string) => `${this.apiServer.APITechnicalRecordServerUri}/vehicles/${searchIdentifier}/tech-records?status=all&metadata=true`,
-      getDocumentUrl: (vin: string) => `${this.apiServer.APIDocumentsServerUri}/vehicles/${vin}/download-file`,
+      // getDocumentUrl: (vin: string) => `${this.apiServer.APIDocumentsServerUri}/vehicles/${vin}/download-file`,
+      getDocumentBlob: (vin: string) => `${this.apiServer.APIDocumentsServerUri}/vehicles/${vin}/download-file`,
       downloadBlobUrl: (url: string) => `${this.apiServer.APIDocumentBlobUri}/${url}`
     };
   }
@@ -32,27 +33,51 @@ export class TechnicalRecordService {
 
   }
 
-  getDocumentUrl(vin: string, fileName: string): Observable<{ blobUrl: string, fileName?: string }> {
-    console.log(`getDocumentUrl vin => ${this.routes.getDocumentUrl(vin)}`);
-    const headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
-    return this.httpClient.get<string>(this.routes.getDocumentUrl(vin), {
-      params: { filename: fileName }, headers: headers, responseType: 'text' as 'json'
+  // getDocumentUrl(vin: string, fileName: string): Observable<{ blobUrl: string, fileName?: string }> {
+  //   console.log(`getDocumentUrl vin => ${this.routes.getDocumentUrl(vin)}`);
+  //   const headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
+  //   return this.httpClient.get<string>(this.routes.getDocumentUrl(vin), {
+  //     params: { filename: fileName }, headers: headers, responseType: 'text' as 'json'
+  //   }).pipe(
+  //     switchMap(response => of({ blobUrl: response, fileName: fileName })),
+  //     tap(_ => console.log(`getDocumentUrl => ${JSON.stringify(_)}`))
+  //   );
+  // }
+
+  getDocumentBlob(vin: string, fileName: string): Observable<{ buffer: ArrayBuffer, fileName?: string }> {
+    console.log(`getDocumentBlob vin => ${this.routes.getDocumentBlob(vin)}`);
+    return this.httpClient.get<{type: string , data: ArrayBuffer}>(this.routes.getDocumentBlob(vin), {
+      params: { filename: fileName }, responseType: 'json'
     }).pipe(
-      switchMap(response => of({ blobUrl: response, fileName: fileName })),
-      tap(_ => console.log(`getDocumentUrl => ${JSON.stringify(_)}`))
+      switchMap(response => of({ buffer: response.data, fileName: fileName })),
+      tap(_ => console.log(`getDocumentBlob => ${JSON.stringify(_)}`))
     );
   }
 
-  downloadBlob(blobUrl: string, fileName: string): Observable<{ blob: Blob, fileName?: string }> {
-    const url = blobUrl.split("cvsb-9213/").pop();
-    console.log(`downloadBlobUrl splitted url => ${url}`);
-    console.log(`downloadBlobUrl route => ${this.routes.downloadBlobUrl(url)}`);
-    const headers = new HttpHeaders().set('Access-Control-Allow-Origin', 'https://vtm.nonprod.cvs.dvsacloud.uk')
-      .set('Access-Control-Allow-Credentials', 'true')
-      .set('Access-Control-Max-Age', '600');
-    return this.httpClient.get<Blob>(this.routes.downloadBlobUrl(url), {
-      headers: headers, observe: 'response', responseType: 'blob' as 'json'
-    }).pipe(
-      switchMap(response => of({ blob: response.body, fileName: fileName })));
-  }
+  // downloadBlob(blobUrl: string, fileName: string): Observable<{ blob: Blob, fileName?: string }> {
+  //   const url = blobUrl.split("cvsb-9213/").pop();
+  //   console.log(`downloadBlobUrl splitted url => ${url}`);
+  //   console.log(`downloadBlobUrl route => ${this.routes.downloadBlobUrl(url)}`);
+  //   const headers = new HttpHeaders().set('Access-Control-Allow-Origin', 'https://vtm.nonprod.cvs.dvsacloud.uk')
+  //     .set('Access-Control-Allow-Credentials', 'true')
+  //     .set('Access-Control-Max-Age', '600');
+  //   return this.httpClient.get(this.routes.downloadBlobUrl(url), {
+  //     observe: 'response', responseType: 'blob' as 'json'
+  //   }).pipe(
+  //     switchMap(response => of({ blob: response.body, fileName: fileName })));
+  // }
+
+  // downloadBlob(vin: string, fileName: string): Observable<{ blob: Blob, fileName?: string }> {
+  //   // const url = blobUrl.split("cvsb-9213/").pop();
+  //   // console.log(`downloadBlobUrl splitted url => ${url}`);
+  //   // console.log(`downloadBlobUrl route => ${this.routes.downloadBlobUrl(url)}`);
+  //   // const headers = new HttpHeaders().set('Access-Control-Allow-Origin', 'https://vtm.nonprod.cvs.dvsacloud.uk')
+  //   //   .set('Access-Control-Allow-Credentials', 'true')
+  //   //   .set('Access-Control-Max-Age', '600');
+  //   const headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
+  //   return this.httpClient.get<Blob>(this.routes.downloadBlobUrl(url), {
+  //     observe: 'response', responseType: 'blob' as 'json'
+  //   }).pipe(
+  //     switchMap(response => of({ blob: response.body, fileName: fileName })));
+  // }
 }
