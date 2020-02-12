5d8d29c29dd9fe28b3764c8bc6777a5b3d70c8be
Răzvan Spasici <rspasici@deloittece.com>
2020-02-12 12:02:19 +0200

    logic for preventing page on edit form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,35 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { AuthenticationGuard, MsAdalAngular6Module, MsAdalAngular6Service } from 'microsoft-adal-angular6';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { MaterialModule } from './material.module';
-import { MatDialogModule } from '@angular/material/dialog';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faSquare, faCheckSquare, faCoffee, faBars } from '@fortawesome/free-solid-svg-icons';
-import { faSquare as farSquare, faCheckSquare as farCheckSquare } from '@fortawesome/free-regular-svg-icons';
-import { faStackOverflow, faGithub, faMedium } from '@fortawesome/free-brands-svg-icons';
-import { StoreModule } from '@ngrx/store';
-import { appReducers } from '@app/store/reducers/app.reducers';
-import { EffectsModule } from '@ngrx/effects';
-import { RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
-import { environment } from '@environments/environment';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from './shared/shared.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppConfig } from '@app/app.config';
-import { AuthTokenInterceptor } from '@app/technical-record-search/auth-token-interceptor';
+import { LandingPageComponent } from '@app/landing-page/landing-page.component';
+import { FooterComponent } from '@app/shell/footer/footer.component';
+import { HeaderComponent } from '@app/shell/header/header.component';
+import { ShellPage } from '@app/shell/shell.page';
 import { CustomSerializer } from '@app/store/reducers';
-import {Router} from '@angular/router';
-import {ShellPage} from '@app/shell/shell.page';
-import {LandingPageComponent} from '@app/landing-page/landing-page.component';
-import {HeaderComponent} from '@app/shell/header/header.component';
-import {FooterComponent} from '@app/shell/footer/footer.component';
-import {TechnicalRecordCreateComponent} from '@app/technical-record-create/technical-record-create.component';
+import { appReducers } from '@app/store/reducers/app.reducers';
+import { TechnicalRecordCreateComponent } from '@app/technical-record-create/technical-record-create.component';
+import { AuthTokenInterceptor } from '@app/technical-record-search/auth-token-interceptor';
+import { environment } from '@environments/environment';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub, faMedium, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare as farCheckSquare, faSquare as farSquare } from '@fortawesome/free-regular-svg-icons';
+import { faBars, faCheckSquare, faCoffee, faSquare } from '@fortawesome/free-solid-svg-icons';
+import { EffectsModule } from '@ngrx/effects';
+import { RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AuthenticationGuard, MsAdalAngular6Module, MsAdalAngular6Service } from 'microsoft-adal-angular6';
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+import { PendingChangesGuard } from './shared/pending-changes-guard/pending-changes.guard';
+import { SharedModule } from './shared/shared.module';
 
 let adalConfig: any; // will be initialized by APP_INITIALIZER
 export function msAdalAngular6ConfigFactory() {
@@ -57,7 +57,7 @@ export const COMPONENTS = [
   LandingPageComponent,
   HeaderComponent,
   FooterComponent,
-  TechnicalRecordCreateComponent
+  TechnicalRecordCreateComponent,
 ];
 
 @NgModule({
@@ -103,6 +103,7 @@ export const COMPONENTS = [
       useClass: AuthTokenInterceptor,
       multi: true
     },
+    PendingChangesGuard,
   ],
   bootstrap: [AppComponent],
 })
