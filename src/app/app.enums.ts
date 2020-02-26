a11c54b090c11ca55e0ad8c849ab30c526d16b93
Irina Stanciu <istanciu@deloittece.com>
2020-02-26 11:17:28 +0200

    fixed change requests on PR

--- src/app/app.enums.ts
+++ src/app/app.enums.ts
@@ -3,3 +3,10 @@ export enum VEHICLE_TYPES {
   HGV = 'hgv',
   TRL = 'trl'
 }
+
+export enum CREATE_PAGE_LABELS {
+  CREATE_VRM_LABEL = 'Vehicle registration mark (VRM)',
+  CREATE_VRM_LABEL_OPTIONAL = 'Vehicle registration mark (VRM - optional)',
+  CREATE_VIN_LABEL_ERROR = 'A technical record with this VIN already exists, check the VIN or change the existing technical record',
+  CREATE_VRM_LABEL_ERROR = 'A technical record with this VRM already exists, check the VRM or change the existing technical record'
+}
--- src/app/technical-record-create/technical-record-create.component.ts
+++ src/app/technical-record-create/technical-record-create.component.ts
@@ -7,6 +7,7 @@ import {Store} from '@ngrx/store';
 import {IAppState} from '@app/store/state/app.state';
 import {CreateTechRecordVM} from '@app/store/state/VehicleTechRecordModel.state';
 import {Observable} from 'rxjs';
+import {CREATE_PAGE_LABELS} from '@app/app.enums';
 
 
 @Component({
@@ -19,11 +20,16 @@ export class TechnicalRecordCreateComponent implements OnInit {
   vehicleTypes = ['PSV', 'HGV', 'Trailer'];
   createTechRecordForm: FormGroup;
   formError$: Observable<any>;
-  formErrors: { vinErr: string, vrmErr: string, vTypeErr: string, requestErr: string[] } = { vinErr: '', vrmErr: '', vTypeErr: '', requestErr: [''] };
+  formErrors: { vinErr: string, vrmErr: string, vTypeErr: string, requestErr: string[] } = {
+    vinErr: '',
+    vrmErr: '',
+    vTypeErr: '',
+    requestErr: ['']
+  };
   hasErrors = false;
   hasVinReqErrors;
   hasVrmReqErrors;
-  vrmLabel = 'Vehicle registration mark (VRM)';
+  vrmLabel = CREATE_PAGE_LABELS.CREATE_VRM_LABEL;
 
   constructor(private _store: Store<IAppState>) {
     this.formError$ = this._store.select(s => s.vehicleTechRecordModel.initialDetails.error);
@@ -36,10 +42,10 @@ export class TechnicalRecordCreateComponent implements OnInit {
       'vrm': new FormControl('', Validators.required)
     });
 
-    this.formError$.subscribe( result => {
+    this.formError$.subscribe(result => {
       this.formErrors.requestErr = result;
-      this.hasVinReqErrors = result.includes('A technical record with this VIN already exists, check the VIN or change the existing technical record');
-      this.hasVrmReqErrors = result.includes('A technical record with this VRM already exists, check the VRM or change the existing technical record');
+      this.hasVinReqErrors = result.includes(CREATE_PAGE_LABELS.CREATE_VIN_LABEL_ERROR);
+      this.hasVrmReqErrors = result.includes(CREATE_PAGE_LABELS.CREATE_VRM_LABEL_ERROR);
     });
   }
 
@@ -47,10 +53,10 @@ export class TechnicalRecordCreateComponent implements OnInit {
     const vrmControl = this.createTechRecordForm.get('vrm');
 
     if ($event.currentTarget.id === 'test-radio-PSV' || $event.currentTarget.id === 'test-radio-HGV') {
-      this.vrmLabel = 'Vehicle registration mark (VRM)';
+      this.vrmLabel = CREATE_PAGE_LABELS.CREATE_VRM_LABEL;
       vrmControl.setValidators([Validators.required, Validators.minLength(1), Validators.maxLength(8)]);
     } else {
-      this.vrmLabel = 'Vehicle registration mark (VRM - optional)';
+      this.vrmLabel = CREATE_PAGE_LABELS.CREATE_VRM_LABEL_OPTIONAL;
       vrmControl.setValidators([Validators.minLength(1), Validators.maxLength(8)]);
     }
     vrmControl.updateValueAndValidity();
@@ -58,23 +64,25 @@ export class TechnicalRecordCreateComponent implements OnInit {
 
   onSubmit() {
 
-    this.formErrors.vinErr   = !this.createTechRecordForm.get('vin').valid ? 'Enter a VIN' : '';
-    this.formErrors.vrmErr   = !this.createTechRecordForm.get('vrm').valid ? 'Enter a VRM' : '';
+    this.formErrors.vinErr = !this.createTechRecordForm.get('vin').valid ? 'Enter a VIN' : '';
+    this.formErrors.vrmErr = !this.createTechRecordForm.get('vrm').valid ? 'Enter a VRM' : '';
     this.formErrors.vTypeErr = !this.createTechRecordForm.get('vehicleType').valid ? 'Select a vehicle type' : '';
     this.hasErrors = !Object.values(this.formErrors).every(val => val === '');
 
-    const createDetails: CreateTechRecordVM = {
-      vin: this.createTechRecordForm.get('vin').value,
-      vrm: this.createTechRecordForm.get('vrm').value,
-      vType: this.createTechRecordForm.get('vehicleType').value,
-      error: ['']
-    };
 
     if (!this.formErrors.vinErr && !this.formErrors.vrmErr && !this.formErrors.vTypeErr) {
+
+      const createDetails: CreateTechRecordVM = {
+        vin: this.createTechRecordForm.get('vin').value,
+        vrm: this.createTechRecordForm.get('vrm').value,
+        vType: this.createTechRecordForm.get('vehicleType').value,
+        error: ['']
+      };
+
       this._store.dispatch(new SetVehicleTechRecordModelVinOnCreate(createDetails));
     }
 
-    setTimeout(function() {
+    setTimeout(function () {
       document.getElementById('test-vin').focus();
       document.getElementById('test-vrm').focus();
     }, 1000);
