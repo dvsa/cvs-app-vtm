46f5e87b55301061f48114163f35f47d70902b7f
Irina Stanciu <istanciu@deloittece.com>
2020-02-26 15:43:33 +0200

    feature/CVSB-10141

--- src/app/pipes/DefaultNullOrEmptyPipe.spec.ts
+++ src/app/pipes/DefaultNullOrEmptyPipe.spec.ts
@@ -8,15 +8,15 @@ describe('DefaultNullOrEmptyPipe', () => {
     expect(pipe).toBeTruthy();
   });
 
-  it('should return Yes if value is true', () => {
+  it('should return Yes if value is boolean', () => {
     const expectedRes = 'Yes';
     const res = pipe.transform(true);
     expect(res).toEqual(expectedRes);
   });
 
   it('should return No if value is false', () => {
-    const expectedRes = 'Yes';
-    const res = pipe.transform(true);
+    const expectedRes = 'No';
+    const res = pipe.transform(false);
     expect(res).toEqual(expectedRes);
   });
 
@@ -36,6 +36,21 @@ describe('DefaultNullOrEmptyPipe', () => {
     const expectedRes = '14/1/2019';
     const res = pipe.transform('2019-02-14T10:36:33.987Z');
     expect(res).toEqual(expectedRes);
+  });
+
+  it('should return value as default for type number', () => {
+    const expectedRes = 3;
+    const res = pipe.transform(3);
+    expect(res).not.toBeNull();
+    expect(res).toEqual(expectedRes);
+  });
+
+  it('should check if date formatted string is same as value', () => {
+    const checkDate = new Date('2019-02-14T10:36:33.987Z');
+    const stringToCheck = '2019-02-14T10:36:33.987Z';
+    pipe.transform(stringToCheck);
+    const str = checkDate.toISOString();
+    expect(str).toEqual(stringToCheck);
   });
 
 });
--- src/app/technical-record-create/technical-record-create.component.spec.ts
+++ src/app/technical-record-create/technical-record-create.component.spec.ts
@@ -1,5 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, getTestBed, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, getTestBed, TestBed, tick} from '@angular/core/testing';
 import { appReducers } from '@app/store/reducers/app.reducers';
 import { IAppState } from '@app/store/state/app.state';
 import { INITIAL_STATE, Store, StoreModule } from '@ngrx/store';
@@ -23,6 +23,7 @@ describe('TechnicalRecordSearchComponent', () => {
   let fixture: ComponentFixture<TechnicalRecordCreateComponent>;
   let store: Store<IAppState>;
   let injector: TestBed;
+  let timerCallback;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -60,7 +61,6 @@ describe('TechnicalRecordSearchComponent', () => {
     component = fixture.componentInstance;
     component.formError$ = of(['test-error']);
     fixture.detectChanges();
-
   });
 
   it('should create', () => {
@@ -99,19 +99,61 @@ describe('TechnicalRecordSearchComponent', () => {
 
   it('should call onSubmit method', () => {
     spyOn(component, 'onSubmit');
-    const el = fixture.debugElement.query(By.css('button')).nativeElement;
-    el.click();
+    const element = fixture.debugElement.query(By.css('button')).nativeElement;
+    element.click();
     expect(component.onSubmit).toHaveBeenCalledTimes(1);
   });
 
-  it('should update values onSubmit', () => {
+  it('should set the focus on timeout', fakeAsync(() => {
+    const element1 = fixture.debugElement.query(By.css('#test-vin')).nativeElement;
+    const element2 = fixture.debugElement.query(By.css('#test-vrm')).nativeElement;
+
+    component.onSubmit();
+    tick(10000);
+    spyOn(element1, 'focus');
+    spyOn(element2, 'focus');
+    expect(element1).not.toBeNull();
+    expect(element2).not.toBeNull();
+
+  }));
+
+  it('should change VRM label on PSV type select', () => {
+    const event = {currentTarget: {checked: false, id: 'test-radio-PSV'}};
+    const vrmControl = component.createTechRecordForm.get('vrm');
+
+    component.setVrmValidators(event);
+    expect(vrmControl).not.toBeNull();
+    expect(component.vrmLabel).toEqual('Vehicle registration mark (VRM)');
+  });
+
+  it('should change VRM label on Trailer type select', () => {
+    const event = {currentTarget: {checked: false, id: 'test-radio-Trailer'}};
+    const vrmControl = component.createTechRecordForm.get('vrm');
+
+    component.setVrmValidators(event);
+    expect(vrmControl).not.toBeNull();
+    expect(component.vrmLabel).toEqual('Vehicle registration mark (VRM - optional)');
+  });
+
+  it('should update formValues onSubmit', () => {
     component.createTechRecordForm.controls.vin.setValue('');
     component.createTechRecordForm.controls.vrm.setValue('');
-    component.createTechRecordForm.controls.vehicleType.setValue('HGV');
+    component.createTechRecordForm.controls.vehicleType.setValue('');
 
     component.onSubmit();
     expect(component.formErrors.vinErr).toEqual('Enter a VIN');
     expect(component.formErrors.vrmErr).toEqual('Enter a VRM');
+    expect(component.formErrors.vTypeErr).toEqual('Select a vehicle type');
+  });
+
+  it('should update formValues onSubmit - empty for form valid', () => {
+    component.createTechRecordForm.controls.vin.setValue('aaa');
+    component.createTechRecordForm.controls.vrm.setValue('sss');
+    component.createTechRecordForm.controls.vehicleType.setValue('dddd');
+
+    component.onSubmit();
+    expect(component.formErrors.vinErr).toEqual('');
+    expect(component.formErrors.vrmErr).toEqual('');
     expect(component.formErrors.vTypeErr).toEqual('');
   });
 
