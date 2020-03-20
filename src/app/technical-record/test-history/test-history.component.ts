00e45f49083c4ab1222a0190d5cb0f4f42211206
Irina Stanciu <istanciu@deloittece.com>
2020-03-20 12:49:45 +0200

    feature/CVSB-13093

--- src/app/technical-record/test-history/test-history.component.ts
+++ src/app/technical-record/test-history/test-history.component.ts
@@ -12,9 +12,24 @@ export class TestHistoryComponent implements OnInit {
   @Input() testResultJson: any;
 
 
-  constructor(public techRecHelpers: TechRecordHelpersService) { }
+  constructor(public techRecHelpers: TechRecordHelpersService) {
+  }
 
   ngOnInit() {
+  }
+
+  setResultColor(testResult: string) {
+    switch (testResult) {
+      case 'pass':
+        return {color: '#00703C'};
+      case 'prs':
+        return {color: '#1D70B8'};
+      case 'fail':
+      case 'abandoned':
+        return {color: '#D4351C'};
+      default:
+        return {color: ''};
+    }
   }
 
 }
