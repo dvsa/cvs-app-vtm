51cfbe27031cca6a63b21ea02c51d7301d7382a8
Irina Stanciu <istanciu@deloittece.com>
2020-05-04 02:49:29 +0300

    Bug fixing on DEVELOP: - feature/CVSB-12898 - feature/CVSB-14720

--- src/app/store/reducers/VehicleTechRecordModel.reducers.spec.ts
+++ src/app/store/reducers/VehicleTechRecordModel.reducers.spec.ts
@@ -1,12 +1,15 @@
-import {VehicleTechRecordModel} from '@app/models/vehicle-tech-record.model';
-import {VehicleTechRecordModelReducers} from '@app/store/reducers/VehicleTechRecordModel.reducers';
+import { VehicleTechRecordModel } from '@app/models/vehicle-tech-record.model';
+import { VehicleTechRecordModelReducers } from '@app/store/reducers/VehicleTechRecordModel.reducers';
 import {
-  GetVehicleTechRecordModel, GetVehicleTechRecordModelHavingStatusAll, GetVehicleTechRecordModelHavingStatusAllFailure,
-  GetVehicleTechRecordModelHavingStatusAllSuccess, GetVehicleTechRecordModelSuccess, SetVehicleTechRecordModelVinOnCreate
+  GetVehicleTechRecordModel,
+  GetVehicleTechRecordModelHavingStatusAll,
+  GetVehicleTechRecordModelHavingStatusAllSuccess,
+  GetVehicleTechRecordModelSuccess,
+  SetVehicleTechRecordModelOnCreate
 } from '../actions/VehicleTechRecordModel.actions';
-import {initialVehicleTechRecordModelState} from '../state/VehicleTechRecordModel.state';
-import {TechRecord} from '@app/models/tech-record.model';
-import {SearchParams} from '@app/models/search-params';
+import { initialVehicleTechRecordModelState } from '../state/VehicleTechRecordModel.state';
+import { TechRecord } from '@app/models/tech-record.model';
+import { SearchParams } from '@app/models/search-params';
 
 const techRecord: TechRecord = {} as TechRecord;
 const vehicleTechRecord: VehicleTechRecordModel = {} as VehicleTechRecordModel;
@@ -14,7 +17,7 @@ const vehicleTechRecord: VehicleTechRecordModel = {} as VehicleTechRecordModel;
 describe('VehicleTechRecordModel Reducer', () => {
   describe('undefined action', () => {
     it('should return the default state', () => {
-      const action = {type: 'NOOP'} as any;
+      const action = { type: 'NOOP' } as any;
       const result = VehicleTechRecordModelReducers(undefined, action);
 
       expect(result).toBe(initialVehicleTechRecordModelState);
@@ -44,7 +47,7 @@ describe('VehicleTechRecordModel Reducer', () => {
 
   describe('[VehicleTechRecordModel] Get VehicleTechRecordModelHavingStatusAll', () => {
     it('should toggle loading state', () => {
-      const searchParams: SearchParams = {searchIdentifier: '1234', searchCriteria: 'all'};
+      const searchParams: SearchParams = { searchIdentifier: '1234', searchCriteria: 'all' };
       const action = new GetVehicleTechRecordModelHavingStatusAll(searchParams);
       const result = VehicleTechRecordModelReducers(initialVehicleTechRecordModelState, action);
       expect(result).toEqual({
@@ -64,27 +67,17 @@ describe('VehicleTechRecordModel Reducer', () => {
     });
   });
 
-  describe('GetVehicleTechRecordModelHavingStatusAllFailure', () => {
-    test('should return state with error', () => {
-      const action = new GetVehicleTechRecordModelHavingStatusAllFailure('error');
-      const result = VehicleTechRecordModelReducers(initialVehicleTechRecordModelState, action);
-
-      expect(result).toMatchObject({
-        ...initialVehicleTechRecordModelState,
-        error: 'error'
-      });
-    });
-  });
-
-  describe('[SetVehicleTechRecordModelVinOnCreate] SetVehicleTechRecordModelVinOnCreate', () => {
+  describe('[SetVehicleTechRecordModelOnCreate] SetVehicleTechRecordModelVinOnCreate', () => {
     it('should update state with first tech record creating details', () => {
-      const action = new SetVehicleTechRecordModelVinOnCreate({vin: 'aaa', vrm: 'bbb', vType: 'PSV', error: []});
+      const action = new SetVehicleTechRecordModelOnCreate({
+        vin: 'aaa',
+        vrm: 'bbb',
+        vType: 'PSV'
+      });
       const result = VehicleTechRecordModelReducers(initialVehicleTechRecordModelState, action);
       expect(result).toEqual({
-        ...initialVehicleTechRecordModelState,
-        initialDetails: {vin: 'aaa', vrm: 'bbb', vType: 'PSV', error: []}
+        ...initialVehicleTechRecordModelState
       });
     });
   });
-
 });
