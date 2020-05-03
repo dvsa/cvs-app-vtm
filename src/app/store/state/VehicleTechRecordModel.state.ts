6559ada1db379575dc2fe2618db9729af0e68058
Irina Stanciu <istanciu@deloittece.com>
2020-05-04 02:49:29 +0300

    Bug fixing on DEVELOP: - feature/CVSB-12898 - feature/CVSB-14720 - add scss guidance, move mockstore into utils, add id and refactor errorState in unit test

--- src/app/store/state/VehicleTechRecordModel.state.ts
+++ src/app/store/state/VehicleTechRecordModel.state.ts
@@ -1,32 +1,16 @@
-import { VehicleTechRecordModel } from '../../models/vehicle-tech-record.model';
+import { VehicleTechRecordModel } from '@app/models/vehicle-tech-record.model';
 import { VIEW_STATE } from '@app/app.enums';
 
 export interface IVehicleTechRecordModelState {
-  initialDetails: CreateTechRecordVM;
   vehicleTechRecordModel: VehicleTechRecordModel[];
   selectedVehicleTechRecordModel: VehicleTechRecordModel[];
   selectedVehicleTechRecord: VehicleTechRecordModel;
   viewState: VIEW_STATE;
-  error?: any;
-}
-
-export interface CreateTechRecordVM {
-  vin: string;
-  vrm: string;
-  vType: string;
-  error: string[];
 }
 
 export const initialVehicleTechRecordModelState: IVehicleTechRecordModelState = {
-  initialDetails: {
-    vin: '',
-    vrm: '',
-    vType: '',
-    error: []
-  },
   vehicleTechRecordModel: null,
   selectedVehicleTechRecordModel: null,
   selectedVehicleTechRecord: null,
-  viewState: VIEW_STATE.VIEW_ONLY,
-  error: null
+  viewState: VIEW_STATE.VIEW_ONLY
 };
