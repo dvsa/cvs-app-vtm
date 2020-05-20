5d50876e332722c108960673273d574b4c8d8066
Olu Alli <oalli@deloitte.co.uk>
2020-05-20 18:33:48 +0100

    Fixes to correct 10175 merging

--- src/app/technical-record/body/body-psv/body-psv.component.spec.ts
+++ src/app/technical-record/body/body-psv/body-psv.component.spec.ts
@@ -1,9 +1,9 @@
 import { ComponentFixture, getTestBed, TestBed } from '@angular/core/testing';
-import { SharedModule } from '@app/shared/shared.module';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { SharedModule } from '@app/shared/shared.module';
 import { TESTING_UTILS } from '../../../utils/testing.utils';
 import { BodyPsvComponent } from './body-psv.component';
-import { TechRecord } from '@app/models/tech-record.model';
 
 describe('PsvBodyComponent', () => {
   let component: BodyPsvComponent;
@@ -28,12 +28,11 @@ describe('PsvBodyComponent', () => {
       chassisMake: 'volvo',
       chassisModel: 't23'
     });
-    
+
     fixture.detectChanges();
   });
 
   it('should create view only with populated data', () => {
-
     expect(component).toBeDefined();
     expect(fixture).toMatchSnapshot();
   });
