067019a8c2c020be0becd1aaba4cbaa3d752cd26
Irina Stanciu <istanciu@deloittece.com>
2020-05-21 14:09:28 +0300

    fix(feature/CVSB-15376): display fixes on defects section

--- src/app/test-record/defects/defects.component.spec.ts
+++ src/app/test-record/defects/defects.component.spec.ts
@@ -1,32 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DefectsComponent } from './defects.component';
 import { SharedModule } from '@app/shared/shared.module';
-import { TestType } from '@app/models/test.type';
-import { TestResultModel } from '@app/models/test-result.model';
+import { TEST_MODEL_UTILS } from '@app/utils/test-models.utils';
+import { Defect } from '@app/models/defect';
 
 describe('DefectsComponent', () => {
   let component: DefectsComponent;
   let fixture: ComponentFixture<DefectsComponent>;
-  const testType = {} as TestType;
-  const testRecord = {} as TestResultModel;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [SharedModule],
-      declarations: [DefectsComponent],
+      declarations: [DefectsComponent]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DefectsComponent);
     component = fixture.componentInstance;
-    component.testType = testType;
-    component.testRecord = testRecord;
-    fixture.detectChanges();
+    const defects = [{ deficiencyId: '123', deficiencySubId: '345' } as Defect];
+    component.testType = TEST_MODEL_UTILS.mockTestType({ defects: defects });
+    component.testRecord = TEST_MODEL_UTILS.mockTestResult();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    fixture.detectChanges();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should match snapshot if test type doesn t have defects with deficiencyId & deficiencySubId', () => {
+    const defects = [{ deficiencyId: null, deficiencySubId: null } as Defect];
+    component.testType = TEST_MODEL_UTILS.mockTestType({ defects: defects });
+    fixture.detectChanges();
+
     expect(fixture).toMatchSnapshot();
   });
 });
