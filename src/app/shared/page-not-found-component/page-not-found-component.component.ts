bb922bfd210851d27104756405c6f07dc056fdf5
jonlazarini <jlazarini@deloitte.co.uk>
2020-05-29 15:08:26 +0100

    fix(global): remove sonarQube errors
    fix(global): remove sonarQube errors
    
    chore(test): ignore config files from coverage

--- src/app/shared/page-not-found-component/page-not-found-component.component.ts
+++ src/app/shared/page-not-found-component/page-not-found-component.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'vtm-page-not-found-component',
-  templateUrl: './page-not-found-component.component.html',
-  styleUrls: ['./page-not-found-component.component.scss']
+  templateUrl: './page-not-found-component.component.html'
 })
 export class PageNotFoundComponentComponent implements OnInit {
 
--- src/app/technical-record/notes/notes.component.ts
+++ src/app/technical-record/notes/notes.component.ts
@@ -4,7 +4,6 @@ import { TechRecord } from '@app/models/tech-record.model';
 @Component({
   selector: 'vtm-notes',
   templateUrl: './notes.component.html',
-  styleUrls: ['../../app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NotesComponent implements OnInit {
