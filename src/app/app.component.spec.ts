964b8cce20b869fa23c33569874a9deaba5b82b7
Thomas.Webb <SA_Thomas.Webb@dvsatest-cloud.uk>
2022-03-04 11:30:00 +0000

    added initial linting for project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,10 +6,10 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     }).compileComponents();
   });
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'vtm'`, () => {
+  it('should have as title \'vtm\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('vtm');
