f86d04cc4b378ecc7da5258842d1b8a4bea1ae5d
NiallBunting <niall.bunting@bjss.com>
2022-03-08 16:40:26 +0000

    Add msal code - WIP
    This code is not working for some reason.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,36 @@
 ///<reference path="govuk.d.ts">
 import { Component } from '@angular/core';
 import { initAll } from 'govuk-frontend/govuk/all';
+import { EventMessage, EventType } from '@azure/msal-browser';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
+@Injectable({
+  providedIn: 'root',
+})
 export class AppComponent {
   title = 'vtm';
 
+  constructor(private msalBroadcastService: MsalBroadcastService) {
+    this.msalBroadcastService = msalBroadcastService;
+  }
+
   ngOnInit() {
     initAll();
+
+    
+this.msalBroadcastService.msalSubject$
+    .pipe(
+        filter((msg: EventMessage) => msg.eventType === EventType.LOGIN_SUCCESS)
+    )
+    .subscribe((result: any) => {
+        console.log(result);
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,63 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
+import { MsalModule, MsalService, MsalGuard, MsalInterceptor, MsalBroadcastService, MsalRedirectComponent } from "@azure/msal-angular";
+import { PublicClientApplication, InteractionType, BrowserCacheLocation } from "@azure/msal-browser";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MsalModule.forRoot( new PublicClientApplication({ // MSAL Configuration
+      auth: {
+        clientId: "",
+        authority: "",
+        redirectUri: "http://localhost:4200",
+      },
+      cache: {
+        cacheLocation : BrowserCacheLocation.LocalStorage,
+        storeAuthStateInCookie: true, // set to true for IE 11
+      },
+      system: {
+        loggerOptions: {
+          loggerCallback: () => {},
+          piiLoggingEnabled: false
+        }
+      }
+    }), {
+        interactionType: InteractionType.Redirect, // MSAL Guard Configuration
+    }, {
+        interactionType: InteractionType.Redirect, // MSAL Interceptor Configuration
+        protectedResourceMap: new Map([
+        //    ['https://graph.microsoft.com/v1.0/me', ['user.read']],
+        //    ['https://api.myapplication.com/users/*', ['customscope.read']],
+        //    ['http://localhost:4200/about/', null] 
+        ])
+    })
   ],
-  providers: [],
-  bootstrap: [AppComponent],
+  providers: [
+    {
+        provide: HTTP_INTERCEPTORS,
+        useClass: MsalInterceptor,
+        multi: true
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService
+  ],
+  bootstrap: [AppComponent, MsalRedirectComponent],
 })
 export class AppModule { }
