a0e3b8102a6a6e69e1f8df344f79dbcbb1ed1288
Niall Bunting <NiallBunting@users.noreply.github.com>
2022-03-09 14:57:27 +0000

    CB2-2984: Add Storybook (#267)
    * feature(cb2-2883): adds govuk styling with working test accordian component
    
    * feature(cb2-2883): govuk fonts and images now handled by angular build process
    
    * feature(cb2-2883): adds govuk default page template
    
    * feature(cb2-2883): moves header and footer into components
    
    * feature(cb2-2883): removes favicon.ico
    
    * Add storybook
    
    * Add documation.json file
    
    * Add stories for all the components
    
    * Remove reference stories
    
    Co-authored-by: Andrew Freemantle (BJSS) <andrew.freemantle@bjss.com>

---
+++ src/app/app.stories.ts
@@ -0,0 +1,13 @@
+import { Meta, Story } from '@storybook/angular';
+
+import { AppComponent } from './app.component';
+
+export default {
+  title: 'App',
+  component: AppComponent,
+} as Meta;
+
+export const Primary: Story = () => ({
+  props: {
+  },
+});
