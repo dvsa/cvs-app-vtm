53963d209439d60f9d123ecf054f072dae4aebde
NiallBunting <niall.bunting@bjss.com>
2022-03-10 13:13:45 +0000

    Add events to the header component

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
+  }
+
+  @Input() username = '';
+
+  @Output() loggedOut = new EventEmitter<string>();
+
+  logout() {
+    this.loggedOut.emit();
   }
 
 }
