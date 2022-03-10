969580f89c6506511442743d7bb8f0c9aa3b3548
NiallBunting <niall.bunting@bjss.com>
2022-03-10 12:27:56 +0000

    Pull the code out to factories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,60 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { MSAL_INSTANCE, MSAL_GUARD_CONFIG, MSAL_INTERCEPTOR_CONFIG } from '@azure/msal-angular';
 
-import { MsalModule, MsalService, MsalGuard, MsalInterceptor, MsalBroadcastService, MsalRedirectComponent } from "@azure/msal-angular";
-import { PublicClientApplication, InteractionType, BrowserCacheLocation } from "@azure/msal-browser";
+import {
+  MsalModule,
+  MsalService,
+  MsalInterceptor,
+  MsalInterceptorConfiguration,
+  MsalGuard,
+  MsalGuardConfiguration,
+  MsalBroadcastService, 
+  MsalRedirectComponent
+} from "@azure/msal-angular";
+import { IPublicClientApplication, PublicClientApplication, InteractionType, BrowserCacheLocation } from "@azure/msal-browser";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { environment } from '../environments/environment';
 
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+        clientId: environment.VTM_CLIENT_ID ? environment.VTM_CLIENT_ID : '',
+        authority: environment.VTM_AUTHORITY_ID ? environment.VTM_AUTHORITY_ID : '',
+        redirectUri: '/',
 
+    },
+    cache: {
+      cacheLocation: BrowserCacheLocation.LocalStorage,
+      storeAuthStateInCookie: true,
+    },
+  });
+}
+
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, Array<string>>();
+  protectedResourceMap.set("https://graph.microsoft.com/v1.0/me", ["user.read"]);
+
+  return {
+    interactionType: InteractionType.Redirect,
+    protectedResourceMap,
+  };
+}
+
+export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return { 
+    interactionType: InteractionType.Redirect,
+    authRequest: {
+      scopes: ['user.read']
+    },
+    loginFailedRoute: ""
+  };
+}
 
 @NgModule({
   declarations: [
@@ -21,38 +65,25 @@ import { FooterComponent } from './layout/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MsalModule.forRoot( new PublicClientApplication({ // MSAL Configuration
-      auth: {
-        clientId: "",
-        authority: "",
-        redirectUri: "http://localhost:4200",
-      },
-      cache: {
-        cacheLocation : BrowserCacheLocation.LocalStorage,
-        storeAuthStateInCookie: true, // set to true for IE 11
-      },
-      system: {
-        loggerOptions: {
-          loggerCallback: () => {},
-          piiLoggingEnabled: false
-        }
-      }
-    }), {
-        interactionType: InteractionType.Redirect, // MSAL Guard Configuration
-    }, {
-        interactionType: InteractionType.Redirect, // MSAL Interceptor Configuration
-        protectedResourceMap: new Map([
-        //    ['https://graph.microsoft.com/v1.0/me', ['user.read']],
-        //    ['https://api.myapplication.com/users/*', ['customscope.read']],
-        //    ['http://localhost:4200/about/', null] 
-        ])
-    })
+    MsalModule
   ],
   providers: [
     {
         provide: HTTP_INTERCEPTORS,
         useClass: MsalInterceptor,
         multi: true
+    },
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory
     },
     MsalService,
     MsalGuard,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,8 @@
 
 export const environment = {
   production: false,
+  VTM_CLIENT_ID: '',
+  VTM_AUTHORITY_ID: '',
 };
 
 /*
