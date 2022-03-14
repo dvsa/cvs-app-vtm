ceb575a7a865081a6d1ae3dbc3c4782ada42d70f
Niall Bunting <NiallBunting@users.noreply.github.com>
2022-03-14 09:32:48 +0000

    feature/CB2-2879: Authentication (#270)
    Add authentication code that uses MSAL to request access from AD.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 ///<reference path="govuk.d.ts">
 import { Component } from '@angular/core';
 import { initAll } from 'govuk-frontend/govuk/all';
+import { UserService } from './user-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'vtm';
+
+  constructor(public userService: UserService) {}
 
   ngOnInit() {
     initAll();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,96 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { MSAL_INSTANCE, MSAL_GUARD_CONFIG, MSAL_INTERCEPTOR_CONFIG } from '@azure/msal-angular';
+
+import {
+  MsalModule,
+  MsalService,
+  MsalInterceptor,
+  MsalInterceptorConfiguration,
+  MsalGuard,
+  MsalGuardConfiguration,
+  MsalBroadcastService, 
+  MsalRedirectComponent
+} from "@azure/msal-angular";
+import { IPublicClientApplication, PublicClientApplication, InteractionType, BrowserCacheLocation } from "@azure/msal-browser";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { environment } from '../environments/environment';
+import { UserService } from './user-service';
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+        clientId: environment.VTM_CLIENT_ID,
+        authority: environment.VTM_AUTHORITY_ID,
+        redirectUri: environment.VTM_REDIRECT_URI,
+
+    },
+    cache: {
+      cacheLocation: BrowserCacheLocation.LocalStorage,
+      storeAuthStateInCookie: true,
+    },
+  });
+}
+
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, Array<string>>();
+  protectedResourceMap.set("https://graph.microsoft.com/v1.0/me", ["user.read"]);
+
+  return {
+    interactionType: InteractionType.Redirect,
+    protectedResourceMap,
+  };
+}
+
+export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return { 
+    interactionType: InteractionType.Redirect,
+    authRequest: {
+      scopes: ['user.read']
+    },
+    loginFailedRoute: ""
+  };
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MsalModule
   ],
-  providers: [],
-  bootstrap: [AppComponent],
+  providers: [
+    {
+        provide: HTTP_INTERCEPTORS,
+        useClass: MsalInterceptor,
+        multi: true
+    },
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService,
+    UserService
+  ],
+  bootstrap: [AppComponent, MsalRedirectComponent],
 })
 export class AppModule { }
