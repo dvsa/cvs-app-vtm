ebf2fc3e341d8ab12a81aca070161c1f2303457b
NiallBunting <niall.bunting@bjss.com>
2022-03-14 17:24:52 +0000

    Token

--- src/environments/environment.deploy.ts
+++ src/environments/environment.deploy.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    VTM_CLIENT_ID: '6226576d-37e9-49eb-b201-ec1eeb0029b6',
-    VTM_AUTHORITY_ID: 'https://login.microsoftonline.com/5c448d90-4ca1-4caf-ab59-0a2aa67d7801',
+    VTM_CLIENT_ID: '',
+    VTM_AUTHORITY_ID: '',
     VTM_REDIRECT_URI: '/',
   };
   
