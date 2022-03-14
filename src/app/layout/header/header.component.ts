7f1c05a22914da612ed42508d4021803a3689300
tom-bjss <92319736+tom-bjss@users.noreply.github.com>
2022-03-14 14:42:24 +0000

    Linting and husky (#273)
    * feat(cb2-2941): added husky/commit hooks and fixed linting issues
    
    * feat(cb2-2941): fix git hooks
    
    * feat(cb2-2941): fix pre-push hook
    
    * feat(cb2-2941): allow lint warnings
    
    * fix(cb2-2941): make commit message less restrictive on jira ticket number
    
    * feat(cb2-2941): added perms for executable for husky scripts
    
    * fix(cb2-2941): removed empty inits and added scanrepo back
    
    * feat(cb2-2941): change scanrepo git log import for improved speed
    
    * feat(cb2-2941): change scanrepo git log import for improved speed
    
    Co-authored-by: Aaron Downing <aaron.downing@bjss.com>
    Co-authored-by: Ian Hodges <doctorhodges@gmail.com>
    Co-authored-by: Thomas.Webb <SA_Thomas.Webb@dvsatest-cloud.uk>

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  ngOnInit(): void {
-  }
+export class HeaderComponent {
 
   @Input() username = '';
 
