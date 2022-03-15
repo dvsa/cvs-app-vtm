ff85719c615af26e8a97a2c474349eb3defbb48c
NiallBunting <niall.bunting@bjss.com>
2022-03-15 10:58:26 +0000

    Fix app.component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,20 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { UserService } from './user-service/user-service';
+import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HeaderComponent } from './layout/header/header.component';
+import { MsalModule, MsalBroadcastService, } from "@azure/msal-angular";
+import { Observable } from 'rxjs';
 
 describe('AppComponent', () => {
+
+  const MockUserService = {
+    getUserNameObservable: jest.fn().mockReturnValue(new Observable()),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
+        StoreModule.forRoot({}),
+        MsalModule,
       ],
       declarations: [
         AppComponent,
         HeaderComponent,
         FooterComponent
       ],
+      providers: [ { provide: UserService , useValue: MockUserService }, ]
     }).compileComponents();
   });
 
