d47c5acd10e27510d05f1b20ad1201312c75060b
Andrew Freemantle (BJSS) <andrew.freemantle@bjss.com>
2022-04-04 13:41:40 +0100

    feat(cb2-2880): updated env config

--- src/environments/environment.deploy.ts
+++ src/environments/environment.deploy.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  VTM_CLIENT_ID: '6226576d-37e9-49eb-b201-ec1eeb0029b6',
-  VTM_AUTHORITY_ID: 'https://login.microsoftonline.com/6c448d90-4ca1-4caf-ab59-0a2aa67d7801',
+  VTM_CLIENT_ID: '',
+  VTM_AUTHORITY_ID: '',
   VTM_REDIRECT_URI: 'http://localhost:4200',
   VTM_API_URI: 'http://localhost:3005'
 };
