5d758b881deecf7a7aeaea70c90dc1cb10ad3b7a
NiallBunting <niall.bunting@bjss.com>
2022-04-04 16:42:15 +0100

    Add a test record to show the item working

--- src/app/models/test-result.model.ts
+++ src/app/models/test-result.model.ts
@@ -1,6 +1,7 @@
 import { TestType } from './test.type';
 import { BodyType } from './body-type';
 
+/*
 export interface TestResultModel {
   reasonForCreation: string;
   testResultId: string;
@@ -45,4 +46,52 @@ export interface TestResultModel {
   lastUpdatedAt: string;
   testHistory: TestResultModel[];
   vehicleSubclass: string[];
+}
+*/
+
+export interface TestResultModel {
+  testResultId: string;
+  systemNumber: string;
+  testerStaffId: string;
+  testStartTimestamp: string | Date;
+  odometerReadingUnits: string;
+  testEndTimestamp: string | Date;
+  testStatus: string;
+  testTypes: any[]; //TODO
+  vehicleClass: BodyType;
+  vehicleSubclass?: string[];
+  vin: string;
+  vehicleSize?: string; // Mandatory for PSV only & not applicable to HGV and TRL
+  testStationName: string;
+  vehicleId?: string; // Not sent from FE, calculated in the BE. When the test result is submitted, in BE, the VRM of the vehicle will be copied into  vehicleId also.
+  vehicleType: string;
+  countryOfRegistration: string;
+  preparerId: string;
+  preparerName: string;
+  odometerReading: number;
+  vehicleConfiguration: string;
+  testStationType: string;
+  reasonForCancellation: string | null;
+  testerName: string;
+  vrm?: string; // Mandatory for PSV and HGV, not applicable to TRL
+  testStationPNumber: string;
+  numberOfSeats?: number; // mandatory for PSV only, not applicable for HGV and TRL
+  noOfAxles: number;
+  numberOfWheelsDriven?: number;
+  testerEmailAddress: string;
+  euVehicleCategory: string;
+  deletionFlag?: boolean | null; // Not sent from FE, calculated in the BE.
+  regnDate?: string | Date; // Used only for PSV and HGV
+  trailerId?: string; // Mandatory for TRL, not applicable to PSV and HGV
+  firstUseDate?: string | Date; // Used only for TRL
+  testVersion?: string;
+  reasonForCreation?: string;
+  createdByName?: string;
+  createdById?: string;
+  createdAt?: string;
+  lastUpdatedByName?: string;
+  lastUpdatedById?: string;
+  lastUpdatedAt?: string;
+  shouldEmailCertificate?: string;
+  testHistory?: TestResultModel[];
 }
