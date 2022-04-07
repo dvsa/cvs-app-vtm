86bc1c2a17ab680530b734e02c122b0e37ff8ff7
NiallBunting <niall.bunting@bjss.com>
2022-04-07 15:05:06 +0100

    Fix linting issues

--- src/app/features/test-record-summary/test-record-summary.component.ts
+++ src/app/features/test-record-summary/test-record-summary.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { TestResultModel } from '../../models/test-result.model';
 
 @Component({
-  selector: 'test-record-summary',
+  selector: 'app-test-record-summary',
   templateUrl: './test-record-summary.component.html',
   styleUrls: ['./test-record-summary.component.scss']
 })
