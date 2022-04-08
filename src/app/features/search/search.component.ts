a712a0d015c89fcc6fefb8154692efcdf8e42701
Andrew Freemantle (BJSS) <andrew.freemantle@bjss.com>
2022-04-08 10:03:54 +0100

    feat(cb2-2880): adds unit tests to increase coverage

--- src/app/features/search/search.component.ts
+++ src/app/features/search/search.component.ts
@@ -22,48 +22,6 @@ export class SearchComponent {
     this.searchError = undefined;
     if (searchTerm) {
       this.technicalRecordService.searchBy({ type: 'vin', searchTerm });
-
-      //   this.technicalRecordService.getByVIN(searchTerm)
-      //   .subscribe((data: VehicleTechRecordModel[]) => {
-      //     if (data.length === 1) {
-      //       this.vehicleTechRecord = data[0];
-      //       if (this.vehicleTechRecord.techRecord.length > 0) {
-      //         this.techRecord = this.vehicleTechRecord.techRecord[0];
-      //       }
-      //     };
-      //   });
-      // } else {
-      //   this.searchError = "Enter a vehicle registration mark, trailer ID or vehicle identification number"
-      // }
     }
   }
-
-  tackBy(index: number): number {
-    return index;
-  }
-
-  // public searchTechRecords(searchIdentifier: string, searchCriteria: string) {
-  // this.isLoading = true;
-  // this.searchParams.searchIdentifier = encodeURIComponent(searchIdentifier);
-
-  // switch (searchCriteria) {
-  //   case SEARCH_CRITERIA.VRM_CRITERIA:
-  //     this.searchParams.searchCriteria = 'vrm';
-  //     break;
-  //   case SEARCH_CRITERIA.FULL_VIN_CRITERIA:
-  //     this.searchParams.searchCriteria = 'vin';
-  //     break;
-  //   case SEARCH_CRITERIA.PARTIAL_VIN_CRITERIA:
-  //     this.searchParams.searchCriteria = 'partialVin';
-  //     break;
-  //   case SEARCH_CRITERIA.TRL_CRITERIA:
-  //     this.searchParams.searchCriteria = 'trailerId';
-  //     break;
-  //   case SEARCH_CRITERIA.ALL_CRITERIA:
-  //   default:
-  //     this.searchParams.searchCriteria = 'all';
-  // }
-
-  // this.store.dispatch(new GetVehicleTechRecordHavingStatusAll(this.searchTerm));
-  // }
 }
