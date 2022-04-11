5cdcad12105d5854a2a4c8a9d48cce0e642f617f
NiallBunting <niall.bunting@bjss.com>
2022-04-11 01:30:11 +0100

    feat(cb2-3291): Add unit test
    This adds a test-record unit test.

--- src/app/services/test-record-service/test-record.service.spec.ts
+++ src/app/services/test-record-service/test-record.service.spec.ts
@@ -3,10 +3,13 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { TestRecordService } from './test-record.service';
 import { initialAppState } from '@store/.';
-import { provideMockStore } from '@ngrx/store/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { Store } from '@ngrx/store';
+import * as TestRecordActions from '../../store/test-records/test-record-service.actions';
 
 describe('TestRecordService', () => {
   let service: TestRecordService;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,9 +18,22 @@ describe('TestRecordService', () => {
     }).compileComponents();
 
     service = TestBed.inject(TestRecordService);
+    store = TestBed.inject(MockStore);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should request id and return the value', (done) => {
+    let serviceObservable = service.getTestRecords('123');
+
+    serviceObservable.subscribe((value) => {
+      expect(value).toStrictEqual([]);
+      done();
+    });
+
+    store.dispatch(TestRecordActions.getBySystemIdSuccess({'testRecords': [] }));
+    
   });
 });
