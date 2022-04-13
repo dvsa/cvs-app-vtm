187ce1ac1b1c026cc0c85b9f4748a3f22ba80e71
NiallBunting <niall.bunting@bjss.com>
2022-04-13 19:16:52 +0100

    Add tests and stories for test-record-summary

--- src/app/features/test-record-summary/test-record-summary.component.spec.ts
+++ src/app/features/test-record-summary/test-record-summary.component.spec.ts
@@ -1,6 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { TestRecordSummaryComponent } from './test-record-summary.component';
+import { TestResultModel } from '../../models/test-result.model';
+
+const fakeRecord: TestResultModel = {
+  testResultId: "test",
+  systemNumber: "test",
+  testerStaffId: "test",
+  testStartTimestamp: "testStartTimestamp",
+  odometerReadingUnits: "test",
+  testEndTimestamp: "test",
+  testStatus: "testStatus",
+  testTypes: [],
+  vehicleClass: {code: "code", description: "test"},
+  vin: "vin",
+  testStationName: "test",
+  vehicleType: "test",
+  countryOfRegistration: "test",
+  preparerId: "test",
+  preparerName: "test",
+  odometerReading: 1,
+  vehicleConfiguration: "test",
+  testStationType: "test",
+  reasonForCancellation: null,
+  testerName: "test",
+  testStationPNumber: "test",
+  noOfAxles: 1,
+  testerEmailAddress: "test",
+  euVehicleCategory: "test",
+}
 
 describe('TestRecordSummaryComponent', () => {
   let component: TestRecordSummaryComponent;
@@ -15,10 +43,32 @@ describe('TestRecordSummaryComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TestRecordSummaryComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show no records found', () => {
+    component.testRecords = [];
+    fixture.detectChanges();
+
+    const heading = fixture.debugElement.query(By.css('.govuk-heading-s'));
+    expect(heading).toBeTruthy();
+    expect(heading.nativeElement.innerHTML).toBe("No test records found.");
+
+    const table = fixture.debugElement.query(By.css('.govuk-table__body'));
+    expect(table).toBeFalsy();
+  });
+
+  it('should show records found', () => {
+    component.testRecords = [fakeRecord];
+    fixture.detectChanges();
+
+    const heading = fixture.debugElement.query(By.css('.govuk-heading-s'));
+    expect(heading).toBeFalsy();
+
+    const table = fixture.debugElement.query(By.css('.govuk-table__body'));
+    expect(table).toBeTruthy();
   });
 });
--- src/app/features/test-record-summary/test-record-summary.stories.ts
+++ src/app/features/test-record-summary/test-record-summary.stories.ts
@@ -1,12 +1,47 @@
 import { Meta, Story } from '@storybook/angular';
-
 import { TestRecordSummaryComponent } from './test-record-summary.component';
+import { TestResultModel } from '../../models/test-result.model';
+
+const fakeRecord: TestResultModel = {
+  testResultId: "test",
+  systemNumber: "test",
+  testerStaffId: "test",
+  testStartTimestamp: "testStartTimestamp",
+  odometerReadingUnits: "test",
+  testEndTimestamp: "test",
+  testStatus: "testStatus",
+  testTypes: [],
+  vehicleClass: {},
+  vin: "vin",
+  testStationName: "test",
+  vehicleType: "test",
+  countryOfRegistration: "test",
+  preparerId: "test",
+  preparerName: "test",
+  odometerReading: 1,
+  vehicleConfiguration: "test",
+  testStationType: "test",
+  reasonForCancellation: null,
+  testerName: "test",
+  testStationPNumber: "test",
+  noOfAxles: 1,
+  testerEmailAddress: "test",
+  euVehicleCategory: "test",
+}
 
 export default {
   title: 'Test Record Summary',
   component: TestRecordSummaryComponent
 } as Meta;
 
-export const Primary: Story = () => ({
+export const NoRecords: Story = () => ({
   props: {}
+});
+
+export const Record: Story = () => ({
+  props: {testRecords: [fakeRecord]}
+});
+
+export const Records: Story = () => ({
+  props: {testRecords: [fakeRecord, {...fakeRecord, testStartTimestamp: "2022-02-02:13.23", vin: "xthoeu213", testStatus: "PASSED"}]}
 });
