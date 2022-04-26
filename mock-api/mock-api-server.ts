d7b573a3c944cf925bf7b71cadf5aea980661286
Thomas.Webb <SA_Thomas.Webb@dvsatest-cloud.uk>
2022-04-26 12:22:15 +0100

    fix(cb2-3291): extended mock api to allow basic error calls

--- mock-api/mock-api-server.ts
+++ mock-api/mock-api-server.ts
@@ -1,19 +1,50 @@
 import * as jsonServer from 'json-server';
 import { mockTestResultList } from '../src/mocks/mock-test-result';
-import { mockVehicleTecnicalRecordList } from '../src/mocks/mock-vehicle-technical-record.mock';
+import { mockVehicleTechnicalRecordList } from '../src/mocks/mock-vehicle-technical-record.mock';
 const server = jsonServer.create();
 const router = jsonServer.router('{}');
 const middlewares = jsonServer.defaults();
+
+const args = require('minimist')(process.argv.slice(2));
 
 server.use(middlewares);
 
 // Add custom routes before JSON Server router
 server.get('/vehicles/*', (req, res) => {
-  res.jsonp(mockVehicleTecnicalRecordList());
+  switch (args['tech-record']) {
+    case 'NotFound':
+      res.status(404);
+      res.statusMessage = 'NotFound';
+      res.jsonp('Error no vehicle found');
+      break;
+    case 'ServiceError':
+      res.status(500);
+      res.statusMessage = 'Unavailable';
+      res.jsonp('Error service unavailable');
+      break;
+    default:
+      res.jsonp(mockVehicleTechnicalRecordList());
+      break;
+  };
 });
 
 server.get('/test-results/:systemId', (req, res) => {
-  res.jsonp(mockTestResultList());
+
+  switch (args['test-result']) {
+    case 'NotFound':
+      res.status(404);
+      res.statusMessage = 'NotFound';
+      res.jsonp('Error no test records found');
+      break;
+    case 'ServiceError':
+      res.status(500);
+      res.statusMessage = 'Unavailable';
+      res.jsonp('Error service unavailable');
+      break;
+    default:
+      res.jsonp(mockTestResultList());
+      break;
+  }
 });
 
 server.use(router);
