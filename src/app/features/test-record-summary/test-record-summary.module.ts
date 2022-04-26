9c5555de509ff516347ca37aedab6a18949044a4
nbrown <Nathan.Brown@frasers.group>
2022-04-26 10:17:11 +0100

    fix(cb2-3291): Remove un-needed module file

--- src/app/features/test-record-summary/test-record-summary.module.ts
+++
@@ -1,9 +0,0 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { SharedModule } from '@shared/shared.module';
-
-@NgModule({
-    imports: [SharedModule]
-})
-
-export class TestRecordSummaryModule {}
