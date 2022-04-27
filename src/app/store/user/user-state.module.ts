1ac8693a0484239791e606c2d1d80fc090dc2578
Mauricio Cadenas <mauricio.cadenas@bjss.com>
2022-04-27 10:57:58 +0100

    fix(cb2-3590): Sync user state with local storage

--- src/app/store/user/user-state.module.ts
+++ src/app/store/user/user-state.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StoreModule } from '@ngrx/store';
+import { NgModule } from '@angular/core';
+import { ActionReducer, MetaReducer, StoreModule } from '@ngrx/store';
+import { localStorageSync } from 'ngrx-store-localstorage';
 import { STORE_FEATURE_USER_KEY, userServiceReducer } from './user-service.reducer';
+
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({ keys: ['username'], rehydrate: true })(reducer);
+}
+
+const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, StoreModule.forFeature(STORE_FEATURE_USER_KEY, userServiceReducer)]
+  imports: [CommonModule, StoreModule.forFeature(STORE_FEATURE_USER_KEY, userServiceReducer, { metaReducers })]
 })
 export class UserStateModule {}
