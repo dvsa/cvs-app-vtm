0f67de84c2ca5f1b8e3291ab67f602503656ced6
Nathan Brown <nathan.brown@bjss.com>
2022-04-28 14:00:46 +0100

    feat(cb2-2890): updated mocks to be able to return types of vehicles

--- mock-api/mock-api-server.ts
+++ mock-api/mock-api-server.ts
@@ -1,3 +1,4 @@
+import { VehicleTypes } from '../src/app/models/vehicle-tech-record.model';
 import * as jsonServer from 'json-server';
 import { mockTestResultList } from '../src/mocks/mock-test-result';
 import { mockVehicleTechnicalRecordList } from '../src/mocks/mock-vehicle-technical-record.mock';
@@ -8,8 +9,14 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 
 // Add custom routes before JSON Server router
-server.get('/vehicles/*', (req, res) => {
-  res.jsonp(mockVehicleTechnicalRecordList());
+server.get('/vehicles/:vin/*', (req, res) => {
+  if (req.params['vin'] === "12345") {
+    res.jsonp(mockVehicleTechnicalRecordList(VehicleTypes.HGV));
+  } else if (req.params['vin'] === "78910") {
+    res.jsonp(mockVehicleTechnicalRecordList(VehicleTypes.TRL));
+  } else {
+    res.jsonp(mockVehicleTechnicalRecordList());
+  }
 });
 
 server.get('/test-results/:systemId', (req, res) => {
--- src/mocks/mock-vehicle-technical-record.mock.ts
+++ src/mocks/mock-vehicle-technical-record.mock.ts
@@ -1,7 +1,18 @@
 import { VehicleTechRecordModel, VehicleSizes, EuVehicleCategories, FrameDescriptions, VehicleConfigurations, FuelTypes, VehicleTypes, StatusCodes} from '../app/models/vehicle-tech-record.model';
 import { createMock, createMockList } from 'ts-auto-mock';
 
-export const mockVehicleTechnicalRecord = (systemNumber: number = 0) =>
+export const mockVehicleTechnicalRecord = (vehicleType: VehicleTypes = VehicleTypes.PSV, systemNumber: number = 0) => {
+  switch (vehicleType) {
+    case VehicleTypes.HGV:
+      return createMockHgv(systemNumber);
+    case VehicleTypes.TRL:
+      return createMockTrl(systemNumber);
+    default:
+      return createMockPsv(systemNumber);
+  };
+};
+
+const createMockPsv = (systemNumber: number): VehicleTechRecordModel => 
   createMock<VehicleTechRecordModel>({
     systemNumber: `SYS${String(systemNumber + 1).padStart(4, '0')}`,
     vin: `XMGDE02FS0H0${12344 + systemNumber + 1}`,
@@ -42,4 +53,80 @@ export const mockVehicleTechnicalRecord = (systemNumber: number = 0) =>
     }]
   });
 
-export const mockVehicleTechnicalRecordList = (items: number = 1) => createMockList<VehicleTechRecordModel>(items, (systemNumber) => mockVehicleTechnicalRecord(systemNumber));
+  const createMockHgv = (systemNumber: number): VehicleTechRecordModel => 
+  createMock<VehicleTechRecordModel>({
+    systemNumber: `SYS${String(systemNumber + 1).padStart(4, '0')}`,
+    vin: `XMGDE02FS0H0${12344 + systemNumber + 1}`,
+
+    primaryVrm: `KP${String(systemNumber + 1).padStart(2, '0')} ABC`,
+    secondaryVrms: [
+      {
+        vrm: '609859Z',
+      }
+    ],
+    techRecord: [{
+      statusCode: StatusCodes.CURRENT,
+      vehicleType: VehicleTypes.HGV,
+      regnDate: "1234",
+      manufactureYear: 2022,
+      noOfAxles: 2,
+      dtpNumber: "1234",
+      axles: {
+        parkingBrakeMrk: true
+      },
+      speedLimiterMrk: true,
+      tachoExemptMrk: true,
+      euroStandard: "123",
+      roadFriendly: true,
+      fuelpropulsionsystem: FuelTypes.HYBRID,
+      drawbarCouplingFitted: true,
+      vehicleClass: {
+        description: "Description",
+      },
+      vehicleConfiguration: VehicleConfigurations.ARTICULATED,
+      offRoad: true,
+      euVehicleCategory: EuVehicleCategories.M1,
+      emissionsLimit: 1234,
+      departmentalVehicleMarker: true,
+    }]
+  });
+
+  const createMockTrl = (systemNumber: number): VehicleTechRecordModel => 
+  createMock<VehicleTechRecordModel>({
+    systemNumber: `SYS${String(systemNumber + 1).padStart(4, '0')}`,
+    vin: `XMGDE02FS0H0${12344 + systemNumber + 1}`,
+
+    primaryVrm: `KP${String(systemNumber + 1).padStart(2, '0')} ABC`,
+    secondaryVrms: [
+      {
+        vrm: '609859Z',
+      }
+    ],
+    techRecord: [{
+      statusCode: StatusCodes.CURRENT,
+      vehicleType: VehicleTypes.TRL,
+      regnDate: "1234",
+      firstUseDate: "1234",
+      manufactureYear: 2022,
+      noOfAxles: 2,
+      dtpNumber: "1234",
+      axles: {
+        parkingBrakeMrk: true
+      },
+      suspensionType: "1",
+      roadFriendly: true,
+      drawbarCouplingFitted: true,
+      vehicleClass: {
+        description: "Description",
+      },
+      vehicleConfiguration: VehicleConfigurations.ARTICULATED,
+      couplingType: "1",
+      maxLoadOnCoupling: 1234,
+      frameDescription: FrameDescriptions.FRAME_SECTION,
+      euVehicleCategory: EuVehicleCategories.M1,
+      departmentalVehicleMarker: true,
+    }]
+  });
+
+
+export const mockVehicleTechnicalRecordList = (vehicleType: VehicleTypes = VehicleTypes.PSV, items: number = 1) => createMockList<VehicleTechRecordModel>(items, (systemNumber) => mockVehicleTechnicalRecord(vehicleType, systemNumber));
