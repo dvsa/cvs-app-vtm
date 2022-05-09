afd18782de11cb55ca7d6830da43759c08935ba8
Nathan Brown <nathan.brown@bjss.com>
2022-05-09 10:04:46 +0100

    fix(cb2-3775): Remove the label to stop duplicate titles showing

--- src/app/forms/templates/trl/trl-tech-record.template.ts
+++ src/app/forms/templates/trl/trl-tech-record.template.ts
@@ -2,7 +2,6 @@ import { FormNode, FormNodeTypes, FormNodeViewTypes } from '../../services/dynam
 
 export const TrlTechRecord: FormNode = {
   name: 'techRecordSummary',
-  label: 'Summary',
   type: FormNodeTypes.GROUP,
   children: [
     {
