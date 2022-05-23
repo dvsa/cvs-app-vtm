3f961bb72704e3324ff24db1def08ac6484516cc
Nathan Brown <nathan.brown@bjss.com>
2022-05-23 14:33:34 +0100

    feat(cb2-3355): create a new selector

--- src/app/store/technical-records/selectors/technical-record-service.selectors.ts
+++ src/app/store/technical-records/selectors/technical-record-service.selectors.ts
@@ -1,5 +1,10 @@
 import { createSelector } from '@ngrx/store';
+import { selectRouteNestedParams } from '@store/router/selectors/router.selectors';
 import { getVehicleTechRecordState } from '../reducers/technical-record-service.reducer';
 
 export const vehicleTechRecords = createSelector(getVehicleTechRecordState, (state) => state.vehicleTechRecords);
 export const technicalRecordsLoadingState = createSelector(getVehicleTechRecordState, (state) => state.loading);
+export const selectVehicleTechnicalRecordByVin = createSelector(vehicleTechRecords, selectRouteNestedParams, (state, { vin }) => {
+    return state.find((record) => record.vin === vin)
+});
+
