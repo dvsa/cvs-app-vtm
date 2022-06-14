bb7234bb4aa4231be5f3fbe13fbea57d0e3ec8b8
Aaron Downing <aaron.downing@bjss.com>
2022-06-14 15:42:13 +0100

    feat(cb2-2891): add dynamic routing based on search criteria

--- src/app/store/technical-records/reducers/technical-record-service.reducer.ts
+++ src/app/store/technical-records/reducers/technical-record-service.reducer.ts
@@ -1,6 +1,6 @@
 import { createFeatureSelector, createReducer, on } from '@ngrx/store';
 import { VehicleTechRecordModel } from 'src/app/models/vehicle-tech-record.model';
-import { getByVIN, getByVINFailure, getByVINSuccess } from '../actions/technical-record-service.actions';
+import { getByPartialVIN, getByPartialVINFailure, getByPartialVINSuccess, getByVIN, getByVINFailure, getByVINSuccess } from '../actions/technical-record-service.actions';
 
 export const STORE_FEATURE_TECHNICAL_RECORDS_KEY = 'TechnicalRecords';
 
@@ -20,5 +20,7 @@ export const vehicleTechRecordReducer = createReducer(
   initialState,
   on(getByVIN, (state) => ({ ...state, vehicleTechRecords: [], loading: true })),
   on(getByVINSuccess, (state, { vehicleTechRecords }) => ({ ...state, vehicleTechRecords, loading: false })),
-  on(getByVINFailure, (state, { error }) => ({ ...state, vehicleTechRecords: [], error, loading: false }))
-);
+  on(getByVINFailure, (state, { error }) => ({ ...state, vehicleTechRecords: [], error, loading: false })),
+  on(getByPartialVIN, (state) => ({ ...state, vehicleTechRecords: [], loading: true })),
+  on(getByPartialVINSuccess, (state, { vehicleTechRecords }) => ({ ...state, vehicleTechRecords, loading: false })),
+  on(getByPartialVINFailure, (state, { error }) => ({ ...state, vehicleTechRecords: [], error, loading: false })),);
