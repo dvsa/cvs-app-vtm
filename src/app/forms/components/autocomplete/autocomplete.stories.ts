98f94da05cf137efc39fa2cf73328211ae730e87
kpearmanbjss <95082261+kpearmanbjss@users.noreply.github.com>
2022-06-14 14:08:21 +0100

    Feat/cb2 4035 auto complete dropdown (#346)
    * Revert "fix(cb2-4099): set default locale for the application (#337)"
    
    This reverts commit c329945241ea44abe4df7b9f397a6babffc4a0e0.
    
    * feat(cb2-4035): added auto complete dropdown component
    
    * feat(cb2-4035): added auto complete dropdown component
    
    * feat(cb2-4035): renamed to autocomplete and added hint
    
    * feat(cb2-4035): renamed to autocomplete and added hint
    
    * feat(cb2-4035): undone accidental changes made to app module
    
    * feat(cb2-4035): reverted changes in home module that were for test purposes

---
+++ src/app/forms/components/autocomplete/autocomplete.stories.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { AutocompleteComponent } from './autocomplete.component';
+
+export default {
+  title: 'Forms/Autocomplete',
+  component: AutocompleteComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [AutocompleteComponent],
+      imports: [CommonModule, FormsModule, ReactiveFormsModule]
+    })
+  ]
+} as Meta;
+
+const Template: Story = (args) => {
+  const { id, label, options, name, hint, value = null, disabled = false, validators = [] } = args;
+  const form = new FormGroup({ [name]: new FormControl({ value, disabled }, validators) });
+  return {
+    component: AutocompleteComponent,
+    template: `<form [formGroup]="form"><app-autocomplete [label]="label" [id]="id" [name]="name" [options]="options" [formControlName]="name">  </app-autocomplete></form>`,
+    props: {
+        id,
+        label,
+        hint,
+        name,
+        options
+      }
+    }
+  };
+
+  const defaultArgs = {
+    label: 'Autocomplete',
+    hint: 'Type any letter to start searching',
+    name: 'name',
+    id: 'name' + '-wrapper',
+    options: ['red', 'yellow', 'blue','green']
+    ,
+  };
+
+  export const Enabled = Template.bind({});
+    Enabled.args = {
+  ...defaultArgs
+};
