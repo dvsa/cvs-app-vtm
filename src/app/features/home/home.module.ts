b5dd7327c512e5091e17fc78f602ec0995e84638
kpearmanbjss <kris.pearman@bjss.com>
2022-06-14 14:05:33 +0100

    feat(cb2-4035): reverted changes in home module that were for test purposes

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,13 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DynamicFormsModule } from '@forms/dynamic-forms.module';
 import { HomeButtonComponent } from './components/home-button/home-button.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent, HomeButtonComponent],
-  imports: [CommonModule, HomeRoutingModule, DynamicFormsModule, FormsModule, ReactiveFormsModule]
+  imports: [CommonModule, HomeRoutingModule]
 })
 export class HomeModule {}
