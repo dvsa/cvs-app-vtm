4a5919af9e76462ac90051c678b00ce41367c62f
Aaron Downing <aaron.downing@bjss.com>
2022-06-15 15:40:30 +0100

    feat(cb2-2891): refactor effects

--- src/app/store/technical-records/effects/technical-record-service.effects.ts
+++ src/app/store/technical-records/effects/technical-record-service.effects.ts
@@ -5,55 +5,43 @@ import { of } from 'rxjs';
 import { catchError, map, mergeMap } from 'rxjs/operators';
 import { getByPartialVIN, getByPartialVINFailure, getByPartialVINSuccess, getByVIN, getByVINFailure, getByVINSuccess } from '../actions/technical-record-service.actions';
 
+function getErrorMessage(error: any, search: string): string {
+  let message = error;
+  if (typeof error === 'object') {
+    if (error.status === 404) {
+      message = 'Vehicle not found, check the vehicle registration mark, trailer ID or vehicle identification number';
+    } else {
+      message = `There was a problem getting the Tech Record by ${search}`;
+    }
+  }
+  return message
+}
+
 @Injectable()
 export class TechnicalRecordServiceEffects {
-  getByVin$ = createEffect(() =>
+  getTechnicalRecord$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(getByVIN),
-      mergeMap((action) =>
-        this.technicalRecordService.getByVIN(action.vin).pipe(
-          map((vehicleTechRecords) => getByVINSuccess({ vehicleTechRecords })),
-          catchError((error) => {
-            let message = error;
-
-            if (typeof error === 'object') {
-              if (error.status === 404) {
-                message = 'Vehicle not found, check the vehicle registration mark, trailer ID or vehicle identification number';
-              } else {
-                message = 'There was a problem getting the Tech Record by VIN';
-              }
-            }
-
-            return of(getByVINFailure({ error: message, anchorLink: 'search-term' }));
-          })
-        )
-      )
+      ofType(getByVIN, getByPartialVIN),
+      mergeMap((action) => {
+        switch (action.type) {
+          case (getByVIN.type):
+            return this.technicalRecordService.getByVIN(action.vin).pipe(
+              map((vehicleTechRecords) => getByVINSuccess({ vehicleTechRecords })),
+              catchError((error) => {
+                return of(getByVINFailure({ error: getErrorMessage(error, 'vin'), anchorLink: 'search-term' }));
+              })
+            )
+          case (getByPartialVIN.type): 
+            return this.technicalRecordService.getByPartialVIN(action.partialVin).pipe(
+              map(vehicleTechRecords => getByPartialVINSuccess({ vehicleTechRecords })),
+              catchError((error) => {
+                return of(getByPartialVINFailure({ error: getErrorMessage(error, 'partialVin'), anchorLink: 'search-term'}))
+              })
+            )
+        }
+      })
     )
   );
-
-  getByPartialVIN$ = createEffect(() => 
-    this.actions$.pipe(
-      ofType(getByPartialVIN),
-      mergeMap((action) => 
-        this.technicalRecordService.getByPartialVIN(action.partialVin).pipe(
-          map(vehicleTechRecords => getByPartialVINSuccess({ vehicleTechRecords })),
-          catchError((error) => {
-            let message = error;
-
-            if (typeof error === 'object') {
-              if (error.status === 404) {
-                message = 'Vehicle not found, check the vehicle registration mark, trailer ID or vehicle identification number';
-              } else {
-                message = 'There was a problem getting the Tech Record by partialVIN';
-              }
-            }
-
-            return of(getByPartialVINFailure({ error: message, anchorLink: 'search-term'}))
-          })
-        )
-      )
-    )
-  )
 
   constructor(private actions$: Actions, private technicalRecordService: TechnicalRecordService) {}
 }
