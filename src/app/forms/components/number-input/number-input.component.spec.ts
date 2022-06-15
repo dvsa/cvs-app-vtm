b1fee6dde13920ef745e51991130d10a0820687e
Guillaume Julien <guillaume.julien@bjss.com>
2022-06-15 12:01:53 +0100

    feat(cb2-4224): fix unit tests

--- src/app/forms/components/number-input/number-input.component.spec.ts
+++ src/app/forms/components/number-input/number-input.component.spec.ts
@@ -1,20 +1,34 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormGroup } from '@angular/forms';
+import { CustomFormControl, FormNodeTypes } from '../../services/dynamic-form.types';
 import { NumberInputComponent } from './number-input.component';
 
+@Component({
+  selector: 'app-host-component',
+  template: `<form [formGroup]="form">
+    <app-text-input name="foo" formControlName="foo"></app-text-input>
+  </form> `,
+  styles: []
+})
+class HostComponent {
+  form = new FormGroup({
+    foo: new CustomFormControl({ name: 'foo', type: FormNodeTypes.CONTROL, children: [] }, '')
+  });
+}
+
 describe('NumberInputComponent', () => {
-  let component: NumberInputComponent;
-  let fixture: ComponentFixture<NumberInputComponent>;
+  let component: HostComponent;
+  let fixture: ComponentFixture<HostComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NumberInputComponent ]
-    })
-    .compileComponents();
+      declarations: [NumberInputComponent]
+    }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NumberInputComponent);
+    fixture = TestBed.createComponent(HostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
