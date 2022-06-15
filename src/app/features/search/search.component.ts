c4d8f4d3e109394412739e1e0a62a74655665a0e
Taha Yazici <taha.yazici@bjss.com>
2022-06-15 15:29:53 +0100

    feat(cb2-4186): design a page to display search results and redesign the errors on the search page

--- src/app/features/search/search.component.ts
+++ src/app/features/search/search.component.ts
@@ -1,81 +1,50 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
-import { typeAnnotation } from '@babel/types';
+import { GlobalError } from '@core/components/global-error/global-error.interface';
 import { GlobalErrorService } from '@core/components/global-error/global-error.service';
-import { VehicleTechRecordModel } from '@models/vehicle-tech-record.model';
-import { select, Store } from '@ngrx/store';
-import { SEARCH_TYPES, TechnicalRecordService } from '@services/technical-record/technical-record.service';
-import { selectQueryParams } from '@store/router/selectors/router.selectors';
-import { map, Observable, Subject, takeUntil } from 'rxjs';
+import { SEARCH_TYPES } from '@services/technical-record/technical-record.service';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html'
 })
-export class SearchComponent implements OnDestroy {
-  vehicleTechRecords$: Observable<Array<VehicleTechRecordModel>>;
-  ngDestroy$ = new Subject();
+export class SearchComponent {
   searchErrorMessage = 'You must provide a vehicle registration mark, trailer ID or vehicle identification number.';
   missingCriteriaErrorMessage = 'You must select a valid search criteria';
 
-  constructor(private technicalRecordService: TechnicalRecordService, public globalErrorService: GlobalErrorService, private store: Store, private router: Router) {
-    this.store.pipe(select(selectQueryParams), takeUntil(this.ngDestroy$)).subscribe((params) => {
-      let type = Object.keys(params)[0];
-      let searchTerm = params[type] as string;
-
-      if (searchTerm && Object.values(SEARCH_TYPES).includes(type as SEARCH_TYPES) && Object.keys(params).length === 1) {
-        this.searchTechRecords(searchTerm, type as SEARCH_TYPES);
-      }
-    });
-
-    this.vehicleTechRecords$ = this.technicalRecordService.vehicleTechRecords$;
-  }
-
-  public searchTechRecords(searchTerm: string, type: SEARCH_TYPES) {
-    this.globalErrorService.clearError();
-
-    searchTerm = searchTerm.trim();
-
-    if (searchTerm) {
-      if(type) {
-        this.technicalRecordService.searchBy({ type, searchTerm });
-      }
-      else {
-        this.globalErrorService.addError({ error: this.missingCriteriaErrorMessage, anchorLink: 'search-term' });
-      }
-    } else {
-      this.globalErrorService.addError({ error: this.searchErrorMessage, anchorLink: 'search-term' });
-    }
-  }
-
-  public getInlineErrorMessage(): Observable<number> {
-    return this.globalErrorService.errors$.pipe(map((errors) => errors.length));
-  }
+  constructor(public globalErrorService: GlobalErrorService, private router: Router) {}
 
   public navigateSearch(term: string, type: string): void {
+    this.globalErrorService.clearError();
+
+    term = term.trim();
+
     let extras: NavigationExtras;
     if (term) {
-      switch(type){
-        case(SEARCH_TYPES.VIN):
-          extras = { queryParams: { vin: term} };
-          this.router.navigate(['/search'], extras);
+      switch (type) {
+        case SEARCH_TYPES.VIN:
+          extras = { queryParams: { vin: term } };
           break;
-        case(SEARCH_TYPES.PARTIAL_VIN):
+        case SEARCH_TYPES.PARTIAL_VIN:
           extras = { queryParams: { partialVin: term } };
-          this.router.navigate(['/search'], extras);
           break;
         default:
-          this.globalErrorService.clearError();
-          this.globalErrorService.addError({ error: this.missingCriteriaErrorMessage, anchorLink: 'search-term' });
+          this.globalErrorService.addError({ error: this.missingCriteriaErrorMessage, anchorLink: 'search-type' });
+          return;
       }
+
+      this.router.navigate(['/search/results'], extras);
     } else {
-      this.globalErrorService.clearError();
       this.globalErrorService.addError({ error: this.searchErrorMessage, anchorLink: 'search-term' });
     }
   }
 
-  ngOnDestroy() {
-    this.ngDestroy$.next(true);
-    this.ngDestroy$.complete();
+  getInlineErrorMessage(name: string): Observable<boolean> {
+    return this.globalErrorService.errors$.pipe(map((errors) => errors.some((error) => error.anchorLink === name)));
+  }
+
+  getErrorByName(errors: GlobalError[], name: string): GlobalError | undefined {
+    return errors.find((error) => error.anchorLink === name);
   }
 }
