c79317ebc3dd41b73be163899d63a541f06a8d4b
Taha Yazici <taha.yazici@bjss.com>
2022-06-15 15:29:53 +0100

    feat(cb2-4186): design a page to display search results and redesign the errors on the search page

--- src/app/features/search/search.component.spec.ts
+++ src/app/features/search/search.component.spec.ts
@@ -1,22 +1,16 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { Params, Router } from '@angular/router';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { GlobalErrorService } from '@core/components/global-error/global-error.service';
-import { DefaultProjectorFn, MemoizedSelector } from '@ngrx/store';
-import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 import { SEARCH_TYPES, TechnicalRecordService } from '@services/technical-record/technical-record.service';
-import { initialAppState, State } from '@store/.';
-import { selectQueryParams } from '@store/router/selectors/router.selectors';
+import { initialAppState } from '@store/.';
 import { SearchComponent } from './search.component';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
-  let technicalRecordService: TechnicalRecordService;
-  let searchBySpy = jest.fn();
-  let store: MockStore<State>;
-  let mockSelectQueryParams: MemoizedSelector<any, Params, DefaultProjectorFn<Params>>;
   let router: Router;
   let globalErrorService: GlobalErrorService;
 
@@ -37,10 +31,6 @@ describe('SearchComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
-    store = TestBed.inject(MockStore);
-    mockSelectQueryParams = store.overrideSelector(selectQueryParams, {});
-    technicalRecordService = TestBed.inject(TechnicalRecordService);
-    technicalRecordService.searchBy = searchBySpy;
     router = TestBed.inject(Router);
     globalErrorService = TestBed.inject(GlobalErrorService);
 
@@ -52,56 +42,6 @@ describe('SearchComponent', () => {
   });
 
   describe('searching', () => {
-    describe('manually', () => {
-      beforeEach(() => {
-        fixture.detectChanges();
-      });
-
-      it('should call the service to search by VIN', () => {
-        const searchParams = { searchTerm: 'A_VIN_', type: SEARCH_TYPES.VIN };
-        component.searchTechRecords(searchParams.searchTerm, searchParams.type);
-
-        expect(searchBySpy).toBeCalledWith(searchParams);
-      });
-
-      it('should call the service to search by partialVin', () => {
-        const searchParams = { searchTerm: 'A_PARTIAL_VIN_', type: SEARCH_TYPES.PARTIAL_VIN};
-        component.searchTechRecords(searchParams.searchTerm, searchParams.type)
-
-        expect(searchBySpy).toBeCalledWith(searchParams);
-      })
-
-      it('should not call service', () => {
-        component.searchTechRecords('', SEARCH_TYPES.VIN);
-        fixture.detectChanges();
-        expect(searchBySpy).not.toHaveBeenCalled();
-      });
-    });
-
-    describe('by URL params', () => {
-      it('should call the service to search by vin with "someVin"', fakeAsync(() => {
-        const vin = 'someVin';
-        mockSelectQueryParams.setResult({ vin: vin });
-        store.refreshState();
-
-        tick();
-        fixture.detectChanges();
-
-        expect(searchBySpy).toHaveBeenCalledWith({ searchTerm: vin, type: 'vin'});
-      }));
-
-      it('should call the service to search by partialVin with "somePartialVin"', fakeAsync(() => {
-        const partialVin = 'somePartialVin';
-        mockSelectQueryParams.setResult({ partialVin: partialVin });
-        store.refreshState();
-
-        tick();
-        fixture.detectChanges();
-
-        expect(searchBySpy).toHaveBeenCalledWith({ searchTerm: partialVin, type: 'partialVin'});
-      }))
-    });
-
     describe('navigateSearch', () => {
       it('should navigate to vin search result', () => {
         const navigateSpy = jest.spyOn(router, 'navigate').mockImplementation(() => Promise.resolve(true));
@@ -109,7 +49,7 @@ describe('SearchComponent', () => {
         const vin = 'someVin';
         component.navigateSearch(vin, SEARCH_TYPES.VIN);
 
-        expect(navigateSpy).toHaveBeenCalledWith(['/search'], { queryParams: { vin: 'someVin'} });
+        expect(navigateSpy).toHaveBeenCalledWith(['/search/results'], { queryParams: { vin: 'someVin' } });
       });
 
       it('should navigate to partialVin search result', () => {
@@ -118,8 +58,8 @@ describe('SearchComponent', () => {
         const partialVin = 'somePartialVin';
         component.navigateSearch(partialVin, SEARCH_TYPES.PARTIAL_VIN);
 
-        expect(navigateSpy).toHaveBeenCalledWith(['/search'], { queryParams: { partialVin: 'somePartialVin'} });
-      })
+        expect(navigateSpy).toHaveBeenCalledWith(['/search/results'], { queryParams: { partialVin: 'somePartialVin' } });
+      });
 
       it('should add error', () => {
         const addErrorSpy = jest.spyOn(globalErrorService, 'addError').mockImplementation(() => {});
--- src/app/features/search/search.module.ts
+++ src/app/features/search/search.module.ts
@@ -6,9 +6,10 @@ import { SharedModule } from '@shared/shared.module';
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchComponent } from './search.component';
 import { SingleSearchResultComponent } from './single-search-result/single-search-result.component';
+import { MultipleSearchResultsComponent } from './multiple-search-results/multiple-search-results.component';
 
 @NgModule({
-  declarations: [SearchComponent, SingleSearchResultComponent],
+  declarations: [SearchComponent, SingleSearchResultComponent, MultipleSearchResultsComponent],
   imports: [CommonModule, DynamicFormsModule, RouterModule, SearchRoutingModule, SharedModule]
 })
 export class SearchModule {}
