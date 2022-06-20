a7e1bfa5cf3889108a2ddd44fd2be39b4973ca9f
Guillaume Julien <guillaume.julien@bjss.com>
2022-06-21 00:40:41 +0100

    feat(cb2-3998): typo

--- src/app/forms/utils/error-message-map.ts
+++ src/app/forms/utils/error-message-map.ts
@@ -1,8 +1,8 @@
 export const ErrorMessageMap: { [key: string]: any } = {
-  ['required']: (err: boolean, label: string) => `${label ?? 'This filed'} is required`,
-  ['pattern']: (err: boolean, label: string) => `${label ?? 'This filed'} must match a pattern`,
+  ['required']: (err: boolean, label: string) => `${label ?? 'This field'} is required`,
+  ['pattern']: (err: boolean, label: string) => `${label ?? 'This field'} must match a pattern`,
   ['customPattern']: (err: { message: string }, label: string) => `${label ?? 'This field'} ${err.message}`,
-  invalidDate: (err: boolean, label: string) => `${label ?? 'This filed'} is an invalid date`,
-  maxlength: (err: { requiredLength: number }, label: string) => `${label ?? 'This filed'} must be less than ${err.requiredLength} characters`,
-  minlength: (err: { requiredLength: number }, label: string) => `${label ?? 'This filed'} must be greater than ${err.requiredLength} characters`
+  invalidDate: (err: boolean, label: string) => `${label ?? 'This field'} is an invalid date`,
+  maxlength: (err: { requiredLength: number }, label: string) => `${label ?? 'This field'} must be less than ${err.requiredLength} characters`,
+  minlength: (err: { requiredLength: number }, label: string) => `${label ?? 'This field'} must be greater than ${err.requiredLength} characters`
 };
