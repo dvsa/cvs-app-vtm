f013cfb503aeb3b1baaa6569752fa1c43b7f6678
mcadenas-bjss <102959178+mcadenas-bjss@users.noreply.github.com>
2022-06-20 16:07:47 +0100

    Date component (#349)
    * date input component
    
    * review comments
    
    * unit tests
    
    * review comments

--- src/app/forms/dynamic-forms.module.ts
+++ src/app/forms/dynamic-forms.module.ts
@@ -6,6 +6,7 @@ import { SharedModule } from '@shared/shared.module';
 import { AutocompleteComponent } from './components/autocomplete/autocomplete.component';
 import { BaseControlComponent } from './components/base-control/base-control.component';
 import { CheckboxGroupComponent } from './components/checkbox-group/checkbox-group.component';
+import { DateComponent } from './components/date/date.component';
 import { DefectComponent } from './components/defect/defect.component';
 import { DefectsComponent } from './components/defects/defects.component';
 import { DynamicFormGroupComponent } from './components/dynamic-form-group/dynamic-form-group.component';
@@ -18,8 +19,8 @@ import { TextAreaComponent } from './components/text-area/text-area.component';
 import { NumberOnlyDirective } from './directives/app-number-only.directive';
 
 @NgModule({
-  declarations: [BaseControlComponent, TextInputComponent, ViewListItemComponent, DynamicFormGroupComponent, ViewCombinationComponent, CheckboxGroupComponent, RadioGroupComponent, DefectComponent, DefectsComponent, AutocompleteComponent, NumberInputComponent, TextAreaComponent, NumberOnlyDirective],
+  declarations: [BaseControlComponent, TextInputComponent, ViewListItemComponent, DynamicFormGroupComponent, ViewCombinationComponent, CheckboxGroupComponent, RadioGroupComponent, DefectComponent, DefectsComponent, AutocompleteComponent, NumberInputComponent, TextAreaComponent, NumberOnlyDirective, DateComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule, SharedModule, RouterModule],
-  exports: [TextInputComponent, ViewListItemComponent, DynamicFormGroupComponent, ViewCombinationComponent, CheckboxGroupComponent, RadioGroupComponent, DefectComponent, DefectsComponent, AutocompleteComponent, NumberInputComponent, TextAreaComponent]
+  exports: [TextInputComponent, ViewListItemComponent, DynamicFormGroupComponent, ViewCombinationComponent, CheckboxGroupComponent, RadioGroupComponent, DefectComponent, DefectsComponent, AutocompleteComponent, NumberInputComponent, TextAreaComponent, DateComponent]
 })
 export class DynamicFormsModule {}
