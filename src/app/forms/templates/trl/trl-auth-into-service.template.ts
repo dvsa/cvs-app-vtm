18201f7dbd6724e3b01cdb6c6dbd1e92146d7a83
Aaron Downing <aaron.downing@bjss.com>
2022-07-01 11:06:07 +0100

    chore(cb2-4170): change date formating

--- src/app/forms/templates/trl/trl-auth-into-service.template.ts
+++ src/app/forms/templates/trl/trl-auth-into-service.template.ts
@@ -13,27 +13,32 @@ export const TrlAuthIntoServiceTemplate: FormNode = {
         {
           name: 'cocIssueDate',
           label: 'COC issue date',
-          type: FormNodeTypes.CONTROL
+          type: FormNodeTypes.CONTROL,
+          viewType: FormNodeViewTypes.DATE
         },
         {
           name: 'dateReceived',
           label: 'Date received',
-          type: FormNodeTypes.CONTROL
+          type: FormNodeTypes.CONTROL,
+          viewType: FormNodeViewTypes.DATE
         },
         {
           name: 'datePending',
           label: 'Date pending',
-          type: FormNodeTypes.CONTROL
+          type: FormNodeTypes.CONTROL,
+          viewType: FormNodeViewTypes.DATE
         },
         {
           name: 'dateAuthorised',
           label: 'Date authorised',
-          type: FormNodeTypes.CONTROL
+          type: FormNodeTypes.CONTROL,
+          viewType: FormNodeViewTypes.DATE
         },
         {
           name: 'dateRejected',
           label: 'Date rejected',
-          type: FormNodeTypes.CONTROL
+          type: FormNodeTypes.CONTROL,
+          viewType: FormNodeViewTypes.DATE
         }
       ]
     }
