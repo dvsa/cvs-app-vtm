7d3b60fc75aee4d69cd87e0524b258d52e871ccf
Nathan Brown <nathan.brown@bjss.com>
2022-07-21 11:27:48 +0100

    feat(cb2-4690): test the new directive

--- src/app/directives/user-role.directive.spec.ts
+++ src/app/directives/user-role.directive.spec.ts
@@ -1,8 +1,52 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { provideMockStore } from '@ngrx/store/testing';
+import { UserService } from '@services/user-service/user-service';
+import { initialAppState } from '@store/.';
+import { of } from 'rxjs';
 import { UserRoleDirective } from './user-role.directive';
 
-// describe('UserDirectiveDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new UserRoleDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
+@Component({
+  template: `
+    <div id="displayBox" *appUserRole="['CVSFullAccess']">
+      <h1>This can display</h1>
+    </div>
+    <div id="hiddenBox" *appUserRole="['No Access']">
+      <h1>This cannot display</h1>
+    </div>
+    <div id="errorBox" *appUserRole="">
+      <h1>This is an error case</h1>
+    </div>
+  `
+})
+class TestComponent {}
+
+describe('UserDirectiveDirective', () => {
+  let fixture: ComponentFixture<TestComponent>;
+  let des;
+
+  beforeEach(async () => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [UserRoleDirective, TestComponent],
+      providers: [provideMockStore({ initialState: initialAppState }), { provide: UserService, useValue: { roles$: of(['CVSFullAccess']) } }]
+    }).createComponent(TestComponent);
+
+    fixture.detectChanges(); // initial binding
+  });
+
+  it('should display the element', () => {
+    const seenBox = fixture.debugElement.queryAll(By.css('#displayBox'));
+    expect(seenBox.length).toEqual(1);
+  });
+
+  it('should hide the element', () => {
+    const hiddenBox = fixture.debugElement.queryAll(By.css('#hiddenBox'));
+    expect(hiddenBox.length).toEqual(0);
+  });
+
+  it('should hide the element when nothing is provided', () => {
+    const errorBox = fixture.debugElement.queryAll(By.css('#errorBox'));
+    expect(errorBox.length).toEqual(0);
+  });
+});
