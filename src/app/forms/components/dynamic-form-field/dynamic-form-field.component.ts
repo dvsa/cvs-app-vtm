108963088bddb350c22a35e6395b5da539629004
Taha Yazici <taha.yazici@bjss.com>
2022-07-27 15:47:33 +0100

    feat(cb2-3950): abstract the source switching of multi select options into its own service

--- src/app/forms/components/dynamic-form-field/dynamic-form-field.component.ts
+++ src/app/forms/components/dynamic-form-field/dynamic-form-field.component.ts
@@ -2,25 +2,19 @@ import { KeyValue } from '@angular/common';
 import { AfterContentInit, Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { CustomFormControl, FormNodeEditTypes, FormNodeOption } from '@forms/services/dynamic-form.types';
-import { ReferenceDataResourceType } from '@models/reference-data.model';
-import { Store } from '@ngrx/store';
-import { ReferenceDataService } from '@services/reference-data/reference-data.service';
-import { TestStationsService } from '@services/test-stations/test-stations.service';
-import { fetchTestStations, TestStationsState } from '@store/test-stations';
+import { MultiOptionsService } from '@forms/services/multi-options.service';
 import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-dynamic-form-field',
-  templateUrl: './dynamic-form-field.component.html'
+  templateUrl: './dynamic-form-field.component.html',
+  providers: [MultiOptionsService]
 })
 export class DynamicFormFieldComponent implements AfterContentInit {
   @Input() control?: KeyValue<string, CustomFormControl>;
   @Input() form?: FormGroup;
 
-  constructor(
-    private referenceDataService: ReferenceDataService,
-    private store: Store<TestStationsState>,
-    private testStationsService: TestStationsService) {}
+  constructor(private optionsService: MultiOptionsService) {}
 
   get formNodeEditTypes(): typeof FormNodeEditTypes {
     return FormNodeEditTypes;
@@ -29,24 +23,16 @@ export class DynamicFormFieldComponent implements AfterContentInit {
   get options(): Observable<FormNodeOption<string | number | boolean>[]> {
     const meta = this.control?.value.meta
 
-    if (meta?.editType === FormNodeEditTypes.AUTOCOMPLETE && meta?.name.startsWith('testStation')) {
-      return this.testStationsService.getTestStationsOptions(meta.name);
-    } else if (meta?.referenceData) {
-      return this.referenceDataService.getReferenceDataOptions((this.control?.value.meta.referenceData ?? '') as ReferenceDataResourceType);
-    } else {
-      return of(this.control?.value.meta.options as FormNodeOption<string | number | boolean>[] ?? []);
-    }
+    return meta?.referenceData
+      ? this.optionsService.getOptions(meta.referenceData)
+      : of(meta?.options as FormNodeOption<string | number | boolean>[] ?? []);
   }
 
   ngAfterContentInit(): void {
-    const meta = this.control?.value.meta
+    const referenceData = this.control?.value.meta?.referenceData;
 
-    if (meta?.referenceData) {
-      this.referenceDataService.loadReferenceData(meta.referenceData);
-    }
-
-    if (meta?.editType === FormNodeEditTypes.AUTOCOMPLETE && meta?.name.startsWith('testStation')) {
-      this.store.dispatch(fetchTestStations());
+    if (referenceData) {
+      this.optionsService.loadOptions(referenceData);
     }
   }
 }
---
+++ src/app/forms/services/multi-options.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@angular/core";
+import { MultiOptions } from "@forms/models/options.model";
+import { ReferenceDataResourceType } from "@models/reference-data.model";
+import { Store } from "@ngrx/store";
+import { ReferenceDataService } from "@services/reference-data/reference-data.service";
+import { TestStationsService } from "@services/test-stations/test-stations.service";
+import { fetchTestStations, TestStationsState } from "@store/test-stations";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class MultiOptionsService {
+  constructor(private referenceDataService: ReferenceDataService, private store: Store<TestStationsState>, private testStationsService: TestStationsService) {}
+
+  getOptions(referenceData: ReferenceDataResourceType | SpecialRefData): Observable<MultiOptions> {
+    switch(referenceData) {
+      case SpecialRefData.TEST_STATION_NAME:
+      case SpecialRefData.TEST_STATION_P_NUMBER:
+        return  this.testStationsService.getTestStationsOptions(referenceData);
+      default:
+        return this.referenceDataService.getReferenceDataOptions((referenceData ?? '') as ReferenceDataResourceType);
+    }
+  }
+
+  loadOptions(referenceData: ReferenceDataResourceType | SpecialRefData): void {
+    switch(referenceData) {
+      case SpecialRefData.TEST_STATION_NAME:
+      case SpecialRefData.TEST_STATION_P_NUMBER:
+        this.store.dispatch(fetchTestStations());
+        break;
+      default:
+        this.referenceDataService.loadReferenceData(referenceData);
+        break;
+    }
+  }
+}
+
+export enum SpecialRefData {
+  TEST_STATION_NAME = 'testStationName',
+  TEST_STATION_P_NUMBER = 'testStationPNUmber'
+}
