775342f3afa8927c4a160288f14e360b79f4ba4c
tom-bjss <92319736+tom-bjss@users.noreply.github.com>
2022-08-01 16:55:36 +0100

    VTM Role Based Access (#433)
    * feat(cb2-4690): create frontend auth (#425)
    
    * feat(cb2-4690): Decode JWT and store it
    
    * feat(cb2-4690): create and add directive to tech record elements
    
    * feat(cb2-4690): repair tests
    
    * feat(cb2-4690): test the new directive
    
    * feat(cb2-4960): Create mock directive and add it to spec components
    
    * feat(cb2-4960): Create and add in new role based guard
    
    * feat(cb2-4960): create guard tests and update directive test
    
    * feat(cb2-4960): add in cvsfullaccess
    
    * Update src/app/directives/user-role.directive.ts
    
    Co-authored-by: Andrew Freemantle (BJSS) <87019731+andrewf-bjss@users.noreply.github.com>
    
    * feat(cb2-4960): PR Feedback
    
    * feat(cb2-4960): Lockfile V1
    
    * feat(cb2-4960): Update guard to remove race condition
    
    Co-authored-by: Andrew Freemantle (BJSS) <87019731+andrewf-bjss@users.noreply.github.com>
    
    * feat(cb2-4690): Update permissions
    
    * feat(cb2-4690): Update permissions again
    
    * Role Guard Fix (#432)
    
    * fix/role-guard
    
    Make the role guard wait for msal service to resolve tokens before checking roles.
    
    * fix: repaired tests
    
    Co-authored-by: Mauricio Cadenas <mauricio.cadenas@bjss.com>
    
    Co-authored-by: Nathan Brown <nathan.brown@bjss.com>
    Co-authored-by: Andrew Freemantle (BJSS) <87019731+andrewf-bjss@users.noreply.github.com>
    Co-authored-by: Mauricio Cadenas <mauricio.cadenas@bjss.com>

---
+++ src/app/directives/user-role.directive.spec.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { provideMockStore } from '@ngrx/store/testing';
+import { UserService } from '@services/user-service/user-service';
+import { initialAppState } from '@store/.';
+import { of } from 'rxjs';
+import { UserRoleDirective } from './user-role.directive';
+
+@Component({
+  template: `
+    <div id="displayBox" *appUserRole="['CVSFullAccess']">
+      <h1>This can display</h1>
+    </div>
+    <div id="hiddenBox" *appUserRole="['No Access']">
+      <h1>This cannot display</h1>
+    </div>
+  `
+})
+class TestComponent {}
+
+@Component({
+  template: `
+    <div id="errorBox" *appUserRole="broken">
+      <h1>This is an error case</h1>
+    </div>
+  `
+})
+class BrokenTestComponent {}
+
+describe('UserDirectiveDirective', () => {
+  let fixture: ComponentFixture<TestComponent>;
+  let brokenFixture: ComponentFixture<BrokenTestComponent>;
+
+  describe('User Directive Works', () => {
+    beforeEach(async () => {
+      fixture = TestBed.configureTestingModule({
+        declarations: [UserRoleDirective, TestComponent],
+        providers: [provideMockStore({ initialState: initialAppState }), { provide: UserService, useValue: { roles$: of(['CVSFullAccess']) } }]
+      }).createComponent(TestComponent);
+
+      fixture.detectChanges(); // initial binding
+    });
+
+    it('should display the element', () => {
+      const seenBox = fixture.debugElement.queryAll(By.css('#displayBox'));
+      expect(seenBox.length).toEqual(1);
+    });
+
+    it('should hide the element', () => {
+      const hiddenBox = fixture.debugElement.queryAll(By.css('#hiddenBox'));
+      expect(hiddenBox.length).toEqual(0);
+    });
+  });
+
+  describe('User Directive Errors', () => {
+    beforeEach(async () => {
+      brokenFixture = TestBed.configureTestingModule({
+        declarations: [UserRoleDirective, BrokenTestComponent],
+        providers: [provideMockStore({ initialState: initialAppState }), { provide: UserService, useValue: { roles$: of(['CVSFullAccess']) } }]
+      }).createComponent(BrokenTestComponent);
+    });
+
+    it('should error when an array is not provided', () => {
+      expect(() => {
+        brokenFixture.detectChanges();
+      }).toThrowError('Roles value is empty or missing');
+    });
+  });
+});
