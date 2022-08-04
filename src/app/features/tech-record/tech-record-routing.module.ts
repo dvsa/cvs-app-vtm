d74b8b7f691dbf482a8dc91bda17cb676f8cd36b
Thomas.Webb <SA_Thomas.Webb@dvsatest-cloud.uk>
2022-08-04 14:33:50 +0100

    feat(cb2-5106): added role guard to tech and test routing

--- src/app/features/tech-record/tech-record-routing.module.ts
+++ src/app/features/tech-record/tech-record-routing.module.ts
@@ -3,24 +3,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { MsalGuard } from '@azure/msal-angular';
 import { TechRecordViewResolver } from 'src/app/resolvers/tech-record-view/tech-record-view.resolver';
 import { TechRecordComponent } from './tech-record.component';
+import { RoleGuard } from '@guards/roles.guard';
+import { Roles } from '@models/roles.enum';
 
 const routes: Routes = [
   {
     path: '',
+    data: {roles: Roles.TechRecordView },
     component: TechRecordComponent,
-    canActivateChild: [MsalGuard],
+    canActivateChild: [MsalGuard, RoleGuard],
     resolve: { load: TechRecordViewResolver }
   },
   {
     path: ':techCreatedAt',
     component: TechRecordComponent,
     data: { title: 'Historic Tech Record' },
+    canActivate: [MsalGuard],
     resolve: { load: TechRecordViewResolver }
   },
   {
     path: 'test-records/:systemNumber/test-result/:testResultId/:testTypeId',
-    data: { title: 'Test Result' },
-    canActivate: [MsalGuard],
+    data: { title: 'Test Result', roles: Roles.TestRecordView },
+    canActivate: [MsalGuard, RoleGuard],
     loadChildren: () => import('../test-records/test-records.module').then(m => m.TestRecordsModule)
   }
 ];
--- src/app/features/test-records/test-records-routing.module.ts
+++ src/app/features/test-records/test-records-routing.module.ts
@@ -4,6 +4,8 @@ import { NoEditGuard } from '@guards/no-edit/no-edit.guard';
 import { TestResultResolver } from 'src/app/resolvers/test-result/test-result.resolver';
 import { AmendedTestRecordComponent } from './views/amended-test-record/amended-test-record.component';
 import { TestRecordComponent } from './views/test-record/test-record.component';
+import { RoleGuard } from '@guards/roles.guard';
+import { Roles } from '@models/roles.enum';
 
 const routes: Routes = [
   {
@@ -17,9 +19,9 @@ const routes: Routes = [
       {
         path: 'amended/:createdAt',
         component: AmendedTestRecordComponent,
-        data: { title: 'Amended Test Result' },
+        data: { title: 'Amended Test Result', roles: Roles.TestRecordUpdate},
         resolve: { load: TestResultResolver },
-        canActivate: [NoEditGuard]
+        canActivate: [NoEditGuard, RoleGuard]
       }
     ]
   }
