033be0d7a42d016258368f2da56205318679edcb
Guillaume Julien <guillaume.julien@bjss.com>
2022-08-11 16:21:05 +0100

    feat(cb2-4089): add test stations loading state

--- src/app/core/components/spinner/spinner.service.ts
+++ src/app/core/components/spinner/spinner.service.ts
@@ -4,28 +4,29 @@ import { getSpinner } from '@store/spinner/selectors/spinner.selectors';
 import { technicalRecordsLoadingState } from '@store/technical-records';
 import { testResultLoadingState } from '@store/test-records';
 import { selectTestTypesLoadingState } from '@store/test-types/selectors/test-types.selectors';
+import { testStationsLoadingState } from '@store/test-stations';
 import { combineLatest, map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpinnerService {
-  globalLoadingState$: Observable<boolean>;
-  testResultLoadingState$: Observable<boolean>;
-  techRecordsLoadingState$: Observable<boolean>;
-  testTypesLoadingState$: Observable<boolean>;
+  globalLoadingState$: Observable<boolean> = this.store.pipe(select(getSpinner));
+  testResultLoadingState$: Observable<boolean> = this.store.pipe(select(testResultLoadingState));
+  techRecordsLoadingState$: Observable<boolean> = this.store.pipe(select(technicalRecordsLoadingState));
+  testTypesLoadingState$: Observable<boolean> = this.store.pipe(select(selectTestTypesLoadingState));
+  testStationsLoadingState$: Observable<boolean> = this.store.pipe(select(testStationsLoadingState));
 
-  constructor(private store: Store) {
-    this.globalLoadingState$ = this.store.pipe(select(getSpinner));
-    this.testResultLoadingState$ = this.store.pipe(select(testResultLoadingState));
-    this.techRecordsLoadingState$ = this.store.pipe(select(technicalRecordsLoadingState));
-    this.testTypesLoadingState$ = this.store.pipe(select(selectTestTypesLoadingState));
-  }
+  constructor(private store: Store) {}
 
   private get reduceLoadingStates$() {
-    return combineLatest([this.globalLoadingState$, this.testResultLoadingState$, this.techRecordsLoadingState$, this.testTypesLoadingState$]).pipe(
-      map(states => states.some(b => b))
-    );
+    return combineLatest([
+      this.globalLoadingState$,
+      this.testResultLoadingState$,
+      this.techRecordsLoadingState$,
+      this.testTypesLoadingState$,
+      this.testStationsLoadingState$
+    ]).pipe(map(states => states.some(b => b)));
   }
 
   get showSpinner$(): Observable<boolean> {
