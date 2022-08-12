d3d3666e0d25955194bfecc8e267c37370793d4f
Guillaume Julien <guillaume.julien@bjss.com>
2022-08-12 11:08:22 +0100

    feat(cb2-4089): delete empty stylesheet

--- src/app/shared/components/icon/icon.component.ts
+++ src/app/shared/components/icon/icon.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-icon',
-  templateUrl: './icon.component.html',
-  styleUrls: ['./icon.component.scss']
+  templateUrl: './icon.component.html'
 })
 export class IconComponent {
   icon = '';
