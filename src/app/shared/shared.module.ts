d447bf3235be4c7e399c148f412e5265df52f98d
Mauricio Cadenas <mauricio.cadenas@bjss.com>
2022-08-12 13:15:47 +0100

    refactor(cb2-4636): remove new unused components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,35 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RoleRequiredDirective } from '../directives/app-role-required.directive';
+import { BannerComponent } from './components/banner/banner.component';
 import { ButtonGroupComponent } from './components/button-group/button-group.component';
 import { ButtonComponent } from './components/button/button.component';
 import { DefaultNullOrEmpty } from './pipes/default-null-or-empty/default-null-or-empty.pipe';
-import { BannerComponent } from './components/banner/banner.component';
-import { TagComponent } from './components/tag/tag.component';
-import { NumberPlateComponent } from './components/number-plate/number-plate.component';
-import { IconComponent } from './components/icon/icon.component';
 
 @NgModule({
-  declarations: [
-    DefaultNullOrEmpty,
-    ButtonGroupComponent,
-    ButtonComponent,
-    BannerComponent,
-    TagComponent,
-    NumberPlateComponent,
-    IconComponent,
-    RoleRequiredDirective
-  ],
+  declarations: [DefaultNullOrEmpty, ButtonGroupComponent, ButtonComponent, BannerComponent, RoleRequiredDirective],
   imports: [CommonModule],
-  exports: [
-    DefaultNullOrEmpty,
-    ButtonGroupComponent,
-    ButtonComponent,
-    BannerComponent,
-    TagComponent,
-    NumberPlateComponent,
-    IconComponent,
-    RoleRequiredDirective
-  ]
+  exports: [DefaultNullOrEmpty, ButtonGroupComponent, ButtonComponent, BannerComponent, RoleRequiredDirective]
 })
 export class SharedModule {}
