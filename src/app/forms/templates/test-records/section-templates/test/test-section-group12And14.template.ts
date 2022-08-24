20d6963565fa7305194ca9971f50c3d439e69ea3
kpearmanbjss <kris.pearman@bjss.com>
2022-08-24 14:29:46 +0100

    feat(cb2-4969): changed reason for abandoning to match up with new logic

--- src/app/forms/templates/test-records/section-templates/test/test-section-group12And14.template.ts
+++ src/app/forms/templates/test-records/section-templates/test/test-section-group12And14.template.ts
@@ -1,5 +1,6 @@
 import { ValidatorNames } from '@forms/models/validators.enum';
 import { FormNode, FormNodeEditTypes, FormNodeTypes, FormNodeViewTypes } from '@forms/services/dynamic-form.types';
+import { TestAbandonmentReasonsPsvData } from '../../test-abandonment-reasons';
 
 export const TestSectionGroup12And14: FormNode = {
   name: 'testSection',
@@ -75,8 +76,16 @@ export const TestSectionGroup12And14: FormNode = {
               name: 'reasonForAbandoning',
               type: FormNodeTypes.CONTROL,
               label: 'Reason for abandoning',
-              value: '',
-              disabled: true
+              editType: FormNodeEditTypes.CHECKBOX,
+              delimited: { regex: '\\. (?<!\\..\\. )', separator: '. ' },
+              required: true,
+              validators: [
+                {
+                  name: ValidatorNames.RequiredIfEquals,
+                  args: { sibling: 'testResult', value: 'abandoned' }
+                }
+              ],
+              options: TestAbandonmentReasonsPsvData
             },
             {
               name: 'additionalCommentsForAbandon',
