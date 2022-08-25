f3e576000d32f99fd4bf9ddddcb6b36dafbbc267
kpearmanbjss <95082261+kpearmanbjss@users.noreply.github.com>
2022-08-25 09:52:14 +0100

    fix(cb2-4969): brought additional comments for abandoning into line with new templates (#456)

--- src/app/forms/templates/test-records/section-templates/test/test-section-group7.template.ts
+++ src/app/forms/templates/test-records/section-templates/test/test-section-group7.template.ts
@@ -107,8 +107,16 @@ export const TestSectionGroup7: FormNode = {
               name: 'additionalCommentsForAbandon',
               type: FormNodeTypes.CONTROL,
               value: '',
+              required: true,
               label: 'Additional details for abandoning',
-              disabled: true
+              editType: FormNodeEditTypes.TEXTAREA,
+              validators: [
+                {
+                  name: ValidatorNames.RequiredIfEquals,
+                  args: { sibling: 'testResult', value: 'abandoned' }
+                },
+                { name: ValidatorNames.MaxLength, args: { length: 500 } }
+              ]
             },
             {
               name: 'testTypeStartTimestamp',
