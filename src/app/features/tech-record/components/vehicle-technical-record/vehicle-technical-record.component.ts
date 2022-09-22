8ee3eaeca59f2572d4700d5522961c18abce4bda
Thomas.Webb <SA_Thomas.Webb@dvsatest-cloud.uk>
2022-09-22 09:51:22 +0100

    feat(cb2-4536): fixed bug with errors not clearing

--- src/app/features/tech-record/components/vehicle-technical-record/vehicle-technical-record.component.ts
+++ src/app/features/tech-record/components/vehicle-technical-record/vehicle-technical-record.component.ts
@@ -4,7 +4,7 @@ import { TestResultModel } from '@models/test-results/test-result.model';
 import { TechRecordModel, VehicleTechRecordModel, VehicleTypes, Vrm } from '@models/vehicle-tech-record.model';
 import { TechnicalRecordService } from '@services/technical-record/technical-record.service';
 import { TestRecordsService } from '@services/test-records/test-records.service';
-import { Observable, of, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { TechRecordSummaryComponent } from '../tech-record-summary/tech-record-summary.component';
 import { GlobalErrorService } from '@core/components/global-error/global-error.service';
 import { DynamicFormService } from '@forms/services/dynamic-form.service';
@@ -70,9 +70,7 @@ export class VehicleTechnicalRecordComponent implements OnInit {
       DynamicFormService.updateValidity(form, errors);
     });
 
-    if (errors.length > 0) {
-      this.errorService.setErrors(errors);
-    }
+    errors.length > 0 ? this.errorService.setErrors(errors) : this.errorService.clearErrors();
 
     return forms.some(form => form.invalid);
   }
