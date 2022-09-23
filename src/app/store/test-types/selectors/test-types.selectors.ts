d6ee98d9d7edd282f04c90401226c31b84476e96
Taha Yazici <taha.yazici@bjss.com>
2022-09-23 10:02:14 +0100

    fix(cb2-5732): fix the test type selector method

--- src/app/store/test-types/selectors/test-types.selectors.ts
+++ src/app/store/test-types/selectors/test-types.selectors.ts
@@ -1,8 +1,9 @@
+import { TestType } from '@api/test-types';
 import { TestTypeCategory } from '@api/test-types/model/testTypeCategory';
 import { TestTypesTaxonomy } from '@api/test-types/model/testTypesTaxonomy';
 import { TestResultModel } from '@models/test-results/test-result.model';
 import { createSelector } from '@ngrx/store';
-import { selectedTestResultState, toEditOrNotToEdit } from '@store/test-records';
+import { toEditOrNotToEdit } from '@store/test-records';
 import { testTypesAdapter, testTypesFeatureState } from '../reducers/test-types.reducer';
 
 const { selectIds, selectEntities, selectAll, selectTotal } = testTypesAdapter.getSelectors();
@@ -54,6 +55,16 @@ export const sortedTestTypes = createSelector(selectTestTypesByVehicleType, test
   };
 
   return sortTestTypes(testTypes);
+});
+
+export const selectTestType = (id: string) => createSelector(selectTestTypesByVehicleType, (testTypes): TestType | undefined => {
+  function usingIdMatch(testType: TestType | TestTypeCategory) {
+    return testType.id === id
+      || (testType.hasOwnProperty('nextTestTypesOrCategories')
+      && (testType as TestTypeCategory).nextTestTypesOrCategories?.some(usingIdMatch));
+  }
+
+  return testTypes.find(usingIdMatch);
 });
 
 function filterTestTypes(testTypes: TestTypesTaxonomy, testResult: TestResultModel): TestTypesTaxonomy {
