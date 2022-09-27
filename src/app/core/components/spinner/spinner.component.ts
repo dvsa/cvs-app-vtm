126a677796a5060d60d20ec0b3d59cf47f235a2e
Mauricio Cadenas <mauricio.cadenas@bjss.com>
2022-09-27 16:04:59 +0100

    WIP

--- src/app/core/components/spinner/spinner.component.ts
+++ src/app/core/components/spinner/spinner.component.ts
@@ -7,5 +7,5 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SpinnerComponent {
-  @Input() loading = false;
+  @Input() loading: boolean | null = false;
 }
