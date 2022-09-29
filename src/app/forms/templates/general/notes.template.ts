66bbc7a2f5acbb75dd33de77da10b258a8cc053b
aaron-downing <92087051+aaron-downing@users.noreply.github.com>
2022-09-29 09:57:39 +0100

    chore(cb2-1): refactor templates to be more generic (#512)
    * chore(cb2-1): refactor templates to be more generic
    
    * chore(cb2-1): pass vehicle type instead of boolean

--- src/app/forms/templates/general/notes.template.ts
+++ src/app/forms/templates/general/notes.template.ts
@@ -1,15 +1,40 @@
-import { FormNode, FormNodeTypes, FormNodeViewTypes } from '../../services/dynamic-form.types';
+import { ValidatorNames } from '@forms/models/validators.enum';
+import { VehicleTypes } from '@models/vehicle-tech-record.model';
+import { FormNode, FormNodeEditTypes, FormNodeTypes, FormNodeViewTypes } from '../../services/dynamic-form.types';
 
-export const NotesTemplate: FormNode = {
-  name: 'notesSection',
-  label: 'Notes',
-  type: FormNodeTypes.GROUP,
-  viewType: FormNodeViewTypes.SUBHEADING,
-  children: [
-    {
-      name: 'notes',
+export function getNotesTemplate(vehicleType: VehicleTypes): FormNode {
+  let form: FormNode = {
+    name: 'notesSection',
+    label: 'Notes',
+    type: FormNodeTypes.GROUP,
+    viewType: FormNodeViewTypes.SUBHEADING,
+    children: []
+  }
+
+  if(vehicleType === VehicleTypes.PSV) {
+    form.children?.push({
+      name: 'remarks',
+      label: 'Notes',
       type: FormNodeTypes.CONTROL,
-      viewType: FormNodeViewTypes.FULLWIDTH
-    }
-  ]
+      viewType: FormNodeViewTypes.STRING,
+      editType: FormNodeEditTypes.TEXTAREA,
+      validators: [{ name: ValidatorNames.MaxLength, args: 800 }]
+    },
+    {
+      name: 'dispensations',
+      type: FormNodeTypes.CONTROL,
+      label: 'Dispensations',
+      viewType: FormNodeViewTypes.STRING,
+      editType: FormNodeEditTypes.TEXTAREA,
+      validators: [{ name: ValidatorNames.MaxLength, args: 160 }]
+    })
+    return form;
+  }
+
+  form.children?.push({
+    name: 'notes',
+    type: FormNodeTypes.CONTROL,
+    viewType: FormNodeViewTypes.FULLWIDTH
+  })
+  return form;
 };
