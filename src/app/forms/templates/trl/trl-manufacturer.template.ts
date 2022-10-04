c5fbca148d90f7a84b4b4ce28af45eaa8ac9d4c3
Aaron Downing <aaron.downing@bjss.com>
2022-10-04 16:30:21 +0100

    feat(cb2-5705): remove redundant code

--- src/app/forms/templates/trl/trl-manufacturer.template.ts
+++ src/app/forms/templates/trl/trl-manufacturer.template.ts
@@ -1,5 +1,5 @@
 import { ValidatorNames } from '@forms/models/validators.enum';
-import { FormNode, FormNodeEditTypes, FormNodeTypes, FormNodeViewTypes, Width } from '../../services/dynamic-form.types';
+import { FormNode, FormNodeEditTypes, FormNodeTypes } from '../../services/dynamic-form.types';
 
 export const TrlManufacturerTemplate: FormNode = {
   name: 'manufacturerSection',
