6a2ad0c72401c7c7173028ef64434b5ecd592f30
Leif Kemp <leif.kemp@bjss.com>
2022-10-13 09:27:18 +0100

    feat(cb2-5881): unit test for createheader

--- src/app/features/test-records/components/vehicle-header/vehicle-header.component.spec.ts
+++ src/app/features/test-records/components/vehicle-header/vehicle-header.component.spec.ts
@@ -8,6 +8,7 @@ import { initialAppState } from '@store/.';
 import { of } from 'rxjs';
 import { ResultOfTestComponent } from '../result-of-test/result-of-test.component';
 import { VehicleHeaderComponent } from './vehicle-header.component';
+import { TechRecordModel, VehicleTypes, VehicleConfigurations } from '@models/vehicle-tech-record.model';
 
 describe('VehicleHeaderComponent', () => {
   let component: VehicleHeaderComponent;
@@ -41,5 +42,19 @@ describe('VehicleHeaderComponent', () => {
 
   it('should display the reading if the unit is undefined', () => {
     expect(component.combinedOdometerReading(1234, undefined)).toEqual('1234 ');
+  });
+
+  it('should display the correct data based on vehicle type', () => {
+    const mockRecord = {
+      vehicleConfiguration: VehicleConfigurations.RIGID,
+      bodyMake: 'testBody',
+      bodyModel: 'testBodyModel',
+      chassisMake: 'testChassis',
+      chassisModel: 'testChassisModel'
+    } as TechRecordModel;
+
+    expect(component.createHeader(mockRecord, VehicleTypes.TRL)).toEqual('rigid');
+    expect(component.createHeader(mockRecord, VehicleTypes.PSV)).toEqual('testBody-testBodyModel');
+    expect(component.createHeader(mockRecord, VehicleTypes.HGV)).toEqual('testChassis-testChassisModel');
   });
 });
