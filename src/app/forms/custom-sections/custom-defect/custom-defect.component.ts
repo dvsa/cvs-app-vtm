45da899bfdb9ad8e855204036064afa6444c4104
Ruairidh Taylor <ruairidh.taylor@bjss.com>
2022-10-17 14:36:58 +0100

    feat(cb2-4228): removed redundant code

--- src/app/forms/custom-sections/custom-defect/custom-defect.component.ts
+++ src/app/forms/custom-sections/custom-defect/custom-defect.component.ts
@@ -2,24 +2,13 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CustomFormGroup } from '@forms/services/dynamic-form.types';
 
 @Component({
-  selector: 'app-custom-defect[index]',
+  selector: 'app-custom-defect[index][form]',
   templateUrl: './custom-defect.component.html',
   styleUrls: ['./custom-defect.component.scss']
 })
-export class CustomDefectComponent implements OnInit {
+export class CustomDefectComponent {
   @Input() form!: CustomFormGroup;
-  @Input() isEditing = false;
   @Input() index!: number;
+  @Input() isEditing = false;
   @Output() removeCustomDefect = new EventEmitter<number>();
-
-  isEditingCustomDefect: boolean = false;
-  sectionFormValues!: CustomFormGroup;
-
-  ngOnInit(): void {
-    this.updateSectionFormValues();
-  }
-
-  updateSectionFormValues(): void {
-    this.sectionFormValues = this.form.value;
-  }
 }
