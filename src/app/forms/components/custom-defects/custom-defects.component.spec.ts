c1e8edef70f878cfb0e7b162f5366a1e9fdb51ed
Ruairidh Taylor <ruairidh.taylor@bjss.com>
2022-10-17 10:33:27 +0100

    feat(cb2-4228): added tests

--- src/app/forms/components/custom-defects/custom-defects.component.spec.ts
+++ src/app/forms/components/custom-defects/custom-defects.component.spec.ts
@@ -1,8 +1,10 @@
 import { DebugElement } from '@angular/core';
-import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { DynamicFormService } from '@forms/services/dynamic-form.service';
+import { CustomFormGroup, FormNode } from '@forms/services/dynamic-form.types';
+import { provideMockStore } from '@ngrx/store/testing';
 import { CustomDefectComponent } from '../custom-defect/custom-defect.component';
 import { CustomDefectsComponent } from './custom-defects.component';
 
@@ -15,7 +17,7 @@ describe('CustomDefectsComponent', () => {
     await TestBed.configureTestingModule({
       imports: [FormsModule, ReactiveFormsModule],
       declarations: [CustomDefectsComponent, CustomDefectComponent],
-      providers: [DynamicFormService]
+      providers: [DynamicFormService, provideMockStore({})]
     }).compileComponents();
   });
 
@@ -32,5 +34,78 @@ describe('CustomDefectsComponent', () => {
 
   it('should render correct header', () => {
     expect(el.query(By.css('h2')).nativeElement.innerHTML).toBe('Custom Defects');
+  });
+
+  describe('add and remove custom defects', () => {
+    const template = <FormNode>{
+      name: 'customDefectsSection',
+      label: 'Custom Defects',
+      type: 'group',
+      children: [
+        {
+          name: 'testTypes',
+          label: 'Test Types',
+          type: 'array',
+          children: [
+            {
+              name: '0',
+              type: 'group',
+              children: [
+                {
+                  name: 'customDefects',
+                  type: 'array',
+                  children: [
+                    {
+                      name: '0',
+                      type: 'group',
+                      children: [
+                        {
+                          name: 'referenceNumber',
+                          label: 'Reference Number',
+                          type: 'control'
+                        },
+                        {
+                          name: 'defectName',
+                          label: 'Defect Name',
+                          type: 'control'
+                        },
+                        {
+                          name: 'defectNotes',
+                          label: 'Defect Notes',
+                          type: 'control'
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    };
+
+    const data = {
+      testTypes: [
+        {
+          customDefects: []
+        }
+      ]
+    };
+
+    it('should add new custom defect', inject([DynamicFormService], (dfs: DynamicFormService) => {
+      component.form = dfs.createForm(template, data) as CustomFormGroup;
+      expect(component.defectCount).toBe(0);
+      component.handleAddCustomDefect();
+      expect(component.defectCount).toBe(1);
+    }));
+
+    it('should remove custom defect', inject([DynamicFormService], (dfs: DynamicFormService) => {
+      component.form = dfs.createForm(template, data) as CustomFormGroup;
+      component.handleAddCustomDefect();
+      expect(component.defectCount).toBe(1);
+      component.handleRemoveDefect(0);
+      expect(component.defectCount).toBe(0);
+    }));
   });
 });
