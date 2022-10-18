97c184f898640f7a9bd8f457560c725de2f10143
hwilderspin <hazel.wilderspin@bjss.com>
2022-10-18 10:28:52 +0100

    feat(cb2-5695): removed redundant title: tyres, made validator not applicable to PSV: approval type, created enum for fuel propulsion system in models, reformatted psv vehicle summary edit, added dropdowns, radios etc

---
+++ src/app/models/fuel-propulsion-system.model.ts
@@ -0,0 +1,9 @@
+export enum FuelPropulsionSystem {
+  DieselPetrol = 'DieselPetrol',
+  Hybrid = 'Hybrid',
+  Electric = 'Electric',
+  CNG = 'CNG',
+  FuelCell = 'Fuel cell',
+  LNG = 'LNG',
+  Other = 'Other'
+}
