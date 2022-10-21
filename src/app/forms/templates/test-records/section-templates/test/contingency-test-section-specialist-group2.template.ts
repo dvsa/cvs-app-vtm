7c573c878bc383022d0158af08e87a04e1fc5c63
leifkemp-bjss <114917018+leifkemp-bjss@users.noreply.github.com>
2022-10-21 10:04:52 +0100

    Update src/app/forms/templates/test-records/section-templates/test/contingency-test-section-specialist-group2.template.ts
    Co-authored-by: gjulien-bjss <104442665+gjulien-bjss@users.noreply.github.com>

--- src/app/forms/templates/test-records/section-templates/test/contingency-test-section-specialist-group2.template.ts
+++ src/app/forms/templates/test-records/section-templates/test/contingency-test-section-specialist-group2.template.ts
@@ -67,36 +67,6 @@ export const ContingencyTestSectionSpecialistGroup2: FormNode = {
               type: FormNodeTypes.CONTROL
             },
             {
-              name: 'reasonForAbandoning',
-              type: FormNodeTypes.CONTROL,
-              label: 'Reason for abandoning',
-              editType: FormNodeEditTypes.CHECKBOX,
-              delimited: { regex: '\\. (?<!\\..\\. )', separator: '. ' },
-              required: true,
-              validators: [
-                {
-                  name: ValidatorNames.RequiredIfEquals,
-                  args: { sibling: 'testResult', value: 'abandoned' }
-                }
-              ],
-              options: TestAbandonmentReasonsPsvData
-            },
-            {
-              name: 'additionalCommentsForAbandon',
-              type: FormNodeTypes.CONTROL,
-              value: '',
-              required: true,
-              label: 'Additional details for abandoning',
-              editType: FormNodeEditTypes.TEXTAREA,
-              validators: [
-                {
-                  name: ValidatorNames.RequiredIfEquals,
-                  args: { sibling: 'testResult', value: 'abandoned' }
-                },
-                { name: ValidatorNames.MaxLength, args: 500 }
-              ]
-            },
-            {
               name: 'certificateNumber',
               label: 'Certificate number',
               value: '',
