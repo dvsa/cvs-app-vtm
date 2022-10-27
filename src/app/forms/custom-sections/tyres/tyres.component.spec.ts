aea30203eebef329bc013da6a279e18fb86a5bd0
Nathan Brown <nathan.brown@bjss.com>
2022-10-27 16:49:50 +0100

    feat(cb2-6095): repair tests

--- src/app/forms/custom-sections/tyres/tyres.component.spec.ts
+++ src/app/forms/custom-sections/tyres/tyres.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DynamicFormsModule } from '@forms/dynamic-forms.module';
+import { createMockPsv } from '@mocks/psv-record.mock';
+import { StoreModule } from '@ngrx/store';
+import { provideMockStore } from '@ngrx/store/testing';
+import { initialAppState, State } from '@store/index';
 
 import { TyresComponent } from './tyres.component';
 
@@ -8,14 +13,16 @@ describe('TyresComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TyresComponent ]
-    })
-    .compileComponents();
+      imports: [DynamicFormsModule, StoreModule.forRoot({})],
+      declarations: [TyresComponent],
+      providers: [provideMockStore<State>({ initialState: initialAppState })]
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TyresComponent);
     component = fixture.componentInstance;
+    component.vehicleTechRecord = createMockPsv(12345).techRecord[0];
     fixture.detectChanges();
   });
 
