5260e0c644913920702ea608a8ca772e2b47806f
Mauricio Cadenas <mauricio.cadenas@bjss.com>
2022-11-07 10:52:08 +0000

    fix(*): critical audit vulnerabilities regarding storybook version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,12 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-controls'],
-  framework: '@storybook/angular',
-  core: {
-    builder: 'webpack5'
+  framework: {
+    name: '@storybook/angular',
+    options: {}
+  },
+  core: {},
+  docsPage: {
+    docs: 'automatic'
   }
 };
