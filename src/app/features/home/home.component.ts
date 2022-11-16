24d2f28445f4a100efa31bf8ec71df0bada26936
Mauricio Cadenas <mauricio.cadenas@bjss.com>
2022-11-16 12:15:23 +0000

    chore(cb2-0000): test 6

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -3,13 +3,9 @@ import { Roles } from '@models/roles.enum';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  templateUrl: './home.component.html'
 })
 export class HomeComponent {
-
-
-
-
   public get Roles() {
     return Roles;
   }
