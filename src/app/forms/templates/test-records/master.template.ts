85d17f884c487dd700982adc8dcfd70abc14146d
Mauricio Cadenas <102959178+mcadenas-bjss@users.noreply.github.com>
2022-12-05 09:16:38 +0000

    feat(cb2-6534): amend dba psv group 4 (#732)
    * feat(cb2-6534): amend dba psv group 4
    
    * feat(cb2-6534): read only reason for creation
    
    * feat(cb2-6534): editable result and reason for creation

--- src/app/forms/templates/test-records/master.template.ts
+++ src/app/forms/templates/test-records/master.template.ts
@@ -1,3 +1,4 @@
+import { TEST_TYPES } from '@forms/models/testTypeId.enum';
 import { FormNode } from '@forms/services/dynamic-form.types';
 import { VehicleTypes } from '@models/vehicle-tech-record.model';
 import { DefectsTpl } from '../general/defect.template';
@@ -5,38 +6,41 @@ import { CustomDefectsSection } from './section-templates/customDefects/custom-d
 import { EmissionsSection } from './section-templates/emissions/emissions-section.template';
 import { NotesSection } from './section-templates/notes/notes-section.template';
 import { reasonForCreationSection } from './section-templates/reasonForCreation/reasonForCreation.template';
-import { RequiredSection } from './section-templates/required/required-hidden-section.template';
+import { CustomDefectsHiddenSection } from './section-templates/required/custom-defects-hidden-section.template';
+import { defectsHiddenSection } from './section-templates/required/defect-hidden-section.template';
 import { RequiredSectionHGVTRL } from './section-templates/required/required-hidden-section-hgv-trl.template';
+import { RequiredSection } from './section-templates/required/required-hidden-section.template';
+import { SeatbeltHiddenSection } from './section-templates/required/seatbelt-hidden-section.template';
 import { SpecialistRequiredSectionHGVTRL } from './section-templates/required/specialist-required-hidden-section-hgv-trl.template';
+import { RequiredSpecialistSection } from './section-templates/required/specialist-required-hidden-section.template';
 import { SeatbeltSection } from './section-templates/seatbelt/seatbelt-section.template';
+import { DeskBasedTestSectionGroup2PSV } from './section-templates/test/desk-based/desk-based-test-section-group2-PSV.template';
+import { amendDeskBasedTestSectionGroup4Psv } from './section-templates/test/desk-based/desk-based-test-section-group4-PSV.template';
+import { SpecialistTestSectionGroup1 } from './section-templates/test/specialist/specialist-test-section-group1.template';
+import { SpecialistTestSectionGroup2 } from './section-templates/test/specialist/specialist-test-section-group2.template';
+import { SpecialistTestSectionGroup3And4 } from './section-templates/test/specialist/specialist-test-section-group3And4.template';
+import { SpecialistTestSectionGroup5 } from './section-templates/test/specialist/specialist-test-section-group5.template';
 import { TestSectionGroup1 } from './section-templates/test/test-section-group1.template';
+import { TestSectionGroup12And14 } from './section-templates/test/test-section-group12And14.template';
+import { TestSectionGroup15And16 } from './section-templates/test/test-section-group15And16.template';
+import { TestSectionGroup2 } from './section-templates/test/test-section-group2.template';
 import { TestSectionGroup3And4And8 } from './section-templates/test/test-section-group3And4And8.template';
 import { TestSectionGroup5And13 } from './section-templates/test/test-section-group5And13.template';
 import { TestSectionGroup6And11 } from './section-templates/test/test-section-group6And11.template';
 import { TestSectionGroup7 } from './section-templates/test/test-section-group7.template';
 import { TestSectionGroup9And10 } from './section-templates/test/test-section-group9And10.template';
-import { TestSectionGroup15And16 } from './section-templates/test/test-section-group15And16.template';
-import { TestSectionGroup12And14 } from './section-templates/test/test-section-group12And14.template';
-import { TestSectionGroup2 } from './section-templates/test/test-section-group2.template';
 import { TestSection } from './section-templates/test/test-section.template';
+import { ContingencyVehicleSectionDefaultPsvHgv } from './section-templates/vehicle/contingency-default-psv-hgv-vehicle-section.template';
 import { VehicleSectionDefaultPsvHgv } from './section-templates/vehicle/default-psv-hgv-vehicle-section.template';
 import { VehicleSectionDefaultTrl } from './section-templates/vehicle/default-trl-vehicle-section.template';
+import { DeskBasedVehicleSectionDefaultPsvHgv } from './section-templates/vehicle/desk-based-default-psv-hgv-vehicle-section.template';
 import { VisitSection } from './section-templates/visit/visit-section.template';
-import { SpecialistTestSectionGroup1 } from './section-templates/test/specialist/specialist-test-section-group1.template';
-import { RequiredSpecialistSection } from './section-templates/required/specialist-required-hidden-section.template';
-import { SpecialistTestSectionGroup5 } from './section-templates/test/specialist/specialist-test-section-group5.template';
-import { SpecialistTestSectionGroup2 } from './section-templates/test/specialist/specialist-test-section-group2.template';
-import { CustomDefectsHiddenSection } from './section-templates/required/custom-defects-hidden-section.template';
-import { SpecialistTestSectionGroup3And4 } from './section-templates/test/specialist/specialist-test-section-group3And4.template';
-import { defectsHiddenSection } from './section-templates/required/defect-hidden-section.template';
-import { SeatbeltHiddenSection } from './section-templates/required/seatbelt-hidden-section.template';
-import { ContingencyVehicleSectionDefaultPsvHgv } from './section-templates/vehicle/contingency-default-psv-hgv-vehicle-section.template';
-import { DeskBasedTestSectionGroup2PSV } from './section-templates/test/desk-based/desk-based-test-section-group2-PSV.template';
-import { TEST_TYPES } from '@forms/models/testTypeId.enum';
-//Keys of root object must a a valid vehicle type.
-//Keys of child object must be a valid test type id.
-//Child object must ALWAYS have a 'default' key.
 
+/**
+ * Keys of root object must a a valid vehicle type.
+ * Keys of child object must be a valid test type id.
+ * Child object must ALWAYS have a 'default' key.
+ */
 export const masterTpl: Record<VehicleTypes, Partial<Record<keyof typeof TEST_TYPES | 'default', Record<string, FormNode>>>> = {
   psv: {
     default: {
@@ -145,6 +149,16 @@ export const masterTpl: Record<VehicleTypes, Partial<Record<keyof typeof TEST_TY
       test: DeskBasedTestSectionGroup2PSV,
       seatbelts: SeatbeltHiddenSection,
       emissions: EmissionsSection,
+      visit: VisitSection,
+      notes: NotesSection,
+      defects: defectsHiddenSection,
+      customDefects: CustomDefectsHiddenSection,
+      reasonForCreation: reasonForCreationSection
+    },
+    testTypesDeskBasedGroup4: {
+      required: RequiredSection,
+      vehicle: DeskBasedVehicleSectionDefaultPsvHgv,
+      test: amendDeskBasedTestSectionGroup4Psv,
       visit: VisitSection,
       notes: NotesSection,
       defects: defectsHiddenSection,
