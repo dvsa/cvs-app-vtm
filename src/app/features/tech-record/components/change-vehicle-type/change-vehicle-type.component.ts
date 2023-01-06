e80614b7cb5bc7574b24e6acb6f3ea23501d9244
hwilderspin <hazel.wilderspin@bjss.com>
2023-01-06 10:20:29 +0000

    feat(cb2-5696): back navigation

--- src/app/features/tech-record/components/change-vehicle-type/change-vehicle-type.component.ts
+++ src/app/features/tech-record/components/change-vehicle-type/change-vehicle-type.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { GlobalErrorService } from '@core/components/global-error/global-error.service';
+import { DynamicFormService } from '@forms/services/dynamic-form.service';
+import { CustomFormGroup, FormNode, FormNodeTypes } from '@forms/services/dynamic-form.types';
 import { VehicleTechRecordModel } from '@models/vehicle-tech-record.model';
 import { TechnicalRecordService } from '@services/technical-record/technical-record.service';
 import { take } from 'rxjs';
@@ -9,11 +13,24 @@ import { take } from 'rxjs';
   styleUrls: ['./change-vehicle-type.component.scss']
 })
 export class ChangeVehicleTypeComponent implements OnInit {
-  constructor(private technicalRecordService: TechnicalRecordService) {
+  constructor(
+    private technicalRecordService: TechnicalRecordService,
+    public globalErrorService: GlobalErrorService,
+    public dfs: DynamicFormService,
+    private location: Location
+  ) {
     this.technicalRecordService.selectedVehicleTechRecord$.pipe(take(1)).subscribe(data => (this.vehicleTechRecord = data));
   }
 
   public vehicleTechRecord?: VehicleTechRecordModel;
+  public form!: CustomFormGroup;
+
+  public template: FormNode = {
+    name: 'selectVehicleType',
+    label: 'Choose vehicle type',
+    value: '',
+    type: FormNodeTypes.CONTROL
+  };
 
   get currentVrm(): string | undefined {
     return this.vehicleTechRecord?.vrms.find(vrm => vrm.isPrimary === true)?.vrm;
@@ -21,5 +38,11 @@ export class ChangeVehicleTypeComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('hi');
+    this.form = this.dfs.createForm(this.template) as CustomFormGroup;
+  }
+
+  navigateBack() {
+    this.globalErrorService.clearErrors();
+    this.location.back();
   }
 }
