0903d7451554998495564e3b4e489e2e42ee2733
hwilderspin <hazel.wilderspin@bjss.com>
2023-01-06 13:25:06 +0000

    feat(cb2-5696): prep test bed

--- src/app/features/tech-record/components/change-vehicle-type/change-vehicle-type.component.spec.ts
+++ src/app/features/tech-record/components/change-vehicle-type/change-vehicle-type.component.spec.ts
@@ -1,24 +1,54 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TechRecord } from '@api/vehicle';
+import { DynamicFormService } from '@forms/services/dynamic-form.service';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Action } from '@ngrx/store';
+import { provideMockStore } from '@ngrx/store/testing';
+import { TechnicalRecordService } from '@services/technical-record/technical-record.service';
+import { initialAppState } from '@store/index';
+import { of, ReplaySubject } from 'rxjs';
+import { TechRecordsModule } from '../../tech-record.module';
 
 import { ChangeVehicleTypeComponent } from './change-vehicle-type.component';
+
+const mockTechRecordService = {
+  editableTechRecord$: of({}),
+  selectedVehicleTechRecord$: of({}),
+  viewableTechRecord$: jest.fn()
+};
+
+const mockDynamicFormService = {
+  createForm: jest.fn()
+};
 
 describe('ChangeVehicleTypeComponent', () => {
   let component: ChangeVehicleTypeComponent;
   let fixture: ComponentFixture<ChangeVehicleTypeComponent>;
+  let actions$ = new ReplaySubject<Action>();
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ChangeVehicleTypeComponent]
+      declarations: [ChangeVehicleTypeComponent],
+      providers: [
+        provideMockActions(() => actions$),
+        provideMockStore({ initialState: initialAppState }),
+
+        { provide: TechnicalRecordService, useValue: mockTechRecordService },
+        { provide: DynamicFormService, useValue: mockDynamicFormService }
+      ]
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ChangeVehicleTypeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should return a technical record', () => {
+    const techRecord = component.vehicleTechRecord;
+    expect(techRecord).toBeTruthy();
   });
 });
