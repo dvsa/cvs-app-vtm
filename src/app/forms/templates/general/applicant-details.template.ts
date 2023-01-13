9910e5117cf7eed638e07de907ca637dd28d89dd
owen <owen.corrigan@bjss.com>
2023-01-13 11:33:40 +0000

    refactor(cb2-7174): changed applicant details country name to country

--- src/app/forms/templates/general/applicant-details.template.ts
+++ src/app/forms/templates/general/applicant-details.template.ts
@@ -43,7 +43,7 @@ export const ApplicantDetails: FormNode = {
           validators: [{ name: ValidatorNames.MaxLength, args: 60 }]
         },
         {
-          name: 'address3',
+          name: 'country',
           label: 'County',
           value: '',
           width: FormNodeWidth.XL,
