9bb96da332ee2a7a6e906aee0a731c19bd32205b
Thomas.Webb <Thomas.Webb@bjss.com>
2023-01-19 11:30:43 +0000

    feat(cb2-5712): added imports to create module

--- src/app/features/tech-record/components/tech-record-create/tech-record-create.module.ts
+++ src/app/features/tech-record/components/tech-record-create/tech-record-create.module.ts
@@ -1,9 +1,13 @@
 import { TechRecordCreateComponent } from './tech-record-create.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { DynamicFormsModule } from '@forms/dynamic-forms.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '@shared/shared.module';
+import { TechRecordsRoutingModule } from '../../tech-record-routing.module';
 
 @NgModule({
   declarations: [TechRecordCreateComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, DynamicFormsModule, ReactiveFormsModule, SharedModule, TechRecordsRoutingModule]
 })
 export class TechRecordCreateModule {}
