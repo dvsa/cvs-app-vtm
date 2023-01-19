2d22560d9272444e162e2b76cc61cf2ebe5d4a84
Thomas.Webb <Thomas.Webb@bjss.com>
2023-01-19 16:50:56 +0000

    feat(cb2-5712): made helper functions for create component

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { MultiOptions } from '@forms/models/options.model';
+import { VehicleTechRecordModel, VehicleTypes } from '@models/vehicle-tech-record.model';
+import { SEARCH_TYPES, TechnicalRecordService } from '@services/technical-record/technical-record.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html'
 })
 export class CreateComponent implements OnInit {
-  constructor() {}
+  private vehicle: Partial<VehicleTechRecordModel> = {};
+
+  private isVinUnique: boolean = true;
+  private isVrmUnique: boolean = true;
+  private isTrailerIdUnique: boolean = true;
+
+  #selectVehicleType: VehicleTypes = VehicleTypes.PSV;
+  #inputVin: string = '';
+  #inputVrmOrTrailerId: string = '';
+
+  constructor(private technicalRecordService: TechnicalRecordService) {}
   get vehicleTypeOptions(): MultiOptions {
     return [
       { label: 'Heavy goods vehicle (HGV)', value: 'hgv' },
@@ -19,6 +31,29 @@ export class CreateComponent implements OnInit {
     console.log('create init');
   }
   handleSubmit() {
-    console.log('submit');
+    this.vehicle.vin = this.#inputVin;
+    this.vehicle.vrms = [{ vrm: this.#inputVrmOrTrailerId, isPrimary: true }];
+    this.vehicle.trailerId = this.#inputVrmOrTrailerId;
+
+    if (this.areValuesUnique()) {
+      console.log('navigate to hydrate record');
+    }
+    console.log('error');
+  }
+
+  private areValuesUnique() {
+    const isTrailer = this.#selectVehicleType == VehicleTypes.TRL;
+
+    this.technicalRecordService.isUnique(this.vehicle.vin!, SEARCH_TYPES.VIN).subscribe(data => (this.isVinUnique = data));
+
+    isTrailer
+      ? this.technicalRecordService.isUnique(this.vehicle.trailerId!, SEARCH_TYPES.TRAILER_ID).subscribe(data => (this.isTrailerIdUnique = data))
+      : this.technicalRecordService.isUnique(this.getPrimaryVRM(), SEARCH_TYPES.VRM).subscribe(data => (this.isVrmUnique = data));
+
+    return isTrailer ? this.isVinUnique && this.isTrailerIdUnique : this.isVinUnique && this.isVrmUnique;
+  }
+
+  private getPrimaryVRM() {
+    return this.vehicle.vrms!.find(vrm => vrm.isPrimary)!.vrm;
   }
 }
