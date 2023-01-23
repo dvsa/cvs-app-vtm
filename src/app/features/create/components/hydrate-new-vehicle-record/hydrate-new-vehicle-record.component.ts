67182d6f69a3743156a98a34e090b3bedd8e8ce4
hwilderspin <hazel.wilderspin@bjss.com>
2023-01-23 09:15:02 +0000

    feat(CB2-5712): accordion form fields for hydrate page

--- src/app/features/create/components/hydrate-new-vehicle-record/hydrate-new-vehicle-record.component.ts
+++ src/app/features/create/components/hydrate-new-vehicle-record/hydrate-new-vehicle-record.component.ts
@@ -1,17 +1,67 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { VehicleTechRecordModel } from '@models/vehicle-tech-record.model';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GlobalErrorService } from '@core/components/global-error/global-error.service';
+import { FormNode } from '@forms/services/dynamic-form.types';
+import { vehicleTemplateMap } from '@forms/utils/tech-record-constants';
+import { TechRecordModel, VehicleTechRecordModel, VehicleTypes } from '@models/vehicle-tech-record.model';
+import { Store } from '@ngrx/store';
+import { TechnicalRecordService } from '@services/technical-record/technical-record.service';
+import { updateEditingTechRecord } from '@store/technical-records/actions/technical-record-service.actions';
+import { TechnicalRecordServiceState } from '@store/technical-records/reducers/technical-record-service.reducer';
+import { Observable, take } from 'rxjs';
 
 @Component({
   selector: 'app-hydrate-new-vehicle-record',
   templateUrl: './hydrate-new-vehicle-record.component.html',
   styleUrls: ['./hydrate-new-vehicle-record.component.scss']
 })
-export class HydrateNewVehicleRecordComponent implements OnInit {
-  @Input() vehicle?: Partial<VehicleTechRecordModel>;
+export class HydrateNewVehicleRecordComponent implements OnInit, OnChanges {
+  vehicleRecord!: VehicleTechRecordModel;
+  middleIndex = 0;
 
-  constructor() {}
+  constructor(
+    private globalErrorService: GlobalErrorService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private store: Store<TechnicalRecordServiceState>,
+    private technicalRecordService: TechnicalRecordService
+  ) {
+    this.technicalRecordService.editableVehicleTechRecord$.pipe(take(1)).subscribe(data => (this.vehicleRecord = data!));
+  }
 
   ngOnInit(): void {
-    console.log('bleh');
+    this.middleIndex = Math.floor(this.templates.length / 2);
+    console.log(this.vehicleRecord);
+    console.log(this.techRecord.vehicleType);
+    console.log(this.templates);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    this.store.dispatch(updateEditingTechRecord({ vehicleTechRecord: this.vehicleRecord }));
+  }
+
+  get templates(): Array<FormNode> {
+    return vehicleTemplateMap.get(this.techRecord.vehicleType)!;
+  }
+
+  get techRecord(): TechRecordModel {
+    return this.vehicleRecord!.techRecord[0];
+  }
+
+  get vrmOrTrailerId(): string {
+    return this.techRecord.vehicleType === VehicleTypes.TRL ? `${this.vehicleRecord.trailerId}` : `${this.vehicleRecord.vrms[0].vrm}`;
+  }
+
+  navigateBack() {
+    this.globalErrorService.clearErrors();
+    this.router.navigate(['..'], { relativeTo: this.route });
+  }
+  handleSubmit() {
+    console.log(this.vehicleRecord);
+    console.log(this.techRecord);
+
+    // dispatch
+    // route to confirmation page
   }
 }
