059841cfd7f1cc402713bf1ab0b83969e42bccf4
leifkemp-bjss <leif.kemp@bjss.com>
2023-02-08 14:31:38 +0000

    feat(cb2-6178): add cherished transfer choice and tests

--- src/app/features/tech-record/components/tech-record-amend-vrm/tech-record-amend-vrm.component.ts
+++ src/app/features/tech-record/components/tech-record-amend-vrm/tech-record-amend-vrm.component.ts
@@ -3,7 +3,7 @@ import { Component, OnChanges, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GlobalErrorService } from '@core/components/global-error/global-error.service';
 import { DynamicFormService } from '@forms/services/dynamic-form.service';
-import { CustomFormGroup, FormNode, FormNodeTypes, FormNodeWidth } from '@forms/services/dynamic-form.types';
+import { CustomFormGroup, FormNode, FormNodeOption, FormNodeTypes, FormNodeWidth } from '@forms/services/dynamic-form.types';
 import { TechRecordModel, VehicleTechRecordModel, Vrm } from '@models/vehicle-tech-record.model';
 import { Store } from '@ngrx/store';
 import { SEARCH_TYPES, TechnicalRecordService } from '@services/technical-record/technical-record.service';
@@ -20,6 +20,11 @@ import { Actions, ofType } from '@ngrx/effects';
   styleUrls: ['./tech-record-amend-vrm.component.scss']
 })
 export class AmendVrmComponent implements OnInit, OnChanges {
+  reasons: Array<FormNodeOption<string>> = [
+    { label: 'Cherished transfer', value: 'true', hint: 'Current VRM will be archived' },
+    { label: 'Correcting an error', value: 'false', hint: 'Current VRM will not be archived' }
+  ];
+
   vehicle?: VehicleTechRecordModel;
   currentTechRecord?: TechRecordModel;
   form: CustomFormGroup;
@@ -85,34 +90,50 @@ export class AmendVrmComponent implements OnInit, OnChanges {
     this.router.navigate(['..'], { relativeTo: this.route });
   }
 
-  handleSubmit(newVrm: string): void {
+  handleSubmit(newVrm: string, cherishedTransfer: string): void {
+    console.log(cherishedTransfer);
     this.globalErrorService.clearErrors();
     if (newVrm === '' || (newVrm === this.vrm ?? '')) {
-      return this.globalErrorService.addError({ error: 'You must provide a new VRM', anchorLink: 'newVrm' });
+      this.globalErrorService.addError({ error: 'You must provide a new VRM', anchorLink: 'newVrm' });
+    }
+    if (cherishedTransfer === '' || cherishedTransfer === undefined) {
+      this.globalErrorService.addError({ error: 'You must provide a reason for amending', anchorLink: 'cherishedTransfer' });
     }
 
-    this.technicalRecordService
-      .isUnique(newVrm, SEARCH_TYPES.VRM)
-      .pipe(
-        take(1),
-        catchError(error => (error.status == 404 ? of(true) : throwError(() => new Error('Error'))))
-      )
-      .subscribe({
-        next: res => {
-          if (!res) return this.globalErrorService.addError({ error: 'VRM already exists', anchorLink: 'newVrm' });
+    this.globalErrorService.errors$.pipe(take(1)).subscribe({
+      next: res => {
+        if (res.length > 0) return;
+        this.technicalRecordService
+          .isUnique(newVrm, SEARCH_TYPES.VRM)
+          .pipe(
+            take(1),
+            catchError(error => (error.status == 404 ? of(true) : throwError(() => new Error('Error'))))
+          )
+          .subscribe({
+            next: res => {
+              if (!res) return this.globalErrorService.addError({ error: 'VRM already exists', anchorLink: 'newVrm' });
 
-          const newVehicleRecord = this.amendVrm(this.vehicle!, newVrm);
+              const newVehicleRecord = this.amendVrm(this.vehicle!, newVrm, cherishedTransfer === 'true');
 
-          this.setReasonForCreation(newVehicleRecord);
-          this.technicalRecordService.updateEditingTechRecord({ ...newVehicleRecord });
-          this.store.dispatch(updateTechRecords({ systemNumber: this.vehicle!.systemNumber }));
-        },
-        error: e => this.globalErrorService.addError({ error: 'Internal Server Error', anchorLink: 'newVrm' })
-      });
+              this.setReasonForCreation(newVehicleRecord);
+              this.technicalRecordService.updateEditingTechRecord({ ...newVehicleRecord });
+              this.store.dispatch(updateTechRecords({ systemNumber: this.vehicle!.systemNumber }));
+            },
+            error: e => this.globalErrorService.addError({ error: 'Internal Server Error', anchorLink: 'newVrm' })
+          });
+      }
+    });
   }
 
-  amendVrm(record: VehicleTechRecordModel, newVrm: string) {
+  amendVrm(record: VehicleTechRecordModel, newVrm: string, cherishedTransfer: boolean) {
     const newModel: VehicleTechRecordModel = cloneDeep(record);
+    console.log(cloneDeep(newModel.vrms));
+    if (!cherishedTransfer) {
+      const primaryVrm = newModel.vrms.find(vrm => vrm.isPrimary);
+      newModel.vrms.splice(newModel.vrms.indexOf(primaryVrm!), 1);
+      console.log(cloneDeep(newModel.vrms));
+    }
+
     newModel.vrms.forEach(x => (x.isPrimary = false));
 
     const existingVrmObject = newModel.vrms.find(vrm => vrm.vrm == newVrm);
@@ -121,6 +142,7 @@ export class AmendVrmComponent implements OnInit, OnChanges {
       newModel.vrms.push(vrmObject);
     } else existingVrmObject.isPrimary = true;
 
+    console.log(cloneDeep(newModel.vrms));
     return newModel;
   }
 
@@ -139,4 +161,6 @@ export class AmendVrmComponent implements OnInit, OnChanges {
   setReasonForCreation(vehicleRecord: VehicleTechRecordModel) {
     if (vehicleRecord.techRecord !== undefined) vehicleRecord.techRecord.forEach(record => (record.reasonForCreation = `Amending VRM.`));
   }
+
+  toggleCherishedTransfer() {}
 }
