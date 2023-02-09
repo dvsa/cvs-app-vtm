c1c8b5c7bf9db66902f9bb75c1093b970be5c104
owen <owen.corrigan@bjss.com>
2023-02-09 16:07:42 +0000

    fix(cb2-7503): tests written to test what is displayed in the vrm fields

--- src/app/features/tech-record/components/tech-record-title/tech-record-title.component.spec.ts
+++ src/app/features/tech-record/components/tech-record-title/tech-record-title.component.spec.ts
@@ -1,64 +1,112 @@
-import { APP_BASE_HREF } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { ActivatedRoute, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { DynamicFormsModule } from '@forms/dynamic-forms.module';
-import { MultiOptionsService } from '@forms/services/multi-options.service';
-import { mockVehicleTechnicalRecord, mockVehicleTechnicalRecordList } from '@mocks/mock-vehicle-technical-record.mock';
-import { vehicleBodyTypeCodeMap } from '@models/body-type-enum';
+import { mockVehicleTechnicalRecord } from '@mocks/mock-vehicle-technical-record.mock';
 import { Roles } from '@models/roles.enum';
-import { VehicleClass } from '@models/vehicle-class.model';
-import { TechRecordModel, VehicleTechRecordModel, VehicleTypes } from '@models/vehicle-tech-record.model';
-import { StoreModule } from '@ngrx/store';
-import { provideMockStore } from '@ngrx/store/testing';
+import { VehicleTypes } from '@models/vehicle-tech-record.model';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { TechnicalRecordService } from '@services/technical-record/technical-record.service';
 import { UserService } from '@services/user-service/user-service';
 import { SharedModule } from '@shared/shared.module';
-import { initialAppState } from '@store/index';
+import { initialAppState, State } from '@store/index';
+import { editableTechRecord } from '@store/technical-records';
 import { Observable, of } from 'rxjs';
-import { TechRecordComponent } from '../../tech-record.component';
-
 import { TechRecordTitleComponent } from './tech-record-title.component';
+
+const MockUserService = {
+  getUserName$: jest.fn().mockReturnValue(new Observable()),
+  roles$: of([Roles.TestResultAmend, Roles.TestResultView])
+};
 
 describe('TechRecordTitleComponent', () => {
   let component: TechRecordTitleComponent;
   let fixture: ComponentFixture<TechRecordTitleComponent>;
-  let router: Router;
+  let store: MockStore<State>;
+  let technicalRecordService: TechnicalRecordService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TechRecordTitleComponent],
       imports: [DynamicFormsModule, HttpClientTestingModule, RouterTestingModule, SharedModule],
-      providers: [MultiOptionsService, provideMockStore({ initialState: initialAppState })]
+      providers: [provideMockStore({ initialState: initialAppState }), { provide: UserService, useValue: MockUserService }, TechnicalRecordService]
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TechRecordTitleComponent);
+    store = TestBed.inject(MockStore);
     component = fixture.componentInstance;
+    technicalRecordService = TestBed.inject(TechnicalRecordService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  describe('the VRM fields', () => {
+    it('should show primary VRM', () => {
+      const mockRecord = mockVehicleTechnicalRecord(VehicleTypes.PSV).techRecord.pop()!;
+      const viewableTechRecordSpy = jest.spyOn(technicalRecordService, 'viewableTechRecord$').mockReturnValue(of(mockRecord));
+      const mockVehicle = {
+        vrms: [
+          { vrm: 'TESTVRM', isPrimary: true },
+          { vrm: 'TESTVRM2', isPrimary: false }
+        ],
+        vin: 'testvin',
+        systemNumber: 'testNumber',
+        techRecord: [mockRecord]
+      };
+      component.vehicle = mockVehicle;
+      store.overrideSelector(editableTechRecord, mockRecord);
+      fixture.detectChanges();
 
-  it('should show primary VRM', () => {
-    const mockRecord = mockVehicleTechnicalRecord(VehicleTypes.PSV).techRecord.pop()!;
-    const mockVehicle = {
-      vrms: [
-        { vrm: 'TESTVRM', isPrimary: true },
-        { vrm: 'TESTVRM2', isPrimary: false }
-      ],
-      vin: 'testvin',
-      systemNumber: 'testNumber',
-      techRecord: [mockRecord]
-    };
-    component.vehicle = mockVehicle;
-    component.currentTechRecord$ = of(mockRecord);
-    fixture.detectChanges();
-    const vrmField = fixture.nativeElement.querySelector('app-number-plate');
-    // expect(vrmField).toBeTruthy()
+      const vrmField = fixture.debugElement.query(By.css('#current-vrm-span')).nativeElement;
+      expect(vrmField.textContent).toContain('TEST VRM');
+    });
+
+    it('should show the newest (last) secondary VRM', () => {
+      const mockRecord = mockVehicleTechnicalRecord(VehicleTypes.PSV).techRecord.pop()!;
+      const viewableTechRecordSpy = jest.spyOn(technicalRecordService, 'viewableTechRecord$').mockReturnValue(of(mockRecord));
+      const mockVehicle = {
+        vrms: [
+          { vrm: 'TESTVRM6', isPrimary: true },
+          { vrm: 'TESTVRM', isPrimary: false },
+          { vrm: 'TESTVRM2', isPrimary: false },
+          { vrm: 'TESTVRM3', isPrimary: false },
+          { vrm: 'TESTVRM4', isPrimary: false },
+          { vrm: 'TESTVRM5', isPrimary: false }
+        ],
+        vin: 'testvin',
+        systemNumber: 'testNumber',
+        techRecord: [mockRecord]
+      };
+      component.vehicle = mockVehicle;
+      store.overrideSelector(editableTechRecord, mockRecord);
+      fixture.detectChanges();
+
+      const vrmField = fixture.debugElement.query(By.css('#previous-vrm-span')).nativeElement;
+      expect(vrmField.textContent).toContain('TESTV RM5');
+      expect(vrmField.textContent).not.toContain('TEST VRM');
+      expect(vrmField.textContent).not.toContain('TESTV RM2');
+      expect(vrmField.textContent).not.toContain('TESTV RM3');
+      expect(vrmField.textContent).not.toContain('TESTV RM4');
+    });
+    it('should not create previous-vrm-span if no secondary vrm exists', () => {
+      const mockRecord = mockVehicleTechnicalRecord(VehicleTypes.PSV).techRecord.pop()!;
+      const viewableTechRecordSpy = jest.spyOn(technicalRecordService, 'viewableTechRecord$').mockReturnValue(of(mockRecord));
+      const mockVehicle = {
+        vrms: [{ vrm: 'TESTVRM', isPrimary: true }],
+        vin: 'testvin',
+        systemNumber: 'testNumber',
+        techRecord: [mockRecord]
+      };
+      component.vehicle = mockVehicle;
+      store.overrideSelector(editableTechRecord, mockRecord);
+      fixture.detectChanges();
+
+      const vrmField = fixture.debugElement.query(By.css('#previous-vrm-span'));
+      expect(vrmField).toBe(null);
+    });
   });
 });
