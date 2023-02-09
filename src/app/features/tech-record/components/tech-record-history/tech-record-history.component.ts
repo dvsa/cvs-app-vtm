903debe96eab47ecc6fcad068646c46e871da863
Ruairidh Taylor <109070456+Ruairidh-BJSS@users.noreply.github.com>
2023-02-09 09:34:20 +0000

    feat(cb2-7512): fix view tech record history (#844)

--- src/app/features/tech-record/components/tech-record-history/tech-record-history.component.ts
+++ src/app/features/tech-record/components/tech-record-history/tech-record-history.component.ts
@@ -41,11 +41,11 @@ export class TechRecordHistoryComponent {
   summaryLinkUrl(techRecord: TechRecordModel) {
     switch (techRecord.statusCode) {
       case StatusCodes.PROVISIONAL:
-        return 'provisional';
+        return `/tech-records/${this.vehicle.systemNumber}/provisional`;
       case StatusCodes.ARCHIVED:
-        return `historic/${this.convertToUnix(techRecord.createdAt)}`;
+        return `/tech-records/${this.vehicle.systemNumber}/historic/${this.convertToUnix(techRecord.createdAt)}`;
       default:
-        return '';
+        return `/tech-records/${this.vehicle.systemNumber}/`;
     }
   }
 }
