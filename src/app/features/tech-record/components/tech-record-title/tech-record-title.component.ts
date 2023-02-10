978e93072bb35e553cbd9cb34c200038b9ccc2c7
owen <owen.corrigan@bjss.com>
2023-02-10 14:02:50 +0000

    feat(cb2-7330): deleted unused code

--- src/app/features/tech-record/components/tech-record-title/tech-record-title.component.ts
+++ src/app/features/tech-record/components/tech-record-title/tech-record-title.component.ts
@@ -14,7 +14,6 @@ import { Observable, take } from 'rxjs';
   styleUrls: ['./tech-record-title.component.scss']
 })
 export class TechRecordTitleComponent implements OnInit {
-  [x: string]: any;
   @Input() vehicle?: VehicleTechRecordModel;
   @Input() actions: TechRecordActions = TechRecordActions.NONE;
   @Input() hideActions: boolean = false;
