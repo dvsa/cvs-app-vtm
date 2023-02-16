b0344d54c69d5bb960c92bbea2b4f46f2d537957
leifkemp-bjss <leif.kemp@bjss.com>
2023-02-16 15:47:00 +0000

    feat(cb2-7471): add generateletter to service

--- src/app/services/technical-record/technical-record.service.ts
+++ src/app/services/technical-record/technical-record.service.ts
@@ -312,10 +312,28 @@ export class TechnicalRecordService {
     return this.http.post<VehicleTechRecordModel>(url, body, { responseType: 'json' });
   }
 
-  generateLetter(techRecord: TechRecordModel, letterType: string) {
-    // TODO: Implement API call when ready
-    console.log('Piiing.');
-    return of(true);
+  generateLetter(
+    vehicleRecord: VehicleTechRecordModel,
+    techRecord: TechRecordModel,
+    letterType: string,
+    paragraphId: number,
+    user: { id?: string; name?: string }
+  ) {
+    const url = `${environment.VTM_API_URI}/vehicles/documents/letter`;
+
+    const body = {
+      vin: vehicleRecord.vin,
+      primaryVrm: undefined,
+      systemNumber: vehicleRecord.systemNumber,
+      trailerId: vehicleRecord.trailerId,
+      msUserDetails: { msOid: user.id, msUser: user.name },
+      techRecord: vehicleRecord.techRecord,
+      vtmUsername: user.name,
+      letterType: letterType,
+      paragraphId: paragraphId
+    };
+
+    return this.http.post<VehicleTechRecordModel>(url, body, { responseType: 'json' });
   }
 
   private formatVrmsForUpdatePayload(vehicleTechRecord: VehicleTechRecordModel): PutVehicleTechRecordModel {
