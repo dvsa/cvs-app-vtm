7f116ec177e52b97d5cb88c9c91891cbdc98affc
owen <owen.corrigan@bjss.com>
2023-02-20 14:02:43 +0000

    feat(cb2-7603): added a missing prefix

--- src/app/store/technical-records/actions/technical-record-service.actions.ts
+++ src/app/store/technical-records/actions/technical-record-service.actions.ts
@@ -54,7 +54,7 @@ export const updateEditingTechRecordCancel = createAction(`${prefix} updateEditi
 export const changeVehicleType = createAction(`${prefix} changeVehicleType`, props<{ vehicleType: VehicleTypes }>());
 
 export const amendVin = createAction(`${prefix} amendVin`, props<{ oldVin: string; newVin: string; systemNumber: string }>());
-export const amendVinSuccess = createAction('amendVin Success');
+export const amendVinSuccess = createAction(`${prefix} amendVin Success`);
 export const amendVinFailure = createOutcomeAction('amendVin');
 
 export const createVehicle = createAction(`${prefix} createVehicle`, props<{ vehicleType: VehicleTypes }>());
