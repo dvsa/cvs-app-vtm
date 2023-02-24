d21476884c8f80e6619f13f8abd2b2fc82cb2e71
leifkemp-bjss <leif.kemp@bjss.com>
2023-02-24 11:26:53 +0000

    feat(cb2-6749): add userservice to unit tests

--- src/app/features/tech-record/components/tech-record-generate-letter/tech-record-generate-letter.component.spec.ts
+++ src/app/features/tech-record/components/tech-record-generate-letter/tech-record-generate-letter.component.spec.ts
@@ -16,6 +16,7 @@ import { GenerateLetterComponent } from './tech-record-generate-letter.component
 import { SharedModule } from '@shared/shared.module';
 import { DynamicFormsModule } from '@forms/dynamic-forms.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { UserService } from '@services/user-service/user-service';
 
 const mockTechRecordService = {
   editableTechRecord$: of({}),
@@ -49,7 +50,13 @@ describe('TechRecordGenerateLetterComponent', () => {
         provideMockStore({ initialState: initialAppState }),
         { provide: ActivatedRoute, useValue: { params: of([{ id: 1 }]) } },
         { provide: DynamicFormService, useValue: mockDynamicFormService },
-        { provide: TechnicalRecordService, useValue: mockTechRecordService }
+        { provide: TechnicalRecordService, useValue: mockTechRecordService },
+        {
+          provide: UserService,
+          useValue: {
+            roles$: of(['TechRecord.Amend'])
+          }
+        }
       ],
       imports: [RouterTestingModule, SharedModule, ReactiveFormsModule, DynamicFormsModule]
     }).compileComponents();
