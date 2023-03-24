64f72d950feb6cae44d794553cb7e286877043e4
Guillaume Julien <guillaume.julien@bjss.com>
2023-03-24 10:07:24 +0000

    fix(cb2-7893): imports

--- src/app/guards/cancel-edit-tech/cancel-edit-tech.guard.ts
+++ src/app/guards/cancel-edit-tech/cancel-edit-tech.guard.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, CanDeactivate } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { State } from '@store/index';
 import { updateEditingTechRecordCancel } from '@store/technical-records';
-import { Observable } from 'rxjs';
 import { TechRecordComponent } from 'src/app/features/tech-record/tech-record.component';
 
 @Injectable({
