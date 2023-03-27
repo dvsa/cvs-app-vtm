aa339956eed2f7af363ac8f10361f2c4a1c6d07e
owen <owen.corrigan@bjss.com>
2023-03-27 10:03:26 +0100

    feat(cb2-7046): gets system number and treats put and post differently

--- src/app/features/tech-record/create-batch-trl/components/batch-trl-template/batch-trl-template.component.ts
+++ src/app/features/tech-record/create-batch-trl/components/batch-trl-template/batch-trl-template.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { VehicleTechRecordModel, VehicleTypes } from '@models/vehicle-tech-record.model';
 import { Store } from '@ngrx/store';
 import { TechnicalRecordService } from '@services/technical-record/technical-record.service';
-import { createVehicleRecord } from '@store/technical-records';
+import { createVehicleRecord, updateEditingTechRecord, updateTechRecords } from '@store/technical-records';
 import { TechnicalRecordServiceState } from '@store/technical-records/reducers/technical-record-service.reducer';
 import { map, Observable, take, withLatestFrom } from 'rxjs';
 import { TechRecordSummaryComponent } from '../../../components/tech-record-summary/tech-record-summary.component';
@@ -60,13 +60,21 @@ export class BatchTrlTemplateComponent {
                   ...record!,
                   vin: v.vin,
                   vrms: v.trailerId ? [{ vrm: v.trailerId, isPrimary: true }] : null,
-                  trailerId: v.trailerId ? v.trailerId : null
+                  trailerId: v.trailerId ? v.trailerId : null,
+                  systemNumber: v.systemNumber ? v.systemNumber : null
                 } as VehicleTechRecordModel)
             )
           )
         )
         .subscribe(vehicleList => {
-          vehicleList.forEach(vehicle => this.store.dispatch(createVehicleRecord({ vehicle })));
+          vehicleList.forEach(vehicle => {
+            if (!vehicle.systemNumber) {
+              this.store.dispatch(createVehicleRecord({ vehicle }));
+            } else {
+              this.technicalRecordService.updateEditingTechRecord(vehicle);
+              this.store.dispatch(updateTechRecords({ systemNumber: vehicle.systemNumber }));
+            }
+          });
 
           this.router.navigate(['batch-results'], { relativeTo: this.route });
         });
