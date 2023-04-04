62f0f41e6caed62a862d7ae1a86c8e6b170a6791
Guillaume Julien <guillaume.julien@bjss.com>
2023-04-04 16:14:21 +0100

    more tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -5,8 +5,8 @@ describe('View the home page', () => {
     cy.visit('');
   });
 
-  it('should create a new vehicle', () => {
-    vin = randomString();
+  it('should create a new PSV', () => {
+    vin = randomString(10);
     cy.intercept('POST', '/develop/vehicles').as('create-vehicle');
     cy.get('#create-new-technical-record-link').click();
     cy.get('#input-vin').type(vin);
@@ -20,7 +20,7 @@ describe('View the home page', () => {
     cy.get('#dtpNumber__option--2').click();
     cy.wait(400);
     cy.get('app-button > #submit-record-continue').click();
-    cy.get('@create-vehicle', { timeout: 10000 }).its('response', { timeout: 10000 }).should('have.property', 'statusCode', 201);
+    cy.get('@create-vehicle').its('response', { timeout: 12000 }).should('have.property', 'statusCode', 201);
   });
 
   it('should amend the vehicle type', () => {
@@ -38,8 +38,65 @@ describe('View the home page', () => {
     cy.wait('@get-vehicle');
     cy.get('#vehicleType').should('have.text', ' HGV ');
   });
+
+  it('should amend the vrm', () => {
+    const newVrm = randomString(7);
+    cy.get('#search-for-technical-record-link').click();
+    cy.get('#search-term').type(vin + '{enter}');
+    cy.get('a').contains('Select technical record').click();
+    cy.get('app-button > #change-vrm-link').click();
+    cy.get('.govuk-input').type(newVrm);
+    cy.get('#-true-radio').click();
+    cy.get('#submit-vrm').click();
+    cy.get('#current-vrm-span').should(vrm => {
+      const vrmSplit = newVrm.slice(0, newVrm.length - 3) + ' ' + newVrm.slice(newVrm.length - 3);
+      expect(vrm).to.contain(vrmSplit.toUpperCase());
+    });
+  });
+
+  it('should promote the record', () => {
+    cy.get('#search-for-technical-record-link').click();
+    cy.get('#search-term').type(vin + '{enter}');
+    cy.get('a').contains('Select technical record').click();
+    cy.get(':nth-child(2) > #promote-link').click();
+    cy.get('#reason').type('testing promote');
+    cy.get('#submit-change-status').click();
+    cy.get('#status-code').should('have.text', 'Current');
+  });
+
+  it('should create a new LGV', () => {
+    vin = randomString(10);
+    cy.intercept('POST', '/develop/vehicles').as('create-vehicle');
+    cy.get('#create-new-technical-record-link').click();
+    cy.get('#input-vin').type(vin);
+    cy.get('#generate-c-t-z-num-true-checkbox').click();
+    cy.get('#change-vehicle-type-select').select('2: lgv');
+    cy.get(':nth-child(6) > #create-record-continue').click();
+    cy.get('.govuk-accordion__show-all-text').click();
+    cy.get('#reasonForCreation').type('testing create');
+    cy.get('#vehicleSubclass-l-checkbox').click();
+    cy.wait(400);
+    cy.get('app-button > #submit-record-continue').click();
+    cy.get('@create-vehicle').its('response', { timeout: 12000 }).should('have.property', 'statusCode', 201);
+  });
+
+  it('should create a DBA', () => {
+    cy.get('#search-for-technical-record-link').click();
+    cy.get('#search-term').type(vin + '{enter}');
+    cy.get('a').contains('Select technical record').click();
+    cy.get('#create-test').click();
+    cy.get('#test-list-item-400', { timeout: 12000 }).click();
+    cy.get('#browse-list-item-413').click();
+    cy.get('#browse-list-item-433').click();
+    cy.get('#reasonForCreation', { timeout: 15000 }).type('testing create DBA');
+    cy.get('#certificateNumber').type('123456');
+    cy.get('#review-test-result').click();
+    cy.get('#submit-test-result').click();
+    cy.get('#test-record-summary-name-0').should('have.text', 'IVA17 Appeal for IVA');
+  });
 });
 
-const randomString = () => {
-  return (Math.random() + 1).toString(36).substring(3);
+const randomString = (length: number = 5) => {
+  const randomString = (Math.random() + 1).toString(36);
+  return randomString.substring(randomString.length - length);
 };
