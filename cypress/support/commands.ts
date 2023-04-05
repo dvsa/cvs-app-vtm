8c66df2eaf7c366543227a96ffe03f8701e7e342
Nathan Brown <nathan.brown@bjss.com>
2023-04-05 16:45:24 +0100

    remove bad command change

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -9,7 +9,6 @@ declare namespace Cypress {
 function loginViaAAD(username: string, password: string) {
   //Login to your AAD tenant.
   cy.visit('');
-  cy.get('input[type="email"]', { timeout: 10000 }).should('be.visible');
   cy.origin(
     'login.microsoftonline.com',
     {
