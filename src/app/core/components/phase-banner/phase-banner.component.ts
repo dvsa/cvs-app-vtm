506af3b706c89d26a0bb8c8e6a5e48b1d364f12a
owen <owen.corrigan@bjss.com>
2023-04-12 15:43:30 +0100

    feat(cb2-7886): centered the link and added feedback uri to environment ts

--- src/app/core/components/phase-banner/phase-banner.component.ts
+++ src/app/core/components/phase-banner/phase-banner.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-phase-banner',
   templateUrl: './phase-banner.component.html',
-  styleUrls: ['./phase-banner.component.scss']
+  styleUrls: ['./phase-banner.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PhaseBannerComponent implements OnInit {
+  private count = 0;
   constructor() {}
 
   ngOnInit(): void {}
+
+  get feedbackUri(): string {
+    this.count++;
+    console.log(this.count);
+    return environment.FEEDBACK_URI;
+  }
 }
