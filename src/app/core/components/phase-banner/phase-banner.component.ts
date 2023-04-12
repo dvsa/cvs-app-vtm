0f7f8e7b40334a27022d01dde4bef6c53e4f72c0
owen <owen.corrigan@bjss.com>
2023-04-12 14:04:38 +0100

    feat(cb2-7886): created the phase banner component

---
+++ src/app/core/components/phase-banner/phase-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phase-banner',
+  templateUrl: './phase-banner.component.html',
+  styleUrls: ['./phase-banner.component.scss']
+})
+export class PhaseBannerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
