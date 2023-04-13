c89dde6b0942e7921dc5cd4f2685b2506f88029b
Emily.Kelly <emily.kelly@bjss.com>
2023-04-13 13:35:39 +0100

    feat(cb2-7981): updated reference data test

--- src/app/features/reference-data/reference-data.component.spec.ts
+++ src/app/features/reference-data/reference-data.component.spec.ts
@@ -1,20 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { initialAppState, State } from '@store/.';
 import { ReferenceDataComponent } from './reference-data.component';
 
 describe('ReferenceDataComponent', () => {
   let component: ReferenceDataComponent;
   let fixture: ComponentFixture<ReferenceDataComponent>;
+  let store: MockStore<State>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ReferenceDataComponent]
+      declarations: [ReferenceDataComponent],
+      imports: [RouterTestingModule],
+      providers: [provideMockStore({ initialState: initialAppState })]
     }).compileComponents();
   });
 
   beforeEach(() => {
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(ReferenceDataComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
