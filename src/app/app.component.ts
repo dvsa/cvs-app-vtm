47f5f886f485ca2b5935fed27dae056b94d15357
Kristian Carr <kristian.carr@bjss.com>
2023-04-14 14:15:10 +0100

    fix(cb2-7887): ensured that subscription to google analytics service is destroyed when appropriate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,22 @@
 ///<reference path="govuk.d.ts">
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router, NavigationEnd, Event, ActivatedRoute } from '@angular/router';
 import { Store, select } from '@ngrx/store';
 import { LoadingService } from '@services/loading/loading.service';
 import { UserService } from '@services/user-service/user-service';
 import { selectRouteData } from '@store/router/selectors/router.selectors';
 import { initAll } from 'govuk-frontend/govuk/all';
-import { take, map } from 'rxjs';
+import { take, map, Subject, takeUntil } from 'rxjs';
 import { State } from './store';
 import { GoogleAnalyticsService } from '@services/google-analytics/google-analytics.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+
   constructor(
     public userService: UserService,
     private loadingService: LoadingService,
@@ -23,15 +24,20 @@ export class AppComponent implements OnInit {
     private store: Store<State>,
     private googleAnalyticsService: GoogleAnalyticsService
   ) {
-    this.router.events.subscribe((event: Event) => {
+    this.router.events.pipe(takeUntil(this.destroy$)).subscribe((event: Event) => {
       if (event instanceof NavigationEnd) {
         this.googleAnalyticsService.pageView(document.title, event.urlAfterRedirects);
       }
-    });
+    })
   }
 
   ngOnInit() {
     initAll();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   get isStandardLayout() {
