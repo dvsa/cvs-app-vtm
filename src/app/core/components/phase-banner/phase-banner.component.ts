03c9f44291b141f8f4ad429e73d510ff0b2ff897
owen <owen.corrigan@bjss.com>
2023-04-14 10:04:53 +0100

    feat(cb2-7886): fixed the path so tests pass

--- src/app/core/components/phase-banner/phase-banner.component.ts
+++ src/app/core/components/phase-banner/phase-banner.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../../../environments/environment.deploy';
 
 @Component({
   selector: 'app-phase-banner',
