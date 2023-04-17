38a1f36e673602ca7a1c2e6ecc1c1f1b459acaef
Emily.Kelly <emily.kelly@bjss.com>
2023-04-17 12:54:14 +0100

    feat(cb2-7981): add country of registration form created

--- src/app/features/reference-data/add-reference-data/add-reference-data.component.ts
+++ src/app/features/reference-data/add-reference-data/add-reference-data.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GlobalErrorService } from '@core/components/global-error/global-error.service';
+import { DynamicFormService } from '@forms/services/dynamic-form.service';
+import { CustomFormControl, CustomFormGroup, FormNodeOption, FormNodeTypes } from '@forms/services/dynamic-form.types';
+import { CustomValidators } from '@forms/validators/custom-validators';
 import { Roles } from '@models/roles.enum';
 
 @Component({
@@ -8,7 +12,33 @@ import { Roles } from '@models/roles.enum';
   templateUrl: './add-reference-data.component.html'
 })
 export class AddReferenceDataComponent implements OnInit {
-  constructor(public globalErrorService: GlobalErrorService, private route: ActivatedRoute, private router: Router) {}
+  form!: CustomFormGroup;
+
+  constructor(public globalErrorService: GlobalErrorService, private route: ActivatedRoute, private router: Router) {
+    this.form = new CustomFormGroup(
+      { name: 'main-form', type: FormNodeTypes.GROUP },
+      {
+        resourceKey: new CustomFormControl(
+          {
+            name: 'resource-key',
+            label: 'resource-key',
+            type: FormNodeTypes.CONTROL
+          },
+          '',
+          [Validators.required]
+        ),
+        description: new CustomFormControl(
+          {
+            name: 'description',
+            label: 'description',
+            type: FormNodeTypes.CONTROL
+          },
+          '',
+          [Validators.required]
+        )
+      }
+    );
+  }
 
   ngOnInit(): void {}
 
@@ -16,6 +46,8 @@ export class AddReferenceDataComponent implements OnInit {
   //     this.router.navigate(['../reference-data/data-type-list'], { relativeTo: this.route });
 
   // }
+
+  ngOnChanges(): void {}
 
   get roles() {
     return Roles;
