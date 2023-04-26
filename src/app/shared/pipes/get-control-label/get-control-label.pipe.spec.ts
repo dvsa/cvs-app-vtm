9961cea6b99ae93bdfed4baabdde4f1dd033cf32
Leif Kemp <leif.kemp@bjss.com>
2023-04-26 09:08:04 +0100

    feat(cb2-7947): added unit tests

--- src/app/shared/pipes/get-control-label/get-control-label.pipe.spec.ts
+++ src/app/shared/pipes/get-control-label/get-control-label.pipe.spec.ts
@@ -1,8 +1,29 @@
 import { GetControlLabelPipe } from './get-control-label.pipe';
+import { FormNode, FormNodeOption } from '@forms/services/dynamic-form.types';
 
 describe('GetControlLabelPipe', () => {
+  let testOptions: FormNodeOption<string>[];
+  let testFormNode: FormNode;
+
+  beforeEach(() => {
+    testOptions = [
+      { label: 'Test', value: 'test' },
+      { label: 'Foo', value: 'bar' }
+    ];
+  });
+
   it('create an instance', () => {
     const pipe = new GetControlLabelPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return the label if one is available', () => {
+    const pipe = new GetControlLabelPipe();
+    expect(pipe.transform('test', testOptions)).toBe('Test');
+  });
+
+  it('should return the value if no label is available', () => {
+    const pipe = new GetControlLabelPipe();
+    expect(pipe.transform('foo', testOptions)).toBe('foo');
   });
 });
