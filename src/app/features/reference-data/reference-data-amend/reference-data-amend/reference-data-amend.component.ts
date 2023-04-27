c77c8ce8b263fdddef6b46daa28a017899364f23
Emily.Kelly <emily.kelly@bjss.com>
2023-04-27 10:11:26 +0100

    feat(cb2-8142): add routing for amend reference data component

---
+++ src/app/features/reference-data/reference-data-amend/reference-data-amend/reference-data-amend.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reference-data-amend',
+  templateUrl: './reference-data-amend.component.html'
+})
+export class ReferenceDataAmendComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
