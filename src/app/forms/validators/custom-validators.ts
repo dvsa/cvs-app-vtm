8cb7ee506d5cd7d3996c4b17a1ea65de3bb576cc
Kristian Carr <kristian.carr@bjss.com>
2023-05-04 11:33:35 +0100

    fix(cb2-8151): added custom validator to handle input length validation for the VRM/TraileId field

--- src/app/forms/validators/custom-validators.ts
+++ src/app/forms/validators/custom-validators.ts
@@ -1,5 +1,6 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 import { CustomFormControl } from '@forms/services/dynamic-form.types';
+import { VehicleTypes } from '@models/vehicle-tech-record.model';
 
 export class CustomValidators {
   static hideIfEmpty = (sibling: string): ValidatorFn => {
@@ -107,6 +108,40 @@ export class CustomValidators {
 
         if (!newValue && (control.value === null || control.value === undefined || control.value === '')) {
           return { requiredIfNotEqual: { sibling: siblingControl.meta.label } };
+        }
+      }
+
+      return null;
+    };
+  };
+
+  static validateVRMTrailerIdLength = (sibling: string): ValidatorFn => {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (!control.value) {
+        return null;
+      }
+      
+      if (control?.parent) {
+        const siblingControl = control.parent.get(sibling) as CustomFormControl;
+        const siblingValue = siblingControl.value;
+
+        const isTrailerValueSelected = siblingValue === VehicleTypes.TRL;
+        
+        if(isTrailerValueSelected) {
+          if(control.value.length < 7) {
+            return { validateVRMTrailerIdLength: { message: 'Trailer ID must be greater than or equal to 7 characters' } };
+          } 
+          else if (control.value.length > 8) {
+            return { validateVRMTrailerIdLength: { message: 'Trailer ID must be less than or equal to 8 characters' } };
+          }
+        }
+        else {
+          if(control.value.length < 1) {
+            return { validateVRMTrailerIdLength: { message: 'VRM must be less than or equal to 1 characters' } };
+          } 
+          else if (control.value.length > 9) {
+            return { validateVRMTrailerIdLength: { message: 'VRM must be less than or equal to 9 characters' } };
+          }
         }
       }
 
