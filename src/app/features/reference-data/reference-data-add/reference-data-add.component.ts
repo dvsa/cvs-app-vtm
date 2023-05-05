cbed043c28471c54eaed6ee0bcc2b0f70c0157be
Emily.Kelly <emily.kelly@bjss.com>
2023-05-05 15:05:19 +0100

    fix(cb2-8192): ref data add renders dynamic form and pulls inputs from templates with validation

--- src/app/features/reference-data/reference-data-add/reference-data-add.component.ts
+++ src/app/features/reference-data/reference-data-add/reference-data-add.component.ts
@@ -1,35 +1,40 @@
-import { Component } from '@angular/core';
-import { Validators } from '@angular/forms';
+import { Component, QueryList, ViewChildren } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GlobalError } from '@core/components/global-error/global-error.interface';
 import { GlobalErrorService } from '@core/components/global-error/global-error.service';
+import { DynamicFormGroupComponent } from '@forms/components/dynamic-form-group/dynamic-form-group.component';
 import { DynamicFormService } from '@forms/services/dynamic-form.service';
-import { CustomFormControl, CustomFormGroup, FormNodeEditTypes, FormNodeTypes, FormNodeWidth } from '@forms/services/dynamic-form.types';
+import { CustomFormGroup, FormNode, FormNodeWidth } from '@forms/services/dynamic-form.types';
+import { template as brakesTemplate } from '@forms/templates/reference-data/brakes';
+import { template as countryOfRegistrationTemplate } from '@forms/templates/reference-data/country-of-registration';
+import { template as hgvTemplate } from '@forms/templates/reference-data/hgv-make';
+import { template as psvTemplate } from '@forms/templates/reference-data/psv-make';
+import { template as reasonsForAbandoningTirTemplate } from '@forms/templates/reference-data/reasons-for-abandoning-TIR';
+import { template as reasonsForAbandoningTrlTemplate } from '@forms/templates/reference-data/reasons-for-abandoning-TRL';
+import { template as reasonsForAbandoningHgvTemplate } from '@forms/templates/reference-data/reasons-for-abandoning-hgv';
+import { template as reasonsForAbandoningPsvTemplate } from '@forms/templates/reference-data/reasons-for-abandoning-psv';
+import { template as specialistReasonsForAbandoningTemplate } from '@forms/templates/reference-data/specialist-reasons-for-abandoning';
+import { template as trlTemplate } from '@forms/templates/reference-data/trl-make';
+import { template as tyresTemplate } from '@forms/templates/reference-data/tyres';
 import { ReferenceDataResourceType } from '@models/reference-data.model';
 import { Roles } from '@models/roles.enum';
 import { Store } from '@ngrx/store';
 import { ReferenceDataService } from '@services/reference-data/reference-data.service';
-import { ReferenceDataState, selectReferenceDataByResourceKey } from '@store/reference-data';
-import { switchMap, take, tap } from 'rxjs';
+import { ReferenceDataState } from '@store/reference-data';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-reference-data-add',
   templateUrl: './reference-data-add.component.html'
 })
-export class AddReferenceDataComponent {
+export class ReferenceDataCreateComponent {
+  isEditing: boolean = true;
   type: ReferenceDataResourceType = ReferenceDataResourceType.Brakes;
+  newRefData: any;
+  isFormDirty: boolean = false;
+  isFormInvalid: boolean = true;
 
-  form: CustomFormGroup = new CustomFormGroup(
-    { name: 'form', type: FormNodeTypes.GROUP },
-    {
-      resourceKey: new CustomFormControl({
-        name: 'resourceKey',
-        label: 'Resource Key',
-        type: FormNodeTypes.CONTROL,
-        editType: FormNodeEditTypes.TEXT
-      })
-    }
-  );
+  @ViewChildren(DynamicFormGroupComponent) sections!: QueryList<DynamicFormGroupComponent>;
 
   constructor(
     public globalErrorService: GlobalErrorService,
@@ -39,34 +44,14 @@ export class AddReferenceDataComponent {
     private store: Store<ReferenceDataState>
   ) {
     this.referenceDataService.loadReferenceData(ReferenceDataResourceType.ReferenceDataAdminType);
+  }
 
-    this.route.queryParams
-      .pipe(
-        take(1),
-        tap(params => (this.type = params['type'])),
-        switchMap(params => this.store.select(selectReferenceDataByResourceKey(ReferenceDataResourceType.ReferenceDataAdminType, params['type'])))
-      )
-      .subscribe(referenceData => {
-        if (!referenceData) return;
+  ngOnInit(): void {
+    this.route.params.pipe(take(1)).subscribe(params => {
+      this.type = params['type'];
 
-        Object.keys(referenceData)
-          .filter(property => property !== 'resourceType' && property !== 'resourceKey')
-          .forEach(property =>
-            this.form.addControl(
-              property,
-              new CustomFormControl(
-                {
-                  name: property,
-                  label: property,
-                  type: FormNodeTypes.CONTROL,
-                  editType: (referenceData as any)[property]
-                },
-                '',
-                [Validators.required]
-              )
-            )
-          );
-      });
+      console.log('This is the type by url params: ', this.type);
+    });
   }
 
   get roles(): typeof Roles {
@@ -77,13 +62,52 @@ export class AddReferenceDataComponent {
     return FormNodeWidth;
   }
 
-  get isFormValid(): boolean {
-    if (!this.form) return false;
-
-    const errors: GlobalError[] = [];
-    DynamicFormService.validate(this.form, errors);
-    this.globalErrorService.setErrors(errors);
-    return this.form.valid;
+  get template(): FormNode {
+    let templateToReturn: FormNode;
+    switch (this.type) {
+      case ReferenceDataResourceType.Brakes:
+        templateToReturn = brakesTemplate;
+        break;
+      case ReferenceDataResourceType.CountryOfRegistration:
+        templateToReturn = countryOfRegistrationTemplate;
+        break;
+      case ReferenceDataResourceType.HgvMake:
+        templateToReturn = hgvTemplate;
+        break;
+      case ReferenceDataResourceType.PsvMake:
+        templateToReturn = psvTemplate;
+        break;
+      case ReferenceDataResourceType.ReasonsForAbandoningHgv:
+        templateToReturn = reasonsForAbandoningHgvTemplate;
+        break;
+      case ReferenceDataResourceType.ReasonsForAbandoningPsv:
+        templateToReturn = reasonsForAbandoningPsvTemplate;
+        break;
+      case ReferenceDataResourceType.ReasonsForAbandoningTrl:
+        templateToReturn = reasonsForAbandoningTrlTemplate;
+        break;
+      case ReferenceDataResourceType.SpecialistReasonsForAbandoning:
+        templateToReturn = specialistReasonsForAbandoningTemplate;
+        break;
+      case ReferenceDataResourceType.TirReasonsForAbandoning:
+        templateToReturn = reasonsForAbandoningTirTemplate;
+        break;
+      case ReferenceDataResourceType.TrlMake:
+        templateToReturn = trlTemplate;
+        break;
+      case ReferenceDataResourceType.Tyres:
+        templateToReturn = tyresTemplate;
+        break;
+      default:
+        templateToReturn = {} as FormNode;
+        break;
+    }
+    templateToReturn.children?.forEach(child => {
+      if (child.name === 'resourceKey') {
+        child.disabled = false;
+      }
+    });
+    return templateToReturn;
   }
 
   titleCaseHeading(input: ReferenceDataResourceType): string {
@@ -100,17 +124,41 @@ export class AddReferenceDataComponent {
   }
 
   handleSubmit() {
-    if (!this.isFormValid) return;
+    this.checkForms();
+
+    if (this.isFormInvalid) return;
 
     const referenceData: any = {};
 
-    Object.keys(this.form.controls)
-      .filter(control => control !== 'resourceKey')
-      .forEach(control => (referenceData[control] = this.form.get(control)?.value));
+    Object.keys(this.newRefData)
+      .filter(newRefDataKey => newRefDataKey !== 'resourceKey')
+      .forEach(dataKey => (referenceData[dataKey] = this.newRefData[dataKey]));
 
     this.referenceDataService
-      .createNewReferenceDataItem(this.type, this.form.get('resourceKey')?.value, referenceData)
+      .createNewReferenceDataItem(this.type, this.newRefData.resourceKey, referenceData)
       .pipe(take(1))
       .subscribe(() => this.navigateBack());
+  }
+
+  handleFormChange(event: any) {
+    this.newRefData = event;
+  }
+
+  checkForms(): void {
+    const forms = this.sections.map(section => section.form) as Array<CustomFormGroup>;
+
+    this.isFormDirty = forms.some(form => form.dirty);
+
+    this.setErrors(forms);
+
+    this.isFormInvalid = forms.some(form => form.invalid);
+  }
+
+  setErrors(forms: Array<CustomFormGroup>): void {
+    const errors: GlobalError[] = [];
+
+    forms.forEach(form => DynamicFormService.validate(form, errors));
+
+    errors.length ? this.globalErrorService.setErrors(errors) : this.globalErrorService.clearErrors();
   }
 }
--- src/app/features/reference-data/reference-data-routing.module.ts
+++ src/app/features/reference-data/reference-data-routing.module.ts
@@ -3,36 +3,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { MsalGuard } from '@azure/msal-angular';
 import { RoleGuard } from '@guards/role-guard/roles.guard';
 import { Roles } from '@models/roles.enum';
-import { AddReferenceDataComponent } from './reference-data-add/reference-data-add.component';
+import { ReferenceDataCreateComponent } from './reference-data-add/reference-data-add.component';
 import { ReferenceDataListComponent } from './reference-data-list/reference-data-list.component';
 import { ReferenceDataSelectTypeComponent } from './reference-data-select-type/reference-data-select-type.component';
 
 const routes: Routes = [
   {
     path: '',
-    pathMatch: 'prefix',
     component: ReferenceDataSelectTypeComponent,
     data: { title: 'Select Reference Data Type', roles: Roles.ReferenceDataView },
     canActivate: [MsalGuard, RoleGuard]
   },
   {
-    path: 'reference-data',
-    data: { title: 'Reference Data', roles: Roles.ReferenceDataView },
-    canActivate: [MsalGuard, RoleGuard],
-    children: [
-      {
-        path: '',
-        component: ReferenceDataListComponent,
-        data: { roles: Roles.ReferenceDataView },
-        canActivate: [MsalGuard, RoleGuard]
-      },
-      {
-        path: 'add',
-        component: AddReferenceDataComponent,
-        data: { roles: Roles.ReferenceDataView },
-        canActivate: [MsalGuard, RoleGuard]
-      }
-    ]
+    path: ':type',
+    component: ReferenceDataListComponent,
+    data: { roles: Roles.ReferenceDataView },
+    canActivate: [MsalGuard, RoleGuard]
+  },
+  {
+    path: ':type/create',
+    component: ReferenceDataCreateComponent,
+    data: { title: 'Add Reference Data', roles: Roles.ReferenceDataAmend },
+    canActivate: [MsalGuard, RoleGuard]
   }
 ];
 
