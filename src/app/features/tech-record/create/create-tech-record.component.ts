c2495115d04746de4376ea6f7e062428f44104be
Shivangi Das (team VTM) <shivangidas@users.noreply.github.com>
2023-05-18 12:08:54 +0100

    fix(cb2-8274): added logging for test (#1026)

--- src/app/features/tech-record/create/create-tech-record.component.ts
+++ src/app/features/tech-record/create/create-tech-record.component.ts
@@ -124,6 +124,7 @@ export class CreateTechRecordComponent implements OnChanges {
 
   async handleSubmit() {
     if (!this.isFormValid) {
+      console.log(this.form.getCleanValue(this.form));
       return;
     }
 
