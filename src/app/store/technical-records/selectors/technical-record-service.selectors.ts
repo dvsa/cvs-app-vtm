df144d20f7958ebf012951f13735059ca2ba44fb
Shivangi Das <shivangi.das7@gmail.com>
2023-05-30 16:59:29 +0100

    fix(cb2-8286): navigate to current record in absense of provisional

--- src/app/store/technical-records/selectors/technical-record-service.selectors.ts
+++ src/app/store/technical-records/selectors/technical-record-service.selectors.ts
@@ -35,10 +35,7 @@ export const selectTechRecord = createSelector(
     const lastTwoUrlParts = url?.split('/').slice(-2);
 
     if (lastTwoUrlParts?.includes(StatusCodes.PROVISIONAL)) {
-      const provisionalRecord = vehicle?.techRecord.find(techRecord => techRecord.statusCode === StatusCodes.PROVISIONAL);
-      if (provisionalRecord) {
-        return provisionalRecord;
-      }
+      return vehicle?.techRecord.find(techRecord => techRecord.statusCode === StatusCodes.PROVISIONAL);
     }
 
     if (techCreatedAt) {
