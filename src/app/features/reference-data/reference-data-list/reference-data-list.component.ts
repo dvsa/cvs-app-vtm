032114d798d3271c23e0ca48198bcf7a30207de6
Emily.Kelly <emily.kelly@bjss.com>
2023-06-05 15:12:04 +0100

    feat(cb2-8194): delete component now uses data driven templates

--- src/app/features/reference-data/reference-data-list/reference-data-list.component.ts
+++ src/app/features/reference-data/reference-data-list/reference-data-list.component.ts
@@ -1,22 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { templateList as brakesTemplateList } from '@forms/templates/reference-data/brakes';
-import { templateList as countryOfRegistrationTemplateList } from '@forms/templates/reference-data/country-of-registration';
-import { templateList as hgvTemplateList } from '@forms/templates/reference-data/hgv-make';
-import { templateList as psvTemplateList } from '@forms/templates/reference-data/psv-make';
-import { templateList as reasonsForAbandoningHgvTemplateList } from '@forms/templates/reference-data/reasons-for-abandoning-hgv';
-import { templateList as reasonsForAbandoningPsvTemplateList } from '@forms/templates/reference-data/reasons-for-abandoning-psv';
-import { templateList as reasonsForAbandoningTirTemplateList } from '@forms/templates/reference-data/reasons-for-abandoning-TIR';
-import { templateList as reasonsForAbandoningTrlTemplateList } from '@forms/templates/reference-data/reasons-for-abandoning-TRL';
-import { templateList as specialistReasonsForAbandoningTemplateList } from '@forms/templates/reference-data/specialist-reasons-for-abandoning';
-import { templateList as trlTemplateList } from '@forms/templates/reference-data/trl-make';
-import { templateList as tyresTemplateList } from '@forms/templates/reference-data/tyres';
 import { ReferenceDataModelBase, ReferenceDataResourceType } from '@models/reference-data.model';
 import { Roles } from '@models/roles.enum';
 import { select, Store } from '@ngrx/store';
 import { ReferenceDataService } from '@services/reference-data/reference-data.service';
 import { selectAllReferenceDataByResourceType, selectReferenceDataByResourceKey } from '@store/reference-data';
-import { Observable, of, take } from 'rxjs';
+import { Observable, take } from 'rxjs';
 
 @Component({
   selector: 'app-reference-data-list',
@@ -31,7 +20,6 @@ export class ReferenceDataListComponent implements OnInit {
     this.route.params.pipe(take(1)).subscribe(params => {
       this.type = params['type'];
 
-      console.log('This is the type by url params: ', this.type);
       this.referenceDataService.loadReferenceData(this.type);
       this.referenceDataService.loadReferenceDataByKey(ReferenceDataResourceType.ReferenceDataAdminType, this.type);
     });
@@ -43,54 +31,6 @@ export class ReferenceDataListComponent implements OnInit {
 
   get data$(): Observable<any[] | undefined> {
     return this.store.pipe(select(selectAllReferenceDataByResourceType(this.type)));
-  }
-
-  get columns$(): Observable<Array<string>> {
-    let templateListToReturn: Array<any>;
-    switch (this.type) {
-      case ReferenceDataResourceType.Brakes:
-        templateListToReturn = brakesTemplateList;
-        break;
-      case ReferenceDataResourceType.CountryOfRegistration:
-        templateListToReturn = countryOfRegistrationTemplateList;
-        break;
-      case ReferenceDataResourceType.HgvMake:
-        templateListToReturn = hgvTemplateList;
-        break;
-      case ReferenceDataResourceType.PsvMake:
-        templateListToReturn = psvTemplateList;
-        break;
-      case ReferenceDataResourceType.ReasonsForAbandoningHgv:
-        templateListToReturn = reasonsForAbandoningHgvTemplateList;
-        break;
-      case ReferenceDataResourceType.ReasonsForAbandoningPsv:
-        templateListToReturn = reasonsForAbandoningPsvTemplateList;
-        break;
-      case ReferenceDataResourceType.ReasonsForAbandoningTrl:
-        templateListToReturn = reasonsForAbandoningTrlTemplateList;
-        break;
-      case ReferenceDataResourceType.SpecialistReasonsForAbandoning:
-        templateListToReturn = specialistReasonsForAbandoningTemplateList;
-        break;
-      case ReferenceDataResourceType.TirReasonsForAbandoning:
-        templateListToReturn = reasonsForAbandoningTirTemplateList;
-        break;
-      case ReferenceDataResourceType.TrlMake:
-        templateListToReturn = trlTemplateList;
-        break;
-      case ReferenceDataResourceType.Tyres:
-        templateListToReturn = tyresTemplateList;
-        break;
-      default:
-        templateListToReturn = [''];
-        break;
-    }
-    console.log(
-      templateListToReturn.filter(data => {
-        if (data.column !== 'resourceKey') data.column;
-      })
-    );
-    return of(templateListToReturn.filter(data => data.column !== 'resourceKey').map(data => data.column));
   }
 
   public get roles(): typeof Roles {
