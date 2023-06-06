509329e4f8cb07dbec98a0f5cec1e2f0612302d4
owen <owen.corrigan@bjss.com>
2023-06-06 09:00:03 +0100

    feat(cb2-8194): removes template from add component

--- src/app/features/reference-data/reference-data-add/reference-data-add.component.ts
+++ src/app/features/reference-data/reference-data-add/reference-data-add.component.ts
@@ -4,24 +4,13 @@ import { GlobalError } from '@core/components/global-error/global-error.interfac
 import { GlobalErrorService } from '@core/components/global-error/global-error.service';
 import { DynamicFormGroupComponent } from '@forms/components/dynamic-form-group/dynamic-form-group.component';
 import { DynamicFormService } from '@forms/services/dynamic-form.service';
-import { CustomFormGroup, FormNode, FormNodeWidth } from '@forms/services/dynamic-form.types';
-import { template as brakesTemplate } from '@forms/templates/reference-data/brakes';
-import { template as countryOfRegistrationTemplate } from '@forms/templates/reference-data/country-of-registration';
-import { template as hgvTemplate } from '@forms/templates/reference-data/hgv-make';
-import { template as psvTemplate } from '@forms/templates/reference-data/psv-make';
-import { template as reasonsForAbandoningHgvTemplate } from '@forms/templates/reference-data/reasons-for-abandoning-hgv';
-import { template as reasonsForAbandoningPsvTemplate } from '@forms/templates/reference-data/reasons-for-abandoning-psv';
-import { template as reasonsForAbandoningTirTemplate } from '@forms/templates/reference-data/reasons-for-abandoning-TIR';
-import { template as reasonsForAbandoningTrlTemplate } from '@forms/templates/reference-data/reasons-for-abandoning-TRL';
-import { template as specialistReasonsForAbandoningTemplate } from '@forms/templates/reference-data/specialist-reasons-for-abandoning';
-import { template as trlTemplate } from '@forms/templates/reference-data/trl-make';
-import { template as tyresTemplate } from '@forms/templates/reference-data/tyres';
+import { CustomFormGroup, FormNodeWidth } from '@forms/services/dynamic-form.types';
 import { ReferenceDataResourceType } from '@models/reference-data.model';
 import { Roles } from '@models/roles.enum';
 import { Store, select } from '@ngrx/store';
 import { ReferenceDataService } from '@services/reference-data/reference-data.service';
 import { ReferenceDataState, selectReferenceDataByResourceKey } from '@store/reference-data';
-import { Observable, catchError, filter, of, switchMap, take, throwError } from 'rxjs';
+import { catchError, filter, of, switchMap, take, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-reference-data-add',
@@ -56,10 +45,6 @@ export class ReferenceDataCreateComponent implements OnInit {
         .subscribe(type => (this.refDataAdminType = type));
     });
   }
-  //I feel like the issue is this being a getter, may want to be stored as a variable then accessed. A bit like tech records
-  // get refDataAdminType$(): Observable<any | undefined> {
-  //   return this.store.pipe(select(selectReferenceDataByResourceKey(ReferenceDataResourceType.ReferenceDataAdminType, this.type)));
-  // }
 
   get roles(): typeof Roles {
     return Roles;
@@ -68,54 +53,6 @@ export class ReferenceDataCreateComponent implements OnInit {
   get widths(): typeof FormNodeWidth {
     return FormNodeWidth;
   }
-
-  // get template(): FormNode {
-  //   let templateToReturn: FormNode;
-  //   switch (this.type) {
-  //     case ReferenceDataResourceType.Brakes:
-  //       templateToReturn = brakesTemplate;
-  //       break;
-  //     case ReferenceDataResourceType.CountryOfRegistration:
-  //       templateToReturn = countryOfRegistrationTemplate;
-  //       break;
-  //     case ReferenceDataResourceType.HgvMake:
-  //       templateToReturn = hgvTemplate;
-  //       break;
-  //     case ReferenceDataResourceType.PsvMake:
-  //       templateToReturn = psvTemplate;
-  //       break;
-  //     case ReferenceDataResourceType.ReasonsForAbandoningHgv:
-  //       templateToReturn = reasonsForAbandoningHgvTemplate;
-  //       break;
-  //     case ReferenceDataResourceType.ReasonsForAbandoningPsv:
-  //       templateToReturn = reasonsForAbandoningPsvTemplate;
-  //       break;
-  //     case ReferenceDataResourceType.ReasonsForAbandoningTrl:
-  //       templateToReturn = reasonsForAbandoningTrlTemplate;
-  //       break;
-  //     case ReferenceDataResourceType.SpecialistReasonsForAbandoning:
-  //       templateToReturn = specialistReasonsForAbandoningTemplate;
-  //       break;
-  //     case ReferenceDataResourceType.TirReasonsForAbandoning:
-  //       templateToReturn = reasonsForAbandoningTirTemplate;
-  //       break;
-  //     case ReferenceDataResourceType.TrlMake:
-  //       templateToReturn = trlTemplate;
-  //       break;
-  //     case ReferenceDataResourceType.Tyres:
-  //       templateToReturn = tyresTemplate;
-  //       break;
-  //     default:
-  //       templateToReturn = {} as FormNode;
-  //       break;
-  //   }
-  //   templateToReturn.children?.forEach(child => {
-  //     if (child.name === 'resourceKey') {
-  //       child.disabled = false;
-  //     }
-  //   });
-  //   return templateToReturn;
-  // }
 
   titleCaseHeading(input: ReferenceDataResourceType): string {
     return this.referenceDataService.macroCasetoTitleCase(input);
