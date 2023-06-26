4b8346ee1fe1ebf53a8867b456bdc5014c22cf1e
Emily.Kelly <emily.kelly@bjss.com>
2023-06-26 16:55:44 +0100

    feat(cb2-8217): ref data pipe to access audit history when item has been deleted

--- src/app/shared/pipes/ref-data-decode/ref-data-decode.pipe.ts
+++ src/app/shared/pipes/ref-data-decode/ref-data-decode.pipe.ts
@@ -1,14 +1,22 @@
 import { OnDestroy, Pipe, PipeTransform } from '@angular/core';
-import { ReferenceDataModelBase, ReferenceDataResourceType } from '@models/reference-data.model';
-import { Store } from '@ngrx/store';
+import { ReferenceDataModelBase, ReferenceDataResourceType, ReferenceDataResourceTypeAudit } from '@models/reference-data.model';
+import { select, Store } from '@ngrx/store';
 import { State } from '@store/index';
-import { fetchReferenceDataByKey, selectReferenceDataByResourceKey } from '@store/reference-data';
-import { map, Observable, of, Subject, takeUntil } from 'rxjs';
+import {
+  fetchReferenceData,
+  fetchReferenceDataByKey,
+  fetchReferenceDataByKeySearch,
+  selectReferenceDataByResourceKey,
+  selectSearchReturn
+} from '@store/reference-data';
+import { it } from 'node:test';
+import { combineLatest, map, Observable, of, skip, skipUntil, skipWhile, Subject, take, takeUntil, withLatestFrom } from 'rxjs';
 
 @Pipe({
   name: 'refDataDecode$'
 })
 export class RefDataDecodePipe implements PipeTransform, OnDestroy {
+  data: any;
   constructor(private store: Store<State>) {}
 
   private destroy$ = new Subject<void>();
@@ -21,19 +29,24 @@ export class RefDataDecodePipe implements PipeTransform, OnDestroy {
   transform(
     value: string | number | undefined,
     resourceType: string | undefined,
-    decodeKey: string = 'description'
+    decodeKey: string | number = 'description'
   ): Observable<string | number | undefined> {
     if (!resourceType || !value) {
       return of(value);
     }
 
-    this.store.dispatch(fetchReferenceDataByKey({ resourceType: resourceType as ReferenceDataResourceType, resourceKey: value }));
+    this.store.dispatch(fetchReferenceData({ resourceType: resourceType as ReferenceDataResourceType }));
+    this.store.dispatch(fetchReferenceDataByKeySearch({ resourceType: (resourceType + '#AUDIT') as ReferenceDataResourceType, resourceKey: value }));
 
-    return this.store.select(selectReferenceDataByResourceKey(resourceType as ReferenceDataResourceType, value)).pipe(
-      takeUntil(this.destroy$),
-      map(refDataItem => {
-        if (!refDataItem) return value;
-
+    return combineLatest([
+      this.store.select(selectReferenceDataByResourceKey(resourceType as ReferenceDataResourceType, value)),
+      this.store.select(selectSearchReturn((resourceType + '#AUDIT') as ReferenceDataResourceTypeAudit))
+    ]).pipe(
+      skipWhile(([_, refDataItemAuditSelector]) => refDataItemAuditSelector === undefined),
+      map(([refDataItem, refDataItemAudit]) => {
+        if (!refDataItem) {
+          return refDataItemAudit?.[0].description ?? value;
+        }
         return refDataItem[decodeKey as keyof ReferenceDataModelBase] ?? value;
       })
     );
