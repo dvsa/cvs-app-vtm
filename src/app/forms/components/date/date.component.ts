074a5e0acfeb2ac225c50edecaf9bc7965ef629e
Daniel Searle <daniel.searle1@dvsa.gov.uk>
2023-07-24 13:12:55 +0100

    feat(cb2-8776): added access descriptor

--- src/app/forms/components/date/date.component.ts
+++ src/app/forms/components/date/date.component.ts
@@ -51,7 +51,6 @@ export class DateComponent extends BaseControlComponent implements OnInit, OnDes
   public year?: number;
   public hour?: number;
   public minute?: number;
-  showError: boolean = false;
 
   constructor(injector: Injector, changeDetectorRef: ChangeDetectorRef) {
     super(injector, changeDetectorRef);
