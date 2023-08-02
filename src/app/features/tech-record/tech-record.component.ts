c5a80f6a71677b9ebddc6425407ec3c668ba870d
owen <owen.corrigan@bjss.com>
2023-08-02 09:07:39 +0100

    feat(cb2-8089): tech record component

--- src/app/features/tech-record/tech-record.component.ts
+++ src/app/features/tech-record/tech-record.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { GlobalError } from '@core/components/global-error/global-error.interface';
 import { GlobalErrorService } from '@core/components/global-error/global-error.service';
 import { Roles } from '@models/roles.enum';
 import { VehicleTechRecordModel } from '@models/vehicle-tech-record.model';
+import { Store } from '@ngrx/store';
 import { TechnicalRecordService } from '@services/technical-record/technical-record.service';
-import { Observable } from 'rxjs';
+import { getTechRecordV3 } from '@store/technical-records';
+import { TechnicalRecordServiceState } from '@store/technical-records/reducers/technical-record-service.reducer';
+import { Observable, of, take } from 'rxjs';
 
 @Component({
   selector: 'app-tech-record',
@@ -13,10 +16,25 @@ import { Observable } from 'rxjs';
 })
 export class TechRecordComponent {
   vehicle$: Observable<VehicleTechRecordModel | undefined>;
+  systemNumber?: string;
+  createdTimestamp?: string;
 
-  constructor(private techRecordService: TechnicalRecordService, private router: Router, public errorService: GlobalErrorService) {
-    this.vehicle$ = this.techRecordService.selectedVehicleTechRecord$;
+  constructor(
+    private techRecordService: TechnicalRecordService,
+    private router: Router,
+    public errorService: GlobalErrorService,
+    private store: Store<TechnicalRecordServiceState>,
+    private route: ActivatedRoute
+  ) {
+    // this.vehicle$ = this.techRecordService.selectedVehicleTechRecord$;
+    this.vehicle$ = of(undefined);
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+
+    this.route.params.pipe(take(1)).subscribe(params => {
+      this.systemNumber = params['systemNumber'];
+      this.createdTimestamp = params['createdTimestamp'];
+    });
+    // this.store.dispatch(getTechRecordV3({systemNumber: this.systemNumber!, createdTimestamp: this.createdTimestamp!}))
   }
 
   get roles() {
