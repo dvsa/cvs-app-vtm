2d62b8c6489699caff838092f1f6f209615da656
Emily R Kelly <101548427+emilyrkelly21@users.noreply.github.com>
2023-08-11 16:54:36 +0100

    feat(cb2-8226): change to email validation error message (#1103)

--- src/app/forms/validators/custom-validators.ts
+++ src/app/forms/validators/custom-validators.ts
@@ -180,7 +180,10 @@ export class CustomValidators {
   }
 
   static email(): ValidatorFn {
-    return this.customPattern(['^[-\\w.\\+]+@[-\\w]+\\.[A-Za-z]{2,}$', 'is an invalid format. An email must include an @ symbol and a .']);
+    return this.customPattern([
+      '^[-\\w.\\+]+@[-\\w]+\\.[A-Za-z]{2,}$',
+      'is an invalid format. An email must include an @ symbol, a full stop and end with two or more characters'
+    ]);
   }
 
   static customPattern([regEx, message]: string[]): ValidatorFn {
