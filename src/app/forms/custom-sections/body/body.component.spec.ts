b37687f562893628ee795241450a7acda2b1b3ca
owen <owen.corrigan@bjss.com>
2023-08-16 15:11:30 +0100

    feat(cb2-8667): started on body component tests

--- src/app/forms/custom-sections/body/body.component.spec.ts
+++ src/app/forms/custom-sections/body/body.component.spec.ts
@@ -11,7 +11,7 @@ import { initialAppState } from '@store/index';
 import { BodyComponent } from './body.component';
 import { ReferenceDataService } from '@services/reference-data/reference-data.service';
 import { UserService } from '@services/user-service/user-service';
-import { VehicleTypes } from '@models/vehicle-tech-record.model';
+import { V3TechRecordModel, VehicleTypes } from '@models/vehicle-tech-record.model';
 
 describe('BodyComponent', () => {
   let component: BodyComponent;
@@ -33,32 +33,39 @@ describe('BodyComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BodyComponent);
     component = fixture.componentInstance;
-    component.techRecord = { systemNumber: 'foo', createdTimestamp: 'bar', vin: 'testVin', techRecord_vehicleType: VehicleTypes.PSV };
+    component.techRecord = {
+      systemNumber: 'foo',
+      createdTimestamp: 'bar',
+      vin: 'testVin',
+      techRecord_vehicleType: VehicleTypes.PSV,
+      techRecord_brakes_dtpNumber: '000000',
+      techRecord_techRecord_bodyModel: 'model'
+    } as unknown as V3TechRecordModel;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  // TODO: V3 Brakes HGV?
-  // describe('The DTpNumber value on this.form', () => {
-  //   it('should match the corresponding values on vehicleTechRecord', () => {
-  //     expect(component.techRecord.brakes!.dtpNumber).toStrictEqual(component.form.value.brakes.dtpNumber);
-  //   });
-  // });
-  // describe('The bodyModel value on this.form', () => {
-  //   it('should match the corresponding values on vehicleTechRecord', () => {
-  //     expect(component.techRecord.bodyModel).toStrictEqual(component.form.value.bodyModel);
-  //   });
-  // });
-  // describe('The bodyMake value on this.form', () => {
-  //   it('should match the corresponding values on vehicleTechRecord', () => {
-  //     expect(component.techRecord.bodyType).toStrictEqual(component.form.controls['bodyType'].value);
-  //   });
-  // });
-  // describe('The bodyModel value on this.form', () => {
-  //   it('should match the corresponding values on vehicleTechRecord', () => {
-  //     expect(component.techRecord.bodyModel).toStrictEqual(component.form.controls['bodyModel'].value);
-  //   });
-  // });
+
+  describe('The DTpNumber value on this.form', () => {
+    it('should match the corresponding values on vehicleTechRecord', () => {
+      expect((component.techRecord as any).techRecord_brakes_dtpNumber).toStrictEqual(component.form.value.brakes.techRecord_brakes_dtpNumber);
+    });
+  });
+  describe('The bodyModel value on this.form', () => {
+    it('should match the corresponding values on vehicleTechRecord', () => {
+      expect((component.techRecord as any).techRecord_techRecord_bodyModel).toStrictEqual(component.form.value.techRecord_bodyModel);
+    });
+  });
+  describe('The bodyMake value on this.form', () => {
+    it('should match the corresponding values on vehicleTechRecord', () => {
+      expect((component.techRecord as any).techRecord.techRecord_bodyType).toStrictEqual(component.form.controls['bodyType'].value);
+    });
+  });
+  describe('The bodyModel value on this.form', () => {
+    it('should match the corresponding values on vehicleTechRecord', () => {
+      expect((component.techRecord as any).techRecord.techRecord_bodyModel).toStrictEqual(component.form.controls['bodyModel'].value);
+    });
+  });
 });
