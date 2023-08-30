2cf9079a901eccb7f2b59894ad235fafa2fc06ff
Guillaume Julien <guillaume.julien@bjss.com>
2023-08-30 11:54:06 +0100

    feat(cb2-8806): generic description

--- src/app/features/tech-record/components/tech-record-summary/tech-record-summary.component.ts
+++ src/app/features/tech-record/components/tech-record-summary/tech-record-summary.component.ts
@@ -73,11 +73,7 @@ export class TechRecordSummaryComponent implements OnInit, OnDestroy {
             techRecord.techRecord_dimensions_axleSpacing = axleSpacing;
             techRecord.techRecord_axles = axles;
           }
-          if (
-            techRecord.techRecord_vehicleType === VehicleTypes.TRL ||
-            techRecord.techRecord_vehicleType === VehicleTypes.HGV ||
-            techRecord.techRecord_vehicleType === VehicleTypes.PSV
-          ) {
+          if ('techRecord_vehicleClass_description' in techRecord) {
             techRecord.techRecord_vehicleClass_code = vehicleClassCodeMap.get(techRecord.techRecord_vehicleClass_description ?? '');
           }
           return techRecord;
