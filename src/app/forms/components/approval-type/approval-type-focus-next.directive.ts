6f7f4a008c38e6b4d484d2e1c20b9e86342cc39a
Daniel Searle <daniel.searle1@dvsa.gov.uk>
2023-09-11 09:54:35 +0100

    feat(cb2-8705): removed logs

--- src/app/forms/components/approval-type/approval-type-focus-next.directive.ts
+++ src/app/forms/components/approval-type/approval-type-focus-next.directive.ts
@@ -12,19 +12,10 @@ export class ApprovalTypeFocusNextDirective {
   @HostListener('input', ['$event'])
   onInput() {
     const { value } = this.el.nativeElement;
-    console.log(value);
-    console.log(this.nextInputId);
-    console.log(this.characterLimit);
-
     if (value.length >= this.characterLimit) {
-      console.log('in if 1');
       const nextInput = document.getElementById(this.nextInputId);
-
-      console.log(nextInput);
       if (nextInput) {
-        console.log('in if 2');
         nextInput.focus();
-        console.log('focussed next input');
       }
     }
   }
