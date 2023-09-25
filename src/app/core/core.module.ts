1166d2a99f227cd6a67993622435e3d01dd3bedb
Tom Crawley <thomas.crawley@dvsa.gov.uk>
2023-09-25 10:00:55 +0100

    feat(cb2-8121): app/core

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { GlobalErrorComponent } from './components/global-error/global-error.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { RouterModule } from '@angular/router';
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ServerErrorComponent } from './components/server-error/server-error.component';
