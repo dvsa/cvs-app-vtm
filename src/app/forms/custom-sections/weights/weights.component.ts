868eb00ae3576eb66c29315ca2d806715f39efd9
Daniel Searle <daniel.searle1@dvsa.gov.uk>
2023-09-26 14:45:20 +0100

    feat(cb2-8714): fix

--- src/app/forms/custom-sections/weights/weights.component.ts
+++ src/app/forms/custom-sections/weights/weights.component.ts
@@ -26,32 +26,96 @@ export class WeightsComponent implements OnInit, OnDestroy, OnChanges {
   private _formSubscription = new Subscription();
   public isError: boolean = false;
   public errorMessage?: string;
+  private ladenWeightOverride: boolean = false;
 
   constructor(public dynamicFormsService: DynamicFormService, private store: Store<TechnicalRecordServiceState>) {}
 
   ngOnInit(): void {
     this.form = this.dynamicFormsService.createForm(this.template, this.vehicleTechRecord) as CustomFormGroup;
-    this._formSubscription = this.form.cleanValueChanges.pipe(debounceTime(400)).subscribe((event: any) => {
-      if (event?.techRecord_axles) {
-        event.techRecord_axles = (event.techRecord_axles as Axle[]).filter(axle => !!axle?.axleNumber);
-      }
-      // Calculate the grossLadenWeight and update it in the form
-      if (this.isPsv) {
-        event.techRecord_grossLadenWeight = this.calculateGrossLadenWeight();
-      }
-      this.formChange.emit(event);
-      if (event.techRecord_grossLadenWeight || event.techRecord_grossKerbWeight) {
-        this.store.dispatch(
-          updateBrakeForces({ grossLadenWeight: event.techRecord_grossLadenWeight, grossKerbWeight: event.techRecord_grossKerbWeight })
-        );
-      }
+
+    // This subscription checks for manual changes to grossLadenWeight
+    const grossLadenWeightChanges = this.form.get('techRecord_grossLadenWeight')?.valueChanges.subscribe(value => {
+      this.ladenWeightOverride = true;
     });
+    if (grossLadenWeightChanges) {
+      this._formSubscription.add(grossLadenWeightChanges);
+    }
+
+    this._formSubscription.add(
+      this.form.cleanValueChanges.pipe(debounceTime(400)).subscribe((event: any) => {
+        // Extract the relevant fields from the event
+        const {
+          techRecord_seatsUpperDeck,
+          techRecord_seatsLowerDeck,
+          techRecord_manufactureYear,
+          techRecord_grossKerbWeight,
+          techRecord_grossLadenWeight // Added this for later dispatch
+        } = event || {};
+
+        console.log(techRecord_seatsUpperDeck);
+        console.log(techRecord_seatsLowerDeck);
+        console.log(techRecord_manufactureYear);
+        console.log(techRecord_grossKerbWeight);
+        console.log(techRecord_grossLadenWeight);
+
+        const shouldRecalculate =
+          techRecord_seatsUpperDeck !== undefined ||
+          techRecord_seatsLowerDeck !== undefined ||
+          techRecord_manufactureYear !== undefined ||
+          techRecord_grossKerbWeight !== undefined;
+
+        if (shouldRecalculate) {
+          this.ladenWeightOverride = false; // Reset the override flag
+        }
+
+        if (event?.techRecord_axles) {
+          event.techRecord_axles = (event.techRecord_axles as Axle[]).filter(axle => !!axle?.axleNumber);
+        }
+
+        if ((this.isPsv && !this.ladenWeightOverride) || shouldRecalculate) {
+          console.log('Recalculating grossLadenWeight');
+          const calculatedWeight = this.calculateGrossLadenWeight();
+          event.techRecord_grossLadenWeight = calculatedWeight;
+
+          // Update the form control value for grossLadenWeight so that the UI updates.
+          this.form.get('techRecord_grossLadenWeight')?.setValue(calculatedWeight, { emitEvent: false });
+        }
+
+        this.formChange.emit(event);
+
+        // The following dispatch now includes both techRecord_grossLadenWeight and techRecord_grossKerbWeight
+        if (techRecord_grossLadenWeight || techRecord_grossKerbWeight) {
+          this.store.dispatch(updateBrakeForces({ grossLadenWeight: techRecord_grossLadenWeight, grossKerbWeight: techRecord_grossKerbWeight }));
+        }
+      })
+    );
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     const { vehicleTechRecord } = changes;
-    if (this.form && vehicleTechRecord?.currentValue && vehicleTechRecord.currentValue !== vehicleTechRecord.previousValue) {
-      this.form?.patchValue(vehicleTechRecord.currentValue, { emitEvent: false });
+    if (this.form && vehicleTechRecord) {
+      const { currentValue, previousValue } = vehicleTechRecord;
+
+      // Check if any of the fields have changed
+      const fieldsChanged = [
+        'techRecord_seatsUpperDeck',
+        'techRecord_seatsLowerDeck',
+        'techRecord_manufactureYear',
+        'techRecord_grossKerbWeight'
+      ].some(field => currentValue[field] !== previousValue[field]);
+
+      if (fieldsChanged) {
+        // Recalculate grossLadenWeight
+        const newGrossLadenWeight = this.calculateGrossLadenWeight();
+
+        // Update form value without emitting event to avoid infinite loop
+        this.form.patchValue(
+          {
+            techRecord_grossLadenWeight: newGrossLadenWeight
+          },
+          { emitEvent: false }
+        );
+      }
     }
   }
 
@@ -124,6 +188,11 @@ export class WeightsComponent implements OnInit, OnDestroy, OnChanges {
     const techRecord_seatsLowerDeck = psvRecord?.techRecord_seatsLowerDeck ?? 0;
     const techRecord_manufactureYear = psvRecord?.techRecord_manufactureYear ?? 0;
     const techRecord_grossKerbWeight = psvRecord?.techRecord_grossKerbWeight ?? 0;
+
+    console.log(techRecord_seatsUpperDeck);
+    console.log(techRecord_seatsLowerDeck);
+    console.log(techRecord_manufactureYear);
+    console.log(techRecord_grossKerbWeight);
 
     const kgAllowedPerPerson = techRecord_manufactureYear >= 1988 ? 65 : 63.5;
 
