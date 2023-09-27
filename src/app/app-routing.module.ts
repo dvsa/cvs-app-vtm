e48bff4aebb1373190af395d434d47a54faee1e8
Tom Crawley <thomas.crawley@dvsa.gov.uk>
2023-09-27 12:26:55 +0100

    feat(cb2-8121): fix lint not passing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MsalGuard } from '@azure/msal-angular';
 import { RoleGuard } from '@guards/role-guard/roles.guard';
-import { TitleResolver } from './resolvers/title/title.resolver';
 import { Roles } from '@models/roles.enum';
-import { TechRecordViewResolver } from './resolvers/tech-record-view/tech-record-view.resolver';
 import { PageNotFoundComponent } from '@core/components/page-not-found/page-not-found.component';
 import { ServerErrorComponent } from '@core/components/server-error/server-error.component';
 import { CancelEditTechGuard } from '@guards/cancel-edit-tech/cancel-edit-tech.guard';
+import { TechRecordViewResolver } from './resolvers/tech-record-view/tech-record-view.resolver';
+import { TitleResolver } from './resolvers/title/title.resolver';
 
 const routes: Routes = [
   {
@@ -19,62 +19,62 @@ const routes: Routes = [
         data: { title: 'Home', roles: Roles.TechRecordView },
         canActivate: [MsalGuard, RoleGuard],
         canDeactivate: [CancelEditTechGuard],
-        loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)
+        loadChildren: () => import('./features/home/home.module').then((m) => m.HomeModule),
       },
       {
         path: 'search',
         data: { title: 'Technical record search', roles: Roles.TechRecordView },
         canActivate: [MsalGuard, RoleGuard],
         canDeactivate: [CancelEditTechGuard],
-        loadChildren: () => import('./features/search/search.module').then(m => m.SearchModule)
+        loadChildren: () => import('./features/search/search.module').then((m) => m.SearchModule),
       },
       {
         path: 'create',
         data: { title: 'Create new technical record', roles: Roles.TechRecordCreate },
         canActivate: [MsalGuard, RoleGuard],
-        loadChildren: () => import('./features/tech-record/create/create-tech-records.module').then(m => m.CreateTechRecordsModule)
+        loadChildren: () => import('./features/tech-record/create/create-tech-records.module').then((m) => m.CreateTechRecordsModule),
       },
       {
         path: 'create-batch',
         data: { title: 'Select Vehicle Type', roles: Roles.TechRecordCreate },
         canActivate: [MsalGuard, RoleGuard],
-        loadChildren: () => import('./features/tech-record/create-batch/create-batch.module').then(m => m.CreateBatchModule)
+        loadChildren: () => import('./features/tech-record/create-batch/create-batch.module').then((m) => m.CreateBatchModule),
       },
       {
         path: 'test-records/:systemNumber/test-result/:testResultId/:testNumber',
         data: { title: 'Test Result', roles: Roles.TestResultView },
         canActivate: [MsalGuard, RoleGuard],
         resolve: { techRecord: TechRecordViewResolver },
-        loadChildren: () => import('./features/test-records/amend/amend-test-records.module').then(m => m.AmendTestRecordsModule)
+        loadChildren: () => import('./features/test-records/amend/amend-test-records.module').then((m) => m.AmendTestRecordsModule),
       },
       {
         path: 'tech-records/:systemNumber/:createdTimestamp',
         data: { title: 'Tech Record', roles: Roles.TechRecordView },
         canActivate: [MsalGuard, RoleGuard],
-        loadChildren: () => import('./features/tech-record/tech-record.module').then(m => m.TechRecordsModule)
+        loadChildren: () => import('./features/tech-record/tech-record.module').then((m) => m.TechRecordsModule),
       },
       {
         path: 'reference-data',
         data: { title: 'Select Reference Data Type', roles: Roles.ReferenceDataView },
         canActivate: [MsalGuard, RoleGuard],
-        loadChildren: () => import('./features/reference-data/reference-data.module').then(m => m.ReferenceDataModule)
+        loadChildren: () => import('./features/reference-data/reference-data.module').then((m) => m.ReferenceDataModule),
       },
       {
         path: 'error',
         pathMatch: 'full',
-        component: ServerErrorComponent
-      }
-    ]
+        component: ServerErrorComponent,
+      },
+    ],
   },
   {
     path: '**',
     pathMatch: 'full',
-    component: PageNotFoundComponent
-  }
+    component: PageNotFoundComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-///<reference path="govuk.d.ts">
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="govuk.d.ts">
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router, NavigationEnd, Event } from '@angular/router';
 import { Store, select } from '@ngrx/store';
@@ -6,13 +7,16 @@ import { LoadingService } from '@services/loading/loading.service';
 import { UserService } from '@services/user-service/user-service';
 import { selectRouteData } from '@store/router/selectors/router.selectors';
 import { initAll } from 'govuk-frontend/govuk/all';
-import { take, map, Subject, takeUntil } from 'rxjs';
-import { State } from './store';
+import {
+  take, map, Subject, takeUntil,
+} from 'rxjs';
 import { GoogleAnalyticsService } from '@services/google-analytics/google-analytics.service';
+import { State } from './store';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
@@ -22,7 +26,7 @@ export class AppComponent implements OnInit, OnDestroy {
     private loadingService: LoadingService,
     private router: Router,
     private store: Store<State>,
-    private googleAnalyticsService: GoogleAnalyticsService
+    private googleAnalyticsService: GoogleAnalyticsService,
   ) {
     this.router.events.pipe(takeUntil(this.destroy$)).subscribe((event: Event) => {
       if (event instanceof NavigationEnd) {
@@ -44,7 +48,7 @@ export class AppComponent implements OnInit, OnDestroy {
     return this.store.pipe(
       take(1),
       select(selectRouteData),
-      map(routeData => routeData && !routeData['isCustomLayout'])
+      map((routeData) => routeData && !routeData['isCustomLayout']),
     );
   }
 
