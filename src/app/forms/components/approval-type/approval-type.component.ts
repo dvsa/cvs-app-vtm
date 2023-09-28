3a35553a8bf72b33b5fea949d3a7d9f229a17629
Daniel Searle <daniel.searle1@dvsa.gov.uk>
2023-09-28 15:34:49 +0100

    fix(cb2-8705): custom id removal and allow special chracters

--- src/app/forms/components/approval-type/approval-type.component.ts
+++ src/app/forms/components/approval-type/approval-type.component.ts
@@ -6,37 +6,87 @@ import { BaseControlComponent } from '../base-control/base-control.component';
 import { FormNodeEditTypes, FormNodeWidth } from '@forms/services/dynamic-form.types';
 
 const patterns: Record<string, RegExp> = {
-  NTA: /^(\w+)$/i,
-  'IVA - DVSA/NI': /^(\w+)$/i,
-  IVA: /^(\w+)$/i,
-  ECTA: /^e(\w{2})\*(\w{4})\/(\w{4})\*(\w{6})$/i,
-  NSSTA: /^e(\w{2})\*NKS\*(\w{6})$/i,
-  ECSSTA: /^e(\w{2})\*KS(\w{2})\/(\w{4})\*(\w{6})$/i,
-  'GB WVTA': /^(\w{3})\*(\w{4})\/(\w{4})\*(\w{7})$/i,
-  'UKNI WVTA': /^(\w+)11\*(\w{4})\/(\w{4})\*(\w{6})$/i,
-  'EU WVTA Pre 23': /^e(\w{2})\*(\w{4})\/(\wt{4})\*(\w{6})$/i,
-  'EU WVTA 23 on': /^e(\w{2})\*(\w{4})\/(\w{4})\*(\w{6})$/i,
-  QNIG: /^e(\w{2})\*(\w{4})\/(\w{4})\*(\w{6})$/i,
-  'Prov.GB WVTA': /^(\w{3})\*(\w{4})\/(\w{4})\*(\w{6})$/i,
-  'Small series': /^(\w+)11\*NKS(\w+\*)?(\w{6})$/i,
-  'IVA - VCA': /^n11\*NIV(\w{2})\/(\w{4})\*(\w{6})$/i
+  NTA: /^(.+)$/i, // 25
+  'IVA - DVSA/NI': /^(.+)$/i, // 25
+  IVA: /^(.+)$/i, //25
+  ECTA: /^e(.{2})\*(.{4})\/(.{4})\*(.{6})$/i, //20
+  NSSTA: /^e(.{2})\*NKS\*(.{6})$/i, //14
+  ECSSTA: /^e(.{2})\*KS(.{2})\/(.{4})\*(.{6})$/i, // 18
+  'GB WVTA': /^(.{3})\*(.{4})\/(.{4})\*(.{7})$/i, // 21
+  'UKNI WVTA': /^(.{1})11\*(.{4})\/(.{4})\*(.{6})$/i, // 20
+  'EU WVTA Pre 23': /^e(.{2})\*(.{4})\/(.{4})\*(.{6})$/i, // 20
+  'EU WVTA 23 on': /^e(.{2})\*(.{4})\/(.{4})\*(.{6})$/i, // 20
+  QNIG: /^e(.{2})\*(.{4})\/(.{4})\*(.{6})$/i, // 20
+  'Prov.GB WVTA': /^(.{3})\*(.{4})\/(.{4})\*(.{6})$/i, // 20
+  'Small series': /^(.)11\*NKS(.{2})\*(.{6})$/i, // 16
+  'IVA - VCA': /^n11\*NIV(.{2})\/(.{4})\*(.{6})$/i // 19
 };
 
-const patternsPartial: Record<string, RegExp> = {
-  NTA: /^(\w+)$/,
-  ECTA: /^(\w{0,2})(\w{0,4})(\w{0,4})(\w{0,6})$/,
-  IVA: /^(\w+)$/,
-  NSSTA: /^(\w{0,2})(\w{0,6})$/,
-  ECSSTA: /^(\w{0,2})(\w{0,2})(\w{0,4})(\w{0,6})$/,
-  'GB WVTA': /^(\w{0,3})(\w{0,4})(\w{0,4})(\w{0,7})$/,
-  'UKNI WVTA': /^(\w{0,1})(\w{0,4})(\w{0,4})(\w{0,6})$/,
-  'EU WVTA Pre 23': /^(\w{0,2})(\w{0,4})(\w{0,4})(\w{0,6})$/,
-  'EU WVTA 23 on': /^(\w{0,2})(\w{0,4})(\w{0,4})(\w{0,6})$/,
-  QNIG: /^(\w{0,2})(\w{0,4})(\w{0,4})(\w{0,6})$/,
-  'Prov.GB WVTA': /^(\w{0,3})(\w{0,4})(\w{0,4})(\w{0,6})$/,
-  'Small series': /^(\w)(\w{0,2})(\w{0,6})$/,
-  'IVA - VCA': /^(\w{0,2})(\w{0,4})(\w{0,6})$/,
-  'IVA - DVSA/NI': /^(\w+)$/
+const patternsPartialMatch: Record<string, RegExp> = {
+  NTA: /^(.+)$/i,
+  'IVA - DVSA/NI': /^(.+)$/i,
+  IVA: /^(.+)$/i,
+  ECTA: /^e(.{0,2})\*(.{0,4})\/(.{0,4})\*(.{0,6})$/i,
+  NSSTA: /^e(.{0,2})\*NKS\*(.{0,6})$/i,
+  ECSSTA: /^e(.{0,2})\*KS(.{0,2})\/(.{0,4})\*(.{0,6})$/i,
+  'GB WVTA': /^(.{0,3})\*(.{0,4})\/(.{0,4})\*(.{0,7})$/i,
+  'UKNI WVTA': /^(.?)11\*(.{0,4})\/(.{0,4})\*(.{0,6})$/i,
+  'EU WVTA Pre 23': /^e(.{0,2})\*(.{0,4})\/(.{0,4})\*(.{0,6})$/i,
+  'EU WVTA 23 on': /^e(.{0,2})\*(.{0,4})\/(.{0,4})\*(.{0,6})$/i,
+  QNIG: /^e(.{0,2})\*(.{0,4})\/(.{0,4})\*(.{0,6})$/i,
+  'Prov.GB WVTA': /^(.{0,3})\*(.{0,4})\/(.{0,4})\*(.{0,6})$/i,
+  'Small series': /^(.?)11\*NKS(.{0,2})\*(.{0,6})$/i,
+  'IVA - VCA': /^n11\*NIV(.{0,2})\/(.{0,4})\*(.{0,6})$/i
+};
+
+const patternsGenericPartialMatch: Record<string, RegExp> = {
+  NTA: /^(.+)$/i,
+  'IVA - DVSA/NI': /^(.+)$/i,
+  IVA: /^(.+)$/i,
+  ECTA: /^(.{0,2})(.{0,4})(.{0,4})(.{0,6})$/i,
+  NSSTA: /^(.{0,2})(.{0,6})$/i,
+  ECSSTA: /^(.{0,2})(.{0,2})(.{0,4})(.{0,6})$/i,
+  'GB WVTA': /^(.{0,3})(.{0,4})(.{0,4})(.{0,7})$/i,
+  'UKNI WVTA': /^(.?)(.{0,4})(.{0,4})(.{0,6})$/i,
+  'EU WVTA Pre 23': /^(.{0,2})(.{0,4})(.{0,4})(.{0,6})$/i,
+  'EU WVTA 23 on': /^(.{0,2})(.{0,4})(.{0,4})(.{0,6})$/i,
+  QNIG: /^(.{0,2})(.{0,4})(.{0,4})(.{0,6})$/i,
+  'Prov.GB WVTA': /^(.{0,3})(.{0,4})(.{0,4})(.{0,6})$/i,
+  'Small series': /^(.?)(.{0,2})(.{0,6})$/i,
+  'IVA - VCA': /^(.{0,2})(.{0,4})(.{0,6})$/i
+};
+
+const characterLimit: Record<string, number> = {
+  NTA: 25,
+  'IVA - DVSA/NI': 25,
+  IVA: 25,
+  ECTA: 20,
+  NSSTA: 14,
+  ECSSTA: 18,
+  'GB WVTA': 21,
+  'UKNI WVTA': 20,
+  'EU WVTA Pre 23': 20,
+  'EU WVTA 23 on': 20,
+  QNIG: 20,
+  'Prov.GB WVTA': 20,
+  'Small series': 16,
+  'IVA - VCA': 19
+};
+const characterLimitGeneric: Record<string, number> = {
+  NTA: 25,
+  'IVA - DVSA/NI': 25,
+  IVA: 25,
+  ECTA: 16,
+  NSSTA: 8,
+  ECSSTA: 14,
+  'GB WVTA': 18,
+  'UKNI WVTA': 15,
+  'EU WVTA Pre 23': 16,
+  'EU WVTA 23 on': 16,
+  QNIG: 16,
+  'Prov.GB WVTA': 17,
+  'Small series': 9,
+  'IVA - VCA': 12
 };
 
 @Component({
@@ -131,73 +181,68 @@ export class ApprovalTypeInputComponent extends BaseControlComponent implements 
   }
 
   valueWriteBack(value: string | null): void {
-    if (!value || !this.approvalType) {
-      return;
-    }
-    switch (this.approvalType) {
-      case 'NTA':
-      case 'IVA':
-      case 'IVA - DVSA/NI':
-        this.extractValuesNtaIva(value, patterns[this.approvalType]);
-        break;
-      case 'ECTA':
-      case 'NSSTA':
-      case 'ECSSTA':
-      case 'GB WVTA':
-      case 'UKNI WVTA':
-      case 'EU WVTA Pre 23':
-      case 'EU WVTA 23 on':
-      case 'QNIG':
-      case 'Prov.GB WVTA':
-      case 'Small series':
-      case 'IVA - VCA':
-        this.extractValues(value);
-        break;
-      default:
-        console.log('Unknown approval type');
+    if (!value || !this.approvalType) return;
+    const NTA_IVA_Types = ['NTA', 'IVA', 'IVA - DVSA/NI'];
+    const OtherTypes = [
+      'ECTA',
+      'NSSTA',
+      'ECSSTA',
+      'GB WVTA',
+      'UKNI WVTA',
+      'EU WVTA Pre 23',
+      'EU WVTA 23 on',
+      'QNIG',
+      'Prov.GB WVTA',
+      'Small series',
+      'IVA - VCA'
+    ];
+
+    if (NTA_IVA_Types.includes(this.approvalType)) {
+      this.extractValuesNtaIva(value, patterns[this.approvalType]);
+    } else if (OtherTypes.includes(this.approvalType)) {
+      this.extractValues(value);
+    } else {
+      console.log('Unknown approval type');
     }
   }
 
-  private extractValues(value: string) {
-    if (!value || !this.approvalType) {
-      return;
+  private extractValues(value: string): void {
+    if (!value || !this.approvalType) return;
+
+    const getMatchedValues = (value: string, pattern: RegExp) => {
+      const result = value.match(pattern);
+      return result ? result.slice(1) : [];
+    };
+
+    const pattern = patterns[this.approvalType];
+    const patternPartial = patternsPartialMatch[this.approvalType] || /^$/;
+
+    let matches = getMatchedValues(value, pattern);
+    if (!matches.length) {
+      const limit = characterLimit[this.approvalType];
+      const limitedValue = value.length > limit ? value.substring(0, limit) : value;
+      matches = getMatchedValues(limitedValue, patternPartial);
+      this.setTypeApprovalNumbers(matches.filter(x => x));
     }
 
-    let matches: string[] = [];
-
-    const primaryPattern = patterns[this.approvalType];
-    const matchResult = value.match(primaryPattern);
-
-    if (!matchResult) {
-      const pattern: RegExp = /e|(\*NKS\*)|(\*NKS)|(n11\*NIV)|\*KS|\*11|[*/]/g;
-      value = value.replace(pattern, '');
-
-      const partialPattern = patternsPartial[this.approvalType] || /^$/;
-      const partialMatchResult =
-        value.length > 16 ? value.substring(0, 16).match(/(\w{1,2})(\w{1,4})(\w{1,4})(\w{1,6})/i) : value.match(partialPattern);
-
-      if (partialMatchResult) {
-        const [, ...capturedGroups] = partialMatchResult;
-        matches.push(...capturedGroups);
-      }
-    } else {
-      const [, ...capturedGroups] = matchResult;
-      matches.push(...capturedGroups);
+    if (!matches.length) {
+      const limit = characterLimitGeneric[this.approvalType];
+      const limitedValue = value.length > limit ? value.substring(0, limit) : value;
+      const genericPattern = patternsGenericPartialMatch[this.approvalType] || /^$/;
+      matches = getMatchedValues(limitedValue, genericPattern);
+      this.setTypeApprovalNumbers(matches.filter(x => x));
     }
 
-    if (!matches?.length) {
+    if (!matches.length) {
       console.error('Unknown approvalType:', this.approvalType);
       return;
     }
-
-    this.setTypeApprovalNumbers(matches.filter(x => x !== null && x !== ''));
   }
 
-  private extractValuesNtaIva(value: string, pattern: RegExp) {
+  private extractValuesNtaIva(value: string, pattern: RegExp): void {
     const matches = value.match(pattern)?.map(x => (x.length > 25 ? x.substring(0, 25) : x));
-    this.setTypeApprovalNumbers(matches ?? []);
+    this.setTypeApprovalNumbers(matches || []);
   }
-
   private setTypeApprovalNumbers(matches: string[]) {
     if (matches) {
       const [techRecord_approvalTypeNumber1, techRecord_approvalTypeNumber2, techRecord_approvalTypeNumber3, techRecord_approvalTypeNumber4] =
@@ -218,28 +263,15 @@ export class ApprovalTypeInputComponent extends BaseControlComponent implements 
    * @returns Subscription
    */
   subscribeAndPropagateChanges() {
-    return combineLatest({
-      approvalTypeNumber1: this.approvalTypeNumber1$,
-      approvalTypeNumber2: this.approvalTypeNumber2$,
-      approvalTypeNumber3: this.approvalTypeNumber3$,
-      approvalTypeNumber4: this.approvalTypeNumber4$
-    })
-      .pipe(
-        filter(
-          ({ approvalTypeNumber1, approvalTypeNumber2, approvalTypeNumber3, approvalTypeNumber4 }) =>
-            approvalTypeNumber1 !== undefined &&
-            approvalTypeNumber2 !== undefined &&
-            approvalTypeNumber3 !== undefined &&
-            approvalTypeNumber4 !== undefined
-        )
-      )
-      .subscribe(({ approvalTypeNumber1, approvalTypeNumber2, approvalTypeNumber3, approvalTypeNumber4 }) => {
+    return combineLatest([this.approvalTypeNumber1$, this.approvalTypeNumber2$, this.approvalTypeNumber3$, this.approvalTypeNumber4$]).subscribe(
+      ([approvalTypeNumber1, approvalTypeNumber2, approvalTypeNumber3, approvalTypeNumber4]) => {
         if (!approvalTypeNumber1 && !approvalTypeNumber2 && !approvalTypeNumber3 && !approvalTypeNumber4) {
           this.onChange(null);
         } else {
           this.onChange(this.processApprovalTypeNumber(approvalTypeNumber1, approvalTypeNumber2, approvalTypeNumber3, approvalTypeNumber4));
         }
-      });
+      }
+    );
   }
 
   validate() {
@@ -397,13 +429,5 @@ export class ApprovalTypeInputComponent extends BaseControlComponent implements 
       default:
         return 'Unknown approval type';
     }
-  }
-
-  getId(name: string) {
-    const id = name + '-day';
-    if (this.control) {
-      this.control.meta.customId = id;
-    }
-    return id;
   }
 }
