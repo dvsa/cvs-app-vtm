405911780963130180f90f8a8a7eda137b7168ab
Jun Yan <jun.yan@bjss.com>
2023-10-03 12:16:21 +0100

    fix(cb2-9200): fix the Axle Weights not incrementing

--- src/app/forms/custom-sections/weights/weights.component.ts
+++ src/app/forms/custom-sections/weights/weights.component.ts
@@ -103,20 +103,23 @@ export class WeightsComponent implements OnInit, OnDestroy, OnChanges {
   }
 
   private handleVehicleTechRecordChange(changes: SimpleChanges): void {
-    if (changes['vehicleTechRecord'] && !this.ladenWeightOverride && this.form) {
-      const { currentValue, previousValue } = changes['vehicleTechRecord'];
+    const { vehicleTechRecord } = changes;
+    if (this.form && vehicleTechRecord && !this.ladenWeightOverride) {
+      const { currentValue, previousValue } = vehicleTechRecord;
 
       const fieldsChanged = [
         'techRecord_seatsUpperDeck',
         'techRecord_seatsLowerDeck',
         'techRecord_manufactureYear',
-        'techRecord_grossKerbWeight',
-        'techRecord_standingCapacity'
+        'techRecord_grossKerbWeight'
       ].some(field => currentValue[field] !== previousValue[field]);
 
-      if (fieldsChanged && currentValue.techRecord_manufactureYear) {
-        this.form.patchValue({ techRecord_grossLadenWeight: this.calculateGrossLadenWeight() }, { emitEvent: false });
+      if (fieldsChanged && this.vehicleTechRecord.techRecord_vehicleType === 'psv') {
+        const newGrossLadenWeight = this.calculateGrossLadenWeight();
+        this.vehicleTechRecord.techRecord_grossLadenWeight = newGrossLadenWeight;
       }
+
+      this.form.patchValue(this.vehicleTechRecord, { emitEvent: false });
     }
   }
 
