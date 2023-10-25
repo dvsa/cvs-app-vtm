9184286259f31d1fb0d7f8d5866dcd2beb537690
pbardy2000 <146740183+pbardy2000@users.noreply.github.com>
2023-10-25 10:38:07 +0100

    feat(cb2-8121): fix linting issue

--- src/app/features/test-records/amend/views/test-record/test-record.component.spec.ts
+++ src/app/features/test-records/amend/views/test-record/test-record.component.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { DebugElement } from '@angular/core';
-import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import {
+  ComponentFixture, fakeAsync, TestBed, tick, waitForAsync,
+} from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { Params } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -18,7 +20,9 @@ import { UserService } from '@services/user-service/user-service';
 import { SharedModule } from '@shared/shared.module';
 import { initialAppState, State } from '@store/.';
 import { routeEditable, selectRouteData, selectRouteNestedParams } from '@store/router/selectors/router.selectors';
-import { initialTestResultsState, isTestTypeKeySame, sectionTemplates, testResultInEdit } from '@store/test-records';
+import {
+  initialTestResultsState, isTestTypeKeySame, sectionTemplates, testResultInEdit,
+} from '@store/test-records';
 import { of, ReplaySubject } from 'rxjs';
 import { DynamicFormsModule } from '../../../../../forms/dynamic-forms.module';
 import { BaseTestRecordComponent } from '../../../components/base-test-record/base-test-record.component';
