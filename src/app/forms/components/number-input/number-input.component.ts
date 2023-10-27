5c329132b338cad7ed6b15257295476a1c26abaf
Jun Yan <jun.yan@bjss.com>
2023-10-27 10:02:29 +0100

    feat(cb2-9604): Added Dimension warnings

--- src/app/forms/components/number-input/number-input.component.ts
+++ src/app/forms/components/number-input/number-input.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component } from '@angular/core';
+import { AfterContentInit, Component, Input } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { BaseControlComponent } from '../base-control/base-control.component';
 
@@ -15,8 +15,29 @@ import { BaseControlComponent } from '../base-control/base-control.component';
   ],
 })
 export class NumberInputComponent extends BaseControlComponent implements AfterContentInit {
+  @Input() targetVehicle?: string | null;
   get style(): string {
     return `govuk-input ${this.width ? `govuk-input--width-${this.width}` : ''}`;
+  }
+
+  get getWarningMessage(): string {
+
+    if (this.isTargetVehicle()) {
+      if (this.isLengthWarning()) return 'This length dimension field value is greater than 12,000mm. Check your input before proceeding';
+      if (this.isWidthWarning()) return 'This width dimension field value is greater than 2,600mm. Check your input before proceeding';
+    }
+    return '';
+  }
+
+  isLengthWarning(): boolean {
+    return this.label === 'Length' && Number(this.value) > 12000;
+  }
+  isWidthWarning(): boolean {
+    return this.label === 'Width' && Number(this.value) > 2600;
+  }
+
+  isTargetVehicle(): boolean {
+    return this.targetVehicle === 'hgv' || this.targetVehicle === 'trl';
   }
 
   override ngAfterContentInit(): void {
