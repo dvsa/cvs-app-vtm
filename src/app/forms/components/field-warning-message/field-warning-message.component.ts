255ec370c0cd6c7f045892fd66eee5b468251abb
Jun Yan <jun.yan@bjss.com>
2023-10-27 13:58:10 +0100

    feat(cb2-9604): removed unnecessary file

--- src/app/forms/components/field-warning-message/field-warning-message.component.ts
+++ src/app/forms/components/field-warning-message/field-warning-message.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 @Component({
   selector: 'app-field-warning-message',
   templateUrl: './field-warning-message.component.html',
-  styleUrls: ['./field-warning-message.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FieldWarningMessageComponent {
