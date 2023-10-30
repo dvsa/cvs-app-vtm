04524d2f06cbfcfb00147ec7bad0e1ab17741da6
owen <owen.corrigan@bjss.com>
2023-10-30 09:39:09 +0000

    feat(cb2-9601): fixes failing test suite for resolver

--- src/app/resolvers/tech-record-review/tech-record-review.resolver.spec.ts
+++ src/app/resolvers/tech-record-review/tech-record-review.resolver.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing';
 
+import { ResolveFn, RouterStateSnapshot } from '@angular/router';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Action } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { State, initialAppState } from '@store/.';
+import { Observable } from 'rxjs';
+import { TestScheduler } from 'rxjs/testing';
 import { TechRecordReviewResolver } from './tech-record-review.resolver';
-
-describe('TechRecordReviewResolver', () => {
-  let resolver: TechRecordReviewResolver;
+describe('TechRecordViewResolver', () => {
+  let resolver: ResolveFn<boolean>;
+  let actions$ = new Observable<Action>();
+  let testScheduler: TestScheduler;
+  const mockSnapshot: any = jest.fn;
+  let store: MockStore<State>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    resolver = TestBed.inject(TechRecordReviewResolver);
+    TestBed.configureTestingModule({
+      providers: [
+        provideMockStore({ initialState: initialAppState }),
+        provideMockActions(() => actions$),
+        { provide: RouterStateSnapshot, useValue: mockSnapshot },
+      ],
+    });
+    store = TestBed.inject(MockStore);
+    resolver = (...resolverParameters) =>
+      TestBed.runInInjectionContext(() => TechRecordReviewResolver(...resolverParameters));
+  });
+
+  beforeEach(() => {
+    testScheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
   });
 
   it('should be created', () => {
     expect(resolver).toBeTruthy();
   });
+
 });
