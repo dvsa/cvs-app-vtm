d0c09dbae54b0a199750f570bc277e7f4fc1b335
Shivangi Das <shivangi.das7@gmail.com>
2023-10-31 12:00:01 +0000

    test(cb2-9183): add tests

--- src/app/resolvers/tech-record-validate/tech-record-validate.resolver.spec.ts
+++ src/app/resolvers/tech-record-validate/tech-record-validate.resolver.spec.ts
@@ -43,7 +43,11 @@ describe('TechRecordViewResolver', () => {
       expect(resolveResult).toBe(true);
       expect(dispatchSpy).toHaveBeenCalledTimes(1);
       expect(dispatchSpy).toHaveBeenCalledWith(expect.objectContaining({
-        vehicleTechRecord: { techRecord_vehicleType: 'hgv', techRecord_vehicleConfiguration: null },
+        vehicleTechRecord: {
+          techRecord_vehicleType: 'hgv',
+          techRecord_vehicleConfiguration: null,
+          techRecord_vehicleClass_description: 'heavy goods vehicle',
+        },
       }));
     });
     it('should dispatch a tech record with null if psv vehicle configuration invalid', async () => {
@@ -71,7 +75,7 @@ describe('TechRecordViewResolver', () => {
       expect(resolveResult).toBe(true);
       expect(dispatchSpy).toHaveBeenCalledTimes(1);
       expect(dispatchSpy).toHaveBeenCalledWith(expect.objectContaining({
-        vehicleTechRecord: { techRecord_vehicleType: 'psv', techRecord_vehicleConfiguration: null },
+        vehicleTechRecord: { techRecord_vehicleType: 'psv', techRecord_vehicleClass_description: null },
       }));
     });
     it('should dispatch a tech record with null if trl vehicle configuration invalid', async () => {
@@ -85,12 +89,16 @@ describe('TechRecordViewResolver', () => {
       expect(resolveResult).toBe(true);
       expect(dispatchSpy).toHaveBeenCalledTimes(1);
       expect(dispatchSpy).toHaveBeenCalledWith(expect.objectContaining({
-        vehicleTechRecord: { techRecord_vehicleType: 'trl', techRecord_vehicleConfiguration: null },
+        vehicleTechRecord: { techRecord_vehicleType: 'trl', techRecord_vehicleConfiguration: null, techRecord_vehicleClass_description: 'trailer' },
       }));
     });
     it('should not dispatch a tech record with trl vehicle configuration is valid', async () => {
       const dispatchSpy = jest.spyOn(store, 'dispatch');
-      jest.spyOn(store, 'select').mockReturnValue(of({ techRecord_vehicleType: 'trl', techRecord_vehicleConfiguration: 'semi-trailer' }));
+      jest.spyOn(store, 'select').mockReturnValue(of({
+        techRecord_vehicleType: 'trl',
+        techRecord_vehicleConfiguration: 'semi-trailer',
+        techRecord_vehicleClass_description: 'trailer',
+      }));
       const result = TestBed.runInInjectionContext(
         () => resolver({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot),
       ) as Observable<boolean>;
@@ -98,6 +106,20 @@ describe('TechRecordViewResolver', () => {
 
       expect(resolveResult).toBe(true);
       expect(dispatchSpy).toHaveBeenCalledTimes(0);
+    });
+    it('should dispatch a tech record with hgv if hgv vehicle class is invalid', async () => {
+      const dispatchSpy = jest.spyOn(store, 'dispatch');
+      jest.spyOn(store, 'select').mockReturnValue(of({ techRecord_vehicleType: 'hgv', techRecord_vehicleClass_description: 'trailer' }));
+      const result = TestBed.runInInjectionContext(
+        () => resolver({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot),
+      ) as Observable<boolean>;
+      const resolveResult = await firstValueFrom(result);
+
+      expect(resolveResult).toBe(true);
+      expect(dispatchSpy).toHaveBeenCalledTimes(1);
+      expect(dispatchSpy).toHaveBeenCalledWith(expect.objectContaining({
+        vehicleTechRecord: { techRecord_vehicleType: 'hgv', techRecord_vehicleClass_description: 'heavy goods vehicle' },
+      }));
     });
   });
 });
