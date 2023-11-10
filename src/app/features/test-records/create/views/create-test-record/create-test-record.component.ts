b1a6a75e2402d39db2ed601517e388a586344469
Shivangi Das <shivangi.das7@gmail.com>
2023-11-10 16:05:48 +0000

    feat(cb2-9039): display message to refresh for some tests

--- src/app/features/test-records/create/views/create-test-record/create-test-record.component.ts
+++ src/app/features/test-records/create/views/create-test-record/create-test-record.component.ts
@@ -4,12 +4,15 @@ import {
 import { ActivatedRoute, Router } from '@angular/router';
 import { GlobalError } from '@core/components/global-error/global-error.interface';
 import { GlobalErrorService } from '@core/components/global-error/global-error.service';
+import { GlobalWarning } from '@core/components/global-warning/global-warning.interface';
+import { GlobalWarningService } from '@core/components/global-warning/global-warning.service';
 import { AbandonDialogComponent } from '@forms/custom-sections/abandon-dialog/abandon-dialog.component';
 import { DynamicFormService } from '@forms/services/dynamic-form.service';
 import { TestModeEnum } from '@models/test-results/test-result-view.enum';
 import { TestResultModel } from '@models/test-results/test-result.model';
 import { TypeOfTest } from '@models/test-results/typeOfTest.enum';
 import { resultOfTestEnum } from '@models/test-types/test-type.model';
+import { StatusCodes } from '@models/vehicle-tech-record.model';
 import { Actions, ofType } from '@ngrx/effects';
 import { Store, select } from '@ngrx/store';
 import { ResultOfTestService } from '@services/result-of-test/result-of-test.service';
@@ -49,6 +52,7 @@ export class CreateTestRecordComponent implements OnInit, OnDestroy, AfterViewIn
     private cdr: ChangeDetectorRef,
     private resultOfTestService: ResultOfTestService,
     private store: Store<State>,
+    private warningService: GlobalWarningService,
   ) {
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
@@ -103,17 +107,27 @@ export class CreateTestRecordComponent implements OnInit, OnDestroy, AfterViewIn
     this.testRecordsService.createTestResult(cloneDeep(testResult));
   }
 
-  handleReview() {
+  async handleReview() {
     if (this.isAnyFormInvalid()) {
       return;
     }
 
     this.errorService.clearErrors();
     this.testMode = TestModeEnum.View;
+
+    const testResult = await firstValueFrom(this.testResult$);
+    if (testResult?.statusCode === StatusCodes.PROVISIONAL
+      && this.testTypeId
+      && this.validateUpdateStatus(testResult.testTypes[0].testResult, this.testTypeId)) {
+      const warnings: GlobalWarning[] = [];
+      warnings.push({ warning: 'This test will update the tech record to current, if the page is showing as provisional then refresh the page' });
+      this.warningService.setWarnings(warnings);
+    }
   }
 
   handleCancel() {
     this.testMode = TestModeEnum.Edit;
+    this.warningService.clearWarnings();
   }
 
   watchForCreateSuccess() {
@@ -187,5 +201,38 @@ export class CreateTestRecordComponent implements OnInit, OnDestroy, AfterViewIn
 
   public get TestModeEnum(): typeof TestModeEnum {
     return TestModeEnum;
+  }
+
+  validateUpdateStatus = (
+    testResult: string,
+    testTypeId: string,
+  ): boolean => (
+    (testResult === 'pass' || testResult === 'prs')
+        && (this.isTestTypeFirstTest(testTypeId)
+          || this.isTestTypeNotifiableAlteration(testTypeId)
+          || this.isTestTypeCOIF(testTypeId)
+          || this.isTestTypeIVA(testTypeId))
+  );
+
+  isTestTypeFirstTest(testTypeId: string): boolean {
+    const firstTestIds = ['41', '95', '65', '66', '67', '103', '104', '82', '83', '119', '120'];
+    return firstTestIds.includes(testTypeId);
+  }
+
+  isTestTypeNotifiableAlteration(testTypeId: string): boolean {
+    const notifiableAlterationIds = ['38', '47', '48'];
+    return notifiableAlterationIds.includes(testTypeId);
+  }
+
+  isTestTypeCOIF(testTypeId: string): boolean {
+    const coifIds = ['142', '143', '175', '176'];
+    return coifIds.includes(testTypeId);
+  }
+
+  isTestTypeIVA(testTypeId: string): boolean {
+    const ivaIds = ['133', '134', '138', '139', '140', '165', '169', '167', '170', '135', '172', '173', '439', '449',
+      '136', '187', '126', '186', '193', '192', '195', '162', '191', '128', '188', '189', '125', '161', '158', '159',
+      '154', '190', '129', '196', '194', '197', '185', '420', '438', '163', '153', '184', '130', '183'];
+    return ivaIds.includes(testTypeId);
   }
 }
