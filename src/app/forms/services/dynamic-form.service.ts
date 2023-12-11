2f99f68637263ee3a829394bf6b8f1799d24381c
JunYanBJSS <144220886+JunYanBJSS@users.noreply.github.com>
2023-12-11 15:40:05 +0000

    fix(cb2-10146): fix global error when fields hidden (#1314)

--- src/app/forms/services/dynamic-form.service.ts
+++ src/app/forms/services/dynamic-form.service.ts
@@ -183,6 +183,7 @@ export class DynamicFormService {
     const { errors } = control;
     const meta = (control as CustomFormControl).meta as FormNode | undefined;
     if (errors) {
+      if (meta?.hide) return;
       const errorList = Object.keys(errors);
       errorList.forEach((error) => {
         validationErrorList.push({
