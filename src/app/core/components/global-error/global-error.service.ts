270c6a57ca8fe452286609de51fc5b0bc82acdf2
pbardy2000 <146740183+pbardy2000@users.noreply.github.com>
2024-01-02 10:13:24 +0000

    fix(cb2-10328): refactor adr component (#1344)

--- src/app/core/components/global-error/global-error.service.ts
+++ src/app/core/components/global-error/global-error.service.ts
@@ -37,4 +37,24 @@ export class GlobalErrorService {
   clearErrors(): void {
     this.store.dispatch(clearError());
   }
+
+  focusAllControls() {
+    document.querySelectorAll(`
+      a[href]:not([tabindex='-1']),
+      area[href]:not([tabindex='-1']),
+      input:not([disabled]):not([tabindex='-1']),
+      select:not([disabled]):not([tabindex='-1']),
+      textarea:not([disabled]):not([tabindex='-1']),
+      button:not([disabled]):not([tabindex='-1']),
+      iframe:not([tabindex='-1']),
+      [tabindex]:not([tabindex='-1']),
+      [contentEditable=true]:not([tabindex='-1'])
+    `)
+      .forEach((element) => {
+        if (element instanceof HTMLElement) {
+          element.focus();
+          element.blur();
+        }
+      });
+  }
 }
