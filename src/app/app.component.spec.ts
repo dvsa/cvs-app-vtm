e9ce2beac23bc16b8bf10d4c495c571998223046
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2024-01-08 09:31:54 +0000

    feat(cb2-10024): fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,6 +14,7 @@ describe('AppComponent', () => {
   const MockUserService = {
     getUserName$: jest.fn().mockReturnValue(new Observable()),
   };
+  (window as any).gtag = () => {};
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
