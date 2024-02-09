f1199450dc3ddd270fdc8f17051fa185c5326bd1
pbardy2000 <146740183+pbardy2000@users.noreply.github.com>
2024-02-09 10:07:07 +0000

    fix(cb2-10591): uncomment custom views for TC2 and TC3 inspections (#1376)
    * fix(cb2-10591): uncomment custom views for TC2 and TC3 inspections
    
    * fix(cb2-10591): minor corrections
    
    * fix(cb2-10591): more minor corrections

--- src/app/forms/templates/general/adr-summary.template.ts
+++ src/app/forms/templates/general/adr-summary.template.ts
@@ -17,6 +17,9 @@ import {
 import { AdrGuidanceNotesComponent } from '@forms/custom-sections/adr-guidance-notes/adr-guidance-notes.component';
 import { AdrTankDetailsM145ViewComponent } from '@forms/custom-sections/adr-tank-details-m145-view/adr-tank-details-m145-view.component';
 import {
+  AdrTankDetailsInitialInspectionViewComponent,
+} from '@forms/custom-sections/adr-tank-details-initial-inspection-view/adr-tank-details-initial-inspection-view.component';
+import {
   AdrTankDetailsSubsequentInspectionsEditComponent,
 } from '@forms/custom-sections/adr-tank-details-subsequent-inspections-edit/adr-tank-details-subsequent-inspections-edit.component';
 import {
@@ -537,9 +540,6 @@ export const AdrSummaryTemplate: FormNode = {
         },
       ],
     },
-    // Note: this used only for the view mode for the ADR Tank Details initial inpsection controls
-    // TODO: uncomment this when needed
-    /*
     {
       name: 'tankInspectionsInitialView',
       type: FormNodeTypes.CONTROL,
@@ -549,11 +549,10 @@ export const AdrSummaryTemplate: FormNode = {
       groups: ['tank_details', 'dangerous_goods'],
       hide: true,
     },
-    */
     {
       name: 'techRecord_adrDetails_tank_tankDetails_tc2Details_tc2Type',
       type: FormNodeTypes.CONTROL,
-      // viewType: FormNodeViewTypes.HIDDEN,
+      viewType: FormNodeViewTypes.HIDDEN,
       editType: FormNodeEditTypes.HIDDEN,
       label: 'TC2: Inspection type',
       value: TC2Types.INITIAL,
@@ -586,7 +585,7 @@ export const AdrSummaryTemplate: FormNode = {
       name: 'techRecord_adrDetails_tank_tankDetails_tc3Details',
       label: 'Subsequent Inspections',
       type: FormNodeTypes.CONTROL,
-      viewType: FormNodeViewTypes.ADRINSPECTIONS, // TODO: replace with custom view type
+      viewType: FormNodeViewTypes.CUSTOM,
       viewComponent: AdrTankDetailsSubsequentInspectionsViewComponent,
       editType: FormNodeEditTypes.CUSTOM,
       editComponent: AdrTankDetailsSubsequentInspectionsEditComponent,
