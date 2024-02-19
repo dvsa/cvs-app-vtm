503e332de75066face46fab3aef477b0c2fc4e3a
pbardy2000 <146740183+pbardy2000@users.noreply.github.com>
2024-02-19 08:34:51 +0000

    feat(cb2-10930): refactor un subsequent inspections component (#1385)
    * feat(cb2-10930): refactor un subsequent inspections component
    
    * feat(cb2-10930): change from javascript void to css
    
    * feat(cb2-10930): move adr validators to own file + add unit tests

--- src/app/forms/custom-sections/adr-tank-statement-un-number-edit/adr-tank-statement-un-number-edit.component.ts
+++ src/app/forms/custom-sections/adr-tank-statement-un-number-edit/adr-tank-statement-un-number-edit.component.ts
@@ -1,17 +1,9 @@
 import {
-  Component, OnDestroy, OnInit,
-  inject,
+  Component, OnDestroy, inject,
 } from '@angular/core';
-import { FormArray, Validators } from '@angular/forms';
-import { GlobalError } from '@core/components/global-error/global-error.interface';
-import { GlobalErrorService } from '@core/components/global-error/global-error.service';
-import { CustomFormControl } from '@forms/services/dynamic-form.types';
-import _ from 'lodash';
-import {
-  ReplaySubject,
-  skip,
-  takeUntil,
-} from 'rxjs';
+import { FormBuilder, Validators } from '@angular/forms';
+import { CustomFormControl, FormNodeTypes } from '@forms/services/dynamic-form.types';
+import { Subject, takeUntil } from 'rxjs';
 import { CustomFormControlComponent } from '../custom-form-control/custom-form-control.component';
 
 @Component({
@@ -19,110 +11,56 @@ import { CustomFormControlComponent } from '../custom-form-control/custom-form-c
   templateUrl: './adr-tank-statement-un-number-edit.component.html',
   styleUrls: ['./adr-tank-statement-un-number-edit.component.scss'],
 })
-export class AdrTankStatementUnNumberEditComponent extends CustomFormControlComponent implements OnInit, OnDestroy {
-  checked = false;
-  submitted = false;
-  destroy$ = new ReplaySubject<boolean>(1);
-  formArray = new FormArray<CustomFormControl>([]);
-  globalErrorService = inject(GlobalErrorService);
+export class AdrTankStatementUnNumberEditComponent extends CustomFormControlComponent implements OnDestroy {
+  fb = inject(FormBuilder);
 
-  ngOnInit() {
-    this.formArray.valueChanges.pipe(takeUntil(this.destroy$)).subscribe((changes) => this.onFormChange(changes));
-    this.globalErrorService.errors$.pipe(skip(1), takeUntil(this.destroy$)).subscribe((errors) => this.onFormSubmitted(errors));
-  }
+  destroy$ = new Subject();
+  formArray = this.fb.array<CustomFormControl>([]);
+
+  onFormArrayChange = this.formArray.valueChanges.pipe(takeUntil(this.destroy$)).subscribe((changes) => {
+    this.control?.patchValue(changes, { emitModelToViewChange: true });
+  });
 
   ngOnDestroy(): void {
     this.destroy$.next(true);
-    this.destroy$.unsubscribe();
+    this.destroy$.complete();
   }
 
   override ngAfterContentInit(): void {
     super.ngAfterContentInit();
     this.buildFormArray();
-
   }
 
   buildFormArray() {
-    const value = this.form?.get(this.name)?.value;
-    const values = Array.isArray(value) && value.length ? value : [''];
-    values.forEach((unNumber: string) => this.addControl(unNumber));
+    const values: string[] = this.control?.value ?? [''];
+    values.forEach((value) => this.addControl(value));
   }
 
-  canAddControl() {
-    return !(this.formArray.length && !this.formArray.at(-1).value);
-  }
+  addControl(value = '') {
+    const lastUnNumber = this.formArray.at(-1);
 
-  addControl(value: string | null = null) {
-    if (!this.control) return;
+    if (this.formArray.length > 0 && (lastUnNumber.invalid || !lastUnNumber.value)) {
+      // If the parent control or lastUnNumber control isn't already invalid set additional errors
+      if (!this.control?.invalid && !lastUnNumber.invalid) {
+        lastUnNumber.setErrors({ required: true });
+      }
 
-    // Prevent adding new controls, whilst previous ones are empty
-    if (!this.canAddControl()) {
-      this.control.markAsTouched();
+      // Mark as touched to show errors
       this.formArray.markAllAsTouched();
+
       return;
     }
 
-    const control = new CustomFormControl({ ...this.control.meta }, value);
-    control.addValidators(Validators.maxLength(1500));
-
-    // If this is a subsequent UN Number, then it is required, and must be filled in, or removed.
-    if (this.formArray.length > 0) {
-      const firstUnNumber = this.formArray.at(0);
-      if (!firstUnNumber.hasValidator(Validators.required)) firstUnNumber.addValidators(Validators.required);
-      control.addValidators(Validators.required);
-      control.meta.validators = undefined;
-    }
-
-    this.formArray.push(control);
-    this.updateControls();
+    this.formArray.push(
+      new CustomFormControl(
+        { type: FormNodeTypes.CONTROL, name: 'UN number' },
+        value,
+        [Validators.minLength(1), Validators.maxLength(1500)],
+      ),
+    );
   }
 
   removeControl(index: number) {
-    if (this.formArray.length < 2) return;
     this.formArray.removeAt(index);
-    this.updateControls();
-
-    if (this.formArray.length === 1) {
-      this.formArray.at(0).meta.customErrorMessage = undefined;
-      this.formArray.at(0).removeValidators(Validators.required);
-    }
-  }
-
-  updateControls() {
-    this.formArray.controls.forEach((control, index) => {
-      // Make all UN NUmbers labels reflect their position in form array
-      control.meta.customId = `${this.control?.meta.name}_${index + 1}`;
-      control.meta.customErrorMessage = control.invalid && control.hasError('maxlength')
-        ? `UN number ${index + 1} must be less than or equal to 1500 characters`
-        : `UN number ${index + 1} is required or remove UN number ${index + 1}`;
-    });
-  }
-
-  onFormChange(changes: (string | null)[] | null) {
-    this.control?.patchValue(changes, { emitModelToViewChange: true });
-    this.updateControls();
-  }
-
-  onFormSubmitted(globalErrors: GlobalError[]) {
-    this.submitted = true;
-
-    // If ANY UN Numbers are invalid, loop through them and add to global errors, but avoid adding duplicates
-    if (this.formArray.invalid) {
-      const firstCtrl = this.formArray.at(0);
-      const customErrorMessage = this.control?.meta.customErrorMessage;
-
-      const formErrors = this.formArray.controls
-        .filter((control) => control.invalid)
-        .map((control) => ({ error: control.meta.customErrorMessage as string, anchorLink: control.meta.customId }));
-
-      const allErrors = _.chain(globalErrors)
-        .filter((error) => !(error.error === customErrorMessage && (firstCtrl.valid || !firstCtrl.hasError(customErrorMessage))))
-        .concat(formErrors)
-        .uniqBy((error) => error.error);
-
-      if (!allErrors.isEqualWith(globalErrors).value()) {
-        this.globalErrorService.setErrors(allErrors.value());
-      }
-    }
   }
 }
