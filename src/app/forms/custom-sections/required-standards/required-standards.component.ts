c4e91f27ab5cbfec9c509aa4d18d6c1037426918
Nathan Brown <nathan.brown@bjss.com>
2024-02-20 11:17:26 +0000

    feat(cb2-10741): pull back and store Required Standards for a new IVA test (#1390)
    * feat(cb2-10741): pull required standards and store on page navigation
    
    * feat(cb2-10741): add validation to required standards button call
    
    * feat(cb2-10741): wipe RS when EU cat changes
    
    * feat(cb2-10741): repair tests and imporve rs state
    
    * feat(cb2-10741): test ngrx methods
    
    * feat(cb2-10741): test ngrx methods
    
    * feat(cb2-10741): broken tests yay
    
    * feat(cb2-10741): repair test
    
    * feat(cb2-10741): repair test
    
    * feat(cb2-10741): add button click tests
    
    * feat(cb2-10741): add resolver test
    
    * feat(cb2-10741): lint fix
    
    * feat(cb2-10741): add linter warning suppresion

--- src/app/forms/custom-sections/required-standards/required-standards.component.ts
+++ src/app/forms/custom-sections/required-standards/required-standards.component.ts
@@ -1,6 +1,9 @@
+import { ViewportScroller } from '@angular/common';
 import {
-  Component, EventEmitter, Input, OnDestroy, OnInit, Output,
+  Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges,
 } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GlobalErrorService } from '@core/components/global-error/global-error.service';
 import { DynamicFormService } from '@forms/services/dynamic-form.service';
 import { CustomFormArray, CustomFormGroup, FormNode } from '@forms/services/dynamic-form.types';
 import { TestResultRequiredStandard } from '@models/test-results/test-result-required-standard.model';
@@ -11,22 +14,28 @@ import { Subscription, debounceTime } from 'rxjs';
   selector: 'app-required-standards[template]',
   templateUrl: './required-standards.component.html',
 })
-export class RequiredStandardsComponent implements OnInit, OnDestroy {
+export class RequiredStandardsComponent implements OnInit, OnDestroy, OnChanges {
   @Input() isEditing = false;
   @Input() template!: FormNode;
-  @Input() requiredStandards?: any[] | null = []; // TODO: fix this to use actual RS
-  @Input() data: Partial<TestResultModel> = {};
+  @Input() testData: Partial<TestResultModel> = {};
 
   @Output() formChange = new EventEmitter();
+  @Output() validateEuVehicleCategory = new EventEmitter();
 
   public form!: CustomFormGroup;
   private formSubscription = new Subscription();
   private requiredStandardsFormArray?: CustomFormArray;
 
-  constructor(private dfs: DynamicFormService) {}
+  constructor(
+    private dfs: DynamicFormService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private viewportScroller: ViewportScroller,
+    private globalErrorService: GlobalErrorService,
+  ) {}
 
   ngOnInit(): void {
-    this.form = this.dfs.createForm(this.template, this.data) as CustomFormGroup;
+    this.form = this.dfs.createForm(this.template, this.testData) as CustomFormGroup;
     this.formSubscription = this.form.cleanValueChanges.pipe(debounceTime(400)).subscribe((event) => {
       this.formChange.emit(event);
     });
@@ -34,6 +43,25 @@ export class RequiredStandardsComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.formSubscription.unsubscribe();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const { testData } = changes;
+
+    if (testData?.currentValue?.euVehicleCategory !== testData?.previousValue?.euVehicleCategory) {
+      this.form?.get(['testTypes', '0', 'requiredStandards'])?.patchValue([], { emitEvent: true });
+    }
+
+  }
+
+  onAddRequiredStandard(): void {
+    this.globalErrorService.clearErrors();
+    if (!this.testData?.euVehicleCategory) {
+      this.validateEuVehicleCategory.emit();
+      this.viewportScroller.scrollToPosition([0, 0]);
+      return;
+    }
+    void this.router.navigate(['selectRequiredStandard'], { queryParamsHandling: 'preserve', relativeTo: this.route });
   }
 
   get requiredStandardsForm(): CustomFormArray {
