f1e81afb462c6ede151f51376d44cfb355da583b
JunYanBJSS <jun.yan@bjss.com>
2024-02-26 15:50:08 +0000

    feat(cb2-8123): replace tech record routes

--- src/app/models/routes.enum.ts
+++ src/app/models/routes.enum.ts
@@ -9,3 +9,29 @@ export enum RootRoutes {
   ERROR = 'error',
   WILCARD = '**',
 }
+
+export enum SearchRoutes {
+  SEARCH_RESULT = 'results',
+}
+
+export enum TechRecordRoutes {
+  TECH_RECORD_CORRECT_ERROR = 'correcting-an-error',
+  TECH_RECORD_NOTIFIABLE_ALTERATION_NEEDED = 'notifiable-alteration-needed',
+  TECH_RECORD_CHANGE_VIN = 'change-vin',
+  TECH_RECORD_CHANGE_VRM = 'change-vrm',
+  TECH_RECORD_REASON_TO_CHANGE_VRM = 'change-vrm/:reason',
+  TECH_RECORD_GENERATE_PLATE = 'generate-plate',
+  TECH_RECORD_GENERATE_LETTER = 'generate-letter',
+  TECH_RECORD_AMEND_REASON = 'amend-reason',
+  TECH_RECORD_CHANGE_STATUS = 'change-status',
+  TECH_RECORD_UNARCHIVE_RECORD = 'unarchive-record',
+  TECH_RECORD_CHANGE_VEHICLE_TYPE = 'change-vehicle-type',
+  TECH_RECORD_CHANGE_VTA_VISIBILITY = 'change-vta-visibility',
+  TECH_RECORD_CORRECT_ERROR_TYRE_SEARCH = 'correcting-an-error/tyre-search/:axleNumber',
+  TECH_RECORD_CORRECT_ERROR_CHANGE_SUMMARY = 'correcting-an-error/change-summary',
+  TECH_RECORD_NOTIFIABLE_ALTERATION_NEEDED_CHANGE_SUMMARY = 'notifiable-alteration-needed/change-summary',
+  TECH_RECORD_NOTIFIABLE_ALTERATION_NEEDED_TYRE_SEARCH = 'notifiable-alteration-needed/tyre-search/:axleNumber',
+  TECH_RECORD_TEST_RECORDS = 'test-records/test-result/:testResultId/:testNumber',
+  TECH_RECORD_TEST_RECORDS_CREATE_TESTS = 'test-records/create-test',
+  TECH_RECORD_ADR_CERTIFICATE = 'adr-certificate',
+}
