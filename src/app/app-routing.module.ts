52a8b45cf634db630ad2ab5f200a6d2ba3c0e20f
JunYanBJSS <jun.yan@bjss.com>
2024-02-26 13:37:20 +0000

    feat(cb2-8123): attempt of refactor magic string

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { RoleGuard } from '@guards/role-guard/roles.guard';
 import { Roles } from '@models/roles.enum';
 import { techRecordViewResolver } from './resolvers/tech-record-view/tech-record-view.resolver';
 import { titleResolver } from './resolvers/title/title.resolver';
+import { RootRoutes } from './shared/routes-constants/route-paths';
 
 const routes: Routes = [
   {
@@ -23,58 +24,58 @@ const routes: Routes = [
         loadChildren: () => import('./features/home/home.module').then((m) => m.HomeModule),
       },
       {
-        path: 'search',
+        path: RootRoutes.SEARCH_TECHNICAL_RECORD,
         data: { title: 'Technical record search', roles: Roles.TechRecordView },
         canActivate: [MsalGuard, RoleGuard],
         canDeactivate: [CancelEditTechGuard],
         loadChildren: () => import('./features/search/search.module').then((m) => m.SearchModule),
       },
       {
-        path: 'create',
+        path: RootRoutes.CREATE_TECHNICAL_RECORD,
         data: { title: 'Create new technical record', roles: Roles.TechRecordCreate },
         canActivate: [MsalGuard, RoleGuard],
         loadChildren: () => import('./features/tech-record/create/create-tech-records.module').then((m) => m.CreateTechRecordsModule),
       },
       {
-        path: 'create-batch',
+        path: RootRoutes.BATCH_CREATE_TECHNICAL_RECORD,
         data: { title: 'Select Vehicle Type', roles: Roles.TechRecordCreate },
         canActivate: [MsalGuard, RoleGuard],
         loadChildren: () => import('./features/tech-record/create-batch/create-batch.module').then((m) => m.CreateBatchModule),
       },
       {
-        path: 'test-records/:systemNumber/test-result/:testResultId/:testNumber',
+        path: RootRoutes.CURRENT_TEST_RESULT,
         data: { title: 'Test Result', roles: Roles.TestResultView },
         canActivate: [MsalGuard, RoleGuard],
         resolve: { techRecord: techRecordViewResolver },
         loadChildren: () => import('./features/test-records/amend/amend-test-records.module').then((m) => m.AmendTestRecordsModule),
       },
       {
-        path: 'tech-records/:systemNumber/:createdTimestamp',
+        path: RootRoutes.CURRENT_TECH_RECORD,
         data: { title: 'Tech Record', roles: Roles.TechRecordView },
         canActivate: [MsalGuard, RoleGuard],
         loadChildren: () => import('./features/tech-record/tech-record.module').then((m) => m.TechRecordsModule),
       },
       {
-        path: 'reference-data',
+        path: RootRoutes.REFERENCE_DATA,
         data: { title: 'Select Reference Data Type', roles: Roles.ReferenceDataView },
         canActivate: [MsalGuard, RoleGuard],
         loadChildren: () => import('./features/reference-data/reference-data.module').then((m) => m.ReferenceDataModule),
       },
       {
-        path: 'feature-toggle',
+        path: RootRoutes.FEATURE_TOGGLE,
         data: { title: 'Feature Toggle', featureToggleName: 'testToggle' },
         canActivate: [MsalGuard, FeatureToggleGuard],
         loadChildren: () => import('./features/feature-toggle/feature-toggle.module').then((m) => m.FeatureToggleModule),
       },
       {
-        path: 'error',
+        path: RootRoutes.ERROR,
         pathMatch: 'full',
         component: ServerErrorComponent,
       },
     ],
   },
   {
-    path: '**',
+    path: RootRoutes.WILCARD,
     pathMatch: 'full',
     component: PageNotFoundComponent,
   },
