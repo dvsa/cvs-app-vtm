e7ec481e97254196c1a30735f26af0af82834c28
Brandon Thomas-Davies <87308252+BrandonT95@users.noreply.github.com>
2024-03-04 15:10:14 +0000

    feat(cb2-11132): only allow new failed iva/msva tests to fetch certs

--- src/app/shared/components/test-certificate/test-certificate.component.ts
+++ src/app/shared/components/test-certificate/test-certificate.component.ts
@@ -1,4 +1,15 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy, Component, inject, Input, OnDestroy, OnInit,
+} from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { isTestTypeOldIvaOrMsva, toEditOrNotToEdit } from '@store/test-records';
+import {
+  Subject, takeUntil, combineLatest, filter,
+} from 'rxjs';
+import { State } from '@store/index';
+import { resultOfTestEnum } from '@models/test-types/test-type.model';
+import { TEST_TYPES_GROUP1_SPEC_TEST, TEST_TYPES_GROUP5_SPEC_TEST } from '@forms/models/testTypeId.enum';
+import { FeatureToggleService } from '@services/feature-toggle-service/feature-toggle-service';
 
 @Component({
   selector: 'app-test-certificate[testNumber][vin]',
@@ -6,10 +17,31 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   styleUrls: ['./test-certificate.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TestCertificateComponent {
+export class TestCertificateComponent implements OnInit, OnDestroy {
+  store: Store<State> = inject(Store<State>);
+  featureToggleService = inject(FeatureToggleService);
   @Input() testNumber!: string;
   @Input() vin!: string;
   @Input() isClickable = true;
+  certNotNeeded: boolean = false;
+  private destroyed$ = new Subject<void>();
+
+  ngOnInit(): void {
+    const isRequiredStandardsEnabled = this.featureToggleService.isFeatureEnabled('requiredStandards');
+    combineLatest([
+      this.store.pipe(select(toEditOrNotToEdit)),
+      this.store.pipe(select(isTestTypeOldIvaOrMsva)),
+    ]).pipe(
+      filter(([testResult]) => !!testResult),
+      takeUntil(this.destroyed$),
+    ).subscribe(([testResult, isOldIvaOrMsva]) => {
+      if (testResult && isRequiredStandardsEnabled) {
+        const { testResult: result, testTypeId: id } = testResult.testTypes[0];
+        const isIvaOrMsvaTest = TEST_TYPES_GROUP1_SPEC_TEST.includes(id) || TEST_TYPES_GROUP5_SPEC_TEST.includes(id);
+        this.certNotNeeded = isOldIvaOrMsva || (isIvaOrMsvaTest && result !== resultOfTestEnum.fail);
+      }
+    });
+  }
 
   get documentParams(): Map<string, string> {
     return new Map([
@@ -20,5 +52,10 @@ export class TestCertificateComponent {
 
   get fileName(): string {
     return `${this.testNumber}_${this.vin}`;
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 }
