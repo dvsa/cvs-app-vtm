491dfa0b1cbce82f78e012ecb1ccff54b6cd1b21
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2024-03-27 15:41:24 +0000

    feat(cb2-11250): blank page link working

---
+++ src/app/features/tech-record/components/tech-record-edit-additional-examiner-note/tech-record-edit-additional-examiner-note.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tech-record-edit-additional-examiner-note',
+  templateUrl: './tech-record-edit-additional-examiner-note.component.html',
+  styleUrls: ['./tech-record-edit-additional-examiner-note.component.scss'],
+})
+export class TechRecordEditAdditionalExaminerNoteComponent implements OnInit, OnDestroy {
+  ngOnDestroy(): void {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
