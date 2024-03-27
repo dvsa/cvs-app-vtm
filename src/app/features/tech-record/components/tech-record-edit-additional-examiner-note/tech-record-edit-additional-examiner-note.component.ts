436cc7ec205ee2f8956654ff2f8be38410400d7c
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2024-03-27 15:44:04 +0000

    feat(cb2-11250): revert unnecessary changes

--- src/app/features/tech-record/components/tech-record-edit-additional-examiner-note/tech-record-edit-additional-examiner-note.component.ts
+++ src/app/features/tech-record/components/tech-record-edit-additional-examiner-note/tech-record-edit-additional-examiner-note.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'tech-record-edit-additional-examiner-note',
   templateUrl: './tech-record-edit-additional-examiner-note.component.html',
   styleUrls: ['./tech-record-edit-additional-examiner-note.component.scss'],
 })
-export class TechRecordEditAdditionalExaminerNoteComponent implements OnInit, OnDestroy {
-  ngOnDestroy(): void {
-  }
-
-  ngOnInit(): void {
-  }
+export class TechRecordEditAdditionalExaminerNoteComponent {
 
 }
