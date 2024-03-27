ee711c0c2bc47a9d7c97a463dcc4ecfd22c010d8
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2024-03-27 15:20:01 +0000

    feat(cb2-11250): set up router link

--- src/app/forms/custom-sections/adr-examiner-notes-history-edit/adr-examiner-notes-history.component-edit.ts
+++ src/app/forms/custom-sections/adr-examiner-notes-history-edit/adr-examiner-notes-history.component-edit.ts
@@ -1,4 +1,4 @@
-import { KeyValue } from '@angular/common';
+import { KeyValue, ViewportScroller } from '@angular/common';
 import {
   AfterContentInit,
   Component, inject, OnDestroy, OnInit,
@@ -9,6 +9,11 @@ import { BaseControlComponent } from '@forms/components/base-control/base-contro
 import { CustomControl, CustomFormControl } from '@forms/services/dynamic-form.types';
 import { TechnicalRecordService } from '@services/technical-record/technical-record.service';
 import { ReplaySubject, takeUntil } from 'rxjs';
+import { updateScrollPosition } from '@store/technical-records';
+import { TechnicalRecordServiceState } from '@store/technical-records/reducers/technical-record-service.reducer';
+import { Store } from '@ngrx/store';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ReasonForEditing } from '@models/vehicle-tech-record.model';
 
 @Component({
   selector: 'app-adr-examiner-notes-history',
@@ -22,6 +27,11 @@ export class AdrExaminerNotesHistoryEditComponent extends BaseControlComponent i
   formArray = new FormArray<CustomFormControl>([]);
   currentTechRecord?: TechRecordType<'hgv' | 'lgv' | 'trl'> = undefined;
   technicalRecordService = inject(TechnicalRecordService);
+  store = inject(Store<TechnicalRecordServiceState>);
+  viewportScroller = inject(ViewportScroller);
+  router = inject(Router);
+  route = inject(ActivatedRoute);
+  editingReason?: ReasonForEditing;
 
   ngOnInit() {
     this.formArray.valueChanges.pipe(takeUntil(this.destroy$)).subscribe((changes) => {
@@ -30,6 +40,7 @@ export class AdrExaminerNotesHistoryEditComponent extends BaseControlComponent i
     this.technicalRecordService.techRecord$.pipe(takeUntil(this.destroy$)).subscribe((currentTechRecord) => {
       this.currentTechRecord = currentTechRecord as TechRecordType<'hgv' | 'lgv' | 'trl'>;
     });
+    this.editingReason = this.route.snapshot.data['reason'];
   }
 
   override ngAfterContentInit() {
@@ -46,6 +57,14 @@ export class AdrExaminerNotesHistoryEditComponent extends BaseControlComponent i
   getAdditionalExaminerNotes() {
     const returnValue = this.currentTechRecord ? this.currentTechRecord.techRecord_adrDetails_additionalExaminerNotes ?? [] : [];
     return returnValue;
+  }
+
+  getEditAdditionalExaminerNotePage(examinerNoteIndex: number) {
+    const route = this.editingReason ? `../${this.editingReason}/edit-examiner-note/${examinerNoteIndex}` : `./tyre-search/${examinerNoteIndex}`;
+
+    this.store.dispatch(updateScrollPosition({ position: this.viewportScroller.getScrollPosition() }));
+
+    void this.router.navigate([route], { relativeTo: this.route, state: this.currentTechRecord });
   }
 
   ngOnDestroy() {
