12cb9755b93dfb745daee67536c7006aa052a98a
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2024-03-28 14:54:43 +0000

    feat(cb2-10033): basic setup and grabbed selected note from record

--- src/app/features/tech-record/components/tech-record-edit-additional-examiner-note/tech-record-edit-additional-examiner-note.component.ts
+++ src/app/features/tech-record/components/tech-record-edit-additional-examiner-note/tech-record-edit-additional-examiner-note.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TechnicalRecordService } from '@services/technical-record/technical-record.service';
+import { TechRecordType } from '@dvsa/cvs-type-definitions/types/v3/tech-record/tech-record-vehicle-type';
+import { ReplaySubject, take, takeUntil } from 'rxjs';
+import { GlobalErrorService } from '@core/components/global-error/global-error.service';
+import { GlobalError } from '@core/components/global-error/global-error.interface';
+import { AdditionalExaminerNotes } from '@dvsa/cvs-type-definitions/types/v3/tech-record/get/hgv/complete';
 
 @Component({
   selector: 'tech-record-edit-additional-examiner-note',
   templateUrl: './tech-record-edit-additional-examiner-note.component.html',
   styleUrls: ['./tech-record-edit-additional-examiner-note.component.scss'],
 })
-export class TechRecordEditAdditionalExaminerNoteComponent {
+export class TechRecordEditAdditionalExaminerNoteComponent implements OnInit {
+  currentTechRecord!: TechRecordType<'hgv' | 'trl' | 'lgv'>;
+  examinerNote!: AdditionalExaminerNotes;
+  examinerNoteIndex!: number;
+  destroy$ = new ReplaySubject<boolean>(1);
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private technicalRecordService: TechnicalRecordService,
+    private globalErrorService: GlobalErrorService,
+  ) { }
+
+  ngOnInit() {
+    this.technicalRecordService.techRecord$.pipe(takeUntil(this.destroy$)).subscribe((currentTechRecord) => {
+      this.currentTechRecord = currentTechRecord as TechRecordType<'hgv' | 'lgv' | 'trl'>;
+    });
+
+    this.route.params.pipe(take(1)).subscribe((params) => {
+      this.examinerNoteIndex = params['examinerNoteIndex'];
+    });
+    const additionalExaminerNotes = this.currentTechRecord?.techRecord_adrDetails_additionalExaminerNotes;
+    if (additionalExaminerNotes) {
+      this.examinerNote = additionalExaminerNotes[this.examinerNoteIndex];
+      console.log(this.examinerNote);
+    }
+  }
+
+  navigateBack() {
+    this.globalErrorService.clearErrors();
+    void this.router.navigate(['../../'], { relativeTo: this.route });
+  }
+
+  handleSubmit(): void {
+    if (!this.isFormValid()) {
+      return;
+    }
+    console.log('should dispatch amendAdditionalExaminerNote action');
+    // this.store.dispatch(
+    //   amendAdditionalExaminerNote({})
+    // );
+  }
+
+  isFormValid(): boolean {
+    this.globalErrorService.clearErrors();
+
+    const errors: GlobalError[] = [];
+
+    // check for errors in form
+
+    if (errors?.length > 0) {
+      this.globalErrorService.setErrors(errors);
+      return false;
+    }
+    return true;
+  }
 
 }
