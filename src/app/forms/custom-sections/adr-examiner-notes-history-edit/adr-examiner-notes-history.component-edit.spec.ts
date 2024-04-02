e7e4fd5308ecf553a0f70498e1bf71e7a469632c
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2024-04-02 09:08:46 +0100

    feat/CB2-11250 - Edit button on additional examiner notes takes you to blank page (#1447)
    * feat(cb2-10241): update adr notes to use custom component
    
    * feat(cb2-11250): format table
    
    * feat(cb2-11250): set up router link
    
    * feat(cb2-11250): blank page link working
    
    * feat(cb2-11250): revert unnecessary changes
    
    * feat(cb2-11250): amend routing function
    
    * feat(cb2-11250): fix linting and add unit test
    
    * feat(cb2-11250): fix linting and add unit test
    
    * feat(cb2-11250): update link design
    
    * feat(cb2-11250): fix button link styling
    
    ---------
    
    Co-authored-by: Brandon Thomas-Davies <87308252+BrandonT95@users.noreply.github.com>

--- src/app/forms/custom-sections/adr-examiner-notes-history-edit/adr-examiner-notes-history.component-edit.spec.ts
+++ src/app/forms/custom-sections/adr-examiner-notes-history-edit/adr-examiner-notes-history.component-edit.spec.ts
@@ -8,6 +8,11 @@ import { DynamicFormsModule } from '@forms/dynamic-forms.module';
 import { mockVehicleTechnicalRecord } from '@mocks/mock-vehicle-technical-record.mock';
 import { VehicleTypes } from '@models/vehicle-tech-record.model';
 import { TechnicalRecordService } from '@services/technical-record/technical-record.service';
+import { provideMockStore } from '@ngrx/store/testing';
+import { initialAppState } from '@store/index';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
 
 const mockTechRecordService = {
   techRecord$: jest.fn(),
@@ -15,16 +20,20 @@ const mockTechRecordService = {
 describe('AdrExaminerNotesHistoryEditComponent', () => {
   let component: AdrExaminerNotesHistoryEditComponent;
   let fixture: ComponentFixture<AdrExaminerNotesHistoryEditComponent>;
+  let router: Router;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AdrExaminerNotesHistoryEditComponent],
-      imports: [DynamicFormsModule, FormsModule, ReactiveFormsModule],
+      imports: [DynamicFormsModule, FormsModule, ReactiveFormsModule, RouterTestingModule],
       providers: [
         { provide: TechnicalRecordService, useValue: mockTechRecordService },
+        provideMockStore({ initialState: initialAppState }),
+        { provide: ActivatedRoute, useValue: { params: of([{ id: 1 }]) } },
       ],
     }).compileComponents();
     fixture = TestBed.createComponent(AdrExaminerNotesHistoryEditComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
   });
   describe('ngOnDestroy', () => {
     it('should call destroy$.next and destroy$.complete', () => {
@@ -54,6 +63,13 @@ describe('AdrExaminerNotesHistoryEditComponent', () => {
       component.currentTechRecord.techRecord_adrDetails_additionalExaminerNotes = [testNote];
       const notes = component.getAdditionalExaminerNotes();
       expect(notes).toEqual([testNote]);
+    });
+  });
+  describe('getEditAdditionalExaminerNotePage', () => {
+    it('should navigate you to the EditAdditionalExaminerNotePage', () => {
+      const routerSpy = jest.spyOn(router, 'navigate');
+      component.getEditAdditionalExaminerNotePage(1);
+      expect(routerSpy).toHaveBeenCalled();
     });
   });
 });
