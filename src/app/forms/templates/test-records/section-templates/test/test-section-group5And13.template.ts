1ff472f612089cc19d0e522d07673f3bc114581c
pbardy2000 <146740183+pbardy2000@users.noreply.github.com>
2024-08-13 10:22:29 +0100

    fix(cb2-13164): fix linting

--- src/app/forms/templates/test-records/section-templates/test/test-section-group5And13.template.ts
+++ src/app/forms/templates/test-records/section-templates/test/test-section-group5And13.template.ts
@@ -1,6 +1,8 @@
 import { AsyncValidatorNames } from '@forms/models/async-validators.enum';
 import { ValidatorNames } from '@forms/models/validators.enum';
-import { FormNode, FormNodeEditTypes, FormNodeTypes, FormNodeViewTypes, FormNodeWidth } from '@forms/services/dynamic-form.types';
+import {
+  FormNode, FormNodeEditTypes, FormNodeTypes, FormNodeViewTypes, FormNodeWidth,
+} from '@forms/services/dynamic-form.types';
 import { ReferenceDataResourceType } from '@models/reference-data.model';
 
 export const TestSectionGroup5And13: FormNode = {
