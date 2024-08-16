ee80f2799f93e27bbe75b47f50fa78f805a37a89
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2024-08-16 09:24:43 +0100

    fix(linting-1): revert api folder changes

--- src/app/api/test-results/api/updateTestResults.service.ts
+++ src/app/api/test-results/api/updateTestResults.service.ts
@@ -10,7 +10,8 @@
  *//* tslint:disable:no-unused-variable member-ordering */
 
 import { Inject, Injectable, Optional }                      from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams, HttpResponse, HttpEvent }                           from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams,
+         HttpResponse, HttpEvent }                           from '@angular/common/http';
 import { CustomHttpUrlEncodingCodec }                        from '../encoder';
 
 import { Observable }                                        from 'rxjs';
