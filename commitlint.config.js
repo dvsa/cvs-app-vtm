02b81af70acae3d0e54336505f6808fd6079826c
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2024-10-04 10:21:26 +0100

    chore: relax rules to not have to provide ticket number in scope

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,42 +1,19 @@
 // https://github.com/conventional-changelog/commitlint/blob/master/@commitlint/config-conventional/index.js
 // https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-rules.md
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    plugins: ['commitlint-plugin-function-rules'],
-    rules: {
-      'type-enum': [
-        2,
-        'always',
-        // in addtition to angular defaults, we add additional type for commits, breaking, breaking_change will force a major release with semantic-release
-        // any body of commit containing BREAKING_CHANGE, BREAKING, etc.. will do too, please refer to the .releaserc.json file
-        ['revert', 'feat', 'test', 'perf', 'fix', 'chore', 'ci', 'docs', 'breaking_change', 'breaking', 'refactor'],
-      ],
-      'subject-case': [1, 'always', 'lower-case'],
-      // disable it to allow release notes from semantic-release to happen when releasing
-      'footer-max-line-length': [0],
-      'body-max-line-length': [0],
-      // level: disabled to use our own rule with commitlint-plugin-function-rules plugin
-      'scope-case': [0],
-      'function-rules/scope-empty': [
-        2,
-        'always',
-        (parsed) => {
-          const ticketNumberFormat = /^[a-z0-9]+-\d+$/;
-          // type(scope?): subject
-          // we want to ticket number to appear in the commit msg as well as scope when we release
-          if (
-            parsed &&
-            ((parsed.scope && parsed.scope.match(ticketNumberFormat) !== null) || parsed.scope === 'release')
-          ) {
-            return [true];
-          } else {
-            return [
-              false,
-              `scope must follow the format defined in the commitlint config. The current format is ${ticketNumberFormat}`,
-            ];
-          }
-        },
-      ],
-    },
-  };
-  
+	extends: ['@commitlint/config-conventional'],
+	plugins: ['commitlint-plugin-function-rules'],
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			// in addtition to angular defaults, we add additional type for commits, breaking, breaking_change will force a major release with semantic-release
+			// any body of commit containing BREAKING_CHANGE, BREAKING, etc.. will do too, please refer to the .releaserc.json file
+			['revert', 'feat', 'test', 'perf', 'fix', 'chore', 'ci', 'docs', 'breaking_change', 'breaking', 'refactor'],
+		],
+		'subject-case': [1, 'always', 'lower-case'],
+		// disable it to allow release notes from semantic-release to happen when releasing
+		'footer-max-line-length': [0],
+		'body-max-line-length': [0],
+	},
+};
