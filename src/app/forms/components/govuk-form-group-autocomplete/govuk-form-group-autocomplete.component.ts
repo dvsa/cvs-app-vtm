38ef113fb09009251b295292404e3de05f14a615
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2024-12-09 12:51:06 +0000

    feat(CB2-14459): autocomplete stuff

--- src/app/forms/components/govuk-form-group-autocomplete/govuk-form-group-autocomplete.component.ts
+++ src/app/forms/components/govuk-form-group-autocomplete/govuk-form-group-autocomplete.component.ts
@@ -1,5 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component, EventEmitter, Input, Output, forwardRef, inject } from '@angular/core';
+import {
+	AfterViewInit,
+	ChangeDetectorRef,
+	Component,
+	EventEmitter,
+	Input,
+	Output,
+	forwardRef,
+	inject,
+} from '@angular/core';
 import {
 	ControlContainer,
 	ControlValueAccessor,
@@ -8,9 +17,10 @@ import {
 	ReactiveFormsModule,
 } from '@angular/forms';
 import { CustomValidators } from '@forms/validators/custom-validators/custom-validators';
-import { MultiOption } from '@models/options.model';
 import { CustomTag, FormNodeWidth } from '@services/dynamic-forms/dynamic-form.types';
 import { SharedModule } from '@shared/shared.module';
+import { enhanceSelectElement } from 'accessible-autocomplete/dist/accessible-autocomplete.min';
+import { Observable, lastValueFrom, takeWhile } from 'rxjs';
 
 @Component({
 	selector: 'govuk-form-group-autocomplete',
@@ -26,7 +36,7 @@ import { SharedModule } from '@shared/shared.module';
 		},
 	],
 })
-export class GovukFormGroupAutocompleteComponent implements ControlValueAccessor {
+export class GovukFormGroupAutocompleteComponent implements ControlValueAccessor, AfterViewInit {
 	@Output() blur = new EventEmitter<FocusEvent>();
 	@Output() focus = new EventEmitter<FocusEvent>();
 
@@ -38,9 +48,6 @@ export class GovukFormGroupAutocompleteComponent implements ControlValueAccessor
 
 	@Input()
 	tags: CustomTag[] = [];
-
-	@Input({ required: true })
-	options!: MultiOption[];
 
 	@Input({ alias: 'hint' })
 	controlHint = '';
@@ -62,8 +69,41 @@ export class GovukFormGroupAutocompleteComponent implements ControlValueAccessor
 
 	@Input() noBottomMargin = false;
 
+	@Input() options$!: Observable<any[]>;
+
 	controlContainer = inject(ControlContainer);
 	cdr = inject(ChangeDetectorRef);
+	document = inject(Document);
+	DROP_DOWN_ARROW =
+		'<svg ' +
+		'class="autocomplete__dropdown-arrow-down" style="height: 17px;" viewBox="0 0 512 512"  ><path d="M256,298.3L256,298.3L256,298.3l174.2-167.2c4.3-4.2,11.4-4.1,15.8,0.2l30.6,29.9c4.4,4.3,4.5,11.3,0.2,15.5L264.1,380.9  c-2.2,2.2-5.2,3.2-8.1,3c-3,0.1-5.9-0.9-8.1-3L35.2,176.7c-4.3-4.2-4.2-11.2,0.2-15.5L66,131.3c4.4-4.3,11.5-4.4,15.8-0.2L256,298.3  z"/></svg>';
+	options: any[] = [];
+
+	ngAfterViewInit(): void {
+		// eslint-disable-next-line @typescript-eslint/no-this-alias
+		const self = this;
+
+		lastValueFrom(this.options$.pipe(takeWhile((options) => !options || options.length === 0, true)))
+			.then((options) => {
+				this.options = options;
+				this.cdr.detectChanges();
+
+				enhanceSelectElement({
+					selectElement: this.document.querySelector(`#${this.id}`),
+					autoselect: false,
+					defaultValue: '',
+					showAllValues: true,
+					confirmOnBlur: false,
+					dropdownArrow: () => this.DROP_DOWN_ARROW,
+					onConfirm(selected) {
+						self.handleChangeForOption(selected);
+					},
+				});
+
+				window.document.querySelector(`#${this.id}`)?.addEventListener('change', (event) => this.handleChange(event));
+			})
+			.catch(() => {});
+	}
 
 	get control() {
 		return this.controlContainer.control?.get(this.controlName);
