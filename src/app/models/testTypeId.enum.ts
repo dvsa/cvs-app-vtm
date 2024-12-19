e9fb4f4b926f3d4021a2a570445d9f6998051572
Brandon Thomas-Davies <87308252+BrandonT95@users.noreply.github.com>
2024-12-19 11:37:38 +0000

    feat(cb2-15580): re-add cvs common package for vtg/vtp12 test type ids

--- src/app/models/testTypeId.enum.ts
+++ src/app/models/testTypeId.enum.ts
@@ -1,3 +1,4 @@
+import { VTG_VTP_12_TEST } from '@dvsa/cvs-microservice-common/classes/testTypes/Constants';
 // Copied from the backend-lambda https://github.com/dvsa/cvs-svc-test-results/blob/3c1ffe344282b5ce918c151a56144b51cbd1c929/src/assets/Enums.ts#L219
 
 // CVSB-10300 - the following constants are based on the grouping of the test-types in the excel
@@ -225,14 +226,7 @@ export const TEST_TYPES_GROUP4_DESK_BASED_TEST: string[] = [
 export const TEST_TYPES_GROUP5_DESK_BASED_TEST: string[] = ['439', '441', '442', '449'];
 
 // Test Type IDs that generate VTP12/VTG12 documentation on test abandonment
-// refactor and use @dvsa/cvs-microservice-common once FE and BE test type ids are aligned
-export const TEST_TYPES_VTP_VTG_12 = [
-	...TEST_TYPES_GROUP1,
-	...TEST_TYPES_GROUP2,
-	...TEST_TYPES_GROUP9_10,
-	...TEST_TYPES_GROUP9_10_CENTRAL_DOCS,
-	...TEST_TYPES_GROUP12_14,
-];
+export const TEST_TYPES_VTP_VTG_12 = [...VTG_VTP_12_TEST.IDS];
 
 export const TEST_TYPES = {
 	testTypesGroup1: TEST_TYPES_GROUP1,
