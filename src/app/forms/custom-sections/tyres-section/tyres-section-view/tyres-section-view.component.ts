e7cf3376b18ef117c31a5ad8c1be39afb3e4ee64
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2025-01-16 10:15:23 +0000

    feat(CB2-15892): remove accompanying method

--- src/app/forms/custom-sections/tyres-section/tyres-section-view/tyres-section-view.component.ts
+++ src/app/forms/custom-sections/tyres-section/tyres-section-view/tyres-section-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { ReferenceDataResourceType, ReferenceDataTyreLoadIndex } from '@models/reference-data.model';
-import { Axle, VehicleTypes } from '@models/vehicle-tech-record.model';
+import { VehicleTypes } from '@models/vehicle-tech-record.model';
 import { Store } from '@ngrx/store';
 import { ReferenceDataService } from '@services/reference-data/reference-data.service';
 import { TechnicalRecordService } from '@services/technical-record/technical-record.service';
@@ -25,7 +25,6 @@ export class TyresSectionViewComponent implements OnDestroy, OnInit {
 
 	ngOnInit(): void {
 		this.loadReferenceData();
-		this.checkAxleWeights();
 	}
 
 	ngOnDestroy(): void {
@@ -42,28 +41,5 @@ export class TyresSectionViewComponent implements OnDestroy, OnInit {
 			.subscribe((tyreLoadIndex) => {
 				this.tyreLoadIndexReferenceData = tyreLoadIndex as ReferenceDataTyreLoadIndex[];
 			});
-	}
-
-	checkAxleWeights() {
-		this.invalidAxles = [];
-		const techRecord = this.techRecord();
-		if (
-			techRecord?.techRecord_vehicleType === VehicleTypes.TRL ||
-			techRecord?.techRecord_vehicleType === VehicleTypes.HGV ||
-			techRecord?.techRecord_vehicleType === VehicleTypes.PSV
-		) {
-			techRecord?.techRecord_axles?.forEach((axle: Axle) => {
-				if (axle.tyres_dataTrAxles && axle.weights_gbWeight && axle.axleNumber) {
-					const weightValue = this.technicalRecordService.getAxleFittingWeightValueFromLoadIndex(
-						axle.tyres_dataTrAxles?.toString(),
-						axle.tyres_fitmentCode,
-						this.tyreLoadIndexReferenceData
-					);
-					if (weightValue && axle.weights_gbWeight > weightValue) {
-						this.invalidAxles.push(axle.axleNumber);
-					}
-				}
-			});
-		}
 	}
 }
