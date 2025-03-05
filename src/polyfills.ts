d6565d23787b55ed9a08c81c7420f1f14750e6dc
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2025-03-05 10:52:11 +0000

    fix(CB2-15793): fix for local

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -56,4 +56,5 @@ import 'zone.js'; // Included with Angular CLI.
 	env: { DEBUG: undefined },
 	version: '',
 };
-(window as any).global.Buffer = (window as any).global.Buffer || require('node:buffer').Buffer;
+// biome-ignore lint/style/useNodejsImportProtocol: Not adding node protocol due to Jasmine error
+global.Buffer = global.Buffer || require('buffer').Buffer;
