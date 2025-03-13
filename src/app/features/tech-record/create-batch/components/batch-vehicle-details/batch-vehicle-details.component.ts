507ef775c9313b073a74c82db5a69122ca32f0d2
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2025-03-13 10:29:10 +0000

    feat(CB2-15792): functionally working

--- src/app/features/tech-record/create-batch/components/batch-vehicle-details/batch-vehicle-details.component.ts
+++ src/app/features/tech-record/create-batch/components/batch-vehicle-details/batch-vehicle-details.component.ts
@@ -15,7 +15,7 @@ import {
 	FormNodeWidth,
 } from '@services/dynamic-forms/dynamic-form.types';
 import { TechnicalRecordService } from '@services/technical-record/technical-record.service';
-import { Observable, Subject, combineLatest, filter, take } from 'rxjs';
+import { Observable, Subject, combineLatest, filter, firstValueFrom, take } from 'rxjs';
 
 @Component({
 	selector: 'app-batch-vehicle-details',
@@ -86,10 +86,10 @@ export class BatchVehicleDetailsComponent implements OnInit, OnDestroy {
 		return FormNodeWidth;
 	}
 
-	get vehicleForm(): FormGroup {
+	vehicleForm(index: number): FormGroup {
 		return this.fb.group({
 			vin: new CustomFormControl(
-				{ name: 'vin', type: FormNodeTypes.CONTROL },
+				{ name: 'vin', type: FormNodeTypes.CONTROL, label: 'VIN', customId: `input-vin${index}` },
 				null,
 				[CustomValidators.alphanumeric(), Validators.minLength(3), Validators.maxLength(21)],
 				this.batchTechRecordService.validateForBatch()
@@ -123,7 +123,7 @@ export class BatchVehicleDetailsComponent implements OnInit, OnDestroy {
 
 	addVehicles(n: number): void {
 		for (let i = 0; i < n; i++) {
-			this.vehicles.push(this.vehicleForm);
+			this.vehicles.push(this.vehicleForm(i));
 		}
 	}
 
@@ -165,29 +165,20 @@ export class BatchVehicleDetailsComponent implements OnInit, OnDestroy {
 	}
 
 	async isFormValid(): Promise<boolean> {
-		console.log('test 1');
 		this.globalErrorService.clearErrors();
 		this.form.markAllAsTouched();
-		console.log('test 2');
 
 		const errors: GlobalError[] = [];
 
 		DynamicFormService.validate(this.form, errors, true);
 		if (errors?.length) {
-			console.log('test 3');
 			this.globalErrorService.setErrors(errors);
 		}
-		console.log('test 4');
 		if (this.cleanEmptyValues(this.vehicles.value).length === 0) {
-			console.log('test 5');
 			this.globalErrorService.addError({ error: 'At least 1 vehicle must be created or updated in a batch' });
 			return false;
 		}
-		if (this.form.status !== 'VALID') {
-			console.log(this.form.status);
-			console.log(this.form);
-			return false;
-		}
+		await firstValueFrom(this.formStatus);
 		const duplicates = this.checkDuplicateVins(this.vehicles.value);
 		if (duplicates.length > 0) {
 			duplicates.forEach((element) => {
@@ -196,7 +187,6 @@ export class BatchVehicleDetailsComponent implements OnInit, OnDestroy {
 					anchorLink: `input-vin${element.anchor.toString()}`,
 				});
 			});
-			console.log('test 6');
 			return false;
 		}
 		return this.form.valid;
