afa5aae5a8c4dc2c0a857c98f985673fe67f91b5
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2025-04-17 08:57:52 +0100

    feat(CB2-14469-DDA-view): view mode

--- src/app/forms/custom-sections/dda-section/dda-section-view/dda-section-view.component.ts
+++ src/app/forms/custom-sections/dda-section/dda-section-view/dda-section-view.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { VehicleTypes } from '@models/vehicle-tech-record.model';
+import { Store } from '@ngrx/store';
+import { DefaultNullOrEmpty } from '@pipes/default-null-or-empty/default-null-or-empty.pipe';
+import { TechnicalRecordService } from '@services/technical-record/technical-record.service';
+import { techRecord } from '@store/technical-records';
+
 @Component({
 	selector: 'app-dda-section-view',
 	templateUrl: './dda-section-view.component.html',
 	styleUrls: ['./dda-section-view.component.scss'],
-	imports: [FormsModule, ReactiveFormsModule],
+	imports: [FormsModule, ReactiveFormsModule, DefaultNullOrEmpty],
 })
-export class DDASectionViewComponent implements OnInit, OnDestroy {
-	ngOnDestroy(): void {}
-
-	ngOnInit(): void {}
+export class DDASectionViewComponent {
+	protected readonly VehicleTypes = VehicleTypes;
+	store = inject(Store);
+	technicalRecordService = inject(TechnicalRecordService);
+	techRecord = this.store.selectSignal(techRecord);
 }
