309d87f8bf0855ee53708473e8ab540013b65970
pbardy2000 <146740183+pbardy2000@users.noreply.github.com>
2025-04-29 09:19:48 +0100

    feat(CB2-16357): use workaround to prevent emojis being pesisted

--- src/app/directives/no-emojis/no-emojis.directive.ts
+++ src/app/directives/no-emojis/no-emojis.directive.ts
@@ -1,18 +1,32 @@
-import { Directive, ElementRef, HostListener, inject } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, inject } from '@angular/core';
+import { NgControl } from '@angular/forms';
 import emojiRegex from 'emoji-regex';
 
 @Directive({
 	selector: '[appNoEmojis]',
 })
-export class NoEmojisDirective {
+export class NoEmojisDirective implements OnInit {
 	private readonly el = inject(ElementRef);
+	private readonly ngControl = inject(NgControl);
 	private readonly emojiRegex = emojiRegex();
 
-	@HostListener('input', ['$event']) onInputChange(event: Event) {
-		const input = this.el.nativeElement.value;
-		this.el.nativeElement.value = input.replace(this.emojiRegex, '');
-		if (input !== this.el.nativeElement.value) {
-			event.stopPropagation();
-		}
+	ngOnInit() {
+		// Prevents emojis being kept in ngModel see: https://angular.love/angular-forms-why-is-ngmodelchange-late-when-updating-ngmodel-value
+		const initialOnChange = (this.ngControl.valueAccessor as any).onChange;
+		(this.ngControl.valueAccessor as any).onChange = (value: string) =>
+			initialOnChange(value.replace(this.emojiRegex, ''));
+	}
+
+	@HostListener('input', ['$event'])
+	onInput(event: Event) {
+		const input = this.el.nativeElement;
+		if (typeof input.value !== 'string') return;
+
+		const value = input.value.replace(this.emojiRegex, '');
+		this.el.nativeElement.value = value;
+
+		event?.preventDefault();
+		event?.stopPropagation();
+		event?.stopImmediatePropagation();
 	}
 }
