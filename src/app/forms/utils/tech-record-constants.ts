0e00ef8349c82826344830c20f6e876592b5c31f
Brandon Thomas-Davies <87308252+BrandonT95@users.noreply.github.com>
2025-04-29 11:47:24 +0100

    feat(CB2-16977): commit refactor progress

--- src/app/forms/utils/tech-record-constants.ts
+++ src/app/forms/utils/tech-record-constants.ts
@@ -1,4 +1,5 @@
-import { CarTechRecord } from '@forms/templates/car/car-tech-record.template';
+import { AdrCertificateTemplate } from '@forms/templates/general/adr-certificate.template';
+import { AdrTemplate } from '@forms/templates/general/adr.template';
 import { ApplicantDetails } from '@forms/templates/general/applicant-details.template';
 import { HgvAndTrlTypeApprovalTemplate } from '@forms/templates/general/approval-type.template';
 import { Audit } from '@forms/templates/general/audit.template';
@@ -6,41 +7,33 @@ import { DocumentsTemplate } from '@forms/templates/general/documents.template';
 import { HgvAndTrlBodyTemplate } from '@forms/templates/general/hgv-trl-body.template';
 import { NotesTemplate } from '@forms/templates/general/notes.template';
 import { PlatesTemplate } from '@forms/templates/general/plates.template';
+import { TechRecordReasonForCreationSection } from '@forms/templates/general/reason-for-creation.template';
 import { HgvDimensionsTemplate } from '@forms/templates/hgv/hgv-dimensions.template';
-import { HgvTechRecord } from '@forms/templates/hgv/hgv-tech-record.template';
 import { tyresTemplateHgv } from '@forms/templates/hgv/hgv-tyres.template';
 import { HgvWeight } from '@forms/templates/hgv/hgv-weight.template';
-import { LgvTechRecord } from '@forms/templates/lgv/lgv-tech-record.template';
-import { MotorcycleTechRecord } from '@forms/templates/motorcycle/motorcycle-tech-record.template';
 import { PsvTypeApprovalTemplate } from '@forms/templates/psv/psv-approval-type.template';
 import { PsvBodyTemplate } from '@forms/templates/psv/psv-body.template';
 import { PsvBrakesTemplate } from '@forms/templates/psv/psv-brakes.template';
 import { PsvDdaTemplate } from '@forms/templates/psv/psv-dda.template';
 import { PsvDimensionsTemplate } from '@forms/templates/psv/psv-dimensions.template';
 import { PsvNotes } from '@forms/templates/psv/psv-notes.template';
-import { PsvTechRecord } from '@forms/templates/psv/psv-tech-record.template';
 import { PsvTyresTemplate } from '@forms/templates/psv/psv-tyres.template';
 import { PsvWeightsTemplate } from '@forms/templates/psv/psv-weight.template';
-import { SmallTrailerTechRecord } from '@forms/templates/small-trailer/small-trailer-tech-record.template';
-import { FormNode } from '@services/dynamic-forms/dynamic-form.types';
-
-import { AdrCertificateTemplate } from '@forms/templates/general/adr-certificate.template';
-import { AdrTemplate } from '@forms/templates/general/adr.template';
-import { TechRecordReasonForCreationSection } from '@forms/templates/general/reason-for-creation.template';
 import { VehicleTypes } from '@models/vehicle-tech-record.model';
+import { FormNode } from '@services/dynamic-forms/dynamic-form.types';
 import { LettersTemplate } from '../templates/general/letters.template';
 import { ManufacturerTemplate } from '../templates/general/manufacturer.template';
 import { TrlAuthIntoServiceTemplate } from '../templates/trl/trl-auth-into-service.template';
 import { TrlBrakesTemplate } from '../templates/trl/trl-brakes.template';
 import { TrlDimensionsTemplate } from '../templates/trl/trl-dimensions.template';
 import { TrlPurchasers } from '../templates/trl/trl-purchaser.template';
-import { TrlTechRecordTemplate } from '../templates/trl/trl-tech-record.template';
 import { tyresTemplateTrl } from '../templates/trl/trl-tyres.template';
 import { TrlWeight } from '../templates/trl/trl-weight.template';
 
 // The map below initializes the array of sections that the *ngFor in tech summary component's template will iterate over.
 // The order in which each section is introduced in the array will determine its order on the page when rendered.
 // Sections which use custom components require a FormNode object with 'name' and 'label' properties.
+const techRecordSection = { name: 'techRecordSummary', label: 'Vehicle summary' } as FormNode;
 
 export const vehicleTemplateMap = new Map<VehicleTypes, Array<FormNode>>([
 	[
@@ -48,7 +41,7 @@ export const vehicleTemplateMap = new Map<VehicleTypes, Array<FormNode>>([
 		[
 			/*  1 */ TechRecordReasonForCreationSection,
 			/*  2 */ PsvNotes,
-			/*  3 */ PsvTechRecord,
+			/*  3 */ techRecordSection,
 			/*  4 */ PsvTypeApprovalTemplate,
 			/*  5 */ PsvBrakesTemplate,
 			/*  6 */ PsvDdaTemplate,
@@ -64,7 +57,7 @@ export const vehicleTemplateMap = new Map<VehicleTypes, Array<FormNode>>([
 		[
 			/*  1 */ TechRecordReasonForCreationSection,
 			/*  2 */ NotesTemplate,
-			/*  3 */ HgvTechRecord,
+			/*  3 */ techRecordSection,
 			/*  4 */ HgvAndTrlTypeApprovalTemplate,
 			/*  5 */ ApplicantDetails,
 			/*  6 */ DocumentsTemplate,
@@ -82,7 +75,7 @@ export const vehicleTemplateMap = new Map<VehicleTypes, Array<FormNode>>([
 		[
 			/*  1 */ TechRecordReasonForCreationSection,
 			/*  2 */ NotesTemplate,
-			/*  3 */ TrlTechRecordTemplate,
+			/*  3 */ techRecordSection,
 			/*  4 */ HgvAndTrlTypeApprovalTemplate,
 			/*  5 */ ApplicantDetails,
 			/*  6 */ DocumentsTemplate,
@@ -104,7 +97,7 @@ export const vehicleTemplateMap = new Map<VehicleTypes, Array<FormNode>>([
 		VehicleTypes.SMALL_TRL,
 		[
 			TechRecordReasonForCreationSection,
-			/* 2 */ SmallTrailerTechRecord,
+			/* 2 */ techRecordSection,
 			/* 3 */ ApplicantDetails,
 			/* 4 */ NotesTemplate,
 			/* 5 */ Audit,
@@ -114,7 +107,7 @@ export const vehicleTemplateMap = new Map<VehicleTypes, Array<FormNode>>([
 		VehicleTypes.LGV,
 		[
 			/* 1 */ TechRecordReasonForCreationSection,
-			/* 2 */ LgvTechRecord,
+			/* 2 */ techRecordSection,
 			/* 3 */ ApplicantDetails,
 			/* 4 */ NotesTemplate,
 			/* 5 */ Audit,
@@ -126,7 +119,7 @@ export const vehicleTemplateMap = new Map<VehicleTypes, Array<FormNode>>([
 		VehicleTypes.CAR,
 		[
 			TechRecordReasonForCreationSection,
-			/* 2 */ CarTechRecord,
+			/* 2 */ techRecordSection,
 			/* 3 */ ApplicantDetails,
 			/* 4 */ NotesTemplate,
 			/* 5 */ Audit,
@@ -136,7 +129,7 @@ export const vehicleTemplateMap = new Map<VehicleTypes, Array<FormNode>>([
 		VehicleTypes.MOTORCYCLE,
 		[
 			TechRecordReasonForCreationSection,
-			/* 2 */ MotorcycleTechRecord,
+			/* 2 */ techRecordSection,
 			/* 3 */ ApplicantDetails,
 			/* 4 */ NotesTemplate,
 			/* 5 */ Audit,
