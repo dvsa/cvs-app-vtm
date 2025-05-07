cd257aa7e8b7f71797c5d7a34b1245033e218dd5
pbardy2000 <146740183+pbardy2000@users.noreply.github.com>
2025-05-07 14:23:58 +0100

    feat(CB2-17487): keep cert box visible on specialist group 1 tests for HGV and TRLs

---
+++ src/app/forms/templates/test-records/section-templates/test/contingency/contingency-test-section-specialist-group1HgvAndTrl.ts
@@ -0,0 +1,233 @@
+import { AsyncValidatorNames } from '@models/async-validators.enum';
+import { TEST_TYPES_GROUP1_SPEC_TEST } from '@models/testTypeId.enum';
+import { ValidatorNames } from '@models/validators.enum';
+import {
+	FormNode,
+	FormNodeEditTypes,
+	FormNodeTypes,
+	FormNodeViewTypes,
+	FormNodeWidth,
+} from '@services/dynamic-forms/dynamic-form.types';
+
+export const ContingencyTestSectionSpecialistGroup1HgvAndTrl: FormNode = {
+	name: 'testSection',
+	label: 'Test',
+	type: FormNodeTypes.GROUP,
+	children: [
+		{
+			name: 'contingencyTestNumber',
+			label: 'Contingency Test Number',
+			type: FormNodeTypes.CONTROL,
+			editType: FormNodeEditTypes.NUMERICSTRING,
+			validators: [
+				{ name: ValidatorNames.MaxLength, args: 8 },
+				{ name: ValidatorNames.MinLength, args: 6 },
+				{ name: ValidatorNames.Required },
+			],
+			width: FormNodeWidth.L,
+		},
+		{
+			name: 'testStartTimestamp',
+			label: 'Test start date',
+			type: FormNodeTypes.CONTROL,
+			viewType: FormNodeViewTypes.HIDDEN,
+			editType: FormNodeEditTypes.HIDDEN,
+			validators: [{ name: ValidatorNames.PastDate }],
+		},
+		{
+			name: 'testEndTimestamp',
+			type: FormNodeTypes.CONTROL,
+			label: 'Test end date',
+			viewType: FormNodeViewTypes.HIDDEN,
+			editType: FormNodeEditTypes.HIDDEN,
+			validators: [{ name: ValidatorNames.AheadOfDate, args: 'testStartTimestamp' }],
+		},
+		{
+			name: 'recalls',
+			type: FormNodeTypes.GROUP,
+			children: [
+				{
+					name: 'hasRecall',
+					value: false,
+					type: FormNodeTypes.CONTROL,
+					viewType: FormNodeViewTypes.HIDDEN,
+					editType: FormNodeEditTypes.HIDDEN,
+				},
+				{
+					name: 'manufacturer',
+					value: null,
+					type: FormNodeTypes.CONTROL,
+					viewType: FormNodeViewTypes.HIDDEN,
+					editType: FormNodeEditTypes.HIDDEN,
+				},
+			],
+		},
+		{
+			name: 'testTypes',
+			label: 'Test Types',
+			type: FormNodeTypes.ARRAY,
+			children: [
+				{
+					name: '0', // it is important here that the name of the node for an ARRAY type should be an index value
+					type: FormNodeTypes.GROUP,
+					children: [
+						{
+							name: 'testResult',
+							label: 'Result',
+							viewType: FormNodeViewTypes.HIDDEN,
+							editType: FormNodeEditTypes.RADIO,
+							options: [
+								{ value: 'pass', label: 'Pass' },
+								{ value: 'fail', label: 'Fail' },
+								{ value: 'prs', label: 'PRS' },
+							],
+							validators: [
+								{ name: ValidatorNames.HideIfNotEqual, args: { sibling: 'reapplicationDate', value: 'fail' } },
+								{ name: ValidatorNames.HideIfNotEqual, args: { sibling: 'centralDocs', value: ['pass', 'prs'] } },
+							],
+							asyncValidators: [
+								{ name: AsyncValidatorNames.ResultDependantOnRequiredStandards },
+								{
+									name: AsyncValidatorNames.HideIfEqualsWithCondition,
+									args: {
+										sibling: 'certificateNumber',
+										value: 'fail',
+										conditions: { field: 'testTypeId', operator: 'equals', value: TEST_TYPES_GROUP1_SPEC_TEST },
+									},
+								},
+							],
+							type: FormNodeTypes.CONTROL,
+						},
+						{
+							name: 'centralDocs',
+							type: FormNodeTypes.GROUP,
+							children: [
+								{
+									name: 'issueRequired',
+									type: FormNodeTypes.CONTROL,
+									label: 'Issue documents centrally',
+									editType: FormNodeEditTypes.RADIO,
+									value: false,
+									options: [
+										{ value: true, label: 'Yes' },
+										{ value: false, label: 'No' },
+									],
+								},
+								{
+									name: 'reasonsForIssue',
+									type: FormNodeTypes.CONTROL,
+									viewType: FormNodeViewTypes.HIDDEN,
+									editType: FormNodeEditTypes.HIDDEN,
+									value: [],
+								},
+							],
+						},
+						{
+							name: 'testTypeName',
+							label: 'Description',
+							value: '',
+							disabled: true,
+
+							type: FormNodeTypes.CONTROL,
+						},
+						{
+							name: 'reasonForAbandoning',
+							type: FormNodeTypes.CONTROL,
+							viewType: FormNodeViewTypes.HIDDEN,
+							editType: FormNodeEditTypes.HIDDEN,
+							value: null,
+							required: true,
+						},
+						{
+							name: 'additionalCommentsForAbandon',
+							type: FormNodeTypes.CONTROL,
+							viewType: FormNodeViewTypes.HIDDEN,
+							editType: FormNodeEditTypes.HIDDEN,
+							value: null,
+							required: true,
+						},
+						{
+							name: 'certificateNumber',
+							label: 'Certificate number',
+							type: FormNodeTypes.CONTROL,
+							viewType: FormNodeViewTypes.STRING,
+							editType: FormNodeEditTypes.TEXT,
+							uppercase: true,
+							validators: [
+								{ name: ValidatorNames.Alphanumeric },
+								{
+									name: ValidatorNames.RequiredIfEquals,
+									args: {
+										sibling: 'testResult',
+										value: ['pass', 'prs'],
+									},
+								},
+							],
+							required: true,
+							value: null,
+						},
+						{
+							name: 'testTypeStartTimestamp',
+							type: FormNodeTypes.CONTROL,
+							value: '',
+							label: 'Test start date and time',
+							viewType: FormNodeViewTypes.DATETIME,
+							editType: FormNodeEditTypes.DATETIME,
+							validators: [
+								{ name: ValidatorNames.Required },
+								{ name: ValidatorNames.PastDate },
+								{ name: ValidatorNames.CopyValueToRootControl, args: 'testStartTimestamp' },
+							],
+						},
+						{
+							name: 'testTypeEndTimestamp',
+							type: FormNodeTypes.CONTROL,
+							value: '',
+							label: 'Test end date and time',
+							viewType: FormNodeViewTypes.DATETIME,
+							editType: FormNodeEditTypes.DATETIME,
+							validators: [
+								{ name: ValidatorNames.Required },
+								{ name: ValidatorNames.PastDate },
+								{ name: ValidatorNames.AheadOfDate, args: 'testTypeStartTimestamp' },
+								{ name: ValidatorNames.CopyValueToRootControl, args: 'testEndTimestamp' },
+							],
+						},
+						{
+							name: 'prohibitionIssued',
+							type: FormNodeTypes.CONTROL,
+							label: 'Prohibition issued',
+							value: null,
+							editType: FormNodeEditTypes.RADIO,
+							options: [
+								{ value: true, label: 'Yes' },
+								{ value: false, label: 'No' },
+							],
+							validators: [{ name: ValidatorNames.Required }],
+						},
+						{
+							name: 'reapplicationDate',
+							label: 'Reapplication date',
+							hint: 'For example, 27 3 2007',
+							editType: FormNodeEditTypes.DATE,
+							viewType: FormNodeViewTypes.DATE,
+							type: FormNodeTypes.CONTROL,
+							groups: ['failOnly'],
+							validators: [
+								{
+									name: ValidatorNames.RequiredIfEquals,
+									args: {
+										sibling: 'testResult',
+										value: ['fail'],
+										customErrorMessage: 'Reapplication date is required',
+									},
+								},
+								{ name: ValidatorNames.FutureDate },
+							],
+						},
+					],
+				},
+			],
+		},
+	],
+};
---
+++ src/app/forms/templates/test-records/section-templates/test/specialist/specialist-test-section-group1HgvAndTrl.ts
@@ -0,0 +1,260 @@
+import { AsyncValidatorNames } from '@models/async-validators.enum';
+import { ReferenceDataResourceType } from '@models/reference-data.model';
+import { TEST_TYPES_GROUP1_SPEC_TEST } from '@models/testTypeId.enum';
+import { ValidatorNames } from '@models/validators.enum';
+import {
+	FormNode,
+	FormNodeEditTypes,
+	FormNodeTypes,
+	FormNodeViewTypes,
+	FormNodeWidth,
+} from '@services/dynamic-forms/dynamic-form.types';
+
+export const SpecialistTestSectionGroup1HgvAndTrl: FormNode = {
+	name: 'testSection',
+	label: 'Test',
+	type: FormNodeTypes.GROUP,
+	children: [
+		{
+			name: 'createdAt',
+			label: 'Created',
+			disabled: true,
+			type: FormNodeTypes.CONTROL,
+			viewType: FormNodeViewTypes.DATE,
+			editType: FormNodeEditTypes.HIDDEN,
+		},
+		{
+			name: 'testStartTimestamp',
+			label: 'Test Date',
+			value: '',
+			disabled: true,
+			type: FormNodeTypes.CONTROL,
+			viewType: FormNodeViewTypes.DATE,
+			editType: FormNodeEditTypes.HIDDEN,
+		},
+		{
+			name: 'recalls',
+			type: FormNodeTypes.GROUP,
+			children: [
+				{
+					name: 'hasRecall',
+					value: false,
+					type: FormNodeTypes.CONTROL,
+					viewType: FormNodeViewTypes.HIDDEN,
+					editType: FormNodeEditTypes.HIDDEN,
+				},
+				{
+					name: 'manufacturer',
+					value: null,
+					type: FormNodeTypes.CONTROL,
+					viewType: FormNodeViewTypes.HIDDEN,
+					editType: FormNodeEditTypes.HIDDEN,
+				},
+			],
+		},
+		{
+			name: 'testTypes',
+			label: 'Test Types',
+			type: FormNodeTypes.ARRAY,
+			children: [
+				{
+					name: '0', // it is important here that the name of the node for an ARRAY type should be an index value
+					type: FormNodeTypes.GROUP,
+					children: [
+						{
+							name: 'testCode',
+							label: 'Test Code',
+							value: '',
+							disabled: true,
+							type: FormNodeTypes.CONTROL,
+							width: FormNodeWidth.XS,
+							editType: FormNodeEditTypes.HIDDEN,
+						},
+						{
+							name: 'testResult',
+							label: 'Result',
+							viewType: FormNodeViewTypes.HIDDEN,
+							editType: FormNodeEditTypes.RADIO,
+							options: [
+								{ value: 'pass', label: 'Pass' },
+								{ value: 'fail', label: 'Fail' },
+								{ value: 'prs', label: 'PRS' },
+								{ value: 'abandoned', label: 'Abandoned' },
+							],
+							validators: [
+								{ name: ValidatorNames.HideIfNotEqual, args: { sibling: 'reasonForAbandoning', value: 'abandoned' } },
+								{
+									name: ValidatorNames.HideIfNotEqual,
+									args: { sibling: 'additionalCommentsForAbandon', value: 'abandoned' },
+								},
+								{ name: ValidatorNames.HideIfNotEqual, args: { sibling: 'centralDocs', value: ['pass', 'prs'] } },
+								{ name: ValidatorNames.HideIfNotEqual, args: { sibling: 'reapplicationDate', value: 'fail' } },
+							],
+							asyncValidators: [
+								{ name: AsyncValidatorNames.ResultDependantOnRequiredStandards },
+								{
+									name: AsyncValidatorNames.HideIfEqualsWithCondition,
+									args: {
+										sibling: 'certificateNumber',
+										value: 'fail',
+										conditions: { field: 'testTypeId', operator: 'equals', value: TEST_TYPES_GROUP1_SPEC_TEST },
+									},
+								},
+							],
+							type: FormNodeTypes.CONTROL,
+						},
+						{
+							name: 'centralDocs',
+							type: FormNodeTypes.GROUP,
+							children: [
+								{
+									name: 'issueRequired',
+									type: FormNodeTypes.CONTROL,
+									label: 'Issue documents centrally',
+									editType: FormNodeEditTypes.RADIO,
+									value: false,
+									options: [
+										{ value: true, label: 'Yes' },
+										{ value: false, label: 'No' },
+									],
+								},
+								{
+									name: 'reasonsForIssue',
+									type: FormNodeTypes.CONTROL,
+									viewType: FormNodeViewTypes.HIDDEN,
+									editType: FormNodeEditTypes.HIDDEN,
+									value: [],
+								},
+							],
+						},
+						{
+							name: 'reasonForAbandoning',
+							type: FormNodeTypes.CONTROL,
+							label: 'Reason for abandoning',
+							editType: FormNodeEditTypes.CHECKBOXGROUP,
+							delimited: { regex: '\\. (?<!\\..\\. )', separator: '. ' },
+							required: true,
+							validators: [
+								{
+									name: ValidatorNames.RequiredIfEquals,
+									args: { sibling: 'testResult', value: ['abandoned'] },
+								},
+							],
+							referenceData: ReferenceDataResourceType.SpecialistReasonsForAbandoning,
+						},
+						{
+							name: 'additionalCommentsForAbandon',
+							type: FormNodeTypes.CONTROL,
+							value: '',
+							required: true,
+							label: 'Additional details for abandoning',
+							editType: FormNodeEditTypes.TEXTAREA,
+							validators: [
+								{
+									name: ValidatorNames.RequiredIfEquals,
+									args: { sibling: 'testResult', value: ['abandoned'] },
+								},
+								{ name: ValidatorNames.MaxLength, args: 500 },
+							],
+						},
+						{
+							name: 'testTypeName',
+							label: 'Description',
+							value: '',
+							disabled: true,
+							type: FormNodeTypes.CONTROL,
+						},
+						{
+							name: 'certificateNumber',
+							label: 'Certificate number',
+							type: FormNodeTypes.CONTROL,
+							editType: FormNodeEditTypes.TEXT,
+							uppercase: true,
+							validators: [
+								{ name: ValidatorNames.Alphanumeric },
+								{
+									name: ValidatorNames.RequiredIfEquals,
+									args: {
+										sibling: 'testResult',
+										value: ['pass', 'prs'],
+									},
+								},
+							],
+							viewType: FormNodeViewTypes.HIDDEN,
+							width: FormNodeWidth.L,
+							required: true,
+							value: null,
+						},
+						{
+							name: 'testNumber',
+							label: 'Test Number',
+							value: '',
+							disabled: true,
+							type: FormNodeTypes.CONTROL,
+							editType: FormNodeEditTypes.HIDDEN,
+						},
+						{
+							name: 'testTypeStartTimestamp',
+							type: FormNodeTypes.CONTROL,
+							value: '',
+							label: 'Start time',
+							viewType: FormNodeViewTypes.TIME,
+							editType: FormNodeEditTypes.DATETIME,
+							validators: [
+								{ name: ValidatorNames.Required },
+								{ name: ValidatorNames.PastDate },
+								{ name: ValidatorNames.CopyValueToRootControl, args: 'testStartTimestamp' },
+							],
+						},
+						{
+							name: 'testTypeEndTimestamp',
+							type: FormNodeTypes.CONTROL,
+							value: '',
+							label: 'End time',
+							viewType: FormNodeViewTypes.TIME,
+							editType: FormNodeEditTypes.DATETIME,
+							validators: [
+								{ name: ValidatorNames.Required },
+								{ name: ValidatorNames.PastDate },
+								{ name: ValidatorNames.AheadOfDate, args: 'testTypeStartTimestamp' },
+								{ name: ValidatorNames.CopyValueToRootControl, args: 'testEndTimestamp' },
+							],
+						},
+						{
+							name: 'prohibitionIssued',
+							type: FormNodeTypes.CONTROL,
+							label: 'Prohibition issued',
+							value: null,
+							editType: FormNodeEditTypes.RADIO,
+							options: [
+								{ value: true, label: 'Yes' },
+								{ value: false, label: 'No' },
+							],
+							validators: [{ name: ValidatorNames.Required }],
+						},
+						{
+							name: 'reapplicationDate',
+							label: 'Reapplication date',
+							hint: 'For example, 27 3 2007',
+							editType: FormNodeEditTypes.DATE,
+							viewType: FormNodeViewTypes.DATE,
+							type: FormNodeTypes.CONTROL,
+							groups: ['failOnly'],
+							validators: [
+								{
+									name: ValidatorNames.RequiredIfEquals,
+									args: {
+										sibling: 'testResult',
+										value: ['fail'],
+										customErrorMessage: 'Reapplication date is required',
+									},
+								},
+								{ name: ValidatorNames.FutureDate },
+							],
+						},
+					],
+				},
+			],
+		},
+	],
+};
