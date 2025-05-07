e1e43ab816040122fc2448860836d6041e1439a8
Thomas Evans <36958694+tomevs88@users.noreply.github.com>
2025-05-07 08:58:24 +0100

    feat(CB2-14487): - lint fix

--- src/app/features/tech-record/components/tech-record-summary-changes/tech-record-summary-changes.component.ts
+++ src/app/features/tech-record/components/tech-record-summary-changes/tech-record-summary-changes.component.ts
@@ -15,8 +15,8 @@ import {
 } from '@dvsa/cvs-type-definitions/types/v3/tech-record/tech-record-verb-vehicle-type';
 import { BrakesSectionComponent } from '@forms/custom-sections/brakes-section/brakes-section.component';
 import { DDASectionComponent } from '@forms/custom-sections/dda-section/dda-section.component';
-import { LettersSectionComponent } from '@forms/custom-sections/letters-section/letters-section.component';
 import { DocumentsSectionComponent } from '@forms/custom-sections/documents-section/documents-section.component';
+import { LettersSectionComponent } from '@forms/custom-sections/letters-section/letters-section.component';
 import { vehicleTemplateMap } from '@forms/utils/tech-record-constants';
 import { Axles, VehicleTypes } from '@models/vehicle-tech-record.model';
 import { Actions, ofType } from '@ngrx/effects';
--- src/app/features/tech-record/components/tech-record-summary/tech-record-summary.component.ts
+++ src/app/features/tech-record/components/tech-record-summary/tech-record-summary.component.ts
@@ -25,8 +25,8 @@ import { BodyComponent } from '@forms/custom-sections/body/body.component';
 import { BrakesSectionComponent } from '@forms/custom-sections/brakes-section/brakes-section.component';
 import { DDASectionComponent } from '@forms/custom-sections/dda-section/dda-section.component';
 import { DimensionsComponent } from '@forms/custom-sections/dimensions/dimensions.component';
-import { LettersSectionComponent } from '@forms/custom-sections/letters-section/letters-section.component';
 import { DocumentsSectionComponent } from '@forms/custom-sections/documents-section/documents-section.component';
+import { LettersSectionComponent } from '@forms/custom-sections/letters-section/letters-section.component';
 import { LettersComponent } from '@forms/custom-sections/letters/letters.component';
 import { PsvBrakesComponent } from '@forms/custom-sections/psv-brakes/psv-brakes.component';
 import { TrlBrakesComponent } from '@forms/custom-sections/trl-brakes/trl-brakes.component';
@@ -107,8 +107,8 @@ import { WeightsComponent as WeightsComponent_1 } from '../../../../forms/custom
 		AsyncPipe,
 		BrakesSectionComponent,
 		DDASectionComponent,
-    LettersSectionComponent,
-    DocumentsSectionComponent,
+		LettersSectionComponent,
+		DocumentsSectionComponent,
 	],
 })
 export class TechRecordSummaryComponent implements OnInit, OnDestroy, AfterViewInit {
