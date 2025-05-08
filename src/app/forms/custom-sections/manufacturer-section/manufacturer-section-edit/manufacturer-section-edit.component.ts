4adf8b3a1e4a30f7a98634c6ef4467279d0971a4
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2025-05-08 14:09:06 +0100

    feat(CB2-14490): minor changes to accommodate regression pack

--- src/app/forms/custom-sections/manufacturer-section/manufacturer-section-edit/manufacturer-section-edit.component.ts
+++ src/app/forms/custom-sections/manufacturer-section/manufacturer-section-edit/manufacturer-section-edit.component.ts
@@ -65,7 +65,7 @@ export class ManufacturerSectionEditComponent implements OnInit, OnDestroy {
 	get defaultFields() {
 		return {
 			techRecord_manufacturerDetails_name: this.fb.control(null, [
-				this.commonValidators.maxLength(150, 'Name or company must be less than or equal to 150 characters'),
+				this.commonValidators.maxLength(150, 'Name must be less than or equal to 150 characters'),
 			]),
 			techRecord_manufacturerDetails_address1: this.fb.control(null, [
 				this.commonValidators.maxLength(60, 'Address line 1 must be less than or equal to 60 characters'),
