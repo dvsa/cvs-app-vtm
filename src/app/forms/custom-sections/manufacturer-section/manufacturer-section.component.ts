161ae90335a7dcec1af999b962cb02b912758796
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2025-05-08 11:32:34 +0100

    feat(CB2-14490): view mode

--- src/app/forms/custom-sections/manufacturer-section/manufacturer-section.component.ts
+++ src/app/forms/custom-sections/manufacturer-section/manufacturer-section.component.ts
@@ -1,6 +1,4 @@
 import { Component, input } from '@angular/core';
-import { LettersSectionEditComponent } from '@forms/custom-sections/letters-section/letters-section-edit/letters-section-edit.component';
-import { LettersSectionViewComponent } from '@forms/custom-sections/letters-section/letters-section-view/letters-section-view.component';
 import { ManufacturerSectionEditComponent } from '@forms/custom-sections/manufacturer-section/manufacturer-section-edit/manufacturer-section-edit.component';
 import { ManufacturerSectionSummaryComponent } from '@forms/custom-sections/manufacturer-section/manufacturer-section-summary/manufacturer-section-summary.component';
 import { ManufacturerSectionViewComponent } from '@forms/custom-sections/manufacturer-section/manufacturer-section-view/manufacturer-section-view.component';
@@ -10,13 +8,7 @@ import { V3TechRecordModel } from '@models/vehicle-tech-record.model';
 	selector: 'app-manufacturer-section',
 	templateUrl: './manufacturer-section.component.html',
 	styleUrls: ['./manufacturer-section.component.scss'],
-	imports: [
-		ManufacturerSectionEditComponent,
-		ManufacturerSectionSummaryComponent,
-		ManufacturerSectionViewComponent,
-		LettersSectionEditComponent,
-		LettersSectionViewComponent,
-	],
+	imports: [ManufacturerSectionEditComponent, ManufacturerSectionSummaryComponent, ManufacturerSectionViewComponent],
 })
 export class ManufacturerSectionComponent {
 	mode = input<Mode>('edit');
