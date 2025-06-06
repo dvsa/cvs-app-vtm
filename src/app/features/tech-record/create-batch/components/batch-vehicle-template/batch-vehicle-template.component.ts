7bac21cbdf9acaad2ba33a5abb228230245a8e43
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2025-06-06 08:40:07 +0100

    feat(CB2-17375): fix lint

--- src/app/features/tech-record/create-batch/components/batch-vehicle-template/batch-vehicle-template.component.ts
+++ src/app/features/tech-record/create-batch/components/batch-vehicle-template/batch-vehicle-template.component.ts
@@ -72,7 +72,12 @@ export class BatchVehicleTemplateComponent {
 			{ name: 'form-group', type: FormNodeTypes.GROUP },
 			{
 				vehicleStatus: new CustomFormControl(
-					{ name: 'change-vehicle-status-select', label: 'Vehicle status', type: FormNodeTypes.CONTROL, customErrorMessage: 'Vehicle status is required' },
+					{
+						name: 'change-vehicle-status-select',
+						label: 'Vehicle status',
+						type: FormNodeTypes.CONTROL,
+						customErrorMessage: 'Vehicle status is required',
+					},
 					'',
 					[Validators.required]
 				),
