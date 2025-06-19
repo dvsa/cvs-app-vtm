daa74152865d0295b9c1ce211dd99d6605548d55
pbardy2000 <146740183+pbardy2000@users.noreply.github.com>
2025-06-19 11:24:10 +0100

    feat(CB2-18047): fix lgv submission issue

--- src/app/features/tech-record/components/tech-record-summary/tech-record-summary.component.ts
+++ src/app/features/tech-record/components/tech-record-summary/tech-record-summary.component.ts
@@ -351,7 +351,7 @@ export class TechRecordSummaryComponent implements OnInit, OnDestroy, AfterViewI
 		this.technicalRecordService.updateEditingTechRecord(this.techRecordCalculated as TechRecordType<'put'>);
 	}
 
-	checkForms(): void {
+	checkForms(): boolean {
 		const forms: Array<CustomFormGroup | CustomFormArray | FormGroup> = this.sections()
 			?.map((section) => section.form)
 			.concat(this.customSectionForms);
@@ -360,7 +360,10 @@ export class TechRecordSummaryComponent implements OnInit, OnDestroy, AfterViewI
 
 		this.setErrors(forms);
 
-		this.isFormInvalid.emit(forms.some((form) => form.invalid || this.form.invalid));
+		const isInvalid = forms.some((form) => form.invalid) || this.form.invalid;
+		this.isFormInvalid.emit(isInvalid);
+
+		return isInvalid;
 	}
 
 	setErrors(forms: Array<CustomFormGroup | CustomFormArray | FormGroup>): void {
