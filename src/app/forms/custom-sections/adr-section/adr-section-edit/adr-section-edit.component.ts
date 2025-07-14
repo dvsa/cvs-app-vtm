c6f09c828a732210fca44dfb386bf2ca5c52b247
pbardy2000 <146740183+pbardy2000@users.noreply.github.com>
2025-07-14 13:57:06 +0100

    fix(CB2-17351): correct validation

--- src/app/forms/custom-sections/adr-section/adr-section-edit/adr-section-edit.component.ts
+++ src/app/forms/custom-sections/adr-section/adr-section-edit/adr-section-edit.component.ts
@@ -162,7 +162,7 @@ export class AdrSectionEditComponent extends EditBaseComponent implements OnInit
 			this.commonValidators.maxLength(1500, 'Additional Details must be less than or equal to 1500 characters'),
 		]),
 		techRecord_adrDetails_tank_tankDetails_specialProvisions: this.fb.control<string | null>(null, [
-			this.commonValidators.maxLength(1500, 'Special provisions must be less than or equal to 1024 characters'),
+			this.commonValidators.maxLength(1024, 'Special provisions must be less than or equal to 1024 characters'),
 		]),
 
 		// Tank Details > Tank Inspections
