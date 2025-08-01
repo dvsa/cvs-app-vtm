d4f79bf3c6ea43b59474916e9f14c32eab93e2f7
Thomas Evans <36958694+tomevs88@users.noreply.github.com>
2025-08-01 14:01:52 +0100

    feat(hotfix): remove intival refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -80,14 +80,12 @@ export class AppComponent implements OnInit, OnDestroy {
 		this.analyticsService.pushToDataLayer({ AppVersionDataLayer: packageInfo.version });
 		await this.analyticsService.setUserId();
 		initAll();
-		this.checkDateChange();
 	}
 
 	ngOnDestroy(): void {
 		this.destroy$.next();
 		this.destroy$.complete();
-		clearInterval(this.interval);
-	}
+  }
 
 	startSentry() {
 		Sentry.init({
@@ -101,19 +99,5 @@ export class AppComponent implements OnInit, OnDestroy {
 			integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
 		});
 		this.sentryInitialized = true;
-	}
-
-	checkDateChange() {
-		this.interval = setInterval(() => {
-			const newDate = new Date();
-			if (newDate.getDate() !== this.currentDate.getDate()) {
-				this.currentDate = newDate;
-				this.reinitializeApp();
-			}
-		}, 21600000); // Check every six hours
-	}
-
-	reinitializeApp() {
-		this.ngOnInit().then((r) => r);
 	}
 }
