238b3de585f6fdf17419ed39575c93a108f6b1eb
pbardy2000 <146740183+pbardy2000@users.noreply.github.com>
2025-08-26 17:45:31 +0100

    fix(CB2-19008): remove required standards feature flag

--- src/app/store/test-records/test-records.effects.ts
+++ src/app/store/test-records/test-records.effects.ts
@@ -193,16 +193,12 @@ export class TestResultsEffects {
 				}
 				const testTypeGroup = TestRecordsService.getTestTypeGroup(testTypeId);
 
-				// tech-debt: feature flag check to be removed when required standard is enabled
-				const isRequiredStandardsEnabled = this.featureToggleService.isFeatureEnabled('requiredstandards');
 				const isIVAorMSVATest =
 					testTypeGroup === 'testTypesSpecialistGroup1' || testTypeGroup === 'testTypesSpecialistGroup5';
 
 				const vehicleTpl = masterTpl[`${vehicleType}`];
 				const testTypeGroupString =
-					(!isRequiredStandardsEnabled || isOldIVAorMSVAtest) && isIVAorMSVATest
-						? `${testTypeGroup}OldIVAorMSVA`
-						: testTypeGroup;
+					isOldIVAorMSVAtest && isIVAorMSVATest ? `${testTypeGroup}OldIVAorMSVA` : testTypeGroup;
 
 				let tpl;
 				if (testTypeGroupString && Object.prototype.hasOwnProperty.call(vehicleTpl, testTypeGroupString)) {
@@ -261,14 +257,11 @@ export class TestResultsEffects {
 				}
 
 				const testTypeGroup = TestRecordsService.getTestTypeGroup(id);
-				// tech-debt: feature flag check to be removed when required standard is enabled
-				const isRequiredStandardsEnabled = this.featureToggleService.isFeatureEnabled('requiredstandards');
 				const isIVAorMSVATest =
 					testTypeGroup === 'testTypesSpecialistGroup1' || testTypeGroup === 'testTypesSpecialistGroup5';
 
 				const vehicleTpl = contingencyTestTemplates[`${vehicleType}`];
-				const testTypeGroupString =
-					!isRequiredStandardsEnabled && isIVAorMSVATest ? `${testTypeGroup}OldIVAorMSVA` : testTypeGroup;
+				const testTypeGroupString = isIVAorMSVATest ? `${testTypeGroup}OldIVAorMSVA` : testTypeGroup;
 
 				const tpl =
 					testTypeGroupString && Object.prototype.hasOwnProperty.call(vehicleTpl, testTypeGroupString)
