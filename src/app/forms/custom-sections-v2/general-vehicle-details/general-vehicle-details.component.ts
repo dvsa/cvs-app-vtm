06603d748b7a1217cabdf21e088e6527f8325c3a
Thomas Evans <36958694+tomevs88@users.noreply.github.com>
2025-08-29 09:15:07 +0100

    feat(cb2-18890): first few inputs

--- src/app/forms/custom-sections-v2/general-vehicle-details/general-vehicle-details.component.ts
+++ src/app/forms/custom-sections-v2/general-vehicle-details/general-vehicle-details.component.ts
@@ -1,9 +1,28 @@
 import { Component, OnDestroy, OnInit, input } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { TechRecordType } from '@dvsa/cvs-type-definitions/types/v3/tech-record/tech-record-vehicle-type';
 import { EditBaseComponent } from '@forms/custom-sections/edit-base-component/edit-base-component';
 import { V3TechRecordModel, VehicleTypes } from '@models/vehicle-tech-record.model';
-import { ReplaySubject } from 'rxjs';
+import { of, ReplaySubject } from 'rxjs';
+import { GovukFormGroupDateComponent } from '@forms/components/govuk-form-group-date/govuk-form-group-date.component';
+import {
+  GovukFormGroupInputComponent
+} from '@forms/components/govuk-form-group-input/govuk-form-group-input.component';
+import { FormNodeWidth, TagTypeLabels } from '@services/dynamic-forms/dynamic-form.types';
+import { TagType } from '@components/tag/tag.component';
+import { AsyncPipe } from '@angular/common';
+import {
+  GovukFormGroupSelectComponent
+} from '@forms/components/govuk-form-group-select/govuk-form-group-select.component';
+import {
+  ALL_VEHICLE_CONFIGURATION_OPTIONS,
+  HGV_PSV_VEHICLE_CONFIGURATION_OPTIONS,
+  MultiOptions,
+  TRL_VEHICLE_CONFIGURATION_OPTIONS,
+} from '@models/options.model';
+import { vehicleBodyTypeCodeMap } from '@models/body-type-enum';
+import { getOptionsFromEnum } from '@forms/utils/enum-map';
+import { VehicleConfiguration } from '@models/vehicle-configuration.enum';
 
 type VehicleSectionForm = Partial<Record<keyof TechRecordType<'hgv' | 'car' | 'psv' | 'lgv' | 'trl'>, FormControl>>;
 
@@ -11,7 +30,13 @@ type VehicleSectionForm = Partial<Record<keyof TechRecordType<'hgv' | 'car' | 'p
 	selector: 'app-general-vehicle-details',
 	templateUrl: './general-vehicle-details.component.html',
 	styleUrls: ['./general-vehicle-details.component.scss'],
-	imports: [],
+  imports: [
+    GovukFormGroupDateComponent,
+    ReactiveFormsModule,
+    GovukFormGroupInputComponent,
+    AsyncPipe,
+    GovukFormGroupSelectComponent,
+  ],
 })
 export class GeneralVehicleDetailsComponent extends EditBaseComponent implements OnInit, OnDestroy {
 	destroy$ = new ReplaySubject<boolean>(1);
@@ -59,12 +84,63 @@ export class GeneralVehicleDetailsComponent extends EditBaseComponent implements
 	}
 
 	get hgvFields(): Partial<Record<keyof TechRecordType<'hgv'>, FormControl>> {
-		return {};
+		return {
+      techRecord_regnDate: this.fb.control<string | null>(null, [
+        this.commonValidators.date('Date of first registration'),
+      ]),
+      techRecord_manufactureYear: this.fb.control<number | null>(null, [
+        this.commonValidators.max(9999, 'Year of manufacture must be less than or equal to 9999'),
+        this.commonValidators.min(1000, 'Year of manufacture must be greater than or equal to 1000'),
+        this.commonValidators.xYearsAfterCurrent(
+          1,
+          `Year of manufacture must be equal to or before ${new Date().getFullYear() + 1}`
+        ),
+      ]),
+      techRecord_brakes_dtpNumber: this.fb.control<string | null>(null, [
+        this.commonValidators.maxLength(6, 'DTp number must be less than or equal to 6 characters'),
+      ]),
+      techRecord_make: this.fb.control<string | null>(null, [
+        this.commonValidators.maxLength(50, 'Body make must be less than or equal to 50 characters'),
+        // this.bodyMakeRequiredWithDangerousGoods(),
+      ]),
+      techRecord_vehicleConfiguration: this.fb.control<VehicleConfiguration | null>(null, [
+        this.commonValidators.required('Vehicle configuration is required'),
+      ]),
+
+    };
 	}
+
+  get vehicleConfigurationOptions() {
+    switch (this.getVehicleType()) {
+      case VehicleTypes.HGV:
+      case VehicleTypes.PSV:
+        return HGV_PSV_VEHICLE_CONFIGURATION_OPTIONS;
+      case VehicleTypes.TRL:
+      case VehicleTypes.SMALL_TRL:
+        return TRL_VEHICLE_CONFIGURATION_OPTIONS;
+      default:
+        return ALL_VEHICLE_CONFIGURATION_OPTIONS;
+    }
+  }
 
 	getVehicleType(): VehicleTypes {
 		return this.technicalRecordService.getVehicleTypeWithSmallTrl(this.techRecord());
 	}
+
+  shouldDisplayFormControl(formControlName: string) {
+    return !!this.form.get(formControlName);
+  }
+
+  get bodyTypes(): MultiOptions {
+    let vehicleType: string = this.techRecord().techRecord_vehicleType;
+
+    if (this.techRecord().techRecord_vehicleType === 'hgv') {
+      vehicleType = `${this.techRecord().techRecord_vehicleConfiguration}Hgv`;
+    }
+    const optionsMap = vehicleBodyTypeCodeMap.get(vehicleType) ?? [];
+    const values = [...optionsMap.values()];
+    return getOptionsFromEnum(values.sort());
+  }
 
 	ngOnDestroy(): void {
 		// Detach all form controls from parent
@@ -74,4 +150,11 @@ export class GeneralVehicleDetailsComponent extends EditBaseComponent implements
 		this.destroy$.next(true);
 		this.destroy$.complete();
 	}
+
+  protected readonly TagTypeLabels = TagTypeLabels;
+  protected readonly FormNodeWidth = FormNodeWidth;
+  protected readonly TagType = TagType;
+  protected readonly VehicleTypes = VehicleTypes;
+  bodyMakes$ = of<MultiOptions | undefined>([]);
+
 }
