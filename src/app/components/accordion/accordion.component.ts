dcafcea592fb9091cea8b60d0272d85fd5fa51c5
pbardy2000 <146740183+pbardy2000@users.noreply.github.com>
2025-09-01 09:22:34 +0100

    feat(CB2-18890): styling

--- src/app/components/accordion/accordion.component.ts
+++ src/app/components/accordion/accordion.component.ts
@@ -17,6 +17,7 @@ export class AccordionComponent {
 	readonly title = input<string | undefined>('');
 	readonly description = input<string | undefined>('');
 	readonly id = input<string | number>('');
+	readonly type = input<AccordionType>('default');
 
 	isExpanded = model<boolean | null | undefined>(false);
 
@@ -36,3 +37,5 @@ export class AccordionComponent {
 		if (sectionName) this.store.dispatch(removeSectionState({ section: sectionName }));
 	}
 }
+
+export type AccordionType = 'default' | 'condensed';
