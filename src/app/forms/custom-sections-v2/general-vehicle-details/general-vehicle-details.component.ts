249234a05e3cb609d706464deed21099fe2e77ec
Thomas Evans <36958694+tomevs88@users.noreply.github.com>
2025-09-04 12:06:30 +0100

    Update src/app/forms/custom-sections-v2/general-vehicle-details/general-vehicle-details.component.ts
    Co-authored-by: Philip Bardy <146740183+pbardy2000@users.noreply.github.com>

--- src/app/forms/custom-sections-v2/general-vehicle-details/general-vehicle-details.component.ts
+++ src/app/forms/custom-sections-v2/general-vehicle-details/general-vehicle-details.component.ts
@@ -139,12 +139,7 @@ export class GeneralVehicleDetailsComponent extends EditBaseComponent implements
 
 		const vehicleType = this.getVehicleType();
 		if (vehicleType === VehicleTypes.TRL) {
-			this.bodyTypes = getOptionsFromEnum(Array.from(trlBodyTypeCodeMap.values()).flat());
-			this.bodyTypes = this.bodyTypes.sort((a, b) => {
-				const labelA = typeof a === 'string' ? a : a.label;
-				const labelB = typeof b === 'string' ? b : b.label;
-				return labelA.localeCompare(labelB);
-			});
+			this.bodyTypes = getOptionsFromEnum(Array.from(trlBodyTypeCodeMap.values()).flat().sort());
 		}
 	}
 
