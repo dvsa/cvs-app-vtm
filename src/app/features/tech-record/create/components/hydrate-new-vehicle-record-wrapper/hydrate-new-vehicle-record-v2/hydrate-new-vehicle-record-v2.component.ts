7bd766d8705d11b901af23987fcacb053b0774de
Brandon Thomas-Davies <87308252+BrandonT95@users.noreply.github.com>
2025-09-05 10:30:57 +0100

    feat(CB2-18833): add unit tests

--- src/app/features/tech-record/create/components/hydrate-new-vehicle-record-wrapper/hydrate-new-vehicle-record-v2/hydrate-new-vehicle-record-v2.component.ts
+++ src/app/features/tech-record/create/components/hydrate-new-vehicle-record-wrapper/hydrate-new-vehicle-record-v2/hydrate-new-vehicle-record-v2.component.ts
@@ -15,6 +15,7 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GlobalErrorService } from '@core/components/global-error/global-error.service';
 import { GeneralVehicleDetailsComponent } from '@forms/custom-sections-v2/general-vehicle-details/general-vehicle-details.component';
+import { ReasonForCreationComponent } from '@forms/custom-sections-v2/reason-for-creation/reason-for-creation.component';
 import { Store } from '@ngrx/store';
 import { ReplaySubject, takeUntil } from 'rxjs';
 
@@ -33,6 +34,7 @@ import { ReplaySubject, takeUntil } from 'rxjs';
 		FormatVehicleTypePipe,
 		GeneralVehicleDetailsComponent,
 		ReactiveFormsModule,
+		ReasonForCreationComponent,
 	],
 })
 export class HydrateNewVehicleRecordV2Component implements OnInit, OnDestroy {
@@ -90,5 +92,13 @@ export class HydrateNewVehicleRecordV2Component implements OnInit, OnDestroy {
 		if (!this.techRecord$()) {
 			this.router.navigate([RootRoutes.CREATE_TECHNICAL_RECORD]);
 		}
+	}
+
+	generateRFCDescription(): string {
+		// TODO: Update this method to return a dynamic description message
+		// based on if user is creating or amending a record.
+		// return "Tell us why you're amending this record.";
+
+		return "Tell us why you're creating this record.";
 	}
 }
