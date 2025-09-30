9665091335873b6095f22a749883a6644e78ee5c
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2025-09-30 11:18:27 +0100

    feat(CB2-19110): minor css changes

--- src/app/forms/custom-sections-v2/dimensions/dimensions.component.ts
+++ src/app/forms/custom-sections-v2/dimensions/dimensions.component.ts
@@ -1,116 +1,114 @@
-import { EditBaseComponent } from '@forms/custom-sections/edit-base-component/edit-base-component';
-import { Component, inject, input, OnDestroy, OnInit } from '@angular/core';
-import { FormArray, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {
-  GovukFormGroupInputComponent
-} from '@forms/components/govuk-form-group-input/govuk-form-group-input.component';
-import { FieldWarningMessageComponent } from '@forms/components/field-warning-message/field-warning-message.component';
-import { FormNodeWidth, TagTypeLabels } from '@services/dynamic-forms/dynamic-form.types';
-import { Actions } from '@ngrx/effects';
-import { TechRecordType } from '@dvsa/cvs-type-definitions/types/v3/tech-record/tech-record-vehicle-type';
-import { ReplaySubject } from 'rxjs';
-import { VehicleTypes } from '@/src/app/models/vehicle-tech-record.model';
 import { TagType } from '@/src/app/components/tag/tag.component';
+import { VehicleTypes } from '@/src/app/models/vehicle-tech-record.model';
+import { Component, OnDestroy, OnInit, inject, input } from '@angular/core';
+import { FormArray, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TechRecordType } from '@dvsa/cvs-type-definitions/types/v3/tech-record/tech-record-vehicle-type';
+import { FieldWarningMessageComponent } from '@forms/components/field-warning-message/field-warning-message.component';
+import { GovukFormGroupInputComponent } from '@forms/components/govuk-form-group-input/govuk-form-group-input.component';
+import { EditBaseComponent } from '@forms/custom-sections/edit-base-component/edit-base-component';
+import { Actions } from '@ngrx/effects';
+import { FormNodeWidth, TagTypeLabels } from '@services/dynamic-forms/dynamic-form.types';
+import { ReplaySubject } from 'rxjs';
 
 @Component({
-  selector: 'app-dimensions',
-  templateUrl: './dimensions.component.html',
-  styleUrls: ['./dimensions.component.scss'],
-  imports: [FormsModule, ReactiveFormsModule, GovukFormGroupInputComponent, FieldWarningMessageComponent],
+	selector: 'app-dimensions',
+	templateUrl: './dimensions.component.html',
+	styleUrls: ['./dimensions.component.scss'],
+	imports: [FormsModule, ReactiveFormsModule, GovukFormGroupInputComponent, FieldWarningMessageComponent],
 })
 export class DimensionsComponent extends EditBaseComponent implements OnInit, OnDestroy {
-  readonly VehicleTypes = VehicleTypes;
-  readonly Widths = FormNodeWidth;
-  readonly TagType = TagType;
-  readonly TagTypeLabels = TagTypeLabels;
+	readonly VehicleTypes = VehicleTypes;
+	readonly Widths = FormNodeWidth;
+	readonly TagType = TagType;
+	readonly TagTypeLabels = TagTypeLabels;
 
-  actions = inject(Actions);
-  techRecord = input.required<TechRecordType<'hgv' | 'trl' | 'psv'>>();
+	actions = inject(Actions);
+	techRecord = input.required<TechRecordType<'hgv' | 'trl' | 'psv'>>();
 
-  destroy$ = new ReplaySubject<boolean>(1);
+	destroy$ = new ReplaySubject<boolean>(1);
 
-  form: FormGroup = this.fb.group({});
+	form: FormGroup = this.fb.group({});
 
-  ngOnInit(): void {
-    this.addControls(this.controlsBasedOffVehicleType, this.form);
+	ngOnInit(): void {
+		this.addControls(this.controlsBasedOffVehicleType, this.form);
 
-    // Attach all form controls to parent
-    this.init(this.form);
-  }
+		// Attach all form controls to parent
+		this.init(this.form);
+	}
 
-  get controlsBasedOffVehicleType() {
-    switch (this.techRecord().techRecord_vehicleType) {
-      // case VehicleTypes.PSV:
-      //   return this.psvControls;
-      case VehicleTypes.HGV:
-        return this.hgvControls;
-      // case VehicleTypes.TRL:
-      //   return this.trlControls;
-      default:
-        return {};
-    }
-  }
+	get controlsBasedOffVehicleType() {
+		switch (this.techRecord().techRecord_vehicleType) {
+			// case VehicleTypes.PSV:
+			//   return this.psvControls;
+			case VehicleTypes.HGV:
+				return this.hgvControls;
+			// case VehicleTypes.TRL:
+			//   return this.trlControls;
+			default:
+				return {};
+		}
+	}
 
-  get hgvControls() {
-    return {
-      techRecord_dimensions_length: this.fb.control<string | null>(null, [
-        this.commonValidators.max(99999, 'Length must be less than or equal to 99999'),
-      ]),
-      techRecord_dimensions_width: this.fb.control<string | null>(null, [
-        this.commonValidators.max(99999, 'Width must be less than or equal to 99999'),
-      ]),
-      techRecord_frontAxleToRearAxle: this.fb.control<string | null>(null, [
-        this.commonValidators.max(99999, 'Front axle to rear axle must be less than or equal to 99999'),
-      ]),
-      techRecord_frontVehicleTo5thWheelCouplingMin: this.fb.control<string | null>(null, [
-        this.commonValidators.max(99999, 'Minimum must be less than or equal to 99999'),
-      ]),
-      techRecord_frontVehicleTo5thWheelCouplingMax: this.fb.control<string | null>(null, [
-        this.commonValidators.max(99999, 'Maximum must be less than or equal to 99999'),
-      ]),
-      techRecord_frontAxleTo5thWheelMin: this.fb.control<string | null>(null, [
-        this.commonValidators.max(99999, 'Minimum must be less than or equal to 99999'),
-      ]),
-      techRecord_frontAxleTo5thWheelMax: this.fb.control<string | null>(null, [
-        this.commonValidators.max(99999, 'Maximum must be less than or equal to 99999'),
-      ]),
-    };
-  }
+	get hgvControls() {
+		return {
+			techRecord_dimensions_length: this.fb.control<string | null>(null, [
+				this.commonValidators.max(99999, 'Length must be less than or equal to 99999mm'),
+			]),
+			techRecord_dimensions_width: this.fb.control<string | null>(null, [
+				this.commonValidators.max(99999, 'Width must be less than or equal to 99999mm'),
+			]),
+			techRecord_frontAxleToRearAxle: this.fb.control<string | null>(null, [
+				this.commonValidators.max(99999, 'Front axle to rear axle must be less than or equal to 99999mm'),
+			]),
+			techRecord_frontVehicleTo5thWheelCouplingMin: this.fb.control<string | null>(null, [
+				this.commonValidators.max(99999, 'Minimum must be less than or equal to 99999mm'),
+			]),
+			techRecord_frontVehicleTo5thWheelCouplingMax: this.fb.control<string | null>(null, [
+				this.commonValidators.max(99999, 'Maximum must be less than or equal to 99999mm'),
+			]),
+			techRecord_frontAxleTo5thWheelMin: this.fb.control<string | null>(null, [
+				this.commonValidators.max(99999, 'Minimum must be less than or equal to 99999mm'),
+			]),
+			techRecord_frontAxleTo5thWheelMax: this.fb.control<string | null>(null, [
+				this.commonValidators.max(99999, 'Maximum must be less than or equal to 99999mm'),
+			]),
+		};
+	}
 
-  get couplingCenterToRearTrlMinWarning() {
-    return Number.parseInt(this.form.get('techRecord_couplingCenterToRearTrlMin')?.value, 10) > 12000
-      ? 'The coupling centre to rear of trailer minimum field value is greater than 12,000mm. Check your input before proceeding'
-      : '';
-  }
+	get couplingCenterToRearTrlMinWarning() {
+		return Number.parseInt(this.form.get('techRecord_couplingCenterToRearTrlMin')?.value, 10) > 12000
+			? 'The coupling centre to rear of trailer minimum field value is greater than 12,000mm. Check your input before proceeding'
+			: '';
+	}
 
-  get couplingCenterToRearTrlMaxWarning() {
-    return Number.parseInt(this.form.get('techRecord_couplingCenterToRearTrlMax')?.value, 10) > 12000
-      ? 'The coupling centre to rear of trailer maximum field value is greater than 12,000mm. Check your input before proceeding'
-      : '';
-  }
+	get couplingCenterToRearTrlMaxWarning() {
+		return Number.parseInt(this.form.get('techRecord_couplingCenterToRearTrlMax')?.value, 10) > 12000
+			? 'The coupling centre to rear of trailer maximum field value is greater than 12,000mm. Check your input before proceeding'
+			: '';
+	}
 
-  get lengthWarning() {
-    return Number.parseInt(this.form.get('techRecord_dimensions_length')?.value, 10) > 12000
-      ? 'This length dimension field value is greater than 12,000mm. Check your input before proceeding'
-      : '';
-  }
+	get lengthWarning() {
+		return Number.parseInt(this.form.get('techRecord_dimensions_length')?.value, 10) > 12000
+			? 'This length dimension field value is greater than 12,000mm. Check your input before proceeding'
+			: '';
+	}
 
-  get widthWarning() {
-    return Number.parseInt(this.form.get('techRecord_dimensions_width')?.value, 10) > 2600
-      ? 'This width dimension field value is greater than 2,600mm. Check your input before proceeding'
-      : '';
-  }
+	get widthWarning() {
+		return Number.parseInt(this.form.get('techRecord_dimensions_width')?.value, 10) > 2600
+			? 'This width dimension field value is greater than 2,600mm. Check your input before proceeding'
+			: '';
+	}
 
-  get axleSpacings(): FormArray<FormGroup> | undefined {
-    return this.parent.get('techRecord_dimensions_axleSpacing') as FormArray;
-  }
+	get axleSpacings(): FormArray<FormGroup> | undefined {
+		return this.parent.get('techRecord_dimensions_axleSpacing') as FormArray;
+	}
 
-  ngOnDestroy(): void {
-    // Detach all form controls from parent
-    this.destroy(this.form);
+	ngOnDestroy(): void {
+		// Detach all form controls from parent
+		this.destroy(this.form);
 
-    // Clear subscriptions
-    this.destroy$.next(true);
-    this.destroy$.complete();
-  }
+		// Clear subscriptions
+		this.destroy$.next(true);
+		this.destroy$.complete();
+	}
 }
