552bddfe83b9a04eabdfd3501060b4f573120695
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2025-10-09 11:14:42 +0100

    feat(CB2-19132): initial commit

--- src/app/features/tech-record/create/components/hydrate-new-vehicle-record-wrapper/hydrate-new-vehicle-record-v2/hydrate-new-vehicle-record-v2.component.ts
+++ src/app/features/tech-record/create/components/hydrate-new-vehicle-record-wrapper/hydrate-new-vehicle-record-v2/hydrate-new-vehicle-record-v2.component.ts
@@ -27,31 +27,33 @@ import { Store } from '@ngrx/store';
 import { AxlesService } from '@services/axles/axles.service';
 import { RouterService } from '@services/router/router.service';
 import { ReplaySubject, map, skipWhile, take, takeUntil } from 'rxjs';
+import { TyresComponent } from '@forms/custom-sections-v2/tyres/tyres.component';
 
 @Component({
 	selector: 'app-hydrate-new-vehicle-record-v2',
 	templateUrl: './hydrate-new-vehicle-record-v2.component.html',
 	styleUrls: ['./hydrate-new-vehicle-record-v2.component.scss'],
-	imports: [
-		AccordionControlComponent,
-		AccordionComponent,
-		NgTemplateOutlet,
-		ButtonComponent,
-		NumberPlateComponent,
-		DefaultNullOrEmpty,
-		TagComponent,
-		FormatVehicleTypePipe,
-		GeneralVehicleDetailsComponent,
-		ReactiveFormsModule,
-		NotesComponent,
-		ReasonForCreationComponent,
-		AdrComponent,
-		LastApplicantComponent,
-		AdrComponent,
-		EmissionsAndExemptionsComponent,
-		WeightsComponent,
-		DimensionsComponent,
-	],
+  imports: [
+    AccordionControlComponent,
+    AccordionComponent,
+    NgTemplateOutlet,
+    ButtonComponent,
+    NumberPlateComponent,
+    DefaultNullOrEmpty,
+    TagComponent,
+    FormatVehicleTypePipe,
+    GeneralVehicleDetailsComponent,
+    ReactiveFormsModule,
+    NotesComponent,
+    ReasonForCreationComponent,
+    AdrComponent,
+    LastApplicantComponent,
+    AdrComponent,
+    EmissionsAndExemptionsComponent,
+    WeightsComponent,
+    DimensionsComponent,
+    TyresComponent,
+  ],
 })
 export class HydrateNewVehicleRecordV2Component implements OnInit, OnDestroy {
 	store = inject(Store);
