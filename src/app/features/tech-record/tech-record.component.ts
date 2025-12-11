6b71b81373c476267ac0c642bdba477a93efb5ed
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2025-12-11 12:26:28 +0000

    chore: update route strategy with smarter rerender check (#2215)

--- src/app/features/tech-record/tech-record.component.ts
+++ src/app/features/tech-record/tech-record.component.ts
@@ -24,7 +24,11 @@ export class TechRecordComponent implements OnInit {
 	createdTimestamp?: string;
 
 	constructor() {
-		this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+		// Only prevent reuse when route path params change i.e. diff tech record
+		// Allow reuse when only query parameters change (e.g., pagination)
+		this.router.routeReuseStrategy.shouldReuseRoute = (future, curr) => {
+			return future.routeConfig === curr.routeConfig && JSON.stringify(future.params) === JSON.stringify(curr.params);
+		};
 	}
 
 	ngOnInit(): void {
--- src/app/features/test-records/create/views/create-test-record/create-test-record.component.ts
+++ src/app/features/test-records/create/views/create-test-record/create-test-record.component.ts
@@ -67,7 +67,11 @@ export class CreateTestRecordComponent implements OnInit, OnDestroy, AfterViewIn
 	techRecord: V3TechRecordModel | undefined = undefined;
 
 	constructor() {
-		this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+		// Only prevent reuse when route path params change i.e. diff tech record
+		// Allow reuse when only query parameters change (e.g., pagination)
+		this.router.routeReuseStrategy.shouldReuseRoute = (future, curr) => {
+			return future.routeConfig === curr.routeConfig && JSON.stringify(future.params) === JSON.stringify(curr.params);
+		};
 	}
 
 	ngOnInit(): void {
