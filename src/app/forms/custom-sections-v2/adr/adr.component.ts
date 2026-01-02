0e7d657809ec745a7674131e49acac809921aa7b
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2026-01-02 14:51:39 +0000

    feat(CB2-18904): fix for a bad fix

--- src/app/forms/custom-sections-v2/adr/adr.component.ts
+++ src/app/forms/custom-sections-v2/adr/adr.component.ts
@@ -228,7 +228,7 @@ export class AdrComponent extends EditBaseComponent implements OnInit, OnDestroy
 		]),
 		techRecord_adrDetails_brakeEndurance: this.fb.control<boolean>(false),
 		techRecord_adrDetails_weight: this.fb.control<number | null>(null, [
-			this.commonValidators.max(99999999, 'Weight (tonnes)', 'adr', 'techRecord_adrDetails_weight'),
+			this.commonValidators.max(99999999, 'Weight (tonnes)', '', 'adr', 'techRecord_adrDetails_weight'),
 			this.adrValidators.requiredWithBrakeEndurance('Weight (tonnes)'),
 			this.commonValidators.pattern('^\\d*(\\.\\d{0,2})?$', 'Weight (tonnes) Max 2 decimal places'),
 		]),
