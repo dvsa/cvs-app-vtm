2344083b20c32e21014a78c7218ecaccb9b1700e
pbardy2000 <146740183+pbardy2000@users.noreply.github.com>
2026-01-05 09:07:20 +0000

    feat(CB2-20216): delete more unused files

--- src/app/models/test-results/models.ts
+++
@@ -1,6 +0,0 @@
-export * from './completeTestResults';
-export * from './completeTestResultsVehicleClass';
-export * from './testResults';
-export * from './testTypeRecords';
-export * from './testTypeResultsModType';
-export * from './testTypes';
