9b2b3363561866cd76ba24961901ffa5a640d3ae
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2026-03-02 09:40:53 +0000

    feat(CB2-20346): voice over for pagination pages

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -95,6 +95,10 @@ export class PaginationComponent implements OnInit, OnDestroy {
 		return Math.ceil(this.numberOfItems() / this.itemsPerPage());
 	}
 
+	get readableTableName() {
+		return this.tableName().replace('-', ' ');
+	}
+
 	/**
 	 * Returns array of visible page buttons.
 	 * Allways returns an odd number of pages while keeping current page in the middle.
