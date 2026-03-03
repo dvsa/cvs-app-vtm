3110234ed3a4ae1ffd495d828d67794dfd3e72db
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2026-03-03 10:42:47 +0000

    feat(CB2-20984): resolve issues amending old contingency tests without media

--- src/app/features/test-records/amend/views/test-record/test-record.component.ts
+++ src/app/features/test-records/amend/views/test-record/test-record.component.ts
@@ -114,6 +114,12 @@ export class TestRecordComponent implements OnInit, OnDestroy {
 		const testResult = await firstValueFrom(this.testResult$);
 		const testResultClone = cloneDeep(testResult) as TestResultSchema;
 
+		for (const defect of testResultClone.testTypes[0].defects) {
+			if (!defect.media) {
+				defect.media = [{ type: 'failReason', path: ' ', reason: 'Contingency test' }];
+			}
+		}
+
 		this.testRecordsService.updateTestResult(testResultClone);
 	}
 
