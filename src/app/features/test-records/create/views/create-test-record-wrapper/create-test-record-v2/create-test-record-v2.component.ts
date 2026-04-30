88c7fbead2f9b1e8bf0dfe34d640c6097d07dae7
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2026-04-30 09:53:16 +0100

    feat(CB2-19601): notes section

--- src/app/features/test-records/create/views/create-test-record-wrapper/create-test-record-v2/create-test-record-v2.component.ts
+++ src/app/features/test-records/create/views/create-test-record-wrapper/create-test-record-v2/create-test-record-v2.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit, inject } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TestResultSchema } from '@dvsa/cvs-type-definitions/types/v1/test-result';
 import { ReasonForCreationComponent } from '@features/test-records/custom-sections/reason-for-creation/reason-for-creation.component';
 import { Modes } from '@models/modes.enum';
@@ -11,7 +11,7 @@ import { ReplaySubject, takeUntil } from 'rxjs';
 	selector: 'app-create-test-record-v2',
 	templateUrl: './create-test-record-v2.component.html',
 	styleUrls: ['./create-test-record-v2.component.scss'],
-	imports: [ReasonForCreationComponent, ReactiveFormsModule],
+	imports: [ReasonForCreationComponent, FormsModule, ReactiveFormsModule],
 })
 export class CreateTestRecordV2Component implements OnDestroy, OnInit {
 	store = inject(Store);
