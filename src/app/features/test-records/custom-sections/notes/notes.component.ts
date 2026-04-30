5a029cb0ab2e87611eafac705593e4479022e89f
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2026-04-30 10:20:00 +0100

    feat(CB2-19601): lint fixes

--- src/app/features/test-records/custom-sections/notes/notes.component.ts
+++ src/app/features/test-records/custom-sections/notes/notes.component.ts
@@ -6,43 +6,43 @@ import { Modes } from '@models/modes.enum';
 import { ReplaySubject } from 'rxjs';
 
 @Component({
-  selector: 'app-test-notes',
-  templateUrl: './notes.component.html',
-  imports: [FormsModule, ReactiveFormsModule, GovukFormGroupTextareaComponent],
-  styleUrls: ['./notes.component.scss'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => NotesComponent),
-      multi: true,
-    },
-  ],
+	selector: 'app-test-notes',
+	templateUrl: './notes.component.html',
+	imports: [FormsModule, ReactiveFormsModule, GovukFormGroupTextareaComponent],
+	styleUrls: ['./notes.component.scss'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => NotesComponent),
+			multi: true,
+		},
+	],
 })
 export class NotesComponent extends BaseTestRecordV2Component implements OnInit, OnDestroy {
-  destroy$ = new ReplaySubject<boolean>(1);
-  mode = input.required<Modes>();
+	destroy$ = new ReplaySubject<boolean>(1);
+	mode = input.required<Modes>();
 
-  testResult$ = this.testRecordService.editingTestResult$;
+	testResult$ = this.testRecordService.editingTestResult$;
 
-  form = this.fb.group({
-    additionalNotesRecorded: this.fb.control('', [
-      this.commonValidators.maxLength(500, 'Additional Notes (optional)', 'notes', 'additionalNotesRecorded'),
-    ]),
-  });
+	form = this.fb.group({
+		additionalNotesRecorded: this.fb.control('', [
+			this.commonValidators.maxLength(500, 'Additional Notes (optional)', 'notes', 'additionalNotesRecorded'),
+		]),
+	});
 
-  ngOnInit(): void {
-    this.init(this.form);
+	ngOnInit(): void {
+		this.init(this.form);
 
-    // Prepopulate form with current test record
-    this.form.patchValue(this.testResult$ as any);
-  }
+		// Prepopulate form with current test record
+		this.form.patchValue(this.testResult$ as any);
+	}
 
-  ngOnDestroy() {
-    // Detach all form controls from parent
-    this.destroy(this.form);
+	ngOnDestroy() {
+		// Detach all form controls from parent
+		this.destroy(this.form);
 
-    // Clear subscriptions
-    this.destroy$.next(true);
-    this.destroy$.complete();
-  }
+		// Clear subscriptions
+		this.destroy$.next(true);
+		this.destroy$.complete();
+	}
 }
