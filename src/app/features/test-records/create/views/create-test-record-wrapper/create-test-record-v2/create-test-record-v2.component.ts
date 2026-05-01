0f985af75e2704736a35ba5380f22523cb2ec138
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2026-05-01 16:57:49 +0100

    feat(CB2-19601): some more bits and bobs

--- src/app/features/test-records/create/views/create-test-record-wrapper/create-test-record-v2/create-test-record-v2.component.ts
+++ src/app/features/test-records/create/views/create-test-record-wrapper/create-test-record-v2/create-test-record-v2.component.ts
@@ -1,8 +1,16 @@
+import { NgTemplateOutlet } from '@angular/common';
 import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AccordionControlComponent } from '@components/accordion-control/accordion-control.component';
+import { AccordionComponent } from '@components/accordion/accordion.component';
+import { FilterByTagsDirective } from '@directives/filter-by-tags/filter-by-tags.directive';
 import { TestResultSchema } from '@dvsa/cvs-type-definitions/types/v1/test-result';
+import { DefectsComponent } from '@features/test-records/custom-sections/defects/defects.component';
+import { NotesComponent } from '@features/test-records/custom-sections/notes/notes.component';
 import { ReasonForCreationComponent } from '@features/test-records/custom-sections/reason-for-creation/reason-for-creation.component';
+import { VisitComponent } from '@features/test-records/custom-sections/visit/visit.component';
 import { Modes } from '@models/modes.enum';
+import { VehicleTypes } from '@models/vehicle-tech-record.model';
 import { Store } from '@ngrx/store';
 import { TestRecordsService } from '@services/test-records/test-records.service';
 import { ReplaySubject, takeUntil } from 'rxjs';
@@ -11,7 +19,18 @@ import { ReplaySubject, takeUntil } from 'rxjs';
 	selector: 'app-create-test-record-v2',
 	templateUrl: './create-test-record-v2.component.html',
 	styleUrls: ['./create-test-record-v2.component.scss'],
-	imports: [ReasonForCreationComponent, FormsModule, ReactiveFormsModule],
+	imports: [
+		ReasonForCreationComponent,
+		FormsModule,
+		ReactiveFormsModule,
+		AccordionControlComponent,
+		AccordionComponent,
+		FilterByTagsDirective,
+		NgTemplateOutlet,
+		DefectsComponent,
+		NotesComponent,
+		VisitComponent,
+	],
 })
 export class CreateTestRecordV2Component implements OnDestroy, OnInit {
 	store = inject(Store);
@@ -38,4 +57,5 @@ export class CreateTestRecordV2Component implements OnDestroy, OnInit {
 	}
 
 	protected readonly Modes = Modes;
+	protected readonly VehicleTypes = VehicleTypes;
 }
