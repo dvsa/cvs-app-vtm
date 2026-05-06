9990cfefea2e18df90dd69f1d2a0b703fbcc4f47
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2026-05-06 13:45:16 +0100

    feat(CB2-19601): stash

--- src/app/features/test-records/custom-sections/visit/visit.component.ts
+++ src/app/features/test-records/custom-sections/visit/visit.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnDestroy, OnInit, forwardRef, input } from '@angular/core';
+import { Component, OnDestroy, OnInit, forwardRef, input, inject } from '@angular/core';
 import { FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { BaseTestRecordV2Component } from '@features/test-records/components/base-test-record-v2/base-test-record-v2.component';
-import { GovukFormGroupTextareaComponent } from '@forms/components/govuk-form-group-textarea/govuk-form-group-textarea.component';
 import { Modes } from '@models/modes.enum';
 import { ReplaySubject } from 'rxjs';
+import {
+  GovukFormGroupSelectComponent
+} from '@forms/components/govuk-form-group-select/govuk-form-group-select.component';
+import { Store } from '@ngrx/store';
+import { testStationNames } from '@store/test-stations';
+import {
+  GovukFormGroupAutocompleteComponent
+} from '@forms/components/govuk-form-group-autocomplete/govuk-form-group-autocomplete.component';
 
 @Component({
 	selector: 'app-test-visit',
 	templateUrl: './visit.component.html',
-	imports: [FormsModule, ReactiveFormsModule, GovukFormGroupTextareaComponent],
+  imports: [FormsModule, ReactiveFormsModule, GovukFormGroupSelectComponent, GovukFormGroupAutocompleteComponent],
 	styleUrls: ['./visit.component.scss'],
 	providers: [
 		{
@@ -21,6 +28,8 @@ import { ReplaySubject } from 'rxjs';
 export class VisitComponent extends BaseTestRecordV2Component implements OnInit, OnDestroy {
 	destroy$ = new ReplaySubject<boolean>(1);
 	mode = input.required<Modes>();
+  store = inject(Store);
+  testStationNames = this.store.select(testStationNames);
 
 	testResult$ = this.testRecordService.editingTestResult$;
 
