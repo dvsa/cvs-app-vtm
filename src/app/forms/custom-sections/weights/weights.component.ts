32eda672b626d0f3f425c9bbfa99c09970a60fe8
Philip Bardy <146740183+pbardy2000@users.noreply.github.com>
2026-06-05 10:35:17 +0100

    feat(CB2-21757): add not applicable checkbox (#2379)

--- src/app/forms/custom-sections/weights/weights.component.ts
+++ src/app/forms/custom-sections/weights/weights.component.ts
@@ -5,9 +5,10 @@ import { selectTechRecord } from '@/src/app/store/technical-records';
 import { testResultInEdit, toEditOrNotToEdit } from '@/src/app/store/test-records';
 import { Component, OnDestroy, OnInit, inject, input, output } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TestResultSchema } from '@dvsa/cvs-type-definitions/types/v1/test-result';
+import { type DesignTrainWeightRequired, TestResultSchema } from '@dvsa/cvs-type-definitions/types/v1/test-result';
 import { Store } from '@ngrx/store';
 import { ReplaySubject, takeUntil } from 'rxjs';
+import { GovukFormGroupCheckboxComponent } from '../../components/govuk-form-group-checkbox/govuk-form-group-checkbox.component';
 import { GovukFormGroupInputComponent } from '../../components/govuk-form-group-input/govuk-form-group-input.component';
 import { CommonValidatorsService } from '../../validators/common-validators.service';
 
@@ -15,7 +16,7 @@ import { CommonValidatorsService } from '../../validators/common-validators.serv
 	selector: 'app-weights',
 	templateUrl: './weights.component.html',
 	styleUrls: ['./weights.component.scss'],
-	imports: [FormsModule, ReactiveFormsModule, GovukFormGroupInputComponent],
+	imports: [FormsModule, ReactiveFormsModule, GovukFormGroupInputComponent, GovukFormGroupCheckboxComponent],
 })
 export class WeightsComponent implements OnInit, OnDestroy {
 	store = inject(Store);
@@ -42,6 +43,10 @@ export class WeightsComponent implements OnInit, OnDestroy {
 				this.commonValidators.min(1, 'Design total axle weight', 'kg'),
 				this.commonValidators.max(99999, 'Design total axle weight', 'kg'),
 			]),
+			designTrainWeightRequired: this.fb.control<DesignTrainWeightRequired | undefined>({
+				value: undefined,
+				disabled: false,
+			}),
 		}),
 	});
 
@@ -51,6 +56,9 @@ export class WeightsComponent implements OnInit, OnDestroy {
 	destroy = new ReplaySubject<boolean>(1);
 	VehicleTypes = VehicleTypes;
 	FormNodeWidth = FormNodeWidth;
+
+	// @TODO: replace with enum when exposed as a .js file in the type definitions package
+	NOT_APPLICABLE = 'N/A' as DesignTrainWeightRequired.NOT_APPLICABLE;
 
 	ngOnInit(): void {
 		this.handleFormChange();
@@ -110,7 +118,12 @@ export class WeightsComponent implements OnInit, OnDestroy {
 				designGrossTrainWeight.patchValue(dgtw);
 				designGrossTrainWeight.disable();
 			}
-			designGrossTrainWeight.addValidators(this.commonValidators.required('Design gross train weight'));
+			designGrossTrainWeight.addValidators(
+				this.commonValidators.applyWhen(
+					() => this.form.controls.weights.controls.designTrainWeightRequired.getRawValue() !== this.NOT_APPLICABLE,
+					this.commonValidators.required('Design gross train weight')
+				)
+			);
 		}
 
 		// If the tech record has a total axle weight, pre-populate the form and disable the field, otherwise require manual input (in create mode)
@@ -121,6 +134,19 @@ export class WeightsComponent implements OnInit, OnDestroy {
 				designGrossAxleWeight.disable();
 			}
 			designGrossAxleWeight.addValidators(this.commonValidators.required('Design total axle weight'));
+		}
+	}
+
+	handleTrainWeightNotApplicable(value: unknown): void {
+		if (typeof value !== 'boolean') return;
+
+		const dgtw = this.form.controls.weights.controls.designGrossTrainWeight;
+		const dtwr = this.form.controls.weights.controls.designTrainWeightRequired;
+		if (value) {
+			dtwr.patchValue(this.NOT_APPLICABLE);
+			dgtw.patchValue(null); // clear design gross train weight
+		} else {
+			dtwr.patchValue(undefined);
 		}
 	}
 }
