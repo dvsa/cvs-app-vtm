db6a6e72f6864b7ec13cb79e4a67dab308e42561
Philip Bardy <146740183+pbardy2000@users.noreply.github.com>
2026-06-08 09:44:02 +0100

    fix(cb2-21757): fix display issue (#2384)

--- src/app/forms/templates/test-records/section-templates/weights/contingency/contingency-weights-trl.template.ts
+++ src/app/forms/templates/test-records/section-templates/weights/contingency/contingency-weights-trl.template.ts
@@ -1,5 +1,10 @@
 import { ValidatorNames } from '@/src/app/models/validators.enum';
-import { FormNode, FormNodeTypes } from '@services/dynamic-forms/dynamic-form.types';
+import {
+	FormNode,
+	FormNodeEditTypes,
+	FormNodeTypes,
+	FormNodeViewTypes,
+} from '@services/dynamic-forms/dynamic-form.types';
 
 export const ContingencyWeightsTRLTpl: FormNode = {
 	name: 'weightsSection',
@@ -29,6 +34,12 @@ export const ContingencyWeightsTRLTpl: FormNode = {
 						{ name: ValidatorNames.Min, args: 1 },
 						{ name: ValidatorNames.Max, args: 99999 },
 					],
+				},
+				{
+					name: 'designTrainWeightRequired',
+					type: FormNodeTypes.CONTROL,
+					viewType: FormNodeViewTypes.HIDDEN,
+					editType: FormNodeEditTypes.HIDDEN,
 				},
 			],
 		},
