c1eab791c530765d7bf6de89e437903fea005ccc
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2026-06-29 11:54:07 +0100

    feat(CB2-21958): clear down form in deactivate guards

--- src/app/guards/cancel-edit-test/cancel-edit-test.guard.ts
+++ src/app/guards/cancel-edit-test/cancel-edit-test.guard.ts
@@ -2,6 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { CanDeactivate } from '@angular/router';
 import { TestRecordComponent } from '@features/test-records/amend/views/test-record-wrapper/test-record/test-record.component';
 import { Store } from '@ngrx/store';
+import { INITIAL_TEST_RESULT_FORM_VALUE, TestService } from '@services/test/test.service';
 import { cancelEditingTestResult } from '@store/test-records';
 
 @Injectable({
@@ -9,9 +10,12 @@ import { cancelEditingTestResult } from '@store/test-records';
 })
 export class CancelEditTestGuard implements CanDeactivate<TestRecordComponent> {
 	store = inject(Store);
+	testService = inject(TestService);
 
 	canDeactivate(): boolean {
 		this.store.dispatch(cancelEditingTestResult());
+		this.testService.form.reset();
+		this.testService.form.patchValue(INITIAL_TEST_RESULT_FORM_VALUE);
 		return true;
 	}
 }
