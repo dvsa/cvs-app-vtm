57985b88d97dd3273201dcb81aa979e319108581
pbardy2000 <146740183+pbardy2000@users.noreply.github.com>
2026-07-03 09:43:33 +0100

    feat(cb2-18666): add view and edit mode

--- src/app/forms/custom-sections/load-status/load-status.component.ts
+++ src/app/forms/custom-sections/load-status/load-status.component.ts
@@ -1,11 +1,14 @@
+import { DefaultNullOrEmpty } from '@/src/app/pipes/default-null-or-empty/default-null-or-empty.pipe';
 import { DynamicFormService } from '@/src/app/services/dynamic-forms/dynamic-form.service';
-import { CustomFormGroup, FormNodeWidth } from '@/src/app/services/dynamic-forms/dynamic-form.types';
-import { NgTemplateOutlet } from '@angular/common';
-import { AfterViewInit, Component, inject, OnDestroy } from '@angular/core';
+import { FormNodeWidth } from '@/src/app/services/dynamic-forms/dynamic-form.types';
+import { selectTechRecord } from '@/src/app/store/technical-records';
+import { testResultInEdit, toEditOrNotToEdit } from '@/src/app/store/test-records';
+import { Component, inject, input, OnDestroy, OnInit, output } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ReasonForNotLoading } from '@dvsa/cvs-type-definitions/types/v1/enums/reasonForNotLoading.enum.js';
 import { UnladenBodyType } from '@dvsa/cvs-type-definitions/types/v1/enums/unladenBodyType.enum.js';
 import { VehicleLoadStatusType } from '@dvsa/cvs-type-definitions/types/v1/enums/vehicleLoadStatus.enum.js';
+import { TestResultSchema } from '@dvsa/cvs-type-definitions/types/v1/test-result';
 import { Store } from '@ngrx/store';
 import { ReplaySubject, takeUntil } from 'rxjs';
 import { GovukFormGroupInputComponent } from '../../components/govuk-form-group-input/govuk-form-group-input.component';
@@ -13,10 +16,8 @@ import { GovukFormGroupRadioComponent } from '../../components/govuk-form-group-
 import { RadioComponent } from '../../components/govuk-form-group-radio/radio/radio.component';
 import { GovukFormGroupSelectComponent } from '../../components/govuk-form-group-select/govuk-form-group-select.component';
 import { GovukFormGroupTextareaComponent } from '../../components/govuk-form-group-textarea/govuk-form-group-textarea.component';
-import { LoadStatusTemplate } from '../../templates/test-records/section-templates/loadStatus/loadStatus.template';
 import { getOptionsFromEnum } from '../../utils/enum-map';
 import { CommonValidatorsService } from '../../validators/common-validators.service';
-import { CustomFormControlComponent } from '../custom-form-control/custom-form-control.component';
 
 @Component({
 	selector: 'app-load-status',
@@ -30,16 +31,60 @@ import { CustomFormControlComponent } from '../custom-form-control/custom-form-c
 		GovukFormGroupSelectComponent,
 		GovukFormGroupInputComponent,
 		GovukFormGroupTextareaComponent,
-		NgTemplateOutlet,
+		DefaultNullOrEmpty,
 	],
 })
-export class LoadStatusComponent extends CustomFormControlComponent implements AfterViewInit, OnDestroy {
+export class LoadStatusComponent implements OnInit, OnDestroy {
 	store = inject(Store);
 	fb = inject(FormBuilder);
 	dfs = inject(DynamicFormService);
 	commonValidators = inject(CommonValidatorsService);
 
-	loadStatusForm?: CustomFormGroup;
+	edit = input(false);
+	isContingencyTest = input(true);
+	data = input<Partial<TestResultSchema>>({});
+	formChange = output<Record<string, any> | [][]>();
+
+	techRecord = this.store.selectSignal(selectTechRecord);
+	testResult = this.store.selectSignal(toEditOrNotToEdit);
+	editingTestResult = this.store.selectSignal(testResultInEdit);
+
+	form = this.fb.group({
+		loadStatus: this.fb.group({
+			vehicleLoadStatus: this.fb.control<VehicleLoadStatusType | null>(null, [
+				this.commonValidators.required('Load status'),
+			]),
+			unladenBodyType: this.fb.control<UnladenBodyType | null>(null, [
+				this.commonValidators.applyWhen(() => this.isUnladenSelected(), this.commonValidators.required('Body type')),
+			]),
+			otherUnladenBodyType: this.fb.control<string | null>(null, [
+				this.commonValidators.applyWhen(
+					() => this.isOtherUnladenBodyTypeRequired(),
+					this.commonValidators.maxLength(200, 'Enter body type')
+				),
+			]),
+			reasonForNotLoading: this.fb.control<ReasonForNotLoading | null>(null, [
+				this.commonValidators.applyWhen(
+					() => this.isUnladenSelected(),
+					this.commonValidators.required('Reason for not loading')
+				),
+			]),
+			partiallyLadenReason: this.fb.control<string | null>(null, [
+				this.commonValidators.maxLength(200, 'Partially laden reason'),
+				this.commonValidators.applyWhen(
+					() => this.isPartiallyLadenSelected(),
+					this.commonValidators.required('Partially laden reason')
+				),
+			]),
+			otherReasonForNotLoading: this.fb.control<string | null>(null, [
+				this.commonValidators.maxLength(200, 'Enter reason for not loading'),
+				this.commonValidators.applyWhen(
+					() => this.isOtherReasonForNotLoadingRequired(),
+					this.commonValidators.required('Enter reason for not loading')
+				),
+			]),
+		}),
+	});
 
 	FORM_NODE_WIDTH = FormNodeWidth;
 	VEHICLE_LOAD_STATUS_TYPES = VehicleLoadStatusType;
@@ -50,16 +95,43 @@ export class LoadStatusComponent extends CustomFormControlComponent implements A
 
 	destroy = new ReplaySubject<boolean>(1);
 
+	ngOnInit(): void {
+		this.handleFormChange();
+	}
+
 	ngOnDestroy(): void {
 		this.destroy.next(true);
 		this.destroy.complete();
 	}
 
-	ngAfterViewInit(): void {
-		this.loadStatusForm = this.dfs.createForm(LoadStatusTemplate) as CustomFormGroup;
-
-		this.loadStatusForm.valueChanges.pipe(takeUntil(this.destroy)).subscribe((value) => {
-			this.writeValue(value);
+	handleFormChange(): void {
+		this.form.valueChanges.pipe(takeUntil(this.destroy)).subscribe(() => {
+			this.formChange.emit(this.form.getRawValue());
+			console.log(this.form.getRawValue());
 		});
+	}
+
+	isUnladenSelected(): boolean {
+		const vehicleLoadStatus = this.form.get('loadStatus.vehicleLoadStatus')?.getRawValue();
+		return vehicleLoadStatus === VehicleLoadStatusType.UNLADEN;
+	}
+
+	isPartiallyLadenSelected(): boolean {
+		const vehicleLoadStatus = this.form.get('loadStatus.vehicleLoadStatus')?.getRawValue();
+		return vehicleLoadStatus === VehicleLoadStatusType.PARTIALLY_LADEN;
+	}
+
+	isOtherUnladenBodyTypeRequired(): boolean {
+		if (!this.isUnladenSelected()) return false;
+
+		const unladenBodyType = this.form.get('loadStatus.unladenBodyType')?.getRawValue();
+		return unladenBodyType === UnladenBodyType.OTHER;
+	}
+
+	isOtherReasonForNotLoadingRequired(): boolean {
+		if (!this.isUnladenSelected()) return false;
+
+		const reasonForNotLoading = this.form.get('loadStatus.reasonForNotLoading')?.getRawValue();
+		return reasonForNotLoading === ReasonForNotLoading.OTHER;
 	}
 }
--- src/app/forms/templates/test-records/section-templates/loadStatus/loadStatus.template.ts
+++ src/app/forms/templates/test-records/section-templates/loadStatus/loadStatus.template.ts
@@ -1,21 +1,12 @@
-import { LoadStatusComponent } from '@/src/app/forms/custom-sections/load-status/load-status.component';
-import { ValidatorNames } from '@/src/app/models/validators.enum';
 import {
 	FormNodeEditTypes,
 	FormNodeTypes,
 	FormNodeViewTypes,
 } from '@/src/app/services/dynamic-forms/dynamic-form.types';
-import { AbstractControl } from '@angular/forms';
-import { ReasonForNotLoading } from '@dvsa/cvs-type-definitions/types/v1/enums/reasonForNotLoading.enum.js';
-import { VehicleLoadStatusType } from '@dvsa/cvs-type-definitions/types/v1/enums/vehicleLoadStatus.enum.js';
 
 export const LoadStatusTemplate = {
 	name: 'loadStatus',
 	type: FormNodeTypes.GROUP,
-	viewType: FormNodeViewTypes.CUSTOM,
-	editType: FormNodeEditTypes.CUSTOM,
-	viewComponent: LoadStatusComponent,
-	editComponent: LoadStatusComponent,
 	children: [
 		{
 			name: 'vehicleLoadStatus',
@@ -23,20 +14,7 @@ export const LoadStatusTemplate = {
 			value: null,
 			type: FormNodeTypes.CONTROL,
 			viewType: FormNodeViewTypes.HIDDEN,
-			editType: FormNodeEditTypes.RADIO,
-			validators: [
-				{
-					name: ValidatorNames.Custom,
-					args: (control: AbstractControl) => {
-						if (control.value === null || control.value === undefined || control.value === '') {
-							const message = 'Load status is required';
-							return { custom: { error: message, message } };
-						}
-
-						return null;
-					},
-				},
-			],
+			editType: FormNodeEditTypes.HIDDEN,
 		},
 		{
 			name: 'unladenBodyType',
@@ -44,21 +22,7 @@ export const LoadStatusTemplate = {
 			value: null,
 			type: FormNodeTypes.CONTROL,
 			viewType: FormNodeViewTypes.HIDDEN,
-			editType: FormNodeEditTypes.DROPDOWN,
-			validators: [
-				{
-					name: ValidatorNames.Custom,
-					args: (control: AbstractControl) => {
-						if (control.value) return null;
-
-						const vehicleLoadStatus = control.parent?.get('vehicleLoadStatus')?.getRawValue();
-						if (vehicleLoadStatus !== VehicleLoadStatusType.UNLADEN) return null;
-
-						const message = 'Body type is required';
-						return { custom: { error: message, message } };
-					},
-				},
-			],
+			editType: FormNodeEditTypes.HIDDEN,
 		},
 		{
 			name: 'otherUnladenBodyType',
@@ -83,13 +47,6 @@ export const LoadStatusTemplate = {
 			type: FormNodeTypes.CONTROL,
 			viewType: FormNodeViewTypes.HIDDEN,
 			editType: FormNodeEditTypes.HIDDEN,
-			validators: [
-				{ name: ValidatorNames.MaxLength, args: 200 },
-				{
-					name: ValidatorNames.RequiredIfEquals,
-					args: { sibling: 'reasonForNotLoading', value: ReasonForNotLoading.OTHER },
-				},
-			],
 		},
 		{
 			name: 'partiallyLadenReason',
@@ -98,13 +55,6 @@ export const LoadStatusTemplate = {
 			type: FormNodeTypes.CONTROL,
 			viewType: FormNodeViewTypes.HIDDEN,
 			editType: FormNodeEditTypes.HIDDEN,
-			validators: [
-				{ name: ValidatorNames.MaxLength, args: 200 },
-				{
-					name: ValidatorNames.RequiredIfEquals,
-					args: { sibling: 'reasonForNotLoading', value: VehicleLoadStatusType.PARTIALLY_LADEN },
-				},
-			],
 		},
 	],
 };
