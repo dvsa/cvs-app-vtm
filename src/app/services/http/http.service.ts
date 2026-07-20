74cb283d03a0522fe48478d8a4dd33a9584474ef
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2026-07-20 10:55:49 +0100

    feat(CB2-22075): new method minus unit tests

--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -469,29 +469,18 @@ export class HttpService {
 
 				return timer(delayMs).pipe(switchMap(() => this.searchTechRecordBySystemNumber(systemNumber)));
 			}),
-			map((results) => results.find((r) => r.techRecord_statusCode !== StatusCodes.ARCHIVED)),
-			takeWhile((record) => !record, true), // include final successful emission
-			last()
-		);
-	}
-
-	waitForCurrentTechRecord(systemNumber: string) {
-		return timer(3000).pipe(
-			switchMap(() => defer(() => this.searchTechRecordBySystemNumber(systemNumber))),
-			expand((results, attempt) => {
-				const record = results.find((r) => r.techRecord_statusCode === StatusCodes.CURRENT);
-
-				if (record) {
-					return EMPTY; // stop retrying
-				}
-
-				const delayMs = Math.min(500 * 2 ** attempt, 5000); // exponential backoff (max 5s)
-
-				return timer(delayMs).pipe(switchMap(() => this.searchTechRecordBySystemNumber(systemNumber)));
+			map((results) => {
+				return (
+					results.find((r) => r.techRecord_statusCode === 'current') ??
+					results.find((r) => r.techRecord_statusCode === 'provisional') ??
+					results.find(
+						(r) =>
+							new Date(r.createdTimestamp).getTime() ===
+							Math.max(...results.map((rec) => new Date(rec.createdTimestamp).getTime()))
+					) ??
+					results[0]
+				);
 			}),
-
-			map((results) => results.find((r) => r.techRecord_statusCode === StatusCodes.CURRENT)),
-
 			takeWhile((record) => !record, true), // include final successful emission
 			last()
 		);
