2acf8ebf7002628c150197199efcf302fe8ea38e
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2026-07-31 10:03:50 +0100

    feat(CB2-20788): remove redundant code

--- src/app/services/defect-media-service/defect-media-service.service.ts
+++ src/app/services/defect-media-service/defect-media-service.service.ts
@@ -34,16 +34,8 @@ export class DefectMediaService {
 		return defect.media.some((media) => this.fileCache[media.path]);
 	}
 
-	hasImages(defect: DefectDetailsSchema): boolean {
-		return defect.media?.some((media) => media.type === 'image') ?? false;
-	}
-
 	getImages(defect: DefectDetailsSchema) {
 		return defect.media?.filter((media) => media.type === 'image') ?? [];
-	}
-
-	hasVideos(defect: DefectDetailsSchema): boolean {
-		return defect.media?.some((media) => media.type === 'video') ?? false;
 	}
 
 	getVideos(defect: DefectDetailsSchema) {
