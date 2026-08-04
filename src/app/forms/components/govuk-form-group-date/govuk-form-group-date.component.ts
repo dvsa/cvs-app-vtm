c78059319e643dd47e62b965a673264063559ba4
Philip Bardy <146740183+pbardy2000@users.noreply.github.com>
2026-08-04 09:37:39 +0100

    fix(cb2-22349): undo changes (#2460)
    * fix(cb2-22349): undo changes
    
    * fix(cb2-22349): snyk fixes

--- src/app/forms/components/govuk-form-group-date/govuk-form-group-date.component.ts
+++ src/app/forms/components/govuk-form-group-date/govuk-form-group-date.component.ts
@@ -97,16 +97,7 @@ export class GovukFormGroupDateComponent
 
 			switch (this.mode()) {
 				case 'iso':
-					this.onChange(
-						new Date(
-							year as number,
-							(month as number) - 1,
-							day as number,
-							Number(hoursStr) || 0,
-							Number(minsStr) || 0,
-							Number(secsStr) || 0
-						).toISOString()
-					);
+					this.onChange(`${year}-${monthStr}-${dayStr}T${hoursStr || '00'}:${minsStr || '00'}:${secsStr || '00'}.000Z`);
 					break;
 				default:
 					this.onChange(`${year}-${monthStr}-${dayStr}`);
