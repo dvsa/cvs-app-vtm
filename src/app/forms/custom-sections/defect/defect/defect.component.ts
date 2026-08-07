886f14f795420549a141be9a6bfa7ce5e445d053
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2026-08-07 14:28:55 +0100

    feat(CB2-20788): apply to old flow

--- src/app/forms/custom-sections/defect/defect/defect.component.ts
+++ src/app/forms/custom-sections/defect/defect/defect.component.ts
@@ -185,14 +185,6 @@ export class DefectComponent implements OnInit, OnDestroy {
 			return 'No media available';
 		}
 
-		// Check for HTTP errors (404 or 500) when fetching media
-		if (this.testResult) {
-			const mediaError = this.defectMediaService?.getMediaFetchError(this.testResult.testResultId);
-			if (mediaError) {
-				return mediaError.message;
-			}
-		}
-
 		if (!this.defectMediaService?.hasMediaInCache(this.defect)) {
 			return 'Media could not be found';
 		}
@@ -201,6 +193,14 @@ export class DefectComponent implements OnInit, OnDestroy {
 			if (reason.type === 'failReason') {
 				const formattedReason = this.defectMediaService?.formatMediaFailureReason(reason.reason) ?? reason.reason;
 				return `No media available - ${formattedReason}`;
+			}
+		}
+
+		// Check for HTTP errors (404 or 500) when fetching media
+		if (this.testResult) {
+			const mediaError = this.defectMediaService?.getMediaFetchError(this.testResult.testResultId);
+			if (mediaError) {
+				return mediaError.message;
 			}
 		}
 
