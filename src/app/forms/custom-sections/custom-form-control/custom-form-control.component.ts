69426ec986224beb742c3300d55052724a62cf1f
Thomas Evans <36958694+tomevs88@users.noreply.github.com>
2026-08-18 08:57:10 +0100

    feat(cb2-2): fix for ng0201 error

--- src/app/forms/custom-sections/custom-form-control/custom-form-control.component.ts
+++ src/app/forms/custom-sections/custom-form-control/custom-form-control.component.ts
@@ -26,7 +26,10 @@ export class CustomFormControlComponent extends BaseControlComponent {
 			if (ngControl.value) {
 				this.name.set(ngControl.key);
 				this.control = ngControl.value;
-				this.form = this.injector.get(FORM_INJECTION_TOKEN) as FormGroup;
+				// FORM_INJECTION_TOKEN is only provided by DynamicFormFieldComponent. Subclasses rendered
+				// outside that machinery (or via ViewListItemComponent, which provides NgControl but not
+				// the form) would otherwise throw NG0201 here.
+				this.form = (this.injector.get(FORM_INJECTION_TOKEN, null) as FormGroup | null) ?? undefined;
 			}
 		}
 	}
