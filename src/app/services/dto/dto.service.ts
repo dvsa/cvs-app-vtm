93c29fc94061a003527d3a0ab5efaf7f888b8708
Thomas Crawley <thomas.crawley@dvsa.gov.uk>
2026-08-19 11:49:43 +0100

    feat(CB2-20027): dto layer first pass

--- src/app/services/dto/dto.service.ts
+++ src/app/services/dto/dto.service.ts
@@ -1,6 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { TestResultSchema } from '@dvsa/cvs-type-definitions/types/v1/test-result';
+import { TEST_TYPES_GROUP9_10_CENTRAL_DOCS } from '@models/testTypeId.enum';
+import { FeatureToggleService } from '@services/feature-toggle-service/feature-toggle-service';
 
 @Injectable({
 	providedIn: 'root',
 })
-export class DTOLayer {}
+export class DTOLayer {
+	featureToggleService = inject(FeatureToggleService);
+
+	transformPayload(editingTestResult: TestResultSchema) {
+		if (
+			!this.featureToggleService.isFeatureEnabled('testresultcreate') ||
+			!this.featureToggleService.isFeatureEnabled('testresultamend')
+		) {
+			return editingTestResult;
+		}
+		const payload = this.stripCentralDocsForNonApplicableTestTypes(editingTestResult);
+		return payload;
+	}
+
+	private stripCentralDocsForNonApplicableTestTypes(editingTestResult: TestResultSchema): TestResultSchema {
+		return {
+			...editingTestResult,
+			testTypes: editingTestResult.testTypes.map((testType) => {
+				if (!TEST_TYPES_GROUP9_10_CENTRAL_DOCS.includes(testType.testTypeId)) {
+					const { centralDocs, ...testTypeWithoutCentralDocs } = testType;
+					return testTypeWithoutCentralDocs;
+				}
+				return testType;
+			}),
+		};
+	}
+}
--- src/app/store/test-records/test-records.effects.ts
+++ src/app/store/test-records/test-records.effects.ts
@@ -14,6 +14,7 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { concatLatestFrom } from '@ngrx/operators';
 import { Store, select } from '@ngrx/store';
 import { AnalyticsService } from '@services/analytics/analytics.service';
+import { DTOLayer } from '@services/dto/dto.service';
 import { DynamicFormService } from '@services/dynamic-forms/dynamic-form.service';
 import { HttpService } from '@services/http/http.service';
 import { TestRecordsService } from '@services/test-records/test-records.service';
@@ -69,6 +70,7 @@ export class TestResultsEffects {
 	private analyticsService = inject(AnalyticsService);
 	private globalErrorService = inject(GlobalErrorService);
 	private testService = inject(TestService);
+	private dtoLayer = inject(DTOLayer);
 
 	fetchTestResultsBySystemNumber$ = createEffect(() =>
 		this.actions$.pipe(
@@ -120,7 +122,9 @@ export class TestResultsEffects {
 			ofType(createTestResult),
 			switchMap((action) => {
 				const testResult = action.value;
-				return this.testRecordsService.postTestResult(testResult).pipe(
+				const cleanedTestResult = this.dtoLayer.transformPayload(testResult);
+
+				return this.testRecordsService.postTestResult(cleanedTestResult).pipe(
 					take(1),
 					map(() => {
 						// if the test type is a failed first test, submit a different action so it does not wait for a current record since promotion doesnt happen
