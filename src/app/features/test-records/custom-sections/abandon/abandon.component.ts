62cffa56080a943a29af8a77522c31962c330925
pbardy2000 <146740183+pbardy2000@users.noreply.github.com>
2026-08-28 14:05:41 +0100

    fix(cb2-22498): add abandon logic

--- src/app/features/test-records/custom-sections/abandon/abandon.component.ts
+++ src/app/features/test-records/custom-sections/abandon/abandon.component.ts
@@ -2,18 +2,8 @@ import { GovukCheckboxGroupComponent } from '@/src/app/forms/components/govuk-ch
 import { GovukFormGroupTextareaComponent } from '@/src/app/forms/components/govuk-form-group-textarea/govuk-form-group-textarea.component';
 import { CommonValidatorsService } from '@/src/app/forms/validators/common-validators.service';
 import { MultiOptions } from '@/src/app/models/options.model';
-import { ReferenceDataResourceType } from '@/src/app/models/reference-data.model';
-import {
-	TEST_TYPES_GROUP1_SPEC_TEST,
-	TEST_TYPES_GROUP2_SPEC_TEST,
-	TEST_TYPES_GROUP3_SPEC_TEST,
-	TEST_TYPES_GROUP4_SPEC_TEST,
-	TEST_TYPES_GROUP5_13,
-	TEST_TYPES_GROUP5_SPEC_TEST,
-	TEST_TYPES_MSVA,
-} from '@/src/app/models/testTypeId.enum';
-import { VehicleTypes } from '@/src/app/models/vehicle-tech-record.model';
 import { MultiOptionsService } from '@/src/app/services/multi-options/multi-options.service';
+import { TestTypeService } from '@/src/app/services/test-type/test-type.service';
 import { TestService } from '@/src/app/services/test/test.service';
 import { selectAllReferenceDataByResourceType } from '@/src/app/store/reference-data';
 import { testResultInEdit } from '@/src/app/store/test-records';
@@ -31,6 +21,7 @@ import { Store } from '@ngrx/store';
 export class AbandonComponent implements OnInit {
 	store = inject(Store);
 	testService = inject(TestService);
+	testTypeService = inject(TestTypeService);
 	optionsService = inject(MultiOptionsService);
 	commonValidators = inject(CommonValidatorsService);
 
@@ -43,59 +34,19 @@ export class AbandonComponent implements OnInit {
 		this.addValidators();
 	}
 
-	getAbandonReasonsResourceType() {
-		const testTypeId = this.testResult()?.testTypes[0].testTypeId || '';
-
-		// Display TIR reasons when abandoning TIR tests
-		if (TEST_TYPES_GROUP5_13.includes(testTypeId)) {
-			return ReferenceDataResourceType.TirReasonsForAbandoning;
-		}
-
-		// Display MSVA reasons when abandoning MSVA tests
-		if (TEST_TYPES_MSVA.includes(testTypeId)) {
-			return ReferenceDataResourceType.MsvaReasonsForAbandoning;
-		}
-
-		// Display specialist reasons when abandoning non-MSVA specialist tests
-		if (
-			[
-				...TEST_TYPES_GROUP1_SPEC_TEST,
-				...TEST_TYPES_GROUP2_SPEC_TEST,
-				...TEST_TYPES_GROUP3_SPEC_TEST,
-				...TEST_TYPES_GROUP4_SPEC_TEST,
-				...TEST_TYPES_GROUP5_SPEC_TEST,
-			].includes(testTypeId)
-		) {
-			return ReferenceDataResourceType.SpecialistReasonsForAbandoning;
-		}
-
-		// Otherwise, display vehicle type specific reasons
-		const vehicleType = this.testResult()?.vehicleType;
-
-		if (vehicleType === VehicleTypes.PSV) {
-			return ReferenceDataResourceType.ReasonsForAbandoningPsv;
-		}
-
-		if (vehicleType === VehicleTypes.HGV) {
-			return ReferenceDataResourceType.ReasonsForAbandoningHgv;
-		}
-
-		if (vehicleType === VehicleTypes.TRL) {
-			return ReferenceDataResourceType.ReasonsForAbandoningTrl;
-		}
-
-		// If we reach here, then we have invalid data
-		throw new Error('Unexpected vehicle type');
-	}
-
 	getAbandonReasonsList(): MultiOptions {
-		const resourceType = this.getAbandonReasonsResourceType();
+		const testResult = this.testResult();
+		if (!testResult) return [];
+
+		const resourceType = this.testTypeService.getAbandonReasonsResourceType(testResult);
 		const referenceData = this.store.selectSignal(selectAllReferenceDataByResourceType(resourceType))() || [];
 		return referenceData.map((reason) => ({ label: `${reason.description}`, value: `${reason.description}` }));
 	}
 
 	loadOptions(): void {
-		this.optionsService.loadOptions(this.getAbandonReasonsResourceType());
+		const testResult = this.testResult();
+		if (!testResult) return;
+		this.optionsService.loadOptions(this.testTypeService.getAbandonReasonsResourceType(testResult));
 	}
 
 	addValidators(): void {
